f7a046f9981bc9be5c48c248162e3a537e0ba37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:56 +0000

    Add package jsonclasses/2.7.6/jsonclasses-2.7.6.tar.gz

---
+++ packages/jsonclasses/jsonclasses-2.7.6.tar.gz/jsonclasses-2.7.6/jsonclasses/modifiers/totitle_modifier.py
@@ -0,0 +1,14 @@
+"""module for totitle modifier."""
+from __future__ import annotations
+from typing import Any, TYPE_CHECKING
+from .modifier import Modifier
+if TYPE_CHECKING:
+    from ..ctx import Ctx
+
+
+class ToTitleModifier(Modifier):
+    """Convert string to title format."""
+
+    def transform(self, ctx: Ctx) -> Any:
+        return ctx.val.title() if isinstance(ctx.val, str) else ctx.val
+
