dc61d9e9c484a6fa18465e06b655ed6d0edc412c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:58 +0000

    Add package aiositi-mex/0.0.1.dev10/aiositi-mex-0.0.1.dev10.tar.gz

---
+++ packages/aiositi-mex/aiositi-mex-0.0.1.dev10.tar.gz/aiositi-mex-0.0.1.dev10/siti/resources/ifpe/r36/reporte_3612.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from ...base import Resource
+from ..base import ReportIFPE, Resendable, Sendable, Updateable
+
+
+class Sobregiros(Resource):
+    numero_sobregiros_otorgados_periodo: int
+    monto_sobregiros_otorgados_periodo: int
+    monto_minimo_sobregiros_otorgados_periodo: int
+    monto_maximo_sobregiros_otorgados_periodo: int
+
+
+class CobroSobregiros(Resource):
+    saldo_inicio_periodo_sobregiros_cobrar: int
+    saldo_final_periodo_sobregiros_cobrar: int
+    monto_sobregiros_cobrados_periodo_reportado: int
+
+
+class InformacionSolicitada(Resource):
+    sobregiros: Sobregiros
+    cobro_sobregiros: CobroSobregiros
+
+
+class Reporte3612(ReportIFPE, Resendable, Sendable, Updateable):
+    _resource = '/IFPE/R26/3612'
+
+    informacion_solicitada: List[InformacionSolicitada]
