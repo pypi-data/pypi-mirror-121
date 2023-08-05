7ef0fe6e9958076f76f4e489cc17cd41c57dddef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:13 +0000

    Add package idem-aiohttp/2.0.0/idem_aiohttp-2.0.0-py3-none-any.whl

---
+++ packages/idem-aiohttp/idem_aiohttp-2.0.0-py3-none-any.whl/idem_aiohttp/tool/type/dict.py
@@ -0,0 +1,17 @@
+from typing import Dict
+from typing import Iterable
+from typing import List
+
+from dict_tools.data import NamespaceDict
+
+
+def namespaced(hub, value):
+    if isinstance(value, Dict):
+        return NamespaceDict(**value)
+    elif isinstance(value, List):
+        ret = []
+        for i in value:
+            ret.append(hub.tool.type.dict.namespaced(i))
+        return ret
+    else:
+        return value
