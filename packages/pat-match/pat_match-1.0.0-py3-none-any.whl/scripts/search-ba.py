ee16f0bc7bfd36a42595739c6d5f01583e0329e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:28 +0000

    Add package pat-match/1.0.0/pat_match-1.0.0-py3-none-any.whl

---
+++ packages/pat-match/pat_match-1.0.0-py3-none-any.whl/scripts/search-ba.py
@@ -0,0 +1,47 @@
+import argparse
+import sys
+
+sys.path.append("../../")
+from p_1.scripts import utils
+
+
+def run_ba(main_str, pattern):
+    border_arr_str = pattern + "$" + main_str
+    n = len(border_arr_str)
+    ba = [0] * n
+    b = 0
+    matched_indices = []
+
+    for i in range(1, n):
+        while b > 0 and border_arr_str[i] != border_arr_str[b]:
+            b = ba[b - 1]
+        if border_arr_str[i] == border_arr_str[b]:
+            b = b + 1
+        else:
+            b = 0
+        ba[i] = b
+
+    for i in range(n):
+        if ba[i] == len(pattern):
+            matched_indices.append(i - 2 * len(pattern) + 1)
+
+    return matched_indices
+
+
+def run(fa_file, fq_file):
+    fasta_seqs = utils.get_seq_from_file(fa_file, "fasta")
+    fastq_seqs = utils.get_seq_from_file(fq_file, "fastq")
+
+    for fastq_seq in fastq_seqs:
+        for fasta_seq in fasta_seqs:
+            matched_indices = run_ba(fasta_seq.seq, fastq_seq.seq)
+            utils.output_sam(matched_indices, fasta_seq, fastq_seq)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Matches a pattern using the border-array approach")
+    parser.add_argument("-fa", "--fasta-file", dest="fasta_file", default="resources/fasta_input.fa", help="fasta file")
+    parser.add_argument("-fq", "--fastq-file", dest="fastq_file", default="resources/fastq_input.fq", help="fastq file")
+    args = parser.parse_args()
+
+    run(args.fasta_file, args.fastq_file)
---
+++ packages/pat-match/pat_match-1.0.0-py3-none-any.whl/scripts/search-naive.py
@@ -0,0 +1,39 @@
+import argparse
+import sys
+
+sys.path.append("../../")
+from p_1.scripts import utils
+
+
+def run_naive(main_str, pattern):
+    ms_len = len(main_str)
+    ss_len = len(pattern)
+    matched_indices = []
+
+    for i in range(ms_len - ss_len + 1):
+        for j in range(ss_len):
+            if main_str[i + j] != pattern[j]:
+                break
+        if j + 1 == ss_len:
+            matched_indices.append(i + 1)
+
+    return matched_indices
+
+
+def run(fa_file, fq_file):
+    fasta_seqs = utils.get_seq_from_file(fa_file, "fasta")
+    fastq_seqs = utils.get_seq_from_file(fq_file, "fastq")
+
+    for fastq_seq in fastq_seqs:
+        for fasta_seq in fasta_seqs:
+            matched_indices = run_naive(fasta_seq.seq, fastq_seq.seq)
+            utils.output_sam(matched_indices, fasta_seq, fastq_seq)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Matches a pattern using the naive approach")
+    parser.add_argument("-fa", "--fasta-file", dest="fasta_file", default="resources/fasta_input.fa", help="fasta file")
+    parser.add_argument("-fq", "--fastq-file", dest="fastq_file", default="resources/fastq_input.fq", help="fastq file")
+    args = parser.parse_args()
+
+    run(args.fasta_file, args.fastq_file)
