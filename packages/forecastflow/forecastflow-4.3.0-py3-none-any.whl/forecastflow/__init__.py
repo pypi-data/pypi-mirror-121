42c9f7d9802cf6318453f6180aa64f7f72fe2509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:59 +0000

    Add package forecastflow/4.3.0/forecastflow-4.3.0-py3-none-any.whl

---
+++ packages/forecastflow/forecastflow-4.3.0-py3-none-any.whl/forecastflow/__init__.py
@@ -0,0 +1,10 @@
+from . import api
+from . import tabpy_support
+from . import config
+from .data_source import DataSource
+from .enums import DataSourceLabel, FileType
+from .model import Model
+from .prediction import Prediction
+from .project import Project
+from .training import ClassifierTrainingSettings, RegressorTrainingSettings
+from .user import User
