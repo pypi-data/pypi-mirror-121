6885bac502a95c717f210d8ba5e32b8168269e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:38 +0000

    Add package cicdcli/1.4/cicdcli-1.4.tar.gz

---
+++ packages/cicdcli/cicdcli-1.4.tar.gz/cicdcli-1.4/buildpan/previous_commit.py
@@ -0,0 +1,43 @@
+"""
+    Title: previous_commit
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 14-09-2021
+    Date Modified: 14-09-2021
+    Description:
+        ###############################################################
+        ## Get previous commit of a repository   ## 
+         ###############################################################
+ """
+
+import subprocess
+from buildpan import setting
+import requests, datetime
+
+info = setting.info
+
+# getting env variable
+get_sha = info["GET_SHA_URL"]
+fetch_log = info["FETCH_LOG_URL"]
+
+
+def prev_commit(path, repo_name, project_id, username):
+    try:
+        curtime = datetime.datetime.now()
+
+        # pooling to get sha for the previous commit
+        response = requests.get(get_sha, repo_name)
+        res=response.content
+        res=str(res)
+        index=res.index("'")
+        index1=res.index("'",index+1)
+        res=res[index+1:index1]
+
+        # restoring to previous commit
+        subprocess.call(["git", "fetch", "origin", res], cwd=path)
+        subprocess.call(["git", "checkout", "FETCH_HEAD"], cwd=path)
+
+        requests.post(fetch_log + "?" +'project_id='+project_id+'&repo_name='+repo_name+'&Time='+str(curtime)+'&user_name='+username+'&message=Pull'+'&status=success&operation=previous commit')
+
+    except Exception as e:
+        requests.post(fetch_log + "?" +'project_id='+project_id+'&repo_name='+repo_name+'&Time='+str(curtime)+'&user_name='+username+'&message=Pull = '+str(e)+'&status=failed&operation=previous commit')
