fc72122ce65671a0da4bda9e9dcfcdeb70dbd3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:51 +0000

    Add package zadnegoale/0.5.2/zadnegoale-0.5.2-py3-none-any.whl

---
+++ packages/zadnegoale/zadnegoale-0.5.2-py3-none-any.whl/zadnegoale/const.py
@@ -0,0 +1,54 @@
+"""Constants for Zadnego Ale library."""
+from typing import Dict, List, Tuple
+
+ATTR_DUSTS: str = "dusts"
+ATTR_ALERTS: str = "alerts"
+
+ENDPOINT: str = "http://api.zadnegoale.pl/"
+
+URL: str = "{}/public/date/{}/region/{}"
+
+ATTR_LEVEL: str = "level"
+ATTR_TREND: str = "trend"
+ATTR_VALUE: str = "value"
+
+TRANSLATE_STATES_MAP: Dict[str, str] = {
+    "Bardzo niskie": "very low",
+    "Bardzo wysokie": "very high",
+    "Bez zmian": "steady",
+    "Brak": "lack",
+    "Niskie": "low",
+    "Silny spadek": "strong falling",
+    "Silny wzrost": "strong rising",
+    "Spadek": "falling",
+    "Wysokie": "high",
+    "Wzrost": "rising",
+    "Średnie": "medium",
+}
+
+TRANSLATE_ALLERGENS_MAP: List[Tuple[str, str]] = [
+    ("alternaria", "alternaria"),
+    ("ambrozja", "ragweed"),
+    ("babka", "plantain"),
+    ("brzoza", "birch_tree"),
+    ("buk", "beech"),
+    ("bylica", "mugwort"),
+    ("cis", "yew"),
+    ("cladosporium", "cladosporium"),
+    ("dąb", "oak"),
+    ("grab", "hornbeam"),
+    ("jesion", "ash_tree"),
+    ("klon", "maple"),
+    ("komosa", "pigweed"),
+    ("leszczyna", "hazel"),
+    ("nawłoć", "goldenrod"),
+    ("olsza", "alder"),
+    ("platan", "plane_tree"),
+    ("pokrzywa", "nettle"),
+    ("sosna", "pine"),
+    ("szczaw", "sorrel"),
+    ("topola", "poplar"),
+    ("trawy", "grass"),
+    ("wierzba", "willow"),
+    ("wiąz", "elm"),
+]
