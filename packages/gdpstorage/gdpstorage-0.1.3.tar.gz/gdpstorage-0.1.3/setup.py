169758045f65dc3180906c00a0e70409397d92f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:14 +0000

    Add package gdpstorage/0.1.3/gdpstorage-0.1.3.tar.gz

---
+++ packages/gdpstorage/gdpstorage-0.1.3.tar.gz/gdpstorage-0.1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+project_urls = {
+  'Project Homepage': 'https://github.com/jadliaissam/django-gdpstorage'
+}
+
+setup(install_requires=['PyDrive2'], project_urls = project_urls)
