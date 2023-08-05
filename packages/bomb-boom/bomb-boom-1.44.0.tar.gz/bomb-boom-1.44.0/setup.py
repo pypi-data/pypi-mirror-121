7a66548dcc983dec9bae7ce4538a2c1029911823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:39 +0000

    Add package bomb-boom/1.44.0/bomb-boom-1.44.0.tar.gz

---
+++ packages/bomb-boom/bomb-boom-1.44.0.tar.gz/bomb-boom-1.44.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['bomb']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bomb-boom',
+    'version': '1.44.0',
+    'description': 'Bomb package',
+    'long_description': None,
+    'author': 'Nikita Barinov',
+    'author_email': 'diadorer@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
