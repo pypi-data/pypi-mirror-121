518206b277ea2a18d8ac44b12426d8b557de230a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:15 +0000

    Add package lpa-input/1.0.10/lpa-input-1.0.10.tar.gz

---
+++ packages/lpa-input/lpa-input-1.0.10.tar.gz/lpa-input-1.0.10/src/lpa/input/boundaries.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Tools for the generation of special conditions at the boundaries.
+"""
+
+from . import *
+
+@beartype
+def IDBC(
+    s: Scalar,
+    p: VectorList,
+    b: ScalarList,
+    t: str,
+) -> tuple:
+    """
+    Return the image dislocations of (p, b) for a circle of radius s.
+
+    Adding the image dislocations allows to simulate correctly the
+    complete diffraction profile (and not only for the asymptotic
+    values of the Fourier variable). However, the formula used here
+    only applies to dislocations of type 'screw'.
+
+    Input:
+        s (Scalar): radius of the region of interest [nm]
+        p (VectorList): dislocation positions [nm]
+        b (ScalarList): dislocation Burgers vectors sense [1]
+        t (str): dislocation type
+
+    Output:
+        cp (VectorList): image dislocation positions [nm]
+        cb (ScalarList): image dislocation Burgers vectors sense [1]
+
+    Input example:
+        s = 2
+        p = np.array([[1, 0], [-1, 0], [0, 0]])
+        b = np.array([1, 1, 1])
+        t = 'screw'
+
+    Output example:
+        cp = np.array([[4, 0], [-4, 0]])
+        cb = np.array([-1, -1, -1])
+
+    Complexity:
+        O( len(p) )
+    """
+    if t == 'screw':
+        n = np.linalg.norm(p, axis=1) # distance to the origin
+        m = n != 0 # mask to avoid division by zero
+        v = p[m] # points different from (0, 0)
+        t = np.arctan2(v[:,1], v[:,0]) # angles of the image dislocations
+        r = s**2/n[m] # radii of the image dislocations
+        cp = np.stack((r*np.cos(t), r*np.sin(t)), axis=1) # image positions
+        cb = -b # image Burgers vector senses
+        return cp, cb
+    else:
+        raise ValueError("cannot calculate images of edge dislocations")
+
+@beartype
+def PBCG(
+    s: Scalar,
+    p: VectorList,
+    b: ScalarList,
+    r: int,
+) -> tuple:
+    """
+    Return the replicated dislocations of (p, b) for a square of side s.
+
+    Input:
+        s (Scalar): side of the region of interest [nm]
+        p (VectorList): dislocation positions [nm]
+        b (ScalarList): dislocation Burgers vectors sense [1]
+        r (int): number of replications at the boundaries
+
+    Output:
+        cp (VectorList): replicated dislocation positions [nm]
+        cb (ScalarList): replicated dislocation Burgers vectors sense [1]
+
+    Input example:
+        s = 1
+        p = np.array([[1,1]])
+        b = np.array([1])
+        r = 1
+
+    Output example:
+        cp = np.array([
+            [0, 2],
+            [2, 2],
+            [2, 0],
+            [0, 0],
+            [0, 1],
+            [1, 2],
+            [2, 1],
+            [1, 0],
+        ])
+        cb = np.array([1])
+
+    Complexity:
+        O( len(p) * r^2 )
+    """
+    u = [] # list containing translated positions
+    for i in range(1, r+1):
+        for j in range(2*i):
+            for k in (1, -1):
+                u.append(np.array((-i*k, (i-j)*k)))
+                u.append(np.array(((i-j)*k,  i*k)))
+    cp = np.concatenate([p + s*d for d in u]) # replicated positions
+    cb = np.tile(b, len(u)) # replicated Burgers vector senses
+    return cp, cb
---
+++ packages/lpa-input/lpa-input-1.0.10.tar.gz/lpa-input-1.0.10/src/lpa/input/parallel.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Tools for parallelizing the spatial analysis of distributions.
+"""
+
+from mpi4py import MPI
+name = MPI.Get_processor_name()
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank() # number of the processor executing this script
+size = comm.Get_size() # number of processors
+root = 0 # master processor
+import warnings
+from . import *
+from . import sets
+from . import analyze
+
+@beartype
+def average_on_cores(
+    w: AnalysisOutput,
+    b: bool = False
+) -> Optional[AnalysisOutput]:
+    """
+    Return the average value of w ​​over the cores.
+
+    When b is True, all cores get the average value. When b is False,
+    the workers get None and the root gets the average value.
+
+    Input:
+        w (AnalysisOutput): worker value
+        b (bool): broadcast the result to all cores
+
+    Output:
+        m (AnalysisOutput): averaged value of w over the cores
+
+    Input example:
+        On master core:
+            w = 1
+            b = False
+        On worker core:
+            w = 0
+            b = False
+
+    Output example:
+        On master core:
+            m = 0.5
+        On worker core:
+            m = None
+    """
+    if not isinstance(w, np.ndarray):
+        w = np.array(w)
+    if rank == root: # if the script is executed by the master
+        m = np.zeros_like(w) # prepare the buffer for data reception
+    else: # if the script is executed by a worker
+        m = None
+    comm.Reduce([w, MPI.DOUBLE], [m, MPI.DOUBLE], op=MPI.SUM, root=root)
+    if rank == root:
+        m = m/size # average the value
+    if b:
+        m = comm.bcast(m, root=root) # broadcast to the workers
+    return m
+
+@beartype
+def export(
+    o: Union[sets.Distribution, sets.Sample],
+    **kwargs,
+) -> None:
+    """
+    Export a complete pooled analysis of the object o of each core.
+
+    Function similar to the export function of the analyze module.
+
+    Input:
+        o (Distribution|Sample): distribution or sample to analyze on the core
+      **expdir (str): export directory
+      **expfmt (str): export format
+      **expstm (str): export stem
+      **title (str): figure title
+    """
+    if rank==root and not o.S is None:
+        msg = ("chosen random seed detected on "+o.name('stm')+" "
+            + "(The use of a seed is not recommended. In order for "
+            + "the distributions or samples not to be identical from "
+            + "one core to another, the seed must not be identical "
+            + "from one core to another.")
+        warnings.warn(msg, Warning)
+    i = average_on_cores(o.i, True) # averaged inter dislocation distance
+    r, iK = analyze.intervals(i, o.s) # intervals to display
+    f = ['KKKK', 'gggg', 'GaGs'] # functions to calculate
+    worker = analyze.calculate(f, o, r) # function results
+    master = [average_on_cores(worker[i]) for i in range(len(worker))]
+    if rank == root:
+        if isinstance(o, sets.Distribution):
+            c = str(size) # number of distributions analyzed
+        else:
+            c = str(len(o)*size) # number of distributions analyzed
+        # optional parameters
+        expdir = getkwa('expdir', kwargs, str, '')
+        expfmt = getkwa('expfmt', kwargs, str, 'pdf')
+        expstm = getkwa('expstm', kwargs, str, c+"_"+o.name('dmgsS', c='stm'))
+        title = getkwa('title', kwargs, str, c+" "+o.name('mgsd', c='ttl'))
+        endkwa(kwargs)
+        # export
+        args = (expdir, expfmt, expstm, title)
+        analyze.plot_KKKK(r[:iK], master[f.index('KKKK')].T[:iK].T, *args)
+        analyze.plot_gggg(r, master[f.index('gggg')], *args)
+        analyze.plot_GaGs(r, master[f.index('GaGs')], *args)
