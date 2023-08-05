56cf0b42bad4a3d22b9070c9245866f34289bc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:20 +0000

    Add package simplePackage3/0.1.0/simplePackage3-0.1.0.tar.gz

---
+++ packages/simplePackage3/simplePackage3-0.1.0.tar.gz/simplePackage3-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simplePackage3',
+    version='0.1.0',    
+    description='A example package',
+    author='Alexey',
+    author_email='ashik2001@yandex.ru',
+    url='https://www.google.com',
+    packages=['simplePackage3'],
+    install_requires=['numpy==1.20.3'],
+)
