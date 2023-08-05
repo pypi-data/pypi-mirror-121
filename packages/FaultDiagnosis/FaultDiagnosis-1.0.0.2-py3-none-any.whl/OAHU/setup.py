2150f76e7c56b7d15dfecd5c8420b072e6a4b5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package FaultDiagnosis/1.0.0.2/FaultDiagnosis-1.0.0.2-py3-none-any.whl

---
+++ packages/FaultDiagnosis/FaultDiagnosis-1.0.0.2-py3-none-any.whl/OAHU/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+name='FaultPredict', # 패키지 이름
+version='1.0.0.1',
+description='Sunghan Fault Disgnosis',
+author='SungHan',
+autoor_email='shbems777@gmail.com',
+url='https://github.com/tathata21/moon',
+license='SH', # 성한에서 정한 라이센스에 따른다.
+py_modules=['op_ahu','fault_detect'], # 패키지에 포함되는 모듈
+python_requires='>=3',
+install_requires=['pymssql'], # 패키지 사용을 위해 필요한 추가 설치 패키지
+packages=['OAHU'] # 패키지가 들어있는 폴더들
+)
