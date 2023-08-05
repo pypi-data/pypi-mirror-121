b19897b67ff2b12ec71df6e234a5a9e17333f956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package vidtoch/0.4.0/vidtoch-0.4.0.tar.gz

---
+++ packages/vidtoch/vidtoch-0.4.0.tar.gz/vidtoch-0.4.0/vidtoch/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from .__utils__ import FFCmdUtils, makeVideo, vTools
+
+NAME = "vidtoch"
+VERSIONNUM = 0, 4, 0
+VERSION = ".".join(map(str, VERSIONNUM))
+AUTHOR = "hrpzcf"
+EMAIL = "hrpzcf@foxmail.com"
+WEBSITE = "https://gitee.com/hrpzcf/vidtoch"
+
+__all__ = ["FFCmdUtils", "makeVideo", "vTools"]
