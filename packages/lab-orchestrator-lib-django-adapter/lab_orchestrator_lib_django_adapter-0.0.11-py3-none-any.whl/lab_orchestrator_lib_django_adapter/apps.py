a1d403cd8d0abb9999e49863721d45f890ac14cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:13 +0000

    Add package lab-orchestrator-lib-django-adapter/0.0.11/lab_orchestrator_lib_django_adapter-0.0.11-py3-none-any.whl

---
+++ packages/lab-orchestrator-lib-django-adapter/lab_orchestrator_lib_django_adapter-0.0.11-py3-none-any.whl/lab_orchestrator_lib_django_adapter/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LabOrchestratorLibDjangoAdapterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lab_orchestrator_lib_django_adapter'
