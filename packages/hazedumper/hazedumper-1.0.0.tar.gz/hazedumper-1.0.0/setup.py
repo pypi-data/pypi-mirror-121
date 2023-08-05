0ba9de2fb59dc1b759bd0aada218ee965a53c8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package hazedumper/1.0.0/hazedumper-1.0.0.tar.gz

---
+++ packages/hazedumper/hazedumper-1.0.0.tar.gz/hazedumper-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='hazedumper',
+    packages=['hazedumper'],
+    version='1.0.0',
+    description='pkg for auto update offsets',
+    author='cxldxice',
+    license='MIT',
+    author_email='cxldxfxtxre@gmail.com',
+    url='https://github.com/cxldxice/hazedumper',
+    keywords=['hazedumper', 'offsets', 'csgo'],
+    install_requires=['requests']
+)
