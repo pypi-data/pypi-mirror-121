02f9113b0f6e46383f80f75d5de0800e94c99efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:03 +0000

    Add package wf-core-data-dashboard/0.0.2/wf_core_data_dashboard-0.0.2-py3-none-any.whl

---
+++ packages/wf-core-data-dashboard/wf_core_data_dashboard-0.0.2-py3-none-any.whl/wf_core_data_dashboard/core.py
@@ -0,0 +1,37 @@
+
+HTML_TEMPLATE_SINGLE_TABLE = '''
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>{title}</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col">
+                <h1>{title}</h1>
+                <h3>{subtitle}</h3>
+                {table_html}
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+  </body>
+</html>
+'''
+
+def single_table_page_html(
+    title,
+    subtitle,
+    table_html
+):
+    page_html = HTML_TEMPLATE_SINGLE_TABLE.format(
+        title=title,
+        subtitle=subtitle,
+        table_html=table_html
+    )
+    return page_html
