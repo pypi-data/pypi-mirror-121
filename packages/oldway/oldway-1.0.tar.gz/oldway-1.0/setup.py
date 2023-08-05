61bf07269daa160b341ad984deca87cef225857b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:01 +0000

    Add package oldway/1.0/oldway-1.0.tar.gz

---
+++ packages/oldway/oldway-1.0.tar.gz/oldway-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+
+
+setup(
+    name="oldway",
+    packages=find_packages(),
+    version="1.0",
+    url="http://www.blueskyonmars.com/",
+    author="narahari_nguyen",
+    author_email="nguyenelizabeth@gmail.com"
+)
