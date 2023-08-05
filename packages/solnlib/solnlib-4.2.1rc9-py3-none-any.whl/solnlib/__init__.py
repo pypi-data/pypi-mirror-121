f03276117db72276fb33cdb3e19a0f48bddfdc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:07 +0000

    Add package solnlib/4.2.1rc9/solnlib-4.2.1rc9-py3-none-any.whl

---
+++ packages/solnlib/solnlib-4.2.1rc9-py3-none-any.whl/solnlib/__init__.py
@@ -0,0 +1,48 @@
+# Copyright 2016 Splunk, Inc.
+# SPDX-FileCopyrightText: 2020 2020
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+The Splunk Software Development Kit for Solutions.
+"""
+
+from . import (
+    acl,
+    conf_manager,
+    credentials,
+    file_monitor,
+    hec_config,
+    log,
+    net_utils,
+    orphan_process_monitor,
+    pattern,
+    server_info,
+    splunk_rest_client,
+    splunkenv,
+    time_parser,
+    timer_queue,
+    user_access,
+    utils,
+)
+
+__all__ = [
+    "acl",
+    "conf_manager",
+    "credentials",
+    "file_monitor",
+    "hec_config",
+    "log",
+    "net_utils",
+    "orphan_process_monitor",
+    "pattern",
+    "server_info",
+    "splunk_rest_client",
+    "splunkenv",
+    "time_parser",
+    "timer_queue",
+    "user_access",
+    "utils",
+]
+
+__version__ = "4.2.1rc9"
