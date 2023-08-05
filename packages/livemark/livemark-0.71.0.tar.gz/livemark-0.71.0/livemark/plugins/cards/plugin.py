cdab7b1458852a4f6d940b328918daaa835e94e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:44 +0000

    Add package livemark/0.71.0/livemark-0.71.0.tar.gz

---
+++ packages/livemark/livemark-0.71.0.tar.gz/livemark-0.71.0/livemark/plugins/cards/plugin.py
@@ -0,0 +1,31 @@
+from ...project import Project
+from ...plugin import Plugin
+from ... import helpers
+
+
+class CardsPlugin(Plugin):
+    identity = "cards"
+
+    # Process
+
+    def process_markup(self, markup):
+        markup.add_style("style.css")
+        markup.add_script("script.js")
+        markup.add_markup("markup.html")
+
+    # Helpers
+
+    @staticmethod
+    def create_card(source, *, code, **context):
+        directory = "assets/cards"
+        helpers.ensure_dir(directory)
+        target = f"{directory}/{code}.html"
+        project = Project(source, target=target, config={"site": False})
+        project.document.read()
+        project.document.get_plugin("logic").context.update(context)
+        project.document.build()
+
+    @staticmethod
+    def delete_cards():
+        target = "assets/cards"
+        helpers.remove_dir(target)
