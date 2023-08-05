fa67d68f141abc2a047f12f76e18541f9281563e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:42 +0000

    Add package ABBA-QuPath-RegistrationExporter/0.1/ABBA-QuPath-RegistrationExporter-0.1.tar.gz

---
+++ packages/ABBA-QuPath-RegistrationExporter/ABBA-QuPath-RegistrationExporter-0.1.tar.gz/ABBA-QuPath-RegistrationExporter-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='ABBA-QuPath-RegistrationExporter',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Nicholas A. Del Grosso',
+    author_email='delgrosso.nick@gmail.com',
+    description='A QuPath-Abba Registration Exporter Application for Mouse Sections',
+    install_requires=requirements,
+    entry_points={'console_scripts':[
+        "regexport=regexport.app:main"
+    ]}
+)
