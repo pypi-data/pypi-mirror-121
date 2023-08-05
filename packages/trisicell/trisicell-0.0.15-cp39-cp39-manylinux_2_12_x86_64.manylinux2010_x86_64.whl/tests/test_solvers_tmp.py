b57f49225659609dd63ef46b3704375d0900f27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package trisicell/0.0.15/trisicell-0.0.15-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/trisicell/trisicell-0.0.15-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/tests/test_solvers_tmp.py
@@ -0,0 +1,51 @@
+import pytest
+
+import trisicell as tsc
+
+from ._helpers import skip_graph_tool, skip_mpi4py, skip_rpy2
+
+
+class TestSolversTmp:
+    def test_iscistree(self):
+        df_in = tsc.datasets.test()
+        df_out = tsc.tl.iscistree(df_in, alpha=0.0000001, beta=0.1)
+        is_cf = tsc.ul.is_conflict_free_gusfield(df_out)
+        assert is_cf
+
+    def test_siclonefit(self):
+        assert True
+
+    def test_infscite(self):
+        assert True
+
+    @skip_graph_tool
+    def test_sbm(self):
+        data = tsc.datasets.test()
+        out = tsc.tl.sbm(data)
+        tree = tsc.ul.to_tree(out)
+        assert len(tree.nodes) == 3
+
+    @skip_mpi4py
+    def test_infercna(self):
+        assert 1 == 1
+
+    @skip_rpy2
+    @pytest.mark.skip(reason="Unable to import a package on GitHub")
+    def test_dendro(self):
+        adata = tsc.datasets.example()
+        tsc.tl.dendro(adata)
+        assert True
+
+    @skip_rpy2
+    @pytest.mark.skip(reason="Takes 6 minutes")
+    def test_cardelino(self):
+        adata = tsc.datasets.example()
+        tsc.tl.cardelino(adata, mode="free", n_clones=11)
+        assert True
+
+    @pytest.mark.skip(reason="Cause make: *** Error!")
+    def test_rscistree(self):
+        adata = tsc.datasets.colorectal2(readcount=True)
+        df_out = tsc.tl.rscistree(adata, mode="haploid")
+        is_cf = tsc.ul.is_conflict_free_gusfield(df_out)
+        assert is_cf
