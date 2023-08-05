feb014ddf2fb88b8fa0353dd163e3a078e5971ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:01 +0000

    Add package mimesis-stats/0.0.2/mimesis_stats-0.0.2.tar.gz

---
+++ packages/mimesis-stats/mimesis_stats-0.0.2.tar.gz/mimesis_stats-0.0.2/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+
+requires = read_requirements("requirements.txt")
+
+dev_requires = read_requirements("requirements.dev.txt") + requires
+
+setuptools.setup(
+    name="mimesis_stats",
+    version="0.0.2",
+    author="Jonathon Mellor",
+    author_email="mellorjonathon1@gmail.com",
+    description=("Extension of mimesis library for data generation with statistical properties"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/jonathonmellor/mimesis-stats/archive/refs/tags/v0.0.2.tar.gz",
+    url="https://github.com/jonathonmellor/mimesis-stats",
+    project_urls={
+        "Bug Tracker": "https://github.com/jonathonmellor/mimesis-stats/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6.8",
+    install_requires=requires,
+    extras_require={"dev": dev_requires, "ci": dev_requires},
+)
