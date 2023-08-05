65466193155726a4d4e23d831dbf14a4b84df399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:41 +0000

    Add package downloadpdf/1.0.3/downloadpdf-1.0.3.tar.gz

---
+++ packages/downloadpdf/downloadpdf-1.0.3.tar.gz/downloadpdf-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='downloadpdf',
+      version='1.0.3',
+      description='Downloads PDFs from URL',
+      url='https://codeanit.com/',
+      author='Anit Shrestha Manandhar',
+      author_email='codeanit@gmail.com',
+      license='MIT',
+      packages=['downloadpdf', 'main', 'util'],
+      zip_safe=False,
+      python_requires='>=3.6',
+      install_requires=['beautifulsoup4==4.10.0'])
