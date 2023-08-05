f16c87493ddf93891354a9c691cf2a8d26109e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:35 +0000

    Add package calc-naive/0.0.1/calc_naive-0.0.1.tar.gz

---
+++ packages/calc-naive/calc_naive-0.0.1.tar.gz/calc_naive-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a very simple calculator for two numbers:+, -,/,*
---
+++ packages/calc-naive/calc_naive-0.0.1.tar.gz/calc_naive-0.0.1/__int__.py
@@ -0,0 +1,12 @@
+def add_numbers(num1,num2):
+	return num1+num2
+
+def sub_numbers(num1,num2):
+	return num1-num2	
+
+
+def mult_numbers(num1,num2):
+	return num1*num2
+
+def did_numbers(num1,num2):
+	return num1/num2
