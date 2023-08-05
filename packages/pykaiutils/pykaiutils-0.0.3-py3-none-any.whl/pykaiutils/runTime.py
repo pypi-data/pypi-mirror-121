b7a54bf2093ef5eb617b2bc131ca935074355172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:19 +0000

    Add package pykaiutils/0.0.3/pykaiutils-0.0.3-py3-none-any.whl

---
+++ packages/pykaiutils/pykaiutils-0.0.3-py3-none-any.whl/pykaiutils/runTime.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @time    ： 2021/9/24 16:34
+# author   ： 贾志凯 15716539228@163.com
+# filename ： runTime.py
+# @software： win10 python3.6.5 PyCharm
+def time():
+    print('ok')
