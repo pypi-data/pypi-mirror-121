0c1231ab2801c73c08e34c3b87eaadee6c3476e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:22 +0000

    Add package general-simulation-framework/0.0.4/general-simulation-framework-0.0.4.tar.gz

---
+++ packages/general-simulation-framework/general-simulation-framework-0.0.4.tar.gz/general-simulation-framework-0.0.4/src/core/entity/properties/__init__.py
@@ -0,0 +1,5 @@
+from .property import Property
+from .boolean_property import BooleanProperty
+from .expression_property import ExpressionProperty
+from .number_property import NumberProperty
+from .string_property import StringProperty
