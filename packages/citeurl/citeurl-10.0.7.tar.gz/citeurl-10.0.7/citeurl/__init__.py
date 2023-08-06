e420f7ec136410aeaec87225178473558d956c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:11 +0000

    Add package citeurl/10.0.7/citeurl-10.0.7.tar.gz

---
+++ packages/citeurl/citeurl-10.0.7.tar.gz/citeurl-10.0.7/citeurl/__init__.py
@@ -0,0 +1,4 @@
+from .citation import Citation
+from .citator import Citator, Template, cite, list_cites
+from .tokens import TokenType, TokenOperation, StringBuilder
+from .hyperlink import insert_links
