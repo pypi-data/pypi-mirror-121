65e87129d0fb7518452b8ef58673a18960de7695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:20 +0000

    Add package ouvidoria-unidombosco/0.0.3/ouvidoria_unidombosco-0.0.3.tar.gz

---
+++ packages/ouvidoria-unidombosco/ouvidoria_unidombosco-0.0.3.tar.gz/ouvidoria_unidombosco-0.0.3/ouvidoria_unidombosco/__init__.py
@@ -0,0 +1,4 @@
+from ouvidoria_unidombosco import OuvidoriaBasePath
+from ouvidoria_unidombosco.api.src.SeleniumHelper import SeleniumHelper
+from ouvidoria_unidombosco.api.src.UnidomboscoNightmare import UnidomboscoNightmare
+from ouvidoria_unidombosco.api.src.Unidombosco import Unidombosco
---
+++ packages/ouvidoria-unidombosco/ouvidoria_unidombosco-0.0.3.tar.gz/ouvidoria_unidombosco-0.0.3/ouvidoria_unidombosco/api/src/Unidombosco.py
@@ -0,0 +1,44 @@
+import time
+from threading import Thread
+from python_helper import Constant as c
+from python_helper import log, ObjectHelper
+import globals
+from ouvidoria_unidombosco.api.src.UnidomboscoNightmare import UnidomboscoNightmare
+from ouvidoria_unidombosco import OuvidoriaBasePath
+
+
+
+
+class Unidombosco:
+
+    def __init__(self, *args, filePath=OuvidoriaBasePath.MODULE_FILE, globalsInstance=None, logDebug=False, logInfo=False, **kwargs):
+        self.unidomboscoNightmareArgs = args
+        self.unidomboscoNightmareKwargs = kwargs
+        self.globalsInstance = globalsInstance if ObjectHelper.isNotNone(globalsInstance) else globals.newGlobalsInstance(filePath
+            , successStatus = True
+            , infoStatus = logInfo
+            , settingStatus = True
+            , debugStatus = logDebug
+            , warningStatus = True
+            , failureStatus = True
+            , errorStatus = True
+            , logStatus = False
+        )
+        # processes = []
+        # for instance in range(instances):
+
+    def runNightmare(self, message: str, studentRegistry: str, name: str, email: str, phoneNumber: str, cpf: str, instances: int = 4):
+        for instanceIndex in range(instances):
+            nightmare = Thread(
+                target = UnidomboscoNightmare(
+                    self.globalsInstance,
+                    instanceIndex,
+                    *self.unidomboscoNightmareArgs,
+                    **self.unidomboscoNightmareKwargs
+                ).run,
+                args = (
+                    str(message), str(studentRegistry), str(name), str(email), str(phoneNumber), str(cpf).replace(c.DOT, c.BLANK)
+                )
+            )
+            nightmare.start()
+            time.sleep(10)
