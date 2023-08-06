75a22dfae28bc6e2621db5a4227c5b88c8ef4a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:46 +0000

    Add package aiositi-mex/0.0.1.dev17/aiositi-mex-0.0.1.dev17.tar.gz

---
+++ packages/aiositi-mex/aiositi-mex-0.0.1.dev17.tar.gz/aiositi-mex-0.0.1.dev17/siti/resources/ifpe/r13/reporte_13221.py
@@ -0,0 +1,12 @@
+from pydantic import conlist
+
+from ..base import ReportIFPE, Resendable, Sendable, Updateable
+from .commons import InformacionFinanciera
+
+
+class Reporte13221(ReportIFPE, Resendable, Sendable, Updateable):
+    _resource = '/IFPE/R13/13221'
+
+    informacion_financiera: conlist(  # type: ignore[valid-type]
+        InformacionFinanciera, min_items=1
+    )
