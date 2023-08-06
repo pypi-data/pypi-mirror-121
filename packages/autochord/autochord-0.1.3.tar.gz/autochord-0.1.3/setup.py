ffc1a2a206e9e712036fca106ed7f76b5d6a4f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:21 +0000

    Add package autochord/0.1.3/autochord-0.1.3.tar.gz

---
+++ packages/autochord/autochord-0.1.3.tar.gz/autochord-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+    	package_data={
+    		"autochord": ["res/nnls-chroma.so"]
+    	},
+    	install_requires=[
+		    "gdown",
+		    "numpy",
+		    "scipy",
+		    "librosa",
+		    "vamp",
+		    "lazycats",
+		    "tensorflow>=2.6"
+		]
+	)
