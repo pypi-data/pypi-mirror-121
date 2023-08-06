814ee2b279b12a60cca2fa7b11fd387dfc4c372a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:11 +0000

    Add package spoko-kawal/0.0/spoko_kawal-0.0.tar.gz

---
+++ packages/spoko-kawal/spoko_kawal-0.0.tar.gz/spoko_kawal-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='spoko_kawal',
+      version='0.0',
+      description='Tells a funniest joke ever!',
+      packages=['spoko_kawal'],
+      author_email='tomek.juszczyszyn@gmail.com',
+      zip_safe=False)
