03de50f3fae368ec8d7f49d7909edc1c65670c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:24 +0000

    Add package localstack/0.12.18.1/localstack-0.12.18.1.tar.gz

---
+++ packages/localstack/localstack-0.12.18.1.tar.gz/localstack-0.12.18.1/localstack/services/awslambda/lambda_utils.py
@@ -0,0 +1,92 @@
+import logging
+import os
+from collections import defaultdict
+from typing import Dict, List, Union
+
+from localstack.utils.aws.aws_models import LambdaFunction
+from localstack.utils.common import to_str
+
+LOG = logging.getLogger(__name__)
+
+# Lambda runtime constants
+LAMBDA_RUNTIME_PYTHON36 = "python3.6"
+LAMBDA_RUNTIME_PYTHON37 = "python3.7"
+LAMBDA_RUNTIME_PYTHON38 = "python3.8"
+LAMBDA_RUNTIME_NODEJS = "nodejs"
+LAMBDA_RUNTIME_NODEJS43 = "nodejs4.3"
+LAMBDA_RUNTIME_NODEJS610 = "nodejs6.10"
+LAMBDA_RUNTIME_NODEJS810 = "nodejs8.10"
+LAMBDA_RUNTIME_NODEJS10X = "nodejs10.x"
+LAMBDA_RUNTIME_NODEJS12X = "nodejs12.x"
+LAMBDA_RUNTIME_NODEJS14X = "nodejs14.x"
+LAMBDA_RUNTIME_JAVA8 = "java8"
+LAMBDA_RUNTIME_JAVA8_AL2 = "java8.al2"
+LAMBDA_RUNTIME_JAVA11 = "java11"
+LAMBDA_RUNTIME_DOTNETCORE2 = "dotnetcore2.0"
+LAMBDA_RUNTIME_DOTNETCORE21 = "dotnetcore2.1"
+LAMBDA_RUNTIME_DOTNETCORE31 = "dotnetcore3.1"
+LAMBDA_RUNTIME_GOLANG = "go1.x"
+LAMBDA_RUNTIME_RUBY = "ruby"
+LAMBDA_RUNTIME_RUBY25 = "ruby2.5"
+LAMBDA_RUNTIME_RUBY27 = "ruby2.7"
+LAMBDA_RUNTIME_PROVIDED = "provided"
+
+# default handler and runtime
+LAMBDA_DEFAULT_HANDLER = "handler.handler"
+LAMBDA_DEFAULT_RUNTIME = LAMBDA_RUNTIME_PYTHON37
+LAMBDA_DEFAULT_STARTING_POSITION = "LATEST"
+
+# List of Dotnet Lambda runtime names
+DOTNET_LAMBDA_RUNTIMES = [
+    LAMBDA_RUNTIME_DOTNETCORE2,
+    LAMBDA_RUNTIME_DOTNETCORE21,
+    LAMBDA_RUNTIME_DOTNETCORE31,
+]
+
+
+def multi_value_dict_for_list(elements: List) -> Dict:
+    temp_mv_dict = defaultdict(list)
+    for key in elements:
+        if isinstance(key, (list, tuple)):
+            key, value = key
+        else:
+            value = elements[key]
+        key = to_str(key)
+        temp_mv_dict[key].append(value)
+
+    return dict((k, tuple(v)) for k, v in temp_mv_dict.items())
+
+
+def get_lambda_runtime(runtime_details: Union[LambdaFunction, str]) -> str:
+    """Return the runtime string from the given LambdaFunction (or runtime string)."""
+    if isinstance(runtime_details, LambdaFunction):
+        runtime_details = runtime_details.runtime
+    if not isinstance(runtime_details, str):
+        LOG.info("Unable to determine Lambda runtime from parameter: %s", runtime_details)
+    return runtime_details or ""
+
+
+def is_provided_runtime(runtime_details: Union[LambdaFunction, str]) -> bool:
+    """Whether the given LambdaFunction uses a 'provided' runtime."""
+    runtime = get_lambda_runtime(runtime_details) or ""
+    return runtime.startswith("provided")
+
+
+def get_handler_file_from_name(handler_name: str, runtime: str = None):
+    runtime = runtime or LAMBDA_DEFAULT_RUNTIME
+    if runtime.startswith(LAMBDA_RUNTIME_PROVIDED):
+        return "bootstrap"
+    delimiter = "."
+    if runtime.startswith(LAMBDA_RUNTIME_NODEJS):
+        file_ext = ".js"
+    elif runtime.startswith(LAMBDA_RUNTIME_GOLANG):
+        file_ext = ""
+    elif runtime.startswith(tuple(DOTNET_LAMBDA_RUNTIMES)):
+        file_ext = ".dll"
+        delimiter = ":"
+    elif runtime.startswith(LAMBDA_RUNTIME_RUBY):
+        file_ext = ".rb"
+    else:
+        handler_name = handler_name.rpartition(delimiter)[0].replace(delimiter, os.path.sep)
+        file_ext = ".py"
+    return "%s%s" % (handler_name.split(delimiter)[0], file_ext)
---
+++ packages/localstack/localstack-0.12.18.1.tar.gz/localstack-0.12.18.1/localstack/utils/aws/request_context.py
@@ -0,0 +1,160 @@
+import logging
+import re
+import threading
+
+from flask import request
+from requests.models import Request
+from requests.structures import CaseInsensitiveDict
+
+from localstack import config
+from localstack.utils.common import empty_context_manager
+from localstack.utils.run import FuncThread
+
+LOG = logging.getLogger(__name__)
+
+THREAD_LOCAL = threading.local()
+
+MARKER_APIGW_REQUEST_REGION = "__apigw_request_region__"
+
+
+def get_proxy_request_for_thread():
+    try:
+        return THREAD_LOCAL.request_context
+    except Exception:
+        return None
+
+
+def get_flask_request_for_thread():
+    try:
+        return Request(
+            url=request.path,
+            data=request.data,
+            headers=CaseInsensitiveDict(request.headers),
+            method=request.method,
+        )
+    except Exception as e:
+        # swallow error: "Working outside of request context."
+        if "Working outside" in str(e):
+            return None
+        raise
+
+
+def extract_region_from_auth_header(headers):
+    # TODO: use method from aws_stack directly (leaving import here for now, to avoid circular dependency)
+    from localstack.utils.aws import aws_stack
+
+    auth = headers.get("Authorization") or ""
+    region = re.sub(r".*Credential=[^/]+/[^/]+/([^/]+)/.*", r"\1", auth)
+    if region == auth:
+        return None
+    region = region or aws_stack.get_local_region()
+    return region
+
+
+def get_request_context():
+    candidates = [get_proxy_request_for_thread(), get_flask_request_for_thread()]
+    for req in candidates:
+        if req is not None:
+            return req
+
+
+class RequestContextManager(object):
+    """Context manager which sets the given request context (i.e., region) for the scope of the block."""
+
+    def __init__(self, request_context):
+        self.request_context = request_context
+
+    def __enter__(self):
+        THREAD_LOCAL.request_context = self.request_context
+
+    def __exit__(self, type, value, traceback):
+        THREAD_LOCAL.request_context = None
+
+
+def get_region_from_request_context():
+    """look up region from request context"""
+
+    if config.USE_SINGLE_REGION:
+        return
+
+    request_context = get_request_context()
+    if not request_context:
+        return
+    region = extract_region_from_auth_header(request_context.headers)
+
+    # Fix region lookup for certain requests, e.g., API gateway invocations
+    #  that do not contain region details in the Authorization header.
+    region = request_context.headers.get(MARKER_APIGW_REQUEST_REGION) or region
+
+    return region
+
+
+def configure_region_for_current_request(region_name: str, service_name: str):
+    """Manually configure (potentially overwrite) the region in the current request context. This may be
+    used by API endpoints that are invoked directly by the user (without specifying AWS Authorization
+    headers), to still enable transparent region lookup via aws_stack.get_region() ..."""
+
+    # TODO: leaving import here for now, to avoid circular dependency
+    from localstack.utils.aws import aws_stack
+
+    request_context = get_request_context()
+    if not request_context:
+        LOG.info(
+            "Unable to set region '%s' in undefined request context: %s",
+            region_name,
+            request_context,
+        )
+        return
+
+    headers = request_context.headers
+    auth_header = headers.get("Authorization")
+    auth_header = auth_header or aws_stack.mock_aws_request_headers(service_name)
+    auth_header = auth_header.replace("/%s/" % aws_stack.get_region(), "/%s/" % region_name)
+    try:
+        headers["Authorization"] = auth_header
+    except Exception as e:
+        if "immutable" not in str(e):
+            raise
+        _context_to_update = get_proxy_request_for_thread() or request
+        _context_to_update.headers = CaseInsensitiveDict({**headers, "Authorization": auth_header})
+
+
+def patch_request_handling():
+
+    if config.USE_SINGLE_REGION:
+        return
+
+    # TODO: move into generic_proxy.py, instead of patching here (leaving import here for now, to avoid circular dependency)
+    from localstack.services import generic_proxy
+
+    def modify_and_forward(method=None, path=None, data_bytes=None, headers=None, *args, **kwargs):
+        """Patch proxy forward method and store request in thread local."""
+        request_context = get_proxy_request_for_thread()
+        context_manager = empty_context_manager()
+        if not request_context:
+            request_context = Request(url=path, data=data_bytes, headers=headers, method=method)
+            context_manager = RequestContextManager(request_context)
+        with context_manager:
+            result = modify_and_forward_orig(
+                method, path, data_bytes=data_bytes, headers=headers, *args, **kwargs
+            )
+        return result
+
+    modify_and_forward_orig = generic_proxy.modify_and_forward
+    generic_proxy.modify_and_forward = modify_and_forward
+
+    # make sure that we inherit THREAD_LOCAL request contexts to spawned sub-threads
+
+    def thread_init(self, *args, **kwargs):
+        self._req_context = get_request_context()
+        return thread_init_orig(self, *args, **kwargs)
+
+    def thread_run(self, *args, **kwargs):
+        if self._req_context:
+            THREAD_LOCAL.request_context = self._req_context
+        return thread_run_orig(self, *args, **kwargs)
+
+    thread_run_orig = FuncThread.run
+    FuncThread.run = thread_run
+    thread_init_orig = FuncThread.__init__
+    FuncThread.__init__ = thread_init
