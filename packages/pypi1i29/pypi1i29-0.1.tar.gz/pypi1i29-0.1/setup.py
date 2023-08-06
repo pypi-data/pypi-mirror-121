145b8e6982bd72b8e8f307721600294e3597420c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:11 +0000

    Add package pypi1i29/0.1/pypi1i29-0.1.tar.gz

---
+++ packages/pypi1i29/pypi1i29-0.1.tar.gz/pypi1i29-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pypi1i29',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pypi1i29'],
+      author = 'Mahesh Khatri',
+      author_email = 'khatrimahesh@gmail.com',
+      zip_safe=False)
