833be9b86d58fbc6627a2c60a3f2af052396f766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:12 +0000

    Add package pyrasta/1.3.11/pyrasta-1.3.11.tar.gz

---
+++ packages/pyrasta/pyrasta-1.3.11.tar.gz/pyrasta-1.3.11/pyrasta/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+__version__ = '1.3.11'
+__author__ = 'Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
