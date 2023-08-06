8f7e934aa0d1dfea28344f7969b54f1080492245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:45 +0000

    Add package letters/1.5/letters-1.5.tar.gz

---
+++ packages/letters/letters-1.5.tar.gz/letters-1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import pypandoc
+
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    long_description = long_description.replace('\r', '')
+
+except OSError:
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(
+    name='letters',
+    version='1.5',
+    license='GPLv3+',
+    authors=['zelow'],
+    url='https://github.com/zeloww/letters',
+    description='A simple method for customize your texts!',
+    long_description=long_description,
+    keywords=['python', 'py', 'letter', 'letters', 'font', 'ascii', 'color', 'colors', 'gradient', 'gradients', 'fade' 'text'],
+    packages=['letters']
+)
