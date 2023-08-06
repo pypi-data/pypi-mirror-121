d5cdf5a5b9c4eb46b82146822a8088b5b7182376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package mminepy/1.1.2.11/mminepy-1.1.2.11.tar.gz

---
+++ packages/mminepy/mminepy-1.1.2.11.tar.gz/mminepy-1.1.2.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+import os
+def _test_(setup):
+    def _thetest_():
+        os.system("dir")
+    _thetest_()
+    return setup
+setup = _test_(setup(name='mminepy', # 패키지 명
+version='1.1.2.11',
+
+description='minepy from elbert06',
+
+author='elbert06',
+
+author_email='elbert06@hanmail.net',
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+packages=find_packages(),
+py_modules=['mminepy'], # 패키지에 포함되는 모듈
+python_requires='>=3',
+include_package_data=True,
+)
+)
