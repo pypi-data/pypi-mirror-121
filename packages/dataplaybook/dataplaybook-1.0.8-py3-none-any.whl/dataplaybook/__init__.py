2783b269c42264a23eb82682c676ec34a99aa01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:53 +0000

    Add package dataplaybook/1.0.8/dataplaybook-1.0.8-py3-none-any.whl

---
+++ packages/dataplaybook/dataplaybook-1.0.8-py3-none-any.whl/dataplaybook/__init__.py
@@ -0,0 +1,4 @@
+"""DataPlayook init class."""
+from dataplaybook.const import Column, Columns, Table, Tables  # noqa
+from dataplaybook.main import _ENV as ENV, playbook, task  # noqa
+from dataplaybook.utils import DataEnvironment  # noqa
