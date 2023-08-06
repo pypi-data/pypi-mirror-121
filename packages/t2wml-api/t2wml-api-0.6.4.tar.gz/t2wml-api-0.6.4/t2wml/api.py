fd3ee28f9740344b51c578fd439d8faf16e8e088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:08 +0000

    Add package t2wml-api/0.6.4/t2wml-api-0.6.4.tar.gz

---
+++ packages/t2wml-api/t2wml-api-0.6.4.tar.gz/t2wml-api-0.6.4/t2wml/api.py
@@ -0,0 +1,17 @@
+import warnings
+from t2wml.settings import t2wml_settings
+from t2wml.wikification.utility_functions import add_entities_from_file, kgtk_to_dict, dict_to_kgtk
+from t2wml.wikification.item_table import Wikifier
+from t2wml.spreadsheets.sheet import Sheet, SpreadsheetFile
+from t2wml.mapping.statement_mapper import YamlMapper, StatementMapper, AnnotationMapper
+from t2wml.wikification.wikidata_provider import SparqlProvider, DictionaryProvider, WikidataProvider
+from t2wml.knowledge_graph import KnowledgeGraph, create_output_from_files
+from t2wml.project import Project
+from t2wml.input_processing.annotation_parsing import Annotation, AnnotationNodeGenerator, get_Pnode, get_Qnode
+from t2wml.input_processing.annotation_suggesting import block_finder, annotation_suggester
+
+
+
+def add_nodes_from_file(file_path: str):
+    warnings.warn("add_nodes_from_file is deprecated, use add_entities_from_file instead", DeprecationWarning)
+    return add_entities_from_file(file_path)
