f8431d66fabfbf65e3279f83130b4a75180169f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:46 +0000

    Add package sc-catnip/0.0.1.4/sc-catnip-0.0.1.4.tar.gz

---
+++ packages/sc-catnip/sc-catnip-0.0.1.4.tar.gz/sc-catnip-0.0.1.4/README.md
@@ -0,0 +1,2 @@
+# sc-catnip
+single-cell Chromatin Accessibility Tools in Python
---
+++ packages/sc-catnip/sc-catnip-0.0.1.4.tar.gz/sc-catnip-0.0.1.4/catnip/_data/__init__.py
@@ -0,0 +1,12 @@
+
+# _data __init__.py
+
+__author__ = ', '.join([
+    'Michael E. Vinyard'
+])
+__email__ = ', '.join([
+    'vinyard@g.harvard.edu',
+])
+
+
+from ._read_10x_h5 import _AnnData_from_10x_h5 as _read_10x_h5 
