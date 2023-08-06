b3ac6c94cf9a1ba82734baf76ffcfacf033b7331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:02 +0000

    Add package booInfer/0.0.1/booInfer-0.0.1.tar.gz

---
+++ packages/booInfer/booInfer-0.0.1.tar.gz/booInfer-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='booInfer',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Žiga Pušnik',
+    author_email='ziga.pusnik@fri.uni-lj.si',
+    packages=[],
+    classifiers=[], 
+)
