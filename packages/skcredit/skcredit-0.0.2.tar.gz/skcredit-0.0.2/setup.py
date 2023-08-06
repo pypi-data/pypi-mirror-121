8d079ca56be95db1c2083ae1bee5df46b2cb53d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:08 +0000

    Add package skcredit/0.0.2/skcredit-0.0.2.tar.gz

---
+++ packages/skcredit/skcredit-0.0.2.tar.gz/skcredit-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skcredit",
+    version="0.0.2",
+    author="JYLFamily",
+    author_email="jiangyilanf@gmail.com",
+    description="scorecard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JYLFamily/scikit-credit",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "joblib",
+        "pandas",
+        "sklearn",
+        "portion",
+        "statsmodels"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+    ],
+)
---
+++ packages/skcredit/skcredit-0.0.2.tar.gz/skcredit-0.0.2/skcredit/tools/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from skcredit.tools.entropy_tools import entropy, mis, cmi
+
+
+__all__ = [
+    "entropy",
+    "mis",
+    "cmi",
+]
