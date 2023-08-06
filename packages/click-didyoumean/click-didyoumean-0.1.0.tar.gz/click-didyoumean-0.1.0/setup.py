eafd2cf5ac4b754760ae0770d961f15b8c6bcfb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:20 +0000

    Add package click-didyoumean/0.1.0/click-didyoumean-0.1.0.tar.gz

---
+++ packages/click-didyoumean/click-didyoumean-0.1.0.tar.gz/click-didyoumean-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['click_didyoumean']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'click-didyoumean',
+    'version': '0.1.0',
+    'description': 'Enables git-like *did-you-mean* feature in click',
+    'long_description': None,
+    'author': 'Timo Furrer',
+    'author_email': 'timo.furrer@roche.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
