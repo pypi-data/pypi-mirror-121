11b1cf6ad61ab7f0e8efae0997c8b246d3f90b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package cpetreanu-gaussian-binomial/0.2/cpetreanu_gaussian_binomial-0.2.tar.gz

---
+++ packages/cpetreanu-gaussian-binomial/cpetreanu_gaussian_binomial-0.2.tar.gz/cpetreanu_gaussian_binomial-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='cpetreanu_gaussian_binomial',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['cpetreanu_gaussian_binomial'],
+      author='cpetreanu',
+      author_email='catanzaropetreanu@gmail.com',
+      zip_safe=False)
