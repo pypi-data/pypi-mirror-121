8e9837366d777d334a34a954e7ed3b949fe9ea11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:11 +0000

    Add package dl-models/0.0.2/dl_models-0.0.2.tar.gz

---
+++ packages/dl-models/dl_models-0.0.2.tar.gz/dl_models-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+from pathlib import Path
+
+
+project_dir = Path(__file__).parent.parent
+
+
+with open(os.path.join(project_dir, "requirements.txt")) as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name="dl_models",
+    version="0.0.2",
+    description="Contains various neural networks",
+    long_description="Contains various neural networks developed using PyTorch",
+    author="Nitin Kumar Mittal",
+    author_email="nitinmittal234@gmail.com",
+    packages=find_packages(include=["dl_models", "dl_models.*"]),
+    install_requires=required_packages,
+)
