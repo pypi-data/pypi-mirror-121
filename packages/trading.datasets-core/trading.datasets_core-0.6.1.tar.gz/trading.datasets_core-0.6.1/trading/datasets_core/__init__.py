e37e194857270ce1dfd28e9f8e2bd6336e238369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package trading.datasets-core/0.6.1/trading.datasets_core-0.6.1.tar.gz

---
+++ packages/trading.datasets-core/trading.datasets_core-0.6.1.tar.gz/trading.datasets_core-0.6.1/trading/datasets_core/__init__.py
@@ -0,0 +1,3 @@
+"""trading-datasets_core Library."""
+
+from trading.datasets_core.metadata.timeframe_unit import TimeframeUnit
---
+++ packages/trading.datasets-core/trading.datasets_core-0.6.1.tar.gz/trading.datasets_core-0.6.1/trading/tests/test_init.py
@@ -0,0 +1,4 @@
+"""Tests for trading.datasets_core."""
+# pylint: disable=missing-class-docstring,missing-function-docstring,line-too-long
+
+from trading.datasets_core import TimeframeUnit  # pylint: disable=unused-import
