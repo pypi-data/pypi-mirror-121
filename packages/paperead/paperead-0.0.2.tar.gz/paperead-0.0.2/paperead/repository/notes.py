e1efb524664530ca8d6101fe55f269588431656e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package paperead/0.0.2/paperead-0.0.2.tar.gz

---
+++ packages/paperead/paperead-0.0.2.tar.gz/paperead-0.0.2/paperead/repository/notes.py
@@ -0,0 +1,63 @@
+import datetime
+import itertools
+import os
+import pathlib
+from dataclasses import asdict, dataclass, field
+from typing import Dict, Iterator, Optional
+
+import yaml
+from dateutil.tz import tzlocal
+
+from . import Description, DescriptionRepository
+from .base import BaseMetadata
+
+
+@dataclass
+class NoteMetadata(BaseMetadata):
+    pass
+
+
+@dataclass
+class Note(Description[NoteMetadata]):
+    @classmethod
+    def __metadata__(cls, name: str = "", text: Optional[str] = None) -> NoteMetadata:
+        if text:
+            return NoteMetadata.fromText(text)
+        else:
+            return NoteMetadata(name)
+
+
+class NoteRepository(DescriptionRepository[Note]):
+    def __init__(self, root: pathlib.Path) -> None:
+        super().__init__(root)
+
+    @classmethod
+    def __description__(cls, id: str, text: Optional[str] = None) -> Note:
+        if text:
+            return Note.fromText(id, text)
+        else:
+            return Note(NoteMetadata(id), id)
+
+
+if __name__ == "__main__":
+    text = NoteMetadata("name").toText()
+    print(text)
+    print(NoteMetadata.fromText(text))
+
+    manager = NoteRepository(pathlib.Path(
+        __file__).parent.parent.parent.parent.parent.joinpath("temp").joinpath("notes"))
+
+    del manager["a"]
+
+    a = manager.create("a")
+    a.content = "# abc\n"
+    manager.update(a)
+
+    print(a)
+    import time
+
+    time.sleep(1)
+
+    print(manager["a"])
+
+    print(list(manager))
