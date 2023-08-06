4461455965c286b920329e71accac330f40266a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package torchlatent/0.4.1/torchlatent-0.4.1.tar.gz

---
+++ packages/torchlatent/torchlatent-0.4.1.tar.gz/torchlatent-0.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+name = 'torchlatent'
+
+setup(
+    name=name,
+    version='0.4.1',
+    packages=[package for package in find_packages() if package.startswith(name)],
+    url='https://github.com/speedcell4/torchlatent',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='High Performance Structured Prediction in PyTorch',
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'torchrua>=0.3.0',
+    ],
+    extras_require={
+        'dev': [
+            'einops',
+            'pytest',
+            'hypothesis',
+            'pytorch-crf',
+        ],
+    }
+)
