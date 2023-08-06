edbf895374bb03fdbe08837afb3edd3328d7346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:30 +0000

    Add package treeo/0.0.3/treeo-0.0.3.tar.gz

---
+++ packages/treeo/treeo-0.0.3.tar.gz/treeo-0.0.3/treeo/types.py
@@ -0,0 +1,170 @@
+import dataclasses
+import typing as tp
+from abc import ABCMeta
+
+import jax
+import jax.numpy as jnp
+import jax.tree_util
+import numpy as np
+import typing_extensions as tpe
+
+from treeo import utils
+
+A = tp.TypeVar("A")
+B = tp.TypeVar("B")
+
+
+class KindMixin:
+    @classmethod
+    def field(
+        cls,
+        default=dataclasses.MISSING,
+        *,
+        node: bool,
+        default_factory=dataclasses.MISSING,
+        init: bool = True,
+        repr: bool = True,
+        hash: tp.Optional[bool] = None,
+        compare: bool = True,
+        opaque: bool = False,
+        opaque_is_equal: tp.Optional[tp.Callable[[utils.Opaque, tp.Any], bool]] = None,
+    ) -> tp.Any:
+        return utils.field(
+            default=default,
+            node=node,
+            kind=cls,
+            default_factory=default_factory,
+            init=init,
+            repr=repr,
+            hash=hash,
+            compare=compare,
+            opaque=opaque,
+            opaque_is_equal=opaque_is_equal,
+        )
+
+    @classmethod
+    def node(
+        cls,
+        default=dataclasses.MISSING,
+        *,
+        default_factory=dataclasses.MISSING,
+        init: bool = True,
+        repr: bool = True,
+        hash: tp.Optional[bool] = None,
+        compare: bool = True,
+        opaque: bool = False,
+        opaque_is_equal: tp.Optional[tp.Callable[[utils.Opaque, tp.Any], bool]] = None,
+    ) -> tp.Any:
+        return utils.node(
+            default=default,
+            kind=cls,
+            default_factory=default_factory,
+            init=init,
+            repr=repr,
+            hash=hash,
+            compare=compare,
+            opaque=opaque,
+            opaque_is_equal=opaque_is_equal,
+        )
+
+    @classmethod
+    def static(
+        cls,
+        default=dataclasses.MISSING,
+        default_factory=dataclasses.MISSING,
+        init: bool = True,
+        repr: bool = True,
+        hash: tp.Optional[bool] = None,
+        compare: bool = True,
+        opaque: bool = False,
+        opaque_is_equal: tp.Optional[tp.Callable[[utils.Opaque, tp.Any], bool]] = None,
+    ) -> tp.Any:
+        return cls.field(
+            default=default,
+            node=False,
+            default_factory=default_factory,
+            init=init,
+            repr=repr,
+            hash=hash,
+            compare=compare,
+            opaque=opaque,
+            opaque_is_equal=opaque_is_equal,
+        )
+
+
+class _TrivialPytree:
+    def tree_flatten(self):
+        tree = vars(self)
+        children = (tree,)
+        return (children, ())
+
+    @classmethod
+    def tree_unflatten(cls, aux, children):
+        (tree,) = children
+
+        obj = cls.__new__(cls)
+        obj.__dict__.update(tree)
+
+        return obj
+
+    def __init_subclass__(cls):
+        jax.tree_util.register_pytree_node_class(cls)
+
+
+class FieldMetadata:
+    node: bool
+    kind: type
+    opaque: bool
+    opaque_is_equal: tp.Optional[tp.Callable[[utils.Opaque, tp.Any], bool]]
+
+    def __init__(self, node, kind, opaque, opaque_is_equal):
+        self.__dict__["node"] = node
+        self.__dict__["kind"] = kind
+        self.__dict__["opaque"] = opaque
+        self.__dict__["opaque_is_equal"] = opaque_is_equal
+
+    def update(self, **kwargs) -> "FieldMetadata":
+        fields = vars(self).copy()
+        fields.update(kwargs)
+        return FieldMetadata(**fields)
+
+    def __setattr__(self, name: str, value: tp.Any) -> None:
+        raise AttributeError(f"FieldMetadata is immutable, cannot set {name}")
+
+
+@jax.tree_util.register_pytree_node_class
+class Nothing:
+    def tree_flatten(self):
+        return (), None
+
+    @classmethod
+    def tree_unflatten(cls, _aux_data, children):
+        return cls()
+
+    def __repr__(self) -> str:
+        return "Nothing"
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Nothing)
+
+
+NOTHING = Nothing()
+
+
+class Missing:
+    pass
+
+
+MISSING = Missing()
+
+
+class Hashable(tp.Generic[A]):
+    """A hashable immutable wrapper around non-hashable values"""
+
+    value: A
+
+    def __init__(self, value: A):
+        self.__dict__["value"] = value
+
+    def __setattr__(self, name: str, value: tp.Any) -> None:
+        raise AttributeError(f"Hashable is immutable")
---
+++ packages/treeo/treeo-0.0.3.tar.gz/treeo-0.0.3/treeo/utils.py
@@ -0,0 +1,162 @@
+import dataclasses
+import typing as tp
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import typing_extensions as tpe
+
+A = tp.TypeVar("A")
+B = tp.TypeVar("B")
+
+key = jax.random.PRNGKey
+_pymap = map
+_pyfilter = filter
+
+OpaqueIsEqual = tp.Optional[tp.Callable[["Opaque", tp.Any], bool]]
+
+
+@tpe.runtime_checkable
+class ArrayLike(tpe.Protocol):
+    shape: tp.Tuple[int, ...]
+    dtype: np.dtype
+
+
+class Opaque(tp.Generic[A]):
+    def __init__(self, value: A, opaque_is_equal: tp.Optional[OpaqueIsEqual]):
+        self.value = value
+        self.opaque_is_equal = opaque_is_equal
+
+    def __repr__(self):
+        return f"Hidden({self.value})"
+
+    def __eq__(self, other):
+        if self.opaque_is_equal is not None:
+            return self.opaque_is_equal(self, other)
+        else:
+            # if both are Opaque and their values are of the same type
+            if isinstance(other, Opaque) and type(self.value) == type(other.value):
+                # if they are array-like also compare their shapes and dtypes
+                if isinstance(self.value, ArrayLike):
+                    other_value = tp.cast(ArrayLike, other.value)
+                    return (
+                        self.value.shape == other_value.shape
+                        and self.value.dtype == other_value.dtype
+                    )
+                else:
+                    # else they are equal
+                    return True
+            else:
+                return False
+
+
+def field(
+    default=dataclasses.MISSING,
+    *,
+    node: bool,
+    kind: type = type(None),
+    default_factory=dataclasses.MISSING,
+    init: bool = True,
+    repr: bool = True,
+    hash: tp.Optional[bool] = None,
+    compare: bool = True,
+    opaque: bool = False,
+    opaque_is_equal: tp.Optional[tp.Callable[[Opaque, tp.Any], bool]] = None,
+) -> tp.Any:
+
+    return dataclasses.field(
+        default=default,
+        metadata={
+            "node": node,
+            "kind": kind,
+            "opaque": opaque,
+            "opaque_is_equal": opaque_is_equal,
+        },
+        default_factory=default_factory,
+        init=init,
+        repr=repr,
+        hash=hash,
+        compare=compare,
+    )
+
+
+def node(
+    default=dataclasses.MISSING,
+    *,
+    kind: type = type(None),
+    default_factory=dataclasses.MISSING,
+    init: bool = True,
+    repr: bool = True,
+    hash: tp.Optional[bool] = None,
+    compare: bool = True,
+    opaque: bool = False,
+    opaque_is_equal: tp.Optional[tp.Callable[[Opaque, tp.Any], bool]] = None,
+) -> tp.Any:
+    return field(
+        default=default,
+        node=True,
+        kind=kind,
+        default_factory=default_factory,
+        init=init,
+        repr=repr,
+        hash=hash,
+        compare=compare,
+        opaque=opaque,
+        opaque_is_equal=opaque_is_equal,
+    )
+
+
+def static(
+    default=dataclasses.MISSING,
+    *,
+    kind: type = type(None),
+    default_factory=dataclasses.MISSING,
+    init: bool = True,
+    repr: bool = True,
+    hash: tp.Optional[bool] = None,
+    compare: bool = True,
+    opaque: bool = False,
+    opaque_is_equal: tp.Optional[tp.Callable[[Opaque, tp.Any], bool]] = None,
+) -> tp.Any:
+    return field(
+        default,
+        node=False,
+        kind=kind,
+        default_factory=default_factory,
+        init=init,
+        repr=repr,
+        hash=hash,
+        compare=compare,
+        opaque=opaque,
+        opaque_is_equal=opaque_is_equal,
+    )
+
+
+def _get_all_annotations(cls: type) -> tp.Dict[str, type]:
+    d = {}
+    for c in reversed(cls.mro()):
+        if hasattr(c, "__annotations__"):
+            d.update(**c.__annotations__)
+    return d
+
+
+def _get_all_vars(cls: type) -> tp.Dict[str, tp.Any]:
+    d = {}
+    for c in reversed(cls.mro()):
+        if hasattr(c, "__dict__"):
+            d.update(vars(c))
+    return d
+
+
+def _all_types(t: type) -> tp.Iterable[type]:
+    return _pyfilter(lambda t: isinstance(t, tp.Type), _all_types_unfiltered(t))
+
+
+def _all_types_unfiltered(t: type) -> tp.Iterable[type]:
+    yield t
+
+    if hasattr(t, "__args__"):
+        yield t.__origin__
+
+        for arg in t.__args__:
+            yield from _all_types_unfiltered(arg)
