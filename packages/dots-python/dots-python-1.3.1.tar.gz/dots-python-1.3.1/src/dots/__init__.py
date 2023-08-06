a497e6e204eb9d541cb07f6a6c6133cba529a130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:51 +0000

    Add package dots-python/1.3.1/dots-python-1.3.1.tar.gz

---
+++ packages/dots-python/dots-python-1.3.1.tar.gz/dots-python-1.3.1/src/dots/__init__.py
@@ -0,0 +1,11 @@
+import requests
+
+api_key = None
+client_id = None
+api_base = 'https://pls.senddots.com/api'
+
+_session = requests.Session()
+
+from dots.invoice import Invoice
+from dots.transaction import Transaction
+from dots.user import User
