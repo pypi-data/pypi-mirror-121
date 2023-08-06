51029c4fe5d57aacde2f46100ce8a4425709835d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:24 +0000

    Add package nr-Aws-ml1/0.1/nr_Aws_ml1-0.1.tar.gz

---
+++ packages/nr-Aws-ml1/nr_Aws_ml1-0.1.tar.gz/nr_Aws_ml1-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nr_Aws_ml1',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['nr_Aws_ml1'],
+      author = 'Nusrat Rani', 
+      zip_safe=False)
