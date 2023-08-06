600b8f127c3589f09aa6b719a34b809c0ba12515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:48 +0000

    Add package uploadsdemo/0.1/uploadsdemo-0.1.tar.gz

---
+++ packages/uploadsdemo/uploadsdemo-0.1.tar.gz/uploadsdemo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='uploadsdemo',
+      version='0.1',
+      description='Gaussian distributions package for pypi upload',
+      packages=['distributions'],
+      zip_safe=False)
