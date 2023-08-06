5226816186ec12071b6381cdc4323dcbad467ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:35 +0000

    Add package di/0.2.6/di-0.2.6.tar.gz

---
+++ packages/di/di-0.2.6.tar.gz/di-0.2.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['di']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['anyio>=3,<4']
+
+setup_kwargs = {
+    'name': 'di',
+    'version': '0.2.6',
+    'description': 'Autowiring dependency injection',
+    'long_description': 'None',
+    'author': 'Adrian Garcia Badaracco',
+    'author_email': 'adrian@adriangb.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.0,<4',
+}
+
+
+setup(**setup_kwargs)
