acaf6ab3935fce6f497a871ededd7d403f89641e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package ypricemagic/0.2.18/ypricemagic-0.2.18.tar.gz

---
+++ packages/ypricemagic/ypricemagic-0.2.18.tar.gz/ypricemagic-0.2.18/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ypricemagic',
+    packages=find_packages(),
+    version='0.2.18',
+    description='Use this tool to extract historical on-chain price data from an archive node. Shoutout to @bantg and @nymmrx for their awesome work on yearn-exporter that made this library possible.',
+    author='BobTheBuidler',
+    author_email='bobthebuidlerdefi@gmail.com',
+    url='https://github.com/BobTheBuidler/ypricemagic',
+    license='MIT',
+    install_requires=[
+        'cachetools>=4.1.1',
+        'eth-brownie>=1.16.0',
+        'joblib>=1.0.1'
+    ],
+    )
