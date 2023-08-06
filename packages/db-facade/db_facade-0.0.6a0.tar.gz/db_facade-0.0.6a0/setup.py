8d2382789835e59c16d18c96276d88095f99a8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:49 +0000

    Add package db-facade/0.0.6a0/db_facade-0.0.6a0.tar.gz

---
+++ packages/db-facade/db_facade-0.0.6a0.tar.gz/db_facade-0.0.6a0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+requires = [
+    'psycopg2',
+    'pgcopy'
+]
+
+dirname = os.path.dirname(__file__)
+readme_path = os.path.join(dirname, 'README.md')
+
+with open(readme_path, 'r') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='db_facade',
+    version='0.0.6-alpha',
+    description='A simple library that provides an easy to use interface for databases.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Anish Utthiran N.C.',
+    author_email='anish051@protonmail.com',
+    license='MIT',
+    packages=find_packages(include=['db_facade']),
+    package_data={'': ['LICENSE']},
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Intended Audience :: Developers',
+    ]
+)
