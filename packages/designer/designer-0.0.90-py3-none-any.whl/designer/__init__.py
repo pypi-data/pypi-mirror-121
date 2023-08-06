91c5e00435fc3686b6dbcc6afabb949c683b6e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:27 +0000

    Add package designer/0.0.90/designer-0.0.90-py3-none-any.whl

---
+++ packages/designer/designer-0.0.90-py3-none-any.whl/designer/__init__.py
@@ -0,0 +1,31 @@
+import pygame
+from designer.Director import *
+from designer.Animation import *
+from designer.DesignerObject import *
+from designer.colors import *
+
+GLOBAL_DIRECTOR = None
+
+__all__ = [
+    'circle',
+    'ellipse',
+    'arc',
+    'line',
+    'rectangle',
+    'text',
+    'shape',
+    'image',
+    'group',
+    'draw',
+    'set_window_color',
+    'set_window_size',
+    'glide_around',
+    'glide_right',
+    'glide_left',
+    'glide_up',
+    'glide_down',
+    'glide_in_degrees',
+    'rotate',
+    'get_height',
+    'get_width'
+]
