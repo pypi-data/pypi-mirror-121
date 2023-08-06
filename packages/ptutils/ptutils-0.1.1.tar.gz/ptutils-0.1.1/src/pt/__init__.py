0b613117a642b9c915f567db6e995bac8885c9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package ptutils/0.1.1/ptutils-0.1.1.tar.gz

---
+++ packages/ptutils/ptutils-0.1.1.tar.gz/ptutils-0.1.1/src/pt/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##################################################################
+# namespace package: pt
+# This is ptyhon2/python3 compatible namespace package.
+# See https://packaging.python.org/guides/packaging-namespace-packages/
+##################################################################
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
