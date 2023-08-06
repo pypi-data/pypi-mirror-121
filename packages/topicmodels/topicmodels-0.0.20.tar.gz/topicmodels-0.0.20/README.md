1a3f0af6eef93c8f31e7828fdef6b83e2a913444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:48 +0000

    Add package topicmodels/0.0.20/topicmodels-0.0.20.tar.gz

---
+++ packages/topicmodels/topicmodels-0.0.20.tar.gz/topicmodels-0.0.20/README.md
@@ -0,0 +1,5 @@
+# Topic Modelling
+
+## Current status
+- Transformer is working.
+- `FAST_TEXT_MODEL_PATH` environment variable needs to be set to point to `fasttext10.bin`
---
+++ packages/topicmodels/topicmodels-0.0.20.tar.gz/topicmodels-0.0.20/src/topicmodels/topic_modelling_nn.py
@@ -0,0 +1,55 @@
+import tensorflow
+import torch
+import keras
+from tensorflow.keras.callbacks import LambdaCallback
+import eval_measure
+from corpus import DocData,DocDatalstm
+import ldann
+from ldann import Doc2Topic,Logger,data_feeder,Doc2Topiclstm
+from eval_measure import custom_evaluator
+import os
+import collections
+import numpy as np
+import random
+import json
+import pandas as pd
+import fasttext
+from scipy import spatial
+
+
+
+def topicmodelingnn(file_name,weight_dir,epochs=1,no_topic=10,topic_word=5,column_name='clean_text'):
+#     datapathlstm=os.path.join(path_name)
+#     print("topic modeLling with nn")
+#     weight_dir=weight_dir
+    print("starting topic modelling with NN")
+    print("reading data")
+    datann = DocData(file_name, ns_rate=1, min_count=1,column_name=column_name)
+    print("done reading data")
+    datann.count_cooccs(os.path.join(r"stt_lemmas.json"))
+    datann.load_cooccs(os.path.join(r"stt_lemmas.json"))
+    f=8
+    lr=0.015
+    
+    print("startin NN model")
+    modelnn = Doc2Topic(datann, n_topics=no_topic, batch_size=1024*f, n_epochs=epochs, lr=lr, l1_doc=0.0000002, l1_word=0.000000015)
+    print("done findig topics")
+    topicwordnn=modelnn.get_topic_words(top_n=topic_word)
+    
+    totalsum=0.0
+    finaltopics=[]
+    for i in range(no_topic):
+        tmptopic=''
+        for j in range(topic_word):
+            totalsum+=topicwordnn[i][j][1]
+            tmptopic+=topicwordnn[i][j][0]
+            if j<(topic_word-1):
+                tmptopic+=' ,'
+#         print(tmptopic)
+        finaltopics.append(tmptopic)
+    totalsum/=no_topic
+    score=totalsum
+#     totalsum/=topic_word
+    finaltopics=pd.DataFrame({'topic':finaltopics})
+    
+    return finaltopics,score
