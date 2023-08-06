8fad382ce3e136e4e4533c68ed15ae287b161d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package beancount-bot/1.0.1/beancount_bot-1.0.1-py3-none-any.whl

---
+++ packages/beancount-bot/beancount_bot-1.0.1-py3-none-any.whl/beancount_bot/__init__.py
@@ -0,0 +1,9 @@
+from beancount_bot.main import main
+from beancount_bot import dispatcher
+from beancount_bot import transaction
+from beancount_bot import builtin
+from beancount_bot import util
+
+__AUTHOR__ = 'KAAAsS'
+__LICENSE__ = "MIT"
+__VERSION__ = "1.0.1"
