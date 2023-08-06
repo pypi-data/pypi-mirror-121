9f1f5020f084bf83a9c9192085debafc5c7ff8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:23 +0000

    Add package pygerber/0.0.0/pygerber-0.0.0.tar.gz

---
+++ packages/pygerber/pygerber-0.0.0.tar.gz/pygerber-0.0.0/tests/test_cli.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import os
+from unittest import TestCase, main
+
+
+class TestCLI(TestCase):
+    def test_render_pillow_single_file(self):
+        save_path = "render.png"
+        if os.path.exists(save_path):
+            os.remove(save_path)
+        os.system(
+            f'python -m pygerber --pillow --file "tests/gerber/s5.grb" -s "{save_path}"'
+        )
+
+    def test_render_pillow_multi_file(self):
+        save_path = "render.png"
+        if os.path.exists(save_path):
+            os.remove(save_path)
+        os.system(
+            f'python -m pygerber --pillow --toml "tests/gerber/pillow/specfile.toml" -s "{save_path}"'
+        )
+
+
+if __name__ == "__main__":
+    main()
