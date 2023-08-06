67c42bf8c88d0c791b48f92fadeb20816f3b33fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:05 +0000

    Add package simplarchiver/1.2.6.1/simplarchiver-1.2.6.1.tar.gz

---
+++ packages/simplarchiver/simplarchiver-1.2.6.1.tar.gz/simplarchiver-1.2.6.1/simplarchiver/example/__init__.py
@@ -0,0 +1,4 @@
+from .sleep import SleepFeeder, SleepDownloader
+from .random import RandomFeeder, RandomFilterFeeder, RandomFilterDownloader
+from .just import JustDownloader, JustLogCallbackDownloader
+from .subprocess import SubprocessDownloader
