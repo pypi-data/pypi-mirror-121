99b2ed23708a76ac86b7de931dd198aa57393799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:51 +0000

    Add package ipylib/0.0.4/ipylib-0.0.4-py3-none-any.whl

---
+++ packages/ipylib/ipylib-0.0.4-py3-none-any.whl/ipylib/iparser.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from idebug import *
+from ipylib.inumber import *
+
+
+__all__ = [
+    'ValueDtypeParser',
+]
+
+def ValueDtypeParser(v, dtype, ndigits=4):
+    try:
+        if dtype == 'int':
+            return iNumber(v)
+        elif dtype == 'int_abs':
+            return abs(iNumber(v))
+        elif dtype == 'float':
+            return iNumber(v, prec=ndigits, sosujeom='.')
+        elif dtype == 'pct':
+            return Percent(v, prec=2, sosujeom='.').pct
+        elif dtype in ['date','time','dt','datetime']:
+            return DatetimeParser(v)
+        elif dtype == 'str':
+            return v
+        else:
+            logger.exception(f"{self} | 정의되지 않은 데이타-타입({dtype})을 입력했다. {locals()}")
+    except Exception as e:
+        logger.exception(f"{self} | 파싱 에러가 발생하면, 입력된 값을 그대로 반환한다. {locals()}")
+        return v
