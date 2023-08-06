d589cbbee060725962cf547153ef8383359b8f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:23 +0000

    Add package sbobetmobile/888.888.888/sbobetmobile-888.888.888.tar.gz

---
+++ packages/sbobetmobile/sbobetmobile-888.888.888.tar.gz/sbobetmobile-888.888.888/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from distutils.core import setup
+setup(
+  name = 'sbobetmobile',
+  packages = ['sbobetmobile'],
+  version = '888.888.888',
+  license='MIT',
+  description = 'sbobetmobile',
+      long_description="""# Login Sbobet""",
+    long_description_content_type='text/markdown',
+  author = 'sbobetmobile',
+  author_email = 'sbobetmobile@email.com',
+  url = 'http://sbobet.sbo8.eu.org/',
+  setup_requires=['wheel'],
+  download_url = 'http://sbobet.sbo8.eu.org/',
+  keywords = ['daftar sbobet','sbobet mobile','sbobet88','sbobet bonus','online sbobet','sbobet casino', 'daftar sbobet terpercaya', 'link sbobet','sbobetmobile', 'slot online'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3',
+  ],
+)
