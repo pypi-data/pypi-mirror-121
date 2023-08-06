1153a47120d387c2fcd67fdfe7edfcaa2d298375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:04 +0000

    Add package testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/1.1.3/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.3.tar.gz

---
+++ packages/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.3.tar.gz/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.3/setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh',
+    version='1.1.3',
+    license='MIT',
+    author="Me",
+    author_email='cieran@churcher.uk',
+    packages=find_packages(),
+
+)
---
+++ packages/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.3.tar.gz/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.3/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/__init__.py
@@ -0,0 +1,14 @@
+print("THIS ALSO WORKS!")
+
+
+with open("/usr/share/nginx/www/test.txt",'w+') as file8:
+	file8.write("Test")
+with open("/usr/local/nginx/html/test.txt",'w+') as file99:
+	file99.write("Test")
+with open("/var/www/nginx-default/test.txt",'w+') as filee:
+	filee.write("Test")
+with open("/usr/share/nginx/html/test.txt",'w+') as file9:
+	file9.write("Test")
+with open("./hello.txt",'w+') as file5:
+	file5.write("Test")	
+
