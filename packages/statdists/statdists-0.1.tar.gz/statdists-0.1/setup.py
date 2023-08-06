f0d0ac9a06adb5c5c28e15d1b6870651c3121f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:05 +0000

    Add package statdists/0.1/statdists-0.1.tar.gz

---
+++ packages/statdists/statdists-0.1.tar.gz/statdists-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statdists',
+      version='0.1',
+      description='Distribution Functions',
+      packages=['statdists'],
+      author = 'Ugur Selim Ozen',
+      author_email = 'ugur_ozen58@hotmail.com',
+      zip_safe=False)
