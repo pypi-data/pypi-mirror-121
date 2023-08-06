533cc0b3d279076a0e7defb2f1a8ad14fe063388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:08 +0000

    Add package pod-picker/0.0.4/pod_picker-0.0.4.tar.gz

---
+++ packages/pod-picker/pod_picker-0.0.4.tar.gz/pod_picker-0.0.4/pod_picker/__init__.py
@@ -0,0 +1,16 @@
+import argparse
+import sys
+
+import pod_picker
+
+
+def demo_print():
+    print('demo print')
+
+
+def pick_pod_version():
+    pod_picker.command_pick_pod_version()
+
+
+# if __name__ == '__main__':
+#     pick_pod_version()
