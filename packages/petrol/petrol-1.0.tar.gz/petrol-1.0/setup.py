8e5be78514580e7ce0e64f7fc0215b2adb5a1d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:08 +0000

    Add package petrol/1.0/petrol-1.0.tar.gz

---
+++ packages/petrol/petrol-1.0.tar.gz/petrol-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='petrol',
+    version='1.0',
+    author='manisha',
+    author_email='manisha.birudoju03@gmail.com',
+    packages=find_packages(),
+)
