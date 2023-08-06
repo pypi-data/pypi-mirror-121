5dee67dd662387ea48efbcc5d9e9078c1f17df9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package pullkin/0.3.1a1/pullkin-0.3.1a1.tar.gz

---
+++ packages/pullkin/pullkin-0.3.1a1.tar.gz/pullkin-0.3.1a1/setup.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+from setuptools import setup, find_packages
+
+push_receiver_classifiers = [
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries"
+]
+
+with open("README.rst", "r") as f:
+    push_receiver_readme = f.read()
+
+setup(
+    name="pullkin",
+    version="0.3.1a1",
+    author="Franc[e]sco & WhiteApfel",
+    author_email="white@pfel.ru",
+    url="https://github.com/WhiteApfel/Pullkin",
+    packages=find_packages("."),
+    description="Like Pushkin, but he subscribe to GCM/FCM and receive notifications",
+    long_description=push_receiver_readme,
+    license="Unlicense",
+    classifiers=push_receiver_classifiers,
+    keywords="fcm gcm push notification firebase google",
+    install_requires=["oscrypto", "protobuf"],
+    extras_require={
+        "listen": ["http-ece"],
+        "example": ["appdirs"]
+    }
+)
