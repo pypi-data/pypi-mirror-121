a2966f6f0246e2bf27973260a796c18d6e9235f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:10 +0000

    Add package pydaptivefiltering/0.1/pydaptivefiltering-0.1.tar.gz

---
+++ packages/pydaptivefiltering/pydaptivefiltering-0.1.tar.gz/pydaptivefiltering-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="pydaptivefiltering",
+    packages=["pydaptivefiltering"],
+    version='0.1',
+    description=" Python 3.x Implementation of the AdaptiveFiltering Toolbox.",
+    author="Bruno Lima Netto",
+    author_email="brunolimanetto@gmail.com",
+    url="https://github.com/BruninLima",
+    download_url="https://github.com/BruninLima/PydaptiveFiltering/archive/refs/tags/v0.1.tar.gz",
+    keywords=["Adaptive", "Filtering", "Digital", "Signal", "Processing"],
+    install_requires=[
+        'numpy',
+
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3'
+    ]
+
+)
