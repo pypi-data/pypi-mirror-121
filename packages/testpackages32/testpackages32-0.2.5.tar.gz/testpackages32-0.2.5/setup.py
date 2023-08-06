11fc7cb13492bc8009e1b253d32c6f8d8b297114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:30 +0000

    Add package testpackages32/0.2.5/testpackages32-0.2.5.tar.gz

---
+++ packages/testpackages32/testpackages32-0.2.5.tar.gz/testpackages32-0.2.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="testpackages32",
+    version="0.2.5",
+    author="Igor Simagin",
+    author_email="igorsim35@gmail.com",
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    packages=find_packages(),
+)
