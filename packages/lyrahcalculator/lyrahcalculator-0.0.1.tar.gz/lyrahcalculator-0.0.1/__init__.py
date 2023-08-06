702123a3be187f789aa25fd793bcdf1aae4a3aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:24 +0000

    Add package lyrahcalculator/0.0.1/lyrahcalculator-0.0.1.tar.gz

---
+++ packages/lyrahcalculator/lyrahcalculator-0.0.1.tar.gz/lyrahcalculator-0.0.1/__init__.py
@@ -0,0 +1,29 @@
+# --// Initialisation
+
+from math import *
+
+# --// Functions
+
+def Add(Num1, Num2):
+    return Num1 + Num2
+
+def Subtract(Num1, Num2):
+    return Num1 - Num2
+
+def Multiply(Num1, Num2):
+    return Num1 * Num2
+
+def Devide(Num1, Num2):
+    return Num1 / Num2
+
+def Absolute(Num):
+    return abs(Num)
+
+def Power(Num1, Num2):
+    return pow(Num1, Num2)  # --/ Number, Exponent value
+
+def Round(Num):
+    return round(Num)
+
+def SquareRoot(Num):
+    return sqrt(Num)
