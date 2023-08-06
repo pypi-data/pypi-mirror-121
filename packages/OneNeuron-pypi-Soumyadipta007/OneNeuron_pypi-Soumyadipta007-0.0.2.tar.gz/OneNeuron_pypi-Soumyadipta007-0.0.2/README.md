acb00afae29c1c253cb426e310a96ce890993bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:36 +0000

    Add package OneNeuron-pypi-Soumyadipta007/0.0.2/OneNeuron_pypi-Soumyadipta007-0.0.2.tar.gz

---
+++ packages/OneNeuron-pypi-Soumyadipta007/OneNeuron_pypi-Soumyadipta007-0.0.2.tar.gz/OneNeuron_pypi-Soumyadipta007-0.0.2/README.md
@@ -0,0 +1,15 @@
+# OneNeuron_pypi
+OneNeuron_pypi
+
+# How to use
+
+```python
+from oneNeuron.__perceptron__ import Perceptron
+#get X and y and then use below commands
+model = Perceptron(eta=eta, epochs=epochs)
+model.fit(X, y)
+```
+# Reference
+[Official python docs](https://packaging.python.org/tutorials/packaging-projects/)
+
+[github doc for gihub actions](https://docs.github.com/en/actions/guides/building-and-testing-python#publishing-to-package-registries)
---
+++ packages/OneNeuron-pypi-Soumyadipta007/OneNeuron_pypi-Soumyadipta007-0.0.2.tar.gz/OneNeuron_pypi-Soumyadipta007-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME ="OneNeuron_pypi"
+USER_NAME="Soumyadipta007"
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=USER_NAME,
+    author_email="soumyadipta0077@gmail.com",
+    description="Implementation of Perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": "https://github.com/{USER_NAME}{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm",
+    ]
+)
