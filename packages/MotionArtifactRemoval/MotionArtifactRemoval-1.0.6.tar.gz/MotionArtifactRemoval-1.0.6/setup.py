5afe0575afaf19c18bb563870c6c8b91b1879494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:03 +0000

    Add package MotionArtifactRemoval/1.0.6/MotionArtifactRemoval-1.0.6.tar.gz

---
+++ packages/MotionArtifactRemoval/MotionArtifactRemoval-1.0.6.tar.gz/MotionArtifactRemoval-1.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='MotionArtifactRemoval',
+    version='1.0.6',    
+    description='Motion Artifact Removal',
+    author='Jim Peterson, Abed Ghanbari',
+    url='https://www.jax.org',
+    author_email='jim.peterson@jax.org',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    # package_data={'': ['MotionArtifactRemoval/ssUNET/weights/dti.pth']},
+    # include_package_data=True,
+    install_requires=required,
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
