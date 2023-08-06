4e8b0803310019902571179e75226fc415c567c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:49 +0000

    Add package caw062-first-package/0.0.4/caw062_first_package-0.0.4.tar.gz

---
+++ packages/caw062-first-package/caw062_first_package-0.0.4.tar.gz/caw062_first_package-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+        name="caw062_first_package", 
+        version=VERSION,
+        author="Caiwei Wendy Wang",
+        author_email="<caw062@ucsd.edu>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+        ]
+)
