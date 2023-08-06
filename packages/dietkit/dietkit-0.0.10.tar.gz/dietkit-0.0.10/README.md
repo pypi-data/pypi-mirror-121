13cc585770bd8d831243dd3a78330f88aba5614c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:25 +0000

    Add package dietkit/0.0.10/dietkit-0.0.10.tar.gz

---
+++ packages/dietkit/dietkit-0.0.10.tar.gz/dietkit-0.0.10/README.md
@@ -0,0 +1,60 @@
+# Dietkit
+Dietkit is a library that provides tools for managing and analyzing diets.
+
+## Class Structure
+This library includes three classes to represent diet: Ingredient, Menu and Diet.  
+Ingredient class stands for grocery ingredients. Each Ingredient instance includes nutrition information.  
+Menu class stands for dishes served in diet. Each Menu instance contains its own ingredients.  
+Diet class stands for diet plan. It is not single diet, but bundle of several diets. It is consist with pair of identifier and contents of diet.  
+Also, Dietkit contains class 'Criteria' which stands for nutrition criteria. It is used as input to the evaluation method to evaluate nutrition of the menu or diet.
+
+## Functions
+Dietkit's function is divided into three main functions: Loader, Evaluator and Visualizer.  
+Loader functions load ingredient, menu or diet data. If the specific file path is not passed, it automatically loads the our sample data.  
+Evaluator functions evaluate the menu or diet in terms of ingredients and nutrition based on user's criteria.
+Visualizer functions graphically visualize the diet's information or evaluation results.
+
+## Dependencis
+ * pandas (>=3.7)
+ * matplotlib (>= 3.0.0)
+ * seaborn (>= 0.11.0)
+
+## Installation
+You can install this package by `pip install dietkit`
+
+## Tip
+If `from dietkit import *` is input, dietkit will import all of its functions and classes. It can take quite a long time (About 3 minutes). So, it is recommended to import only the functions to be used.
+
+## About sample data
+The sample ingredient data were extracted from the 9th revision of the National Standard Food Components provided by Rural Development Administration of Korean government.  
+The sample menu data is collected from Center for Children's food service management under the Ministry of Food and Drug safety of the Korean government.  
+The sample diet data is work of our research.  
+You can find detailed information about sample data in the study: 'Creating the K–MIND dataset for dietplanning and healthcare research: Byintegrating the capabilities of combinatorialoptimization, experts, and controllablegeneration'.
+
+## Related documents
+This package is subject results of the research: [link TBD]
+You can check detail information about this package and its sample data in supplementary material of above research: [link TBD]
+
+## License
+The source code for Dietkit is subject to the LGPL license.  
+However, the sample data following another license.
+
+### License about sample data
+#### License for sample inigredient data
+##### Subject files
+'sample_ingredients_eng.csv', 'sample_ingredients_kor.csv'
+##### Information
+The ingredient data is distributed under the Korea Open Government License Type 1. In this license, user can use the data freely and without fee regardless of its commercial use, and can change or modify the data to create secondary works. However, the Rural Development Administration of Korean government, which is source of data, must be indicated.
+
+#### License for sample menu data
+##### Subject files
+'sample_menus_eng.csv', 'sample_menus_kor.csv'
+##### Information
+The menu data is permitted to be used under the following conditions: The Center for Children’s Food Service Management in South Korea should be indicated as the author. Redistribution of the menu data and its derivative works are allowed. It should be used only for non-commerical purposes.
+
+#### License for sample diet data
+##### Subject files
+'sample_diet_expert_eng.csv', 'sample_diet_expert_kor.csv', 'sample_diet_ml_eng.csv', 'sample_diet_ml_kor.csv', 'sample_diet_or_eng.csv', 'sample_diet_or_kor.csv'
+
+##### Information
+The 'CC BY-NC 4.0' license is applied to the diet data. When using diet data, the authors of this paper should be indicated as the attribution parties. Redistribution of diet data and its derivative works are allowed. Data should be used only for non-commercial purposes.
---
+++ packages/dietkit/dietkit-0.0.10.tar.gz/dietkit-0.0.10/src/dietkit/loader.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from .elements import Ingredient, Menu, Diet
+from os.path import dirname
+
+def load_ingredient(file_path = None, sample_language = 'eng'):
+    """
+    Load ingredient data. If file_path is not passed, method loads sample data. It return instance dictinary of loaded data, which has key: name of ingredient and value: ingredient instance. 
+    """
+    sample_language = sample_language.lower()
+    if file_path == None:
+        if sample_language == 'eng':
+            file_path = dirname(__file__) + '/samples/sample_ingredients_eng.csv'
+        elif sample_language == 'kor':
+            file_path = dirname(__file__) + '/samples/sample_ingredients_kor.csv'
+        else:
+            raise NameError("The available sample_language is 'kor' or 'eng'")
+
+    raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = 0)
+    sample_ingredients = {}
+    for idx in raw_df.index:
+        sample_ingredients[idx] = Ingredient(name = idx, nutrition = pd.to_numeric(raw_df.loc[idx].iloc[1:]).to_dict(), category = raw_df.loc[idx, 'Category'])
+    '''
+    if add_to_global:
+        for key, value in sample_ingredients.items():
+            var_name = re.sub('[^0-9a-zA-Zㄱ-힗]', '', key)
+            globals()[var_name] = value
+    '''
+    return sample_ingredients
+
+def load_menu(ingredients = load_ingredient(), file_path = None, sample_language = 'eng'):
+    """
+    Load menu data. If file_path is not passed, method loads sample data. It return instance dictinary of loaded data, which has key: name of menu and value: menu instance.
+    """
+    sample_language = sample_language.lower()
+    if file_path == None:
+        if sample_language == 'eng':
+            file_path = dirname(__file__) + '/samples/sample_menus_eng.csv'
+        elif sample_language == 'kor':
+            file_path = dirname(__file__) + '/samples/sample_menus_kor.csv'
+        else:
+            raise NameError("The available sample_language is 'kor' or 'eng'")
+    
+    raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = None)
+    raw_ingredients = pd.DataFrame(data = raw_df['weight'].values, index = pd.MultiIndex.from_frame(raw_df.fillna(method = 'ffill')[['name', 'ingredient']]), columns = ['weight'])
+    info = raw_df.loc[pd.notna(raw_df['name']), ['name', 'category', 'note']].set_index('name')
+    del raw_df
+
+    sample_menus = {}
+    for menu_name in info.index:
+        temp_ingredients = raw_ingredients.xs(menu_name)['weight'].to_dict()
+        converted_ingredients = {}
+        for temp_ing in temp_ingredients.keys():
+            converted_ingredients[ingredients[temp_ing]] = temp_ingredients[temp_ing]
+        sample_menus[menu_name] = Menu(name = menu_name, ingredients = converted_ingredients, category = info.loc[menu_name, 'category'], note = info.loc[menu_name, 'note'])
+    '''
+    if add_to_global:
+        for key, value in sample_menus.items():
+            var_name = re.sub('[^0-9a-zA-Zㄱ-힗]', '', key)
+            globals()[var_name] = value
+    '''
+    return sample_menus
+
+def load_diet(menus = load_menu(), num_loads = 100, file_path = None, sample_language = 'eng', sample_name = None):
+    """
+    Load diet data. If file_path is not passed, method loads sample data. You can select which sample data will be loaded by specify 'sample_name' and 'sample_language'. See readme to know what sample names and languages are available.
+    """
+    sample_language = sample_language.lower()
+    assert sample_language in ['eng', 'kor'], "The available sample_language is 'kor' or 'eng'"
+
+    sample_name = sample_name.lower()
+    assert sample_name in ['expert', 'or', 'ml'], "The available sample_name should be one of 'expert', 'or' and 'ml'"
+
+    if file_path == None:
+        file_path = dirname(__file__) + '/samples/sample_diet_' + sample_name + '_' + sample_language + '.csv'
+
+    raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = 0).iloc[:num_loads]
+    assert num_loads <= len(raw_df), "Requested a larger number of diets than the data have."
+    converted_dict = {}
+    for idx in raw_df.index:
+        converted_menus = []
+        for raw_menu in list(raw_df.loc[idx].values):
+            converted_menus.append(menus[raw_menu])
+        converted_dict[idx] = converted_menus
+    sample_diet = Diet(converted_dict)
+    return sample_diet
