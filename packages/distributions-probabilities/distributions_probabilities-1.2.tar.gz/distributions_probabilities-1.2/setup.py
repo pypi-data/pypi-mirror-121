5fb23fea7f2ca5d4b3e154011bd18730dec1e0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:16 +0000

    Add package distributions-probabilities/1.2/distributions_probabilities-1.2.tar.gz

---
+++ packages/distributions-probabilities/distributions_probabilities-1.2.tar.gz/distributions_probabilities-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_probabilities',
+    version='1.2',
+    description='Gaussian and binomial distribution',
+    packages=['distributions_probabilities'],
+    author = 'Alexander Karari',
+    author_email = 'mc8testmail@gmail.com',
+    zip_safe=False)
