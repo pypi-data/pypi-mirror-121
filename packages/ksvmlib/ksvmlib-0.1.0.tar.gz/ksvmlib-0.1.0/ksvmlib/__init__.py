77aa2ba360a18e8562dc08a71a6bbdc86455c090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package ksvmlib/0.1.0/ksvmlib-0.1.0.tar.gz

---
+++ packages/ksvmlib/ksvmlib-0.1.0.tar.gz/ksvmlib-0.1.0/ksvmlib/__init__.py
@@ -0,0 +1,8 @@
+from .ksvm import KSVM as KSVM
+from gplib import kernel_functions as ker
+from gplib import fitting_methods as fit
+from . import metrics as me
+from gplib import data_manipulations as dm
+from . import plot
+
+__version__ = '0.1.0'
