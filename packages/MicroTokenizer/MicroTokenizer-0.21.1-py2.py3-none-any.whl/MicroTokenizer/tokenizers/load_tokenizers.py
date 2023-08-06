7c00d2882e23f6a212e287dbc6b33720de9fe851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:01 +0000

    Add package MicroTokenizer/0.21.1/MicroTokenizer-0.21.1-py2.py3-none-any.whl

---
+++ packages/MicroTokenizer/MicroTokenizer-0.21.1-py2.py3-none-any.whl/MicroTokenizer/tokenizers/load_tokenizers.py
@@ -0,0 +1,8 @@
+from typing import List
+from MicroTokenizer.tokenizers.base_tokenizer import BaseTokenizer
+
+
+
+def load_tokenizers(model_dir: str) -> List[BaseTokenizer]:
+    """Load tokenizers from model_dir"""
+    pass
