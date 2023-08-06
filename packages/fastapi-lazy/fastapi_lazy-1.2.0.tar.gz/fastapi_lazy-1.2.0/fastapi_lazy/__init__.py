e7833865a19c16326f6665a56d203b01895145c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:37 +0000

    Add package fastapi-lazy/1.2.0/fastapi_lazy-1.2.0.tar.gz

---
+++ packages/fastapi-lazy/fastapi_lazy-1.2.0.tar.gz/fastapi_lazy-1.2.0/fastapi_lazy/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.2.0"
+
+"""
+    fastapi-lazy - A Lazy package-starter for FastAPI applications.
+"""
+
+import fastapi_lazy.auth
+import fastapi_lazy.database
+import fastapi_lazy.generator
+
+__all__ = [
+    "auth",
+    "database",
+    "generator",
+]
