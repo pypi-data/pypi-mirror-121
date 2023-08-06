36cdbf6c62be44e73749e28e90f75d4c4ff51a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:41 +0000

    Add package sort-natural-keys/0.0.0/sort_natural_keys-0.0.0.tar.gz

---
+++ packages/sort-natural-keys/sort_natural_keys-0.0.0.tar.gz/sort_natural_keys-0.0.0/README.md
@@ -0,0 +1,4 @@
+# Sort-natural-keys
+
+
+
---
+++ packages/sort-natural-keys/sort_natural_keys-0.0.0.tar.gz/sort_natural_keys-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sort_natural_keys',
+      version='0.0.0',
+      description='Sorts with natural keys an array of string.',
+      url='https://gitlab.com/quotengo/util/sort-natural-keys.git',
+      author='Jean-Sébastien Patenaude',
+      author_email='js.patenaude.quotengo@gmail.com',
+      license='MIT',
+      packages=['sort_natural_keys'],
+      zip_safe=False)
