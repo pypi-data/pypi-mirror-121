13b03f35e782162e6ca1d9481ecd93ab95f51f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:09 +0000

    Add package cicdtest/4.33/cicdtest-4.33.tar.gz

---
+++ packages/cicdtest/cicdtest-4.33.tar.gz/cicdtest-4.33/buildpan/setting.py
@@ -0,0 +1,23 @@
+"""
+    Title: setting.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 05-09-2021
+    Date Modified: 22-09-2021
+    Description:
+        ###############################################################
+        ## Create a environment variable in dictionary   ## 
+         ###############################################################
+ """
+
+
+info = {
+    "HOST" : "35.225.89.124",
+    "PUSH_COMMIT_URL" : "http://35.225.89.124/push_commit",
+    "FETCH_LOG_URL" : "http://35.225.89.124/fetch_log",
+    "GET_SHA_URL" : "http://35.225.89.124/get_sha",
+    "key" : "HB2xRCZSyKYMcTS9rm",
+    "secret_key" : "pQA5FMShe6deqr2b8Eq3GfDBmhEnn77C",
+    "version": "4.33"
+}
+
