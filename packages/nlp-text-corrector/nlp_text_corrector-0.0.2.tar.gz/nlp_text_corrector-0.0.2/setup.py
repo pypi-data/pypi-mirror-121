720cf76cb0047df94c2bf5f72a426a1c9e079ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package nlp-text-corrector/0.0.2/nlp_text_corrector-0.0.2.tar.gz

---
+++ packages/nlp-text-corrector/nlp_text_corrector-0.0.2.tar.gz/nlp_text_corrector-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+setup(
+    name='nlp_text_corrector',
+    packages=find_packages(include=['nlp_text_corrector']),
+    version='0.0.2',
+    description='IAMAI ASR Post Process Library',
+    url='https://github.com/iAmPlus/nlp-text-corrector',
+    author='Manoj Preveen',
+    author_email='manoj.velusamy@iamplus.com',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
