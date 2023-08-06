6054b0317727fc5970786a0b22bdc48f1ad83919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:18 +0000

    Add package gauss-bio-dist/1.5/gauss_bio_dist-1.5.tar.gz

---
+++ packages/gauss-bio-dist/gauss_bio_dist-1.5.tar.gz/gauss_bio_dist-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_bio_dist',
+      version='1.5',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_bio_dist'],
+      author='Ensiyeh Raoufi',
+      author_email='raoufien@gmail.com',
+      zip_safe=False)
