5fd89cd935616c8fd8933abcee392b6673bf6d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:35 +0000

    Add package justhacking/1.0.1/justhacking-1.0.1-py3-none-any.whl

---
+++ packages/justhacking/justhacking-1.0.1-py3-none-any.whl/justhacking/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from justhacking import *
+
+# Main Logical flow
+def starthack():
+  banner()
+  typit('[#] Hacking System')
+  dot_loading()
+  tqdm_loading('\n[%] Scanning target network')
+  typit('[+] Network Vulnerable\n > Setting up hacks to crack into the network')
+  time.sleep(0.5)
+  typit('.........')
+  time.sleep(0.5)
+  typit('\n[!] Cracking into the network\n > Hijacking sys inet ports 46654-65535 ')
+  time.sleep(0.5)
+  typit('.........')
+  time.sleep(0.2)
+  typit('\n[FPD Alert] Firewall Protection Detected !')
+  time.sleep(0.1)
+  typit('\n[:)] Cracking Firewall in : 3   2   1')
+  time.sleep(0.1)
+  typit('\n[FPD Vulnerable with ixss photom circuits]')
+  time.sleep(0.3)
+  typit('\n-----------Deactivating-------Firewall--------------')
+  time.sleep(0.2)
+  typit('\n[+] Firewall Deactivated successfully')
+  time.sleep(0.2)
+  alert('Attack Results', 'System got Hacked\nGained os full access')
+  typit('\n[!] System got Hacked Gained os full access')
+  clear()
+  typit("Follow on Twitter: @HrDivinemonk")
+  typit('\nGive a star on Pypi & Github\nhttps://github.com/divinemonk/justhacking')
+  time.sleep(5)
+  typit('\n\n      [Hacker is Back]\n')
+  banner()
+
+starthack()
