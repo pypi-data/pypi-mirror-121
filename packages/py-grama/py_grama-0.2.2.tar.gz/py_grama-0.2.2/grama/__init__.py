23444d9b016c247193cb3d19f3f61ee625cc9952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:19 +0000

    Add package py-grama/0.2.2/py_grama-0.2.2.tar.gz

---
+++ packages/py-grama/py_grama-0.2.2.tar.gz/py_grama-0.2.2/grama/__init__.py
@@ -0,0 +1,29 @@
+from .marginals import *
+from .tools import *
+from .core import *
+
+# Import dfply
+# --------------------------------------------------
+from .dfply import *
+
+# Add functionality to dfply
+from .string_helpers import *
+from .mutate_helpers import *
+
+## Load grama tools
+# --------------------------------------------------
+from .eval_defaults import *
+from .tran_tools import *
+
+from .comp_building import *
+from .comp_metamodels import *
+from .eval_random import *
+from .eval_tail import *
+from .eval_opt import *
+from .plot_auto import *
+from .tran_pivot import *
+from .tran_shapley import *
+from .tran_summaries import *
+from .support import *
+
+from .fit_synonyms import *
---
+++ packages/py-grama/py_grama-0.2.2.tar.gz/py_grama-0.2.2/grama/eval/eval_pyDOE.py
@@ -0,0 +1,69 @@
+__all__ = ["eval_lhs", "ev_lhs"]
+
+try:
+    from pyDOE import lhs
+
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("module pyDOE not found")
+
+import warnings
+import grama as gr
+from .. import add_pipe, pipe, custom_formatwarning
+from scipy.stats import norm, lognorm
+from numbers import Integral
+from numpy import tile, linspace, zeros, isfinite
+from numpy.linalg import cholesky, inv
+from numpy.random import random
+from numpy.random import seed as set_seed
+from pandas import DataFrame
+from toolz import curry
+
+
+## Latin Hypercube Sampling (LHS)
+# --------------------------------------------------
+@curry
+def eval_lhs(
+    model, n=1, df_det=None, seed=None, append=True, skip=False, criterion=None
+):
+    r"""Latin Hypercube evaluation
+    Evaluates a given model on a latin hypercube sample (LHS) using the model's
+    density.
+    Args:
+        model (gr.Model): Model to evaluate
+        n (numeric): Number of LHS samples to draw
+        df_det (DataFrame): Deterministic levels for evaluation; use "nom"
+            for nominal deterministic levels.
+        seed (int): Random seed to use
+        append (bool): Append results to conservative inputs?
+        skip (bool): Skip evaluation of the functions?
+        criterion (str): flag for LHS sample criterion
+            allowable values: None, "center" ("c"), "maxmin" ("m"),
+            "centermaxmin" ("cm"), "correlation" ("corr")
+    Returns:
+        DataFrame: Results of evaluation or unevaluated design
+    Notes:
+        - Wrapper on pyDOE.lhs
+    """
+    ## Set seed only if given
+    if seed is not None:
+        set_seed(seed)
+
+    ## Ensure sample count is int
+    if not isinstance(n, Integral):
+        print("eval_lhs() is rounding n...")
+        n = int(n)
+
+    ## Draw samples
+    df_quant = DataFrame(data=lhs(model.n_var_rand, samples=n), columns=model.var_rand)
+
+    ## Convert samples to desired marginals
+    df_rand = model.density.pr2sample(df_quant)
+    ## Construct outer-product DOE
+    df_samp = model.var_outer(df_rand, df_det=df_det)
+
+    if skip:
+        return df_samp
+    return gr.eval_df(model, df=df_samp, append=append)
+
+
+ev_lhs = add_pipe(eval_lhs)
