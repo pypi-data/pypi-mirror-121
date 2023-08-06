ca530ac88b38d801b8a5fd3ac73f04262bbe8ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:11 +0000

    Add package distributions-ahm/0.1/distributions-ahm-0.1.tar.gz

---
+++ packages/distributions-ahm/distributions-ahm-0.1.tar.gz/distributions-ahm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-ahm',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-ahm'],
+      zip_safe=False,
+      author = 'ahmed makhlouf',
+      author_email = 'ahmedhamada496@gmail.com')
