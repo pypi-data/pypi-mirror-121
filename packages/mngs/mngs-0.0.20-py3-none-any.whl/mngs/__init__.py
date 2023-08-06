28de881e9599f5e45c4cc23b8f532cb89bd5a799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:51 +0000

    Add package mngs/0.0.20/mngs-0.0.20-py3-none-any.whl

---
+++ packages/mngs/mngs-0.0.20-py3-none-any.whl/mngs/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Time-stamp: "2021-09-25 11:04:46 (ylab)"
+
+from . import dsp
+from . import general
+from . import ml
+from . import plt
+from . import stats
+from . import resource
+
+
+__copyright__ = "Copyright (C) 2021 Yusuke Watanabe"
+__version__ = "0.0.20"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/mngs"
