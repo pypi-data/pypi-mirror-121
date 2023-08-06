6b432b514fd25bc91aa52e7db0443420a6197123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:11 +0000

    Add package whoi-nlu-server-messages/0.0.31/whoi-nlu-server-messages-0.0.31.tar.gz

---
+++ packages/whoi-nlu-server-messages/whoi-nlu-server-messages-0.0.31.tar.gz/whoi-nlu-server-messages-0.0.31/include/whoi_nlu_server_messages/NetworkDescriptionMessage.py
@@ -0,0 +1,17 @@
+import dataclasses
+
+from whoi_nlu_server_messages.CBorMessage import CBorMessage
+
+
+@dataclasses.dataclass
+class NetworkDescriptionMessage(CBorMessage):
+    control_hostname: str = ""
+    control_port: int = 0
+    data_hostname: str = ""
+    data_port: int = 0
+    data_heavy_hostname: str = ""
+    data_heavy_port: int = 0
+    speech_to_text_hostname: str = ""
+    speech_to_text_port: int = 0
+    h2sl_hostname: str = ""
+    h2sl_port: int = 0
