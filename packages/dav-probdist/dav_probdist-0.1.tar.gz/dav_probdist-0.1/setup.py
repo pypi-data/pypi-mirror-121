6701041b5ca378fc57bc002dd8fb9e51c703f993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:41 +0000

    Add package dav-probdist/0.1/dav_probdist-0.1.tar.gz

---
+++ packages/dav-probdist/dav_probdist-0.1.tar.gz/dav_probdist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dav_probdist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dav_probdist'],
+      author = 'David Hazarika',
+      zip_safe=False)
