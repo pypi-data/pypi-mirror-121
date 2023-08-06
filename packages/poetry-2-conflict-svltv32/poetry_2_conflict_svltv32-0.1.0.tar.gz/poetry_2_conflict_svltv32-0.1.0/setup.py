379e46dd10a88ec61ca883fd764e6c0bb4f5925b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:00 +0000

    Add package poetry-2-conflict-svltv32/0.1.0/poetry_2_conflict_svltv32-0.1.0.tar.gz

---
+++ packages/poetry-2-conflict-svltv32/poetry_2_conflict_svltv32-0.1.0.tar.gz/poetry_2_conflict_svltv32-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_2_conflict_svltv32']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry_1_conflict_svltv32']
+
+setup_kwargs = {
+    'name': 'poetry-2-conflict-svltv32',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Test',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
