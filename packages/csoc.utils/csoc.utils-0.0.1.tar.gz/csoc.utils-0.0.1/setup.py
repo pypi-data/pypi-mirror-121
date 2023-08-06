edb8311eca0f802b87da7f2e1dc9f794b8237b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:59 +0000

    Add package csoc.utils/0.0.1/csoc.utils-0.0.1.tar.gz

---
+++ packages/csoc.utils/csoc.utils-0.0.1.tar.gz/csoc.utils-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="csoc.utils",
+    version="0.0.1",
+    author="CrowdStrike",
+    maintainer="CrowdStrike",
+    description="Retired CrowdStrike utilities package",
+    keywords=["retired"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
