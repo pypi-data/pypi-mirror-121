e093525487345c5ac063cac1e21b7eec59d3f5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package distributions-samlee688/0.1/distributions-samlee688-0.1.tar.gz

---
+++ packages/distributions-samlee688/distributions-samlee688-0.1.tar.gz/distributions-samlee688-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-samlee688',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-samlee688'],
+      zip_safe=False)
