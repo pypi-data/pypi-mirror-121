c934c7a1ff6835120f4b8fcffeb3ee6cfe62df14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:05 +0000

    Add package vutils-testing/0.2.0/vutils-testing-0.2.0.tar.gz

---
+++ packages/vutils-testing/vutils-testing-0.2.0.tar.gz/vutils-testing-0.2.0/ChangeLog.md
@@ -0,0 +1,15 @@
+# Change Log
+
+## 0.2.0
+
+* added functions
+  * `cover_typing`
+  * `make_callable`
+  * `make_mock`
+  * `make_type`
+* added classes
+  * `PatcherFactory`
+
+## 0.1.0
+
+* Initial development version
---
+++ packages/vutils-testing/vutils-testing-0.2.0.tar.gz/vutils-testing-0.2.0/tests/unit/common.py
@@ -0,0 +1,17 @@
+#                                                         -*- coding: utf-8 -*-
+# File:    ./tests/unit/common.py
+# Author:  Jiří Kučera <sanczes AT gmail.com>
+# Date:    2021-09-22 23:46:47 +0200
+# Project: vutils-testing: Auxiliary library for writing tests
+#
+# SPDX-License-Identifier: MIT
+#
+"""Shared test code and data."""
+
+SYMBOLS = (
+    "_ReturnsType",
+    "_SetupFuncType",
+    "_BasesType",
+    "_MembersType",
+    "_make_patch",
+)
