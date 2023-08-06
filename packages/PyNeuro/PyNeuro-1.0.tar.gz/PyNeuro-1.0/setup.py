013893c9f2f4fd82779dddcef8a57368b50071ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package PyNeuro/1.0/PyNeuro-1.0.tar.gz

---
+++ packages/PyNeuro/PyNeuro-1.0.tar.gz/PyNeuro-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+
+setup(name="PyNeuro",
+      version="1.0",
+      description="Library for connect with Neurosky's Mindwave EEG headset via TCP Socket",
+      author="Zach Wang",
+      author_email="wangziqi0325@gmail.com",
+      license="MIT",
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+      ],
+      url="https://github.com/ZACHSTRIVES/PyNeuro",
+      packages=['PyNeuro'],
+
+
+      )
