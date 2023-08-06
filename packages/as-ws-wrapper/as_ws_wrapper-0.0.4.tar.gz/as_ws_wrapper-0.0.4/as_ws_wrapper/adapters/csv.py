0a89b5780ed80c35e5e1787189bf8e8bfca8f6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package as-ws-wrapper/0.0.4/as_ws_wrapper-0.0.4.tar.gz

---
+++ packages/as-ws-wrapper/as_ws_wrapper-0.0.4.tar.gz/as_ws_wrapper-0.0.4/as_ws_wrapper/adapters/csv.py
@@ -0,0 +1,70 @@
+from csv import DictReader, DictWriter
+from io import StringIO
+from typing import List, Union
+
+from pydantic import BaseModel
+
+from ..models.base import TransactionModel
+
+
+class PydanticCSVAdapter:
+    """
+    Adaptador
+
+    BaseModel(pydantic) <===> csv
+    """
+
+    COMMA = ","
+    SEMICOLON = ";"
+    SPACE = " "
+    TAB = "\t"
+    PIPE = "|"
+    USE_BYTES = True
+    PYDANTIC_CLASS = TransactionModel
+
+    def pydantic_to_csv(
+        self,
+        instances: List[BaseModel],
+        pydantic_class: BaseModel.__class__ = PYDANTIC_CLASS,
+        delimiter=SEMICOLON,
+        use_bytes=USE_BYTES,
+    ):
+        instances_data = [instance.dict() for instance in instances]
+
+        buffer = StringIO()
+
+        writer = DictWriter(
+            buffer, list(pydantic_class.__fields__.keys()), delimiter=delimiter
+        )
+        writer.writeheader()
+        writer.writerows(instances_data)
+
+        result = buffer.getvalue()
+
+        if use_bytes:
+            result = result.encode()
+
+        return result
+
+    def csv_to_pydantic(
+        self,
+        csv: Union[str, bytes],
+        pydantic_class: BaseModel.__class__ = PYDANTIC_CLASS,
+        delimiter=SEMICOLON,
+        use_bytes=USE_BYTES,
+    ):
+        _input = csv
+
+        if use_bytes:
+            _input = _input.decode()
+
+        buffer = StringIO(_input)
+        reader = DictReader(
+            buffer, list(pydantic_class.__fields__.keys()), delimiter=delimiter
+        )
+        instances = []
+        for index, row in enumerate(reader):
+            if index == 0:
+                continue
+            instances.append(pydantic_class(**row))
+        return instances
