5b01deffa0d005e5ae9b7bd4a4033bd7e27d44c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:46 +0000

    Add package django-webpack-dev-server/0.0.12/django-webpack-dev-server-0.0.12.tar.gz

---
+++ packages/django-webpack-dev-server/django-webpack-dev-server-0.0.12.tar.gz/django-webpack-dev-server-0.0.12/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+package_description = 'A Django app to create configuration files for frontend Javascript framework.'
+
+setup(
+    name='django-webpack-dev-server',
+    version='0.0.12',
+    packages=find_packages(),
+    license='MIT',
+    author='Jiten Sidhpura',
+    author_email='jitensidhpura2000@gmail.com',
+    description=package_description,
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/Jitensid/django-webpack-dev-server',
+    download_url='https://github.com/Jitensid/django-webpack-dev-server/archive/refs/tags/0.0.12.tar.gz',
+    install_requires=['requests', 'progressbar2', 'python-dotenv'],
+    keywords=['django', 'webpack', 'webpack_dev_server', 'react'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
