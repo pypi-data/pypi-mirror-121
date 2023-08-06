f63d7ae3da7504f2ad123916b1b9807c0ced3487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:33 +0000

    Add package pipelinewise-target-snowflake/1.14.0/pipelinewise-target-snowflake-1.14.0.tar.gz

---
+++ packages/pipelinewise-target-snowflake/pipelinewise-target-snowflake-1.14.0.tar.gz/pipelinewise-target-snowflake-1.14.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name="pipelinewise-target-snowflake",
+      version="1.14.0",
+      description="Singer.io target for loading data to Snowflake - PipelineWise compatible",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author="Wise",
+      url='https://github.com/transferwise/pipelinewise-target-snowflake',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=["target_snowflake"],
+      install_requires=[
+          'pipelinewise-singer-python==1.*',
+          'snowflake-connector-python[pandas]==2.4.6',
+          'inflection==0.5.1',
+          'joblib==1.0.1',
+          'numpy<1.21.0',
+          'python-dateutil==2.8.2'
+      ],
+      extras_require={
+          "test": [
+              "pylint==2.8.3",
+              'pytest==6.2.4',
+              'pytest-cov==2.12.1',
+              "python-dotenv==0.18.0"
+          ]
+      },
+      entry_points="""
+          [console_scripts]
+          target-snowflake=target_snowflake:main
+      """,
+      packages=find_packages(exclude=['tests*']),
+      )
