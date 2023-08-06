e8c0cf429751f0c32f7a15da1702dc9ba4e8e1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:45 +0000

    Add package vigorish/0.6.3/vigorish-0.6.3-py3-none-any.whl

---
+++ packages/vigorish/vigorish-0.6.3-py3-none-any.whl/vigorish/models/pitch_type_percentiles.py
@@ -0,0 +1,124 @@
+from sqlalchemy import Boolean, Column, Float, Integer, String
+
+import vigorish.database as db
+
+
+class PitchTypePercentile(db.Base):
+
+    __tablename__ = "pitch_type_percentiles"
+    id = Column(Integer, primary_key=True)
+    stat_name = Column(String)
+    pitch_type = Column(String)
+    thrown_r = Column(Boolean)
+    thrown_l = Column(Boolean)
+    thrown_both = Column(Boolean)
+    percentile = Column(Float)
+    stat_value = Column(Float)
+
+    @classmethod
+    def get_percentile_for_rhp_pos_stat(cls, db_session, stat_name, pfx_metrics):
+        stat_value = getattr(pfx_metrics, stat_name)
+        pt_percentile = (
+            db_session.query(cls)
+            .filter_by(stat_name=stat_name)
+            .filter_by(thrown_r=True)
+            .filter_by(pitch_type=str(pfx_metrics.pitch_type))
+            .filter(cls.stat_value >= stat_value)
+            .first()
+        )
+        return (
+            (round(stat_value, ndigits=1), pt_percentile.percentile)
+            if pt_percentile
+            else (round(stat_value, ndigits=1), 100.0)
+        )
+
+    @classmethod
+    def get_percentile_for_lhp_pos_stat(cls, db_session, stat_name, pfx_metrics):
+        stat_value = getattr(pfx_metrics, stat_name)
+        pt_percentile = (
+            db_session.query(cls)
+            .filter_by(stat_name=stat_name)
+            .filter_by(thrown_l=True)
+            .filter_by(pitch_type=str(pfx_metrics.pitch_type))
+            .filter(cls.stat_value >= stat_value)
+            .first()
+        )
+        return (
+            (round(stat_value, ndigits=1), pt_percentile.percentile)
+            if pt_percentile
+            else (round(stat_value, ndigits=1), 100.0)
+        )
+
+    @classmethod
+    def get_percentile_for_rhp_neg_stat(cls, db_session, stat_name, pfx_metrics):
+        stat_value = getattr(pfx_metrics, stat_name)
+        pt_percentile = (
+            db_session.query(cls)
+            .filter_by(stat_name=stat_name)
+            .filter_by(thrown_r=True)
+            .filter_by(pitch_type=str(pfx_metrics.pitch_type))
+            .filter(cls.stat_value <= stat_value)
+            .first()
+        )
+        return (
+            (round(stat_value, ndigits=1), pt_percentile.percentile)
+            if pt_percentile
+            else (round(stat_value, ndigits=1), 100.0)
+        )
+
+    @classmethod
+    def get_percentile_for_lhp_neg_stat(cls, db_session, stat_name, pfx_metrics):
+        stat_value = getattr(pfx_metrics, stat_name)
+        pt_percentile = (
+            db_session.query(cls)
+            .filter_by(stat_name=stat_name)
+            .filter_by(thrown_l=True)
+            .filter_by(pitch_type=str(pfx_metrics.pitch_type))
+            .filter(cls.stat_value <= stat_value)
+            .first()
+        )
+        return (
+            (round(stat_value, ndigits=1), pt_percentile.percentile)
+            if pt_percentile
+            else (round(stat_value, ndigits=1), 100.0)
+        )
+
+    @classmethod
+    def calculate_pitch_type_percentiles(cls, db_session, p_throws, pfx):
+        return (
+            cls.calculate_pitch_type_percentiles_for_rhp(db_session, pfx)
+            if p_throws == "R"
+            else cls.calculate_pitch_type_percentiles_for_lhp(db_session, pfx)
+        )
+
+    @classmethod
+    def calculate_pitch_type_percentiles_for_rhp(cls, db_session, pfx):
+        return {
+            "pitch_type": str(pfx.pitch_type),
+            "avg_speed": cls.get_percentile_for_rhp_pos_stat(db_session, "avg_speed", pfx),
+            "ops": cls.get_percentile_for_rhp_neg_stat(db_session, "ops", pfx),
+            "zone_rate": cls.get_percentile_for_rhp_pos_stat(db_session, "zone_rate", pfx),
+            "o_swing_rate": cls.get_percentile_for_rhp_pos_stat(db_session, "o_swing_rate", pfx),
+            "whiff_rate": cls.get_percentile_for_rhp_pos_stat(db_session, "whiff_rate", pfx),
+            "bad_whiff_rate": cls.get_percentile_for_rhp_pos_stat(db_session, "bad_whiff_rate", pfx),
+            "contact_rate": cls.get_percentile_for_rhp_neg_stat(db_session, "contact_rate", pfx),
+            "ground_ball_rate": cls.get_percentile_for_rhp_pos_stat(db_session, "ground_ball_rate", pfx),
+            "barrel_rate": cls.get_percentile_for_rhp_neg_stat(db_session, "barrel_rate", pfx),
+            "avg_exit_velocity": cls.get_percentile_for_rhp_neg_stat(db_session, "avg_launch_speed", pfx),
+        }
+
+    @classmethod
+    def calculate_pitch_type_percentiles_for_lhp(cls, db_session, pfx):
+        return {
+            "pitch_type": str(pfx.pitch_type),
+            "avg_speed": cls.get_percentile_for_lhp_pos_stat(db_session, "avg_speed", pfx),
+            "ops": cls.get_percentile_for_lhp_neg_stat(db_session, "ops", pfx),
+            "zone_rate": cls.get_percentile_for_lhp_pos_stat(db_session, "zone_rate", pfx),
+            "o_swing_rate": cls.get_percentile_for_lhp_pos_stat(db_session, "o_swing_rate", pfx),
+            "whiff_rate": cls.get_percentile_for_lhp_pos_stat(db_session, "whiff_rate", pfx),
+            "bad_whiff_rate": cls.get_percentile_for_lhp_pos_stat(db_session, "bad_whiff_rate", pfx),
+            "contact_rate": cls.get_percentile_for_lhp_neg_stat(db_session, "contact_rate", pfx),
+            "ground_ball_rate": cls.get_percentile_for_lhp_pos_stat(db_session, "ground_ball_rate", pfx),
+            "barrel_rate": cls.get_percentile_for_lhp_neg_stat(db_session, "barrel_rate", pfx),
+            "avg_exit_velocity": cls.get_percentile_for_lhp_neg_stat(db_session, "avg_launch_speed", pfx),
+        }
