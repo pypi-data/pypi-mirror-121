ecb18f4427d3b47d3b6a2ad1e255cd81c854e462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:15 +0000

    Add package discord-port/1.3/discord_port-1.3.tar.gz

---
+++ packages/discord-port/discord_port-1.3.tar.gz/discord_port-1.3/discord_port/__init__.py
@@ -0,0 +1,8 @@
+"""
+discord_port is a simple way to make auth with the Discord API!
+"""
+from tools.auth import AuthClient as AuthClient
+from tools.user import UserAPI as UserAPI
+
+__name__ = "discord_port"
+__version__ = '1.3'
