a513a2447488706d4832f29bef1036f7b7eec94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:38 +0000

    Add package functest-plearner/1.0/functest_plearner-1.0.tar.gz

---
+++ packages/functest-plearner/functest_plearner-1.0.tar.gz/functest_plearner-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="functest_plearner",
+    version="1.0",
+    py_modules=['searchphrase'],
+    author='piy',
+    author_email='',
+    description='sdfsa',
+    url=''
+)
