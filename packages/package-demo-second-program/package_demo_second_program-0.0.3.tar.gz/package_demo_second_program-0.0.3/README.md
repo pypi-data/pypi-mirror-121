b9a3e6953adfe03cd84f2a0f673761d851f0a2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:29 +0000

    Add package package-demo-second-program/0.0.3/package_demo_second_program-0.0.3.tar.gz

---
+++ packages/package-demo-second-program/package_demo_second_program-0.0.3.tar.gz/package_demo_second_program-0.0.3/README.md
@@ -0,0 +1,8 @@
+# packagedemo
+demo for package
+
+##How to use this
+```python
+import second_program
+second_program.setName("<YOUR NAME>")
+```
---
+++ packages/package-demo-second-program/package_demo_second_program-0.0.3.tar.gz/package_demo_second_program-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME ="package_demo_second_program"
+USER_NAME ="RinkiDS"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}",
+    version="0.0.3",
+    author=USER_NAME,
+    author_email="rinki.sharma@gmail.com",
+    description="A small  package example",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
