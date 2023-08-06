9d12f09373a2e19c75f57cb2d292b0efaef2b417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:58 +0000

    Add package datamaestro-text/2021.9.29/datamaestro_text-2021.9.29-py3-none-any.whl

---
+++ packages/datamaestro-text/datamaestro_text-2021.9.29-py3-none-any.whl/datamaestro_text/data/ir/trec.py
@@ -0,0 +1,58 @@
+from typing import List
+from datamaestro.definitions import argument, Option
+from datamaestro.data import Base
+from experimaestro import documentation, Param
+from pathlib import Path
+from datamaestro_text.data.ir import (
+    AdhocDocuments,
+    AdhocTopics,
+    AdhocAssessments,
+    AdhocRun,
+)
+
+
+class TrecAdhocTopics(AdhocTopics):
+    path: Option[Path]
+    parts: Option[List[str]]
+
+    @documentation
+    def iter(self):
+        """Iterate over TREC adhoc topics"""
+        import datamaestro_text.interfaces.trec as trec
+
+        yield from trec.parse_query_format(self.path)
+
+
+class TrecAdhocAssessments(AdhocAssessments):
+    path: Option[Path]
+
+    def trecpath(self):
+        return self.path
+
+    @documentation
+    def iter(self):
+        """Iterate over TREC adhoc topics"""
+        import datamaestro_text.interfaces.trec as trec
+
+        yield from trec.parse_qrels(self.path)
+
+
+@argument("path", type=Path)
+class TrecAdhocRun(AdhocRun):
+    pass
+
+
+@argument(
+    "metrics",
+    required=False,
+    type=List[str],
+    help="List of reported metrics (None if all from trec_eval)",
+)
+@argument("results", type=Path, help="Main results")
+@argument("detailed", type=Path, required=False, help="Results per topic (if any)")
+class TrecAdhocResults(Base):
+    pass
+
+
+class TipsterCollection(AdhocDocuments):
+    path: Param[Path]
