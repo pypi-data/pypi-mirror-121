06a906fd1eaae4d33ad464868434a9fb9de1d382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:31 +0000

    Add package OneCord/3.0.0b0/OneCord-3.0.0b0-py3-none-any.whl

---
+++ packages/OneCord/OneCord-3.0.0b0-py3-none-any.whl/onecord/ui/__init__.py
@@ -0,0 +1,15 @@
+"""
+onecord.ui
+~~~~~~~~~~~
+
+Bot UI Kit helper for the onecord API
+
+:copyright: (c) 2015-present Rapptz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .view import *
+from .item import *
+from .button import *
+from .select import *
