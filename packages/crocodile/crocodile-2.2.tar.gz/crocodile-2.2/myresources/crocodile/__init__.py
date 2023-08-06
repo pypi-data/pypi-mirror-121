fd5f3b2fea9e3a6d06a612a57da5ba2ba1f48b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:11 +0000

    Add package crocodile/2.2/crocodile-2.2.tar.gz

---
+++ packages/crocodile/crocodile-2.2.tar.gz/crocodile-2.2/myresources/crocodile/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "2.2"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+release_notes = """
+
+Fixed deep learning problems
+"""
