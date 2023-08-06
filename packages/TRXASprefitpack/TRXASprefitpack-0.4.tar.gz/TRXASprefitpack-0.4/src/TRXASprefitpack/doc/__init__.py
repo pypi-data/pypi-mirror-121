dccde01ac629f435b74a9ecaf6a52b0a3b6d81d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:39 +0000

    Add package TRXASprefitpack/0.4/TRXASprefitpack-0.4.tar.gz

---
+++ packages/TRXASprefitpack/TRXASprefitpack-0.4.tar.gz/TRXASprefitpack-0.4/src/TRXASprefitpack/doc/__init__.py
@@ -0,0 +1,9 @@
+'''
+doc:
+Submodule for the TRXASprefitpack documentation
+
+:copyright: 2021 by pistack (Junho Lee).
+:license: LGPL3.
+'''
+
+from .info import __info__
---
+++ packages/TRXASprefitpack/TRXASprefitpack-0.4.tar.gz/TRXASprefitpack-0.4/src/TRXASprefitpack/mathfun/__init__.py
@@ -0,0 +1,13 @@
+'''
+mathfun:
+submodule for the mathematical functions for TRXASprefitpack
+
+:copyright: 2021 by pistack (Junho Lee).
+:license: LGPL3.
+'''
+
+from .mathfun import exp_conv_gau, exp_conv_cauchy
+from .mathfun import solve_model, compute_model
+from .mathfun import compute_signal_gau, compute_signal_cauchy
+from .mathfun import compute_signal_pvoigt
+from .mathfun import model_n_comp_conv, fact_anal_exp_conv
