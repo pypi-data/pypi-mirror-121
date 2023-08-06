86ffad8aeec37ab2a0232c8552fa9d342f9dfffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:11 +0000

    Add package distrib-probab/0.1/distrib_probab-0.1.tar.gz

---
+++ packages/distrib-probab/distrib_probab-0.1.tar.gz/distrib_probab-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distrib_probab',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distrib_probab'],
+      author = 'Stephen G',
+      zip_safe=False)
