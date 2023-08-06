77694d1fa49d1f653d07ba44402daaafe5463588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:32 +0000

    Add package EduKTM/0.0.9/EduKTM-0.0.9.tar.gz

---
+++ packages/EduKTM/EduKTM-0.0.9.tar.gz/EduKTM-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+test_deps = [
+    'pytest>=4',
+    'pytest-cov>=2.6.0',
+    'pytest-flake8',
+]
+
+setup(
+    name='EduKTM',
+    version='0.0.9',
+    extras_require={
+        'test': test_deps,
+    },
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "tqdm",
+        "numpy>=1.16.5",
+        "scikit-learn",
+        "pandas",
+    ],  # And any other dependencies for needs
+    entry_points={
+    },
+)
