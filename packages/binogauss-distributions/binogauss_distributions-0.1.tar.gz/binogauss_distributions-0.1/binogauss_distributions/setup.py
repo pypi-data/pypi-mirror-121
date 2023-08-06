f6b67780ec9d6e1e2e7963e3e5a29810799ad8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:23 +0000

    Add package binogauss-distributions/0.1/binogauss_distributions-0.1.tar.gz

---
+++ packages/binogauss-distributions/binogauss_distributions-0.1.tar.gz/binogauss_distributions-0.1/binogauss_distributions/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Samson Tontoye',
+      author_email = 'godfatherfigure1@gmail.com',
+      zip_safe=False)
---
+++ packages/binogauss-distributions/binogauss_distributions-0.1.tar.gz/binogauss_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binogauss_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['binogauss_distributions'],
+      author = 'Samson Tontoye',
+      author_email = 'godfatherfigure1@gmail.com',
+      zip_safe=False)
