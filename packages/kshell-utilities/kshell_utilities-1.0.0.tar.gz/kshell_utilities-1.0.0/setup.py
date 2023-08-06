f94610bb59fb38e99173e11a18aacc3a1dc0b3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:41 +0000

    Add package kshell-utilities/1.0.0/kshell_utilities-1.0.0.tar.gz

---
+++ packages/kshell-utilities/kshell_utilities-1.0.0.tar.gz/kshell_utilities-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'kshell_utilities',
+    version = '1.0.0',    
+    description = 'Handy utilities for handling nuclear shell model calculations from KSHELL',
+    url = 'https://github.com/GaffaSnobb/kshell_utilities',
+    author = 'Jon Kristian Dahl',
+    author_email = 'jonkd@uio.no',
+    packages = ['kshell_utilities', 'tests'],
+    install_requires = ['numpy', 'matplotlib'],
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+)
