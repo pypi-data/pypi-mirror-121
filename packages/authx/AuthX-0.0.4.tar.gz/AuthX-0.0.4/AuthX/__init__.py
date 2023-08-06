fa6c9ea801a07111c59daef2009c1f15c4472907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package authx/0.0.4/AuthX-0.0.4.tar.gz

---
+++ packages/authx/AuthX-0.0.4.tar.gz/AuthX-0.0.4/AuthX/__init__.py
@@ -0,0 +1,7 @@
+"""Ready to use and customizable Authentications and Oauth2 management for FastAPI"""
+
+__version__ = "0.0.4"
+
+from AuthX.main import Authentication, AuthX, User
+
+__all__ = ["AuthX", "Authentication", "User"]
