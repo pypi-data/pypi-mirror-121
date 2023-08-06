f71422eadcafaabd3dd4968a75dfab0e7ce4e8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:44 +0000

    Add package subsy/0.9.1/subsy-0.9.1-py3-none-any.whl

---
+++ packages/subsy/subsy-0.9.1-py3-none-any.whl/subsy/meta.py
@@ -0,0 +1,9 @@
+﻿"""Meta-information about the package."""
+
+title     = 'Subsy'
+synopsis  = 'Access to subtitles from various file formats'
+version   = '0.9.1'
+date      = '2021–09–30'
+author    = 'John Hennig'
+copyright = 'Johhn Hennig'
+license   = 'MIT, © 2021 John Hennig'
