f03c42152d93805e9d74198dec19f1cc3c38c1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:48 +0000

    Add package mitoinstaller/0.0.79/mitoinstaller-0.0.79.tar.gz

---
+++ packages/mitoinstaller/mitoinstaller-0.0.79.tar.gz/mitoinstaller-0.0.79/mitoinstaller/installer_steps/installer_step_utils.py
@@ -0,0 +1,33 @@
+from mitoinstaller.log_utils import log_error
+import traceback
+from typing import List
+
+from mitoinstaller.installer_steps.installer_step import InstallerStep
+
+
+def run_installer_steps(installer_steps: List[InstallerStep]):
+    """
+    A helper function that runs the installer steps, returning
+    True if they all terminate without error, and False if any
+    of them return with an error.
+    """
+
+    for installer_step in installer_steps:
+        try:
+            # Print the step name, so the user knows what's going on
+            print(installer_step.installer_step_name)
+            installer_step.execute()
+        except:
+            log_error(installer_step.installer_step_name)
+
+            # If we error on a non optional step, then return False
+            # and continue if it was an optional step
+            if installer_step.optional:
+                continue
+            else:
+                return False
+    
+    return True
+
+
+    
