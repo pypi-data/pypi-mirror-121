5dd2ba4bc826298aef68405f4a3f887c8f4dae20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package eh-probability/0.1/eh_probability-0.1.tar.gz

---
+++ packages/eh-probability/eh_probability-0.1.tar.gz/eh_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eh_probability',
+      version='0.1',
+      description='Gaussian and binomoal distributions',
+      packages=['eh_probability'],
+      zip_safe=False)
