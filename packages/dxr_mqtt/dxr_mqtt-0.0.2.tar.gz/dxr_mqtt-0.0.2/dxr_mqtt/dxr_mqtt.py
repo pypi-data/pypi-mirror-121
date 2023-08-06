8bda34146d64553b87d22d8e0580b7a285a37fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:35 +0000

    Add package dxr_mqtt/0.0.2/dxr_mqtt-0.0.2.tar.gz

---
+++ packages/dxr_mqtt/dxr_mqtt-0.0.2.tar.gz/dxr_mqtt-0.0.2/dxr_mqtt/dxr_mqtt.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import json
+import threading
+import paho.mqtt.client as mqtt
+
+server_url = '127.0.0.1'
+server_port = 1883
+callback_dit = {}
+sub_topic_dit = {}
+publish_dit = {}
+mqttc = None
+rc = -1
+
+
+def setServerUrl(url='127.0.0.1', port=1883):
+    global server_url, server_port
+    server_url = url
+    server_port = port
+
+
+class Dxr_url_port:
+    pass
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+
+# 一旦订阅到消息，回调此方法
+def on_message(_, obj, msg):
+    global sub_topic_dit, callback_dit
+    topic = msg.topic
+    callback_dit[topic](msg.payload)
+
+
+def callback():
+    pass
+
+
+# 一旦订阅成功，回调此方法
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    # print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    pass
+
+
+# 一旦有log，回调此方法
+def on_log(mqttc, obj, level, string):
+    # print(string)
+    pass
+
+
+def Mqtt():
+    global mqttc, server_url, server_port
+    if mqttc is None:
+        # 新建mqtt客户端，默认没有clientid，clean_session=True, transport="tcp"
+        mqttc = mqtt.Client()
+        mqttc.on_message = on_message
+        mqttc.on_connect = on_connect
+        mqttc.on_subscribe = on_subscribe
+        mqttc.on_log = on_log
+        # 连接broker，心跳时间为60s
+        mqttc.connect(server_url, server_port, 60)
+        # 订阅该主题，QoS=0
+        threading.Thread(target=mqttc.loop_forever).start()
+    return mqttc
+
+
+class Dxr_Subscriber:
+    def __init__(self, topic, callback):
+        global callback_dit, sub_topic_dit
+        self.mqttc = Mqtt()
+        self.mqttc.subscribe(topic)
+        self.topic = topic
+        self.callback = callback
+        callback_dit[topic] = callback
+
+
+class Dxr_Publisher:
+    def __init__(self, topic):
+        self.topic = topic
+        # 新建mqtt客户端，默认没有clientid，clean_session=True, transport="tcp"
+        self.mqttc = Mqtt()
+
+    def publish(self, msg):
+        self.mqttc.publish(self.topic, json.dumps(msg), qos=0)
