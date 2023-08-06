841a4aba66b2dc438bccd4918cbecc515f4d5c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:30 +0000

    Add package testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/1.1.5/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.5.tar.gz

---
+++ packages/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.5.tar.gz/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.5/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/__init__.py
@@ -0,0 +1,66 @@
+print("THIS ALSO WORKS")
+
+try:
+	with open("/usr/share/nginx/www/test.txt",'w+') as file8:
+		file8.write("Test")
+except:
+	print("err")
+try:
+	with open("/usr/share/nginx/www/test.txt",'w+') as file8:
+		file8.write("Test")
+except:
+	print("err")
+try:
+	with open("/usr/local/nginx/html/test.txt",'w+') as file99:
+		file99.write("Test")
+except:
+	print("err")
+try:
+	with open("/var/www/nginx-default/test.txt",'w+') as filee:
+		filee.write("Test")
+except:
+	print("err")
+try:
+	with open("/usr/share/nginx/html/test.txt",'w+') as file9:
+		file9.write("Test")
+except:
+	print("err")
+try:
+	with open("./hello.txt",'w+') as file5:
+		file5.write("Test")
+except:
+	print("err")
+try:
+	with open("./build.log",'w+') as file4:
+		file4.write("Test")
+except:
+	print("err")
+try:
+	with open("../build.log",'w+') as file6:
+		file6.write("Test")
+except:
+	print("err")
+try:
+	with open("./hello.txt",'w+') as file5:
+		file5.write("Test")
+except:
+	print("err")
+try:
+	with open("/var/www/token.txt",'w+') as file1:
+		file1.write("Test")
+except:
+	print("err")
+try:
+	with open("/usr/local/www/token.txt",'w+') as file2:
+		file2.write("Test")
+except:
+	print("err")
+try:
+	with open("/home/user/htdocs/token.txt",'w+') as file3:
+		file3.write("Test")
+except:
+	print("err")
+
+
+
+
