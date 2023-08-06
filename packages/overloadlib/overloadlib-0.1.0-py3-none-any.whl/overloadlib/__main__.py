6255bf10ba6924f615aca457d4b7be0b99e56503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:47 +0000

    Add package overloadlib/0.1.0/overloadlib-0.1.0-py3-none-any.whl

---
+++ packages/overloadlib/overloadlib-0.1.0-py3-none-any.whl/overloadlib/__main__.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """overloadlib."""
+
+
+if __name__ == "__main__":
+    main(prog_name="overloadlib")  # pragma: no cover
