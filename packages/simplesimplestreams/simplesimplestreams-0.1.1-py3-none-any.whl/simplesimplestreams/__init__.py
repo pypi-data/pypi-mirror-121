9aea4b583ab75668eb28f24e07735d969e05fdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package simplesimplestreams/0.1.1/simplesimplestreams-0.1.1-py3-none-any.whl

---
+++ packages/simplesimplestreams/simplesimplestreams-0.1.1-py3-none-any.whl/simplesimplestreams/__init__.py
@@ -0,0 +1,13 @@
+from .simplestreams import *
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "StreamIndex",
+    "Stream",
+    "ProductVersionItem",
+    "ProductVersion",
+    "Product",
+    "Products",
+    "SimpleStreamsClient",
+]
