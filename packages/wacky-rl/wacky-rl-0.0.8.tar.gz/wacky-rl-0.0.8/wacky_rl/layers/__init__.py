79f8756677b4664fc195dce71e77133a97a578ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:43 +0000

    Add package wacky-rl/0.0.8/wacky-rl-0.0.8.tar.gz

---
+++ packages/wacky-rl/wacky-rl-0.0.8.tar.gz/wacky-rl-0.0.8/wacky_rl/layers/__init__.py
@@ -0,0 +1,6 @@
+#from wacky_rl.layers.actor_layer import DiscreteActionLayer
+#from wacky_rl.layers.actor_layer import ContinActionLayer
+
+from wacky_rl.layers.contin_actor_layer import ContinActionLayer
+from wacky_rl.layers.discrete_actor_layer import DiscreteActionLayer
+from wacky_rl.layers.recurrent_encoder import RecurrentEncoder
