0343d7fa918248abb491519954af88a7ec615ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:42 +0000

    Add package gens/0.1/gens-0.1-py3-none-any.whl

---
+++ packages/gens/gens-0.1-py3-none-any.whl/gens/__init__.py
@@ -0,0 +1,15 @@
+"""
+Initializing the Python package
+"""
+
+from .main import generate, generate_id, generate_password
+
+
+__version__ = '0.1'
+
+__all__ = (
+    '__version__',
+    'generate',
+    'generate_id',
+    'generate_password',
+)
