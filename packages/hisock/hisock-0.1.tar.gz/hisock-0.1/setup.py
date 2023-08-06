3a14c8620ddeb6b66abe389f771d6feeaf86b1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:15 +0000

    Add package hisock/0.1/hisock-0.1.tar.gz

---
+++ packages/hisock/hisock-0.1.tar.gz/hisock-0.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+ROOT = pathlib.Path(__file__).parent
+
+README = (ROOT / "README.md").read_text()
+
+setup(
+    name="hisock",
+    version="0.1",
+    description="A higher-level extension of the socket module, with simpler and more efficient usages",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SSS-Says-Snek/hisock",
+    author="SSS-Says-Snek",
+    author_email="bmcomis2018@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    install_requires=[
+        "pytest>=6.2.5",
+        "cryptography>=3.4.8"
+    ],
+    packages=[
+        'hisock'
+    ]
+)
