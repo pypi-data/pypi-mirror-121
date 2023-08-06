01fe6a6ffbd5a56ad48823c1827fa7d10f989b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:17 +0000

    Add package sra-py/0.1.3/sra_py-0.1.3.tar.gz

---
+++ packages/sra-py/sra_py-0.1.3.tar.gz/sra_py-0.1.3/sra_py/sra_py/canvas.py
@@ -0,0 +1,53 @@
+import requests
+import io
+import aiohttp
+
+SR = "https://some-random-api.ml/canvas/"
+
+async def invert(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/invert?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
+
+async def wasted(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/wasted?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
+
+async def jail(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/jail?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
+
+async def mission_passed(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/passed?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
+
+async def glass(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/glass?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
+
+async def comrade(avatar_url: str):
+	async with aiohttp.ClientSession() as se:
+		async with se.get(f"https://some-random-api.ml/canvas/comrade?avatar={avatar_url}") as img:
+			im = io.BytesIO(await img.read())
+			await se.close()
+
+	return im
