c95bb1070b67a8ec4901734e131f61acd2b0ae0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:40 +0000

    Add package monitor-air-quality/2.0.1/monitor_air_quality-2.0.1.tar.gz

---
+++ packages/monitor-air-quality/monitor_air_quality-2.0.1.tar.gz/monitor_air_quality-2.0.1/setup.py
@@ -0,0 +1,27 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='monitor_air_quality',  # Required
+    version='2.0.1',  # Required
+    description='Read data from sds011 sensor, gather purpleair and notion data and POST to an API',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/gene1wood/monitor_air_quality',  # Optional
+    author='Gene Wood',  # Optional
+    author_email='gene_wood@cementhorizon.com',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    python_requires='>=3',
+    entry_points = {
+        'console_scripts': ['monitor_air_quality=monitor_air_quality.monitor_air_quality:main'],
+    },
+    install_requires=['PyYAML', 'requests', 'py-sds011', 'python-aqi', 'xdg'],
+    include_package_data=True,
+)
