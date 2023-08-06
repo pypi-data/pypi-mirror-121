e9811276417818a0ff1b926b564a4e58b3ee8407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:12 +0000

    Add package Pyfrog/0.0.13/Pyfrog-0.0.13.tar.gz

---
+++ packages/Pyfrog/Pyfrog-0.0.13.tar.gz/Pyfrog-0.0.13/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Pyfrog - Frog checker.'
+LONG_DESCRIPTION = 'Py frog - Frog checker by frogyfrog.'
+
+# Setting up
+setup(
+    name="Pyfrog",
+    version=VERSION,
+    author="Frog frog",
+    author_email="<frog@frogyfrog.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'frog'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
