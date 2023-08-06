494a5c20b5d1dbd913b6a576da6d7d042340a3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:35 +0000

    Add package Gsauce-pyds/0.0.5/Gsauce_pyds-0.0.5-py3-none-any.whl

---
+++ packages/Gsauce-pyds/Gsauce_pyds-0.0.5-py3-none-any.whl/dstructure/__init__.py
@@ -0,0 +1,14 @@
+from .LinkedList import Node
+from .LinkedList import SLL
+from .LinkedList import CircularList
+from .DoublyLinkedList import Node
+from .DoublyLinkedList import DoublyLL
+from .DoublyLinkedList import CircularlyDLL
+from .stack import Stack
+from .stack import lstack
+
+from .queue import *
+
+print('''Hello there! Greetings human! ( ͡° ͜ʖ ͡°) 
+Make sure to go through the README.md file or the docs. 
+Be careful with indexing \n Happy Coding!!! (ᵔᴥᵔ) ''')
