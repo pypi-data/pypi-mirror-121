19a700cc2790bd9b8ec38086ec5d0e712a67fd25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:59 +0000

    Add package pystein/0.5.3/pystein-0.5.3.tar.gz

---
+++ packages/pystein/pystein-0.5.3.tar.gz/pystein-0.5.3/pystein/tests/test_pynstein.py
@@ -0,0 +1,35 @@
+"""Unittests for pystein package
+
+Presently only a dummy test to confirm repo setup and CI integration
+"""
+
+import pathlib
+
+import pystein
+from pystein import tests
+
+
+class TestPyStein:
+    """Test pystein package"""
+
+    def test_package_version(self):
+        """Consistency test for version numbers"""
+        exp = (0, 5, 3)
+        msg = 'Collapse Package {comp} Version Mismatch: Expected {exp:d}, got {got:d}'
+        assert pystein.__MAJOR__ == exp[0], msg.format(comp='MAJOR', exp=exp[0], got=pystein.__MAJOR__)
+        assert pystein.__MINOR__ == exp[1], msg.format(comp='MINOR', exp=exp[1], got=pystein.__MINOR__)
+        assert pystein.__MICRO__ == exp[2], msg.format(comp='MICRO', exp=exp[2], got=pystein.__MICRO__)
+
+    def test_test_root(self):
+        """Test test root dir"""
+        exp = pathlib.Path(__file__).parent.parent
+        assert tests.TEST_ROOT == exp, 'Collapse Test Directory moved. Expected {}, got {}'.format(exp.as_posix(), tests.TEST_ROOT.as_posix())
+
+    def test_run_tests(self, mocker):
+        """The trick here is to duck punch the pytest main function to short-circuit this call"""
+        mocker.patch(
+            # Don't want to invoke pytest from within build suite
+            'pytest.main',
+            return_value=None,
+        )
+        tests.run_tests()
---
+++ packages/pystein/pystein-0.5.3.tar.gz/pystein-0.5.3/setup.py
@@ -0,0 +1,39 @@
+"""Setup file
+"""
+
+import setuptools
+
+import pystein
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='pystein',
+                 version=pystein.__version__,
+                 description='Collapse',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 python_requires='>=3.7',
+                 url=pystein.__github_url__,
+                 author='James Kennington',
+                 author_email='jwkennington@psu.edu',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 install_requires=[
+                     'matplotlib',
+                     'numpy',
+                     'palettable',
+                     'pytest',
+                     'scipy',
+                     'simpy',
+                 ],
+                 classifiers=[
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 3.7",
+                     "Programming Language :: Python :: 3.8",
+                     "Operating System :: MacOS",
+                     "Operating System :: POSIX :: Linux",
+                 ],
+                 zip_safe=False,
+                 include_package_data=True,
+                 )
