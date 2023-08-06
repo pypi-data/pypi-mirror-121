bdb4cac8a825bae0b0febf2af462e016026e5ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package pytest-testrail-appetize/1.0.0/pytest_testrail_appetize-1.0.0.tar.gz

---
+++ packages/pytest-testrail-appetize/pytest_testrail_appetize-1.0.0.tar.gz/pytest_testrail_appetize-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(fname) as f:
+        return f.read()
+
+
+setup(
+    name='pytest_testrail_appetize',
+    description='pytest plugin for creating TestRail runs and adding results',
+    long_description=read_file('README.rst'),
+    version='1.0.0',
+    author='Appetize',
+    author_email='chris.williams@appetize.com',
+    download_url='https://github.com/AppetizeAutomation/pytest-testrail/archive/refs/tags/v1.0.1.tar.gz',
+    url='https://github.com/AppetizeAutomation/pytest-testrail',
+    packages=[
+        'pytest_testrail_appetize',
+    ],
+    package_dir={'pytest_testrail_appetize': 'pytest_testrail_appetize'},
+    install_requires=[
+        'pytest>=3.6',
+        'requests>=2.20.0',
+    ],
+    include_package_data=True,
+    entry_points={'pytest11': ['pytest-testrail = pytest_testrail.conftest']},
+)
