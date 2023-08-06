45ef2ab4e11df71b9e732c22d17be2dcc711f1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:14 +0000

    Add package irr/0.2/irr-0.2.tar.gz

---
+++ packages/irr/irr-0.2.tar.gz/irr-0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='irr',
+      version='0.2',
+      description='numpy.irr but works',
+      author='Leonid Shestera',
+      author_email='leonid@shestera.com',
+      packages=['irr'],
+)
