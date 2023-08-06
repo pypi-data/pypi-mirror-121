52f35d6e117f205eb22a987b530ec325579d8ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:36 +0000

    Add package PasswordCardGenerator/1.3.9/PasswordCardGenerator-1.3.9.tar.gz

---
+++ packages/PasswordCardGenerator/PasswordCardGenerator-1.3.9.tar.gz/PasswordCardGenerator-1.3.9/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+# Get Long Description
+with open("README.md", "r") as readme:
+    long_description = readme.read().replace("Â", "")
+# Get requirements
+with open("requirements.txt", "r") as requirements:
+    reqs = requirements.read().splitlines()
+
+setup(
+    name="PasswordCardGenerator",
+    version="1.3.9",
+    # Major version 1
+    # Minor version 3
+    # Maintenance version 9
+
+    author="DerSchinken",
+    maintainer="DerSchinken",
+    description="A Password Card Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license_files=["LICENSE"],
+    packages=[
+        "PasswordCardGenerator",
+        "PasswordCardGenerator.GUI",
+        "PasswordCardGenerator.Web"
+    ],
+    package_data={
+        "PasswordCardGenerator": ["*.TTF"],
+        "PasswordCardGenerator.GUI": ["*.png", "*.dd"],  # dd - dont delete
+        "PasswordCardGenerator.Web": ["*.html", "*.css", "*.js", "*.png"]
+    },
+    include_package_data=True,
+    install_requires=reqs,
+    python_requires=">= 3.6",
+    project_urls={
+        "Homepage": "http://index12.bplaced.net/",
+        "Github": "https://github.com/DerSchinken/PasswordCardGenerator",
+    },
+    keyword=[
+        "PasswordCardGenerator",
+        "Password Card Generator",
+        "Password",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.10"
+    ],
+)
