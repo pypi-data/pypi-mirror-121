6505dd0926c01628656b614747144e40acbc6af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:49 +0000

    Add package AutoROM/0.4.0/AutoROM-0.4.0.tar.gz

---
+++ packages/AutoROM/AutoROM-0.4.0.tar.gz/AutoROM-0.4.0/AutoROM/licensed/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import pathlib
+
+from setuptools.command.install import install
+
+class InstallCommand(install):
+    def run(self):
+        super().run()
+
+        from AutoROM import main as AutoROM
+        download_dir = pathlib.Path(self.install_lib) / "AutoROM" / "roms"
+        download_dir.mkdir(exist_ok=False, parents=True)
+        AutoROM(True, download_dir, False)
+
+setuptools.setup(
+    name="AutoROM-licensed-roms",
+    version="0.3.0",
+    zip_safe=False,
+    cmdclass={"install": InstallCommand},
+    install_requires=f"""
+        click
+        requests
+        tqdm
+        importlib-resources; python_version < "3.9"
+    """,
+)
