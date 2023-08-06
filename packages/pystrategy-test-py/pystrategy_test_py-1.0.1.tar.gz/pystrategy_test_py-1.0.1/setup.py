7fe4a40779209950256b60c51fea3c23d5b06259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:37 +0000

    Add package pystrategy-test-py/1.0.1/pystrategy_test_py-1.0.1.tar.gz

---
+++ packages/pystrategy-test-py/pystrategy_test_py-1.0.1.tar.gz/pystrategy_test_py-1.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pystrategy_test_py',
+    version="1.0.1",
+    author = 'Francisco A. Prieto Rodriguez, Francisco de Asís Fernández Navarro, David Becerra Alonso',
+    description = 'Evaluation of strategies portfolios',
+    url = 'https://github.com/faprieto96/pystrategy_test_py',
+    packages = setuptools.find_packages(),
+    classifiers= [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0'
+)
