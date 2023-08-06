20bce6845f78f6e1772fe67053e6925e2e2e040a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:07 +0000

    Add package NHentaidesu/1.3/NHentaidesu-1.3.tar.gz

---
+++ packages/NHentaidesu/NHentaidesu-1.3.tar.gz/NHentaidesu-1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='NHentaidesu',
+    version='1.3',
+    description='NHentai API wrapper with hybrid runtime.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/rushkii/NHentaidesu',
+    author='Kiizuha',
+    author_email='riskimuhammmad1@gmail.com',
+    keywords='nhentai nhentaidesu nhentai-python nhentai python nhentai api',
+    license='MIT',
+    packages=find_packages(exclude=['downloads', 'example.py']),
+    install_requires=[
+        'aiofiles',
+        'bs4',
+        'aiohttp',
+        'img2pdf',
+        'dateparser',
+        'python-magic'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
