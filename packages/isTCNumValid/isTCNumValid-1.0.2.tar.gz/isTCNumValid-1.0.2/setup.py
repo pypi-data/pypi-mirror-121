79e33ff2000dc03bb56e86e149e4c95e4ae35dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:32 +0000

    Add package isTCNumValid/1.0.2/isTCNumValid-1.0.2.tar.gz

---
+++ packages/isTCNumValid/isTCNumValid-1.0.2.tar.gz/isTCNumValid-1.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='isTCNumValid',
+    version='1.0.2',
+    license='MIT',
+    author="Cagatay URESIN",
+    author_email='cagatayuresin@gmail.com',
+    description="A module can say a TCNum is valid or not according to offical TCNum algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_dir={'': 'src'},
+    url='https://github.com/cagatayuresin/isTCNumValid',
+    keywords='tc, turkey, turkish, citizen, number',
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+         ],
+)
