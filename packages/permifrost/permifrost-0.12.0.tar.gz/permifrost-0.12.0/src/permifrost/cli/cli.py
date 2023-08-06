f092f0704b7809922019962df87fa491eb314614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:16 +0000

    Add package permifrost/0.12.0/permifrost-0.12.0.tar.gz

---
+++ packages/permifrost/permifrost-0.12.0.tar.gz/permifrost-0.12.0/src/permifrost/cli/cli.py
@@ -0,0 +1,29 @@
+import logging
+
+import click
+
+import permifrost
+from permifrost.core.logging import setup_logging
+
+logger = logging.getLogger(__name__)
+
+
+LEVELS = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
+
+
+@click.group(invoke_without_command=True, no_args_is_help=True)
+@click.option("--log-level", type=click.Choice(list(LEVELS.keys())), default="info")
+@click.option("-v", "--verbose", count=True)
+@click.version_option(version=permifrost.__version__, prog_name="permifrost")
+@click.pass_context
+def cli(ctx, log_level, verbose):
+    setup_logging(log_level=LEVELS[log_level])
+
+    ctx.ensure_object(dict)
+    ctx.obj["verbosity"] = verbose
