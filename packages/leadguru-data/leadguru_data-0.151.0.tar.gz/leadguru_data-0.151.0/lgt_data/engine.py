f43287a109080471e164310da322004093543bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:56 +0000

    Add package leadguru-data/0.151.0/leadguru_data-0.151.0.tar.gz

---
+++ packages/leadguru-data/leadguru_data-0.151.0.tar.gz/leadguru_data-0.151.0/lgt_data/engine.py
@@ -0,0 +1,13 @@
+import os
+from mongoengine import connect, Document, DateTimeField, StringField
+
+connect(host=os.environ.get('MONGO_CONNECTION_STRING', 'mongodb://127.0.0.1:27017/'), db="lgt_admin")
+
+class DelayedJob(Document):
+    created_at = DateTimeField(required=True)
+    scheduled_at = DateTimeField(required=True)
+    job_type = StringField(required=True)
+    data = StringField(required=True)
+    executed_at: DateTimeField(required=True)
+
+    meta = { 'indexes': [('executed_at', '-scheduled_at') ] }
