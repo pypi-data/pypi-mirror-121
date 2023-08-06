1aa84fb0757f6e72d908f66b16b9ce80e30461f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:31 +0000

    Add package testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/1.1.9/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.9.tar.gz

---
+++ packages/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.9.tar.gz/testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh-1.1.9/setup.py
@@ -0,0 +1,16 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='testpackageasfgdresfrgdewrewfdewrghkrlgjlsjerjselkwehjawjekh',
+    version='1.1.9',
+    license='MIT',
+    author="Me",
+    author_email='cieran@churcher.uk',
+    packages=find_packages(),
+
+)
+
+
