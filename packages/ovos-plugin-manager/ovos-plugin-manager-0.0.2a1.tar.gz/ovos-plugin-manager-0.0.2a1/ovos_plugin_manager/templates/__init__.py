ad02f3cf2e710667d401ed2a546c50d7f69eee35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:22 +0000

    Add package ovos-plugin-manager/0.0.2a1/ovos-plugin-manager-0.0.2a1.tar.gz

---
+++ packages/ovos-plugin-manager/ovos-plugin-manager-0.0.2a1.tar.gz/ovos-plugin-manager-0.0.2a1/ovos_plugin_manager/templates/__init__.py
@@ -0,0 +1,7 @@
+"""
+this module is meant to enable usage of mycroft plugins inside and outside
+mycroft, importing from here will make things work as planned in mycroft,
+but if outside mycroft things will still work
+
+The main use case is for plugins to be used across different projects
+"""
