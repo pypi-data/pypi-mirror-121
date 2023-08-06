875dc6b79d018d7cb974b5157a3af0cb44fbe98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:19 +0000

    Add package ouvidoria-unidombosco/0.0.30/ouvidoria_unidombosco-0.0.30.tar.gz

---
+++ packages/ouvidoria-unidombosco/ouvidoria_unidombosco-0.0.30.tar.gz/ouvidoria_unidombosco-0.0.30/ouvidoria_unidombosco/__init__.py
@@ -0,0 +1,5 @@
+from ouvidoria_unidombosco import OuvidoriaBasePath
+from ouvidoria_unidombosco.api.src.SeleniumHelper import SeleniumHelper
+from ouvidoria_unidombosco.api.src.UnidomboscoComplain import UnidomboscoComplain
+from ouvidoria_unidombosco.api.src.UnidomboscoNightmare import UnidomboscoNightmare
+from ouvidoria_unidombosco.api.src.Unidombosco import Unidombosco
