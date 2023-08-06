eb641fea8c5e360258f43c0b58489420f885c0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:26 +0000

    Add package aliyun-python-sdk-sddp/1.0.3/aliyun-python-sdk-sddp-1.0.3.tar.gz

---
+++ packages/aliyun-python-sdk-sddp/aliyun-python-sdk-sddp-1.0.3.tar.gz/aliyun-python-sdk-sddp-1.0.3/aliyunsdksddp/request/v20190103/ModifyRuleRequest.py
@@ -0,0 +1,122 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdksddp.endpoint import endpoint_data
+
+class ModifyRuleRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Sddp', '2019-01-03', 'ModifyRule')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_WarnLevel(self):
+		return self.get_query_params().get('WarnLevel')
+
+	def set_WarnLevel(self,WarnLevel):
+		self.add_query_param('WarnLevel',WarnLevel)
+
+	def get_ProductCode(self):
+		return self.get_query_params().get('ProductCode')
+
+	def set_ProductCode(self,ProductCode):
+		self.add_query_param('ProductCode',ProductCode)
+
+	def get_ProductId(self):
+		return self.get_query_params().get('ProductId')
+
+	def set_ProductId(self,ProductId):
+		self.add_query_param('ProductId',ProductId)
+
+	def get_Description(self):
+		return self.get_query_params().get('Description')
+
+	def set_Description(self,Description):
+		self.add_query_param('Description',Description)
+
+	def get_RiskLevelId(self):
+		return self.get_query_params().get('RiskLevelId')
+
+	def set_RiskLevelId(self,RiskLevelId):
+		self.add_query_param('RiskLevelId',RiskLevelId)
+
+	def get_Content(self):
+		return self.get_query_params().get('Content')
+
+	def set_Content(self,Content):
+		self.add_query_param('Content',Content)
+
+	def get_Id(self):
+		return self.get_query_params().get('Id')
+
+	def set_Id(self,Id):
+		self.add_query_param('Id',Id)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
+
+	def get_RuleType(self):
+		return self.get_query_params().get('RuleType')
+
+	def set_RuleType(self,RuleType):
+		self.add_query_param('RuleType',RuleType)
+
+	def get_StatExpress(self):
+		return self.get_query_params().get('StatExpress')
+
+	def set_StatExpress(self,StatExpress):
+		self.add_query_param('StatExpress',StatExpress)
+
+	def get_ContentCategory(self):
+		return self.get_query_params().get('ContentCategory')
+
+	def set_ContentCategory(self,ContentCategory):
+		self.add_query_param('ContentCategory',ContentCategory)
+
+	def get_CustomType(self):
+		return self.get_query_params().get('CustomType')
+
+	def set_CustomType(self,CustomType):
+		self.add_query_param('CustomType',CustomType)
+
+	def get_Target(self):
+		return self.get_query_params().get('Target')
+
+	def set_Target(self,Target):
+		self.add_query_param('Target',Target)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_Category(self):
+		return self.get_query_params().get('Category')
+
+	def set_Category(self,Category):
+		self.add_query_param('Category',Category)
