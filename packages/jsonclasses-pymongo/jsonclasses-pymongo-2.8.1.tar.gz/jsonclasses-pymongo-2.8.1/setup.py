77de2ae192b9d5b3f14db8cb1108a338858bd53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:55 +0000

    Add package jsonclasses-pymongo/2.8.1/jsonclasses-pymongo-2.8.1.tar.gz

---
+++ packages/jsonclasses-pymongo/jsonclasses-pymongo-2.8.1.tar.gz/jsonclasses-pymongo-2.8.1/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='jsonclasses-pymongo',
+      version='2.8.1',
+      description='jsonclasses integration with pymongo',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      author='Fillmula Inc.',
+      author_email='victor.teo@fillmula.com',
+      license='MIT',
+      packages=find_packages(exclude=("tests")),
+      package_data={'jsonclasses_pymongo': ['py.typed']},
+      zip_safe=False,
+      url='https://github.com/fillmula/jsonclasses-pymongo',
+      include_package_data=True,
+      python_requires='>=3.9',
+      install_requires=[
+            'jsonclasses-orm==2.8.1',
+            'pymongo>=3.12.0,<4.0.0'
+      ])
