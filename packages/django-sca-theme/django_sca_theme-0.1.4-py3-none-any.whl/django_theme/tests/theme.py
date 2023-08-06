49c8e0a14467f8749e1788323940ea00b62dada9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:00 +0000

    Add package django-sca-theme/0.1.4/django_sca_theme-0.1.4-py3-none-any.whl

---
+++ packages/django-sca-theme/django_sca_theme-0.1.4-py3-none-any.whl/django_theme/tests/theme.py
@@ -0,0 +1,44 @@
+from django.core.files import File
+
+from ..models import Theme
+
+
+class ThemeCase:
+    """
+    create a sample theme
+    """
+
+    def __init__(self, corporate_logo_path="fixtures/test_file/image.jpg", tab_logo_path="fixtures/test_file/AAV43679.png"):
+        self.theme = {
+            "theme_id": "1",
+            "title": "天和双利",
+            "theme_color": "#e60000",
+            "aux_color": "#666666",
+        }
+        self.theme_object = self.create_theme()
+
+        self.theme_object.corporate_logo.save(
+            "logo.jpg", File(open(corporate_logo_path, "rb"))
+        )
+        self.theme_object.tab_logo.save(
+            "tab_logo.png", File(open(tab_logo_path, "rb"))
+        )
+
+    def attach_file(self):
+        return [
+            (
+                "corporate_logo",
+                ("logo.jpg", open("fixtures/test_file/image.jpg", "rb"), "image/jpg"),
+            ),
+            (
+                "tab_logo",
+                (
+                    "tab_logo.png",
+                    open("fixtures/test_file/AAV43679.png", "rb"),
+                    "image/png",
+                ),
+            ),
+        ]
+
+    def create_theme(self):
+        return Theme.objects.create(**self.theme)
