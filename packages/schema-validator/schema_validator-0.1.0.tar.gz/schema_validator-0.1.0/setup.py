13e21d52a82f590699ab3879ac7693cb4f2a5821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package schema-validator/0.1.0/schema_validator-0.1.0.tar.gz

---
+++ packages/schema-validator/schema_validator-0.1.0.tar.gz/schema_validator-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['schema_validator']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flask>=2.0', 'pydantic>=1.8', 'pyhumps>=1.6.1']
+
+extras_require = \
+{':python_version < "3.8"': ['typing_extensions']}
+
+setup_kwargs = {
+    'name': 'schema-validator',
+    'version': '0.1.0',
+    'description': 'A flask extension to provide schema validation with pydantic.',
+    'long_description': 'flask-dantic-schema\n============\n\n#### Generate for quart-schema\n\n[![Build Status](https://app.travis-ci.com/huangxiaohen2738/flask-dantic-schema.svg?branch=main)](https://app.travis-ci.com/huangxiaohen2738/flask-dantic-schema)\n\n\n\n### Install\n\n - `pip install schema-validator`\n\n### How to use\n\n```\n    from dataclasses import dataclass\n    from datetime import datetime\n    from typing import Optional\n    from pydantic import BaseModel\n\n    from flask import Flask\n    from schema_validator import FlaskSchema, validate\n\n    app = Flask(__name__)\n    \n    FlaskSchema(app)\n    \n    OR\n    \n    schema = FlaskSchema()\n    schema.init_app(app)\n\n    @dataclass\n    class Todo:\n        task: str\n        due: Optional[datetime]\n\n    class TodoResponse(BaseModel):\n        id: int\n        name: str\n\n    @app.post("/")\n    @validate(body=Todo, responses={200: TodoResponse})\n    def create_todo():\n        ... # Do something with data, e.g. save to the DB\n        return data\n       \n    app.cli.add_command(generate_schema_command)\n    \n    virtualenv:  flask schema swagger.json -> generate json swagger\n```\n',
+    'author': 'hs',
+    'author_email': 'huangxiaohen2738@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
