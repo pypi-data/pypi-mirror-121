79125ebf3047140bae61b0378f0f8cb6e9cbf9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package log-report/1.1/log_report-1.1.tar.gz

---
+++ packages/log-report/log_report-1.1.tar.gz/log_report-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='log_report',
+    version='1.1',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    author="ME IS ME WHO ELSE",
+    install_requires=[
+          'markdown',
+          'argparse'
+      ]
+)
