3f4b45314c263984e216ec8401fc67c51fe66e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:46 +0000

    Add package jnt-django-admin-tools/0.9.44/jnt_django_admin_tools-0.9.44-py3-none-any.whl

---
+++ packages/jnt-django-admin-tools/jnt_django_admin_tools-0.9.44-py3-none-any.whl/jnt_admin_tools/mixins/readonly_widgets.py
@@ -0,0 +1,62 @@
+import types
+import typing as ty
+
+from django.core.exceptions import FieldDoesNotExist
+from django.core.handlers.asgi import ASGIRequest
+from django.db import models
+
+from jnt_admin_tools.db.fields import GenericForeignKey
+from jnt_admin_tools.widgets.readonly import (
+    CharChoiceReadonlyWidget,
+    ForeignKeyReadonlyWidget,
+    GenericForeignKeyReadonlyWidget,
+    ManyToManyReadonlyWidget,
+    PermissionSelectMultipleReadonlyWidget,
+)
+from jnt_admin_tools.widgets.readonly.base import BaseReadOnlyWidget
+from django.contrib.auth.models import Permission
+
+READONLY_WIDGETS = types.MappingProxyType(
+    {
+        models.ForeignKey: ForeignKeyReadonlyWidget(),
+        models.OneToOneField: ForeignKeyReadonlyWidget(),
+        models.ManyToManyField: ManyToManyReadonlyWidget(),
+        models.ManyToOneRel: ManyToManyReadonlyWidget(),
+        models.CharField: CharChoiceReadonlyWidget(),
+        models.TextField: CharChoiceReadonlyWidget(),
+        GenericForeignKey: GenericForeignKeyReadonlyWidget(),
+    },
+)
+
+
+class ReadonlyWidgetsMixin:
+    def readonly_widget(
+        self,
+        field_name: str,
+        request: ASGIRequest = None,
+    ) -> ty.Optional[BaseReadOnlyWidget]:
+        """Return readonly widget for admin readonly field."""
+        if callable(field_name) or getattr(self, field_name, None):
+            return None
+
+        try:
+            model_field = self.model._meta.get_field(field_name)
+        except FieldDoesNotExist:
+            return None
+
+        field_class = model_field.__class__
+
+        if issubclass(field_class, models.ManyToManyField) and issubclass(
+            model_field.related_model,
+            Permission,
+        ):
+            return PermissionSelectMultipleReadonlyWidget()
+
+        return self.readonly_widgets(request).get(field_class)
+
+    def readonly_widgets(
+        self,
+        request: ASGIRequest = None,
+    ) -> ty.Dict[models.Field, BaseReadOnlyWidget]:
+        """Return available readonly_widgets."""
+        return dict(READONLY_WIDGETS)
