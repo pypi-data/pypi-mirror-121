f309e422aa4215ce4d0bab89140f09476c2abe5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:23 +0000

    Add package minos-cli/0.0.1/minos_cli-0.0.1-py3-none-any.whl

---
+++ packages/minos-cli/minos_cli-0.0.1-py3-none-any.whl/HISTORY.md
@@ -0,0 +1,6 @@
+# History
+
+## 0.0.1 (2021-09-27)
+
+* Implement basic `minos microservice new` and `minos microservice init` commands.
+* First version on pypi.
---
+++ packages/minos-cli/minos_cli-0.0.1-py3-none-any.whl/minos/cli/__init__.py
@@ -0,0 +1,14 @@
+__author__ = """Clariteia Devs"""
+__email__ = "devs@clariteia.com"
+__version__ = "0.0.1"
+
+from .constants import (
+    MICROSERVICE_TEMPLATE_PATH,
+)
+from .generators import (
+    MicroserviceGenerator,
+)
+from .main import (
+    app,
+    main,
+)
