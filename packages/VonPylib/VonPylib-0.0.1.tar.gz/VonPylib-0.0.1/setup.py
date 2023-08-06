f4797eb24c4fd1c175d439ca4e2230c39690dc27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:11 +0000

    Add package VonPylib/0.0.1/VonPylib-0.0.1.tar.gz

---
+++ packages/VonPylib/VonPylib-0.0.1.tar.gz/VonPylib-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'VonPylib',
+    version = '0.0.1',
+    description = 'My own python liberary',
+    py_modules = ["terminal_font",
+                    "reprap_arm"],
+    package_dir = {'': 'src'},
+    # Optional
+    author = 'Xuming Feng',
+    author_email = 'voicevon@gmail.com',
+    url = 'https://github.com/voicevon/VonPylib',
+
+
+)
