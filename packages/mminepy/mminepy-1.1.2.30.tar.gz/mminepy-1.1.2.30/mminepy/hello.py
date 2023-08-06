e2ee2eb6c5a2f35fc257bb11e7bc149fbbb59211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:20 +0000

    Add package mminepy/1.1.2.30/mminepy-1.1.2.30.tar.gz

---
+++ packages/mminepy/mminepy-1.1.2.30.tar.gz/mminepy-1.1.2.30/mminepy/hello.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import getpass
+def gegus():
+    print("loading")
+    g = sys.version
+    h = g.split(".")
+    m = str(h[0])+str(h[1])
+    os.system('setx hello "C:/Users/'+getpass.getuser()+'/AppData/Local/Programs/Python/Python'+m+'/Lib/site-packages/mminepy-1.1.2.30.dist-info;"')
+    os.system('setx path "%path%;%hello%;" -m')
+    # setx path "%path%;C:/Users/KIMMJU/AppData/Local/Programs/Python/Python37/Lib/site-packages/mminepy-1.1.1.17.dist-info;
