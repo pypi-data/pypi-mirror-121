7b5e338e5717a22b751df95eabe9149153feadb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:15 +0000

    Add package carrotColors/0.0.3/carrotColors-0.0.3.tar.gz

---
+++ packages/carrotColors/carrotColors-0.0.3.tar.gz/carrotColors-0.0.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="carrotColors",
+        version="0.0.3",
+        author="Carrot-top Craig",
+        description="A carrot-themed color palette for python",
+        packages=["carrotColors"]
+)
