9bd338c376bcda7b72fbc46958dbf7f83dda32a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package test-user-auth/1.0.0/test_user_auth-1.0.0.tar.gz

---
+++ packages/test-user-auth/test_user_auth-1.0.0.tar.gz/test_user_auth-1.0.0/main.py
@@ -0,0 +1,43 @@
+from IdentityService.service import IdentityService
+# import pytest
+
+# pytest
+
+
+file_name_option = input(
+    'Do you want to specify a different \
+    \n from default ("data_file.json) file name? (Y/N) ')
+if file_name_option.lower()=="y":
+    data_file_name=input('Specify file name: ')
+    service = IdentityService(f'{data_file_name}.json')
+
+else: service = IdentityService()
+
+
+
+def crucial_credentials():
+    #DRY
+    username=input('*USERNAME: ')
+    password=input('*PASSWORD: ')
+
+    return username, password
+
+while True:
+    intro_choice=input('Do you want to authinticate user? (Y/N) ')
+    if intro_choice.lower()=='y':
+        auth=crucial_credentials()
+        if service.authenticate(*auth):
+            print('User is registerd.')
+        else: print('User is not registered.')
+    elif intro_choice.lower()!='y':
+        register_choice=input('Do ou want regiser a new user? Y/N: ')
+        if register_choice.lower()=='y':
+            username, password = crucial_credentials()
+            properties={}
+            properties['age']=input('Enter your age: ')
+            properties['sex']=input('Enter your sex: ')
+            service.register(username, password, **properties)
+        
+        else: 
+            pass
+
