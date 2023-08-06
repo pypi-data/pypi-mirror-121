c6318b1dec31c425497d4248208632214e481bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:35 +0000

    Add package hamlet/9.6.2/hamlet-9.6.2-py2.py3-none-any.whl

---
+++ packages/hamlet/hamlet-9.6.2-py2.py3-none-any.whl/hamlet/backend/automation_tasks/save_repos/__init__.py
@@ -0,0 +1,43 @@
+from hamlet.backend.automation_tasks.base import AutomationRunner
+from hamlet.backend.automation import (
+    set_automation_context,
+    construct_tree,
+    save_cmdb_repos,
+)
+
+
+class SaveCMDBAutomationRunner(AutomationRunner):
+    def __init__(
+        self, account_repos, commit_message, product_repos, reference, tag, **kwargs
+    ):
+        super().__init__()
+
+        self._context_env = {
+            "DEFER_REPO_PUSH": "false",
+            **kwargs,
+        }
+
+        self._script_list = [
+            {
+                "func": construct_tree.run,
+                "args": {
+                    "use_existing_tree": True,
+                    "_is_cli": True,
+                },
+            },
+            {
+                "func": set_automation_context.run,
+                "args": {"_is_cli": True, "context_credentials": False},
+            },
+            {
+                "func": save_cmdb_repos.run,
+                "args": {
+                    "account_repos": account_repos,
+                    "commit_message": commit_message,
+                    "product_repos": product_repos,
+                    "reference": reference,
+                    "tag": tag,
+                    "_is_cli": True,
+                },
+            },
+        ]
