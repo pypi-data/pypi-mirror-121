c663a2292f3f55ea86541109552e44a9a7479387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:02 +0000

    Add package data-validation/0.1.0/data-validation-0.1.0.tar.gz

---
+++ packages/data-validation/data-validation-0.1.0.tar.gz/data-validation-0.1.0/AUTHORS.rst
@@ -0,0 +1,5 @@
+============
+Contributors
+============
+
+* Bruno Ferraz do Amaral <amaralbf@gmail.com>
---
+++ packages/data-validation/data-validation-0.1.0.tar.gz/data-validation-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='data-validation',
+    version='0.1.0',
+    description='',
+    long_description=None,
+    author='Bruno Amaral',
+    author_email='amaralbf@gmail.com',
+    package_dir={'': 'src'},
+    packages=['data_validation'],
+    python_requires='>=3.6,<4.0',
+)
