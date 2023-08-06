f58fa421c195715287f46c8b85c51cca61fc5a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:32 +0000

    Add package toyota-na/0.0.5/toyota_na-0.0.5-py3-none-any.whl

---
+++ packages/toyota-na/toyota_na-0.0.5-py3-none-any.whl/toyota_na/client.py
@@ -0,0 +1,50 @@
+from urllib.parse import urljoin
+import aiohttp
+import logging
+
+from .auth import ToyotaOneAuth
+
+
+class ToyotaOneClient:
+    API_GATEWAY = "https://oneapi.telematicsct.com/"
+    API_KEY = "Y1aVonEtOa18cDwNLGTjt1zqD7aLahwc30WvvvQE"
+
+    def __init__(self, auth=None) -> None:
+        self.auth = auth or ToyotaOneAuth()
+    
+    async def _auth_headers(self):
+        return {
+            "AUTHORIZATION": "Bearer " + await self.auth.get_access_token(),
+            "X-API-KEY": self.API_KEY,
+            "X-GUID": await self.auth.get_guid()
+        }
+
+    async def api_get(self, endpoint, header_params=None):
+        headers = await self._auth_headers()
+        if header_params:
+            headers.update(header_params)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(urljoin(self.API_GATEWAY, endpoint), headers=headers) as resp:
+                resp.raise_for_status()
+                try:
+                    resp_json = await resp.json()
+                    logging.info("Response status: %s", resp_json["status"])
+                    return resp_json["payload"]
+                except:
+                    logging.error("Error parsing response: %s", await resp.text())
+                    raise
+
+    async def get_user_vehicle_list(self):
+        return await self.api_get("v2/vehicle/guid")
+
+    async def get_vehicle_detail(self, vin):
+        return await self.api_get("v1/one/vehicle", {"VIN": vin})
+
+    async def get_vehicle_health_report(self, vin):
+        return await self.api_get("v1/vehiclehealth/report", {"VIN": vin})
+
+    async def get_vehicle_health_status(self, vin):
+        return await self.api_get("v1/vehiclehealth/status", {"VIN": vin})
+
+    async def get_vehicle_status(self, vin):
+        return await self.api_get("v1/global/remote/status", {"VIN": vin})
