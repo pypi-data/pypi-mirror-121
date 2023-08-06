d0c4e6eeefb6a6de91ce36fa8e8354ea85682184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:49 +0000

    Add package raya/0.6.6rc1/raya-0.6.6rc1-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/raya/raya-0.6.6rc1-cp38-cp38-manylinux1_x86_64.whl/raya/exceptions.py
@@ -0,0 +1,64 @@
+# System 
+
+# System Exceptions
+from raya.raya import RayaException
+from raya.raya import RayaAbortException
+
+# Application Exceptions
+from raya.raya import RayaApplicationException
+from raya.raya import RayaInvalidAppNameException
+from raya.raya import RayaRestfulModeException
+
+# Value Exceptions
+from raya.raya import RayaValueException
+from raya.raya import RayaInvalidNumericRange
+from raya.raya import RayaInvalidRGBRange
+from raya.raya import RayaInvalidHSVRange
+
+# Controller Exceptions
+from raya.raya import RayaControllerException
+
+# Listener Exceptions
+from raya.raya import RayaListenerException
+from raya.raya import RayaListenerAlreadyCreated
+from raya.raya import RayaListenerUnknown
+from raya.raya import RayaInvalidCallback
+
+# Arms Exceptions
+from raya.raya import RayaArmsException
+from raya.raya import RayaArmsJointsPosition
+from raya.raya import RayaArmsControllerNotReady
+from raya.raya import RayaArmsNotValidArmName
+from raya.raya import RayaArmsNotValidJointName
+from raya.raya import RayaArmsNotMoving
+
+# Cameras Exceptions
+from raya.raya import RayaCamerasException
+from raya.raya import RayaInvalidCameraName
+from raya.raya import RayaCameraNotEnabled
+
+# Lidar Exceptions
+from raya.raya import RayaLidarException
+
+# Motion Exceptions
+from raya.raya import RayaMotionException
+
+# Sensors Exceptions
+from raya.raya import RayaSensorsException
+from raya.raya import RayaSensorsUnknownPath
+from raya.raya import RayaSensorsIncompatiblePath
+from raya.raya import RayaSensorsInvalidPath
+from raya.raya import RayaSensorsInvalidColorName
+
+# Restful Exception
+from raya.raya import RayaRestfulException
+from raya.raya import RayaNotInRestfulMode
+from raya.raya import RayaInRestfulMode
+from raya.raya import RayaRestfulNotRegisteredApp
+from raya.raya import RayaRestfulCommandAlreadyRegistered
+from raya.raya import RayaRestfulNotRegisteredCommand
+
+# Interactions Exception
+from raya.raya import RayaInteractionsException
+from raya.raya import RayaInteractionsUnknownGroup
+from raya.raya import RayaInteractionsNotValidID
