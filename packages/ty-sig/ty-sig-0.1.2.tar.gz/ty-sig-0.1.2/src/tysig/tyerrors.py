f43cbd8150226cccd23c474354eb0ce0dd46b3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package ty-sig/0.1.2/ty-sig-0.1.2.tar.gz

---
+++ packages/ty-sig/ty-sig-0.1.2.tar.gz/ty-sig-0.1.2/src/tysig/tyerrors.py
@@ -0,0 +1,18 @@
+"""
+    (c) 2021 Usman Ahmad https://github.com/selphaware
+
+    tyerrors.py
+
+    Error messages used by tysig.py
+"""
+
+
+DEFAULT_ERROR = "'{}' default value is of type '{}', but expecting type '{}'"
+
+SIG_TYPE_ERROR = "Expected type or tuple in signature instead found '{}'"
+
+ARGS_ERROR = "'{}' type should be '{}' instead found '{}'. If "\
+             "you're using GenericAlias, VariadicGenericAlias, or SpecialForm"\
+             " types then please check the sub argument types are correct"
+
+UNEXP_ERROR = "Found unexpected extra arguments: {}"
