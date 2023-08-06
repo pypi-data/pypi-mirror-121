2bbf3caf400b30de1f4ba6695d9cc6305aaddff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:32 +0000

    Add package hopla-cli/0.0.12a0/hopla_cli-0.0.12a0-py3-none-any.whl

---
+++ packages/hopla-cli/hopla_cli-0.0.12a0-py3-none-any.whl/hopla/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Module that calls the entry command
+"""
+from . import entry_cmd
+
+if __name__ == "__main__":
+    entry_cmd()
---
+++ packages/hopla-cli/hopla_cli-0.0.12a0-py3-none-any.whl/hopla/cli/version.py
@@ -0,0 +1,42 @@
+"""
+The module with CLI code that handles the `hopla version` command.
+"""
+
+import logging
+
+import click
+
+log = logging.getLogger()
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+PATCH_VERSION = 12
+PRE_RELEASE = "alpha"
+
+
+def hopla_version() -> str:
+    """
+    Prints the hopla version (Not to be confused with the habitica API version)
+
+    The output should satisfy [semantic versioning](https://semver.org/)
+    TODO: verify if this is the case
+    """
+
+    version_core = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
+    if PRE_RELEASE is not None and PRE_RELEASE != "":
+        pre_release = f"-{PRE_RELEASE}"
+    else:
+        pre_release = ""
+
+    return version_core + pre_release
+
+
+@click.command()
+def version():
+    """Print the Hopla version string.
+
+    \f
+    :return:
+    """
+    log.debug("hopla version")
+    click.echo(hopla_version())
