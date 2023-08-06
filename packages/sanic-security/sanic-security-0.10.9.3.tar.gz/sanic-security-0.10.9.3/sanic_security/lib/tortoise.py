4aa58ea0cb5979bbfb0756ab19159067d8172680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package sanic-security/0.10.9.3/sanic-security-0.10.9.3.tar.gz

---
+++ packages/sanic-security/sanic-security-0.10.9.3.tar.gz/sanic-security-0.10.9.3/sanic_security/lib/tortoise.py
@@ -0,0 +1,30 @@
+from sanic import Sanic
+from tortoise import Tortoise
+
+from sanic_security.utils import config
+
+
+def initialize_security_orm(app: Sanic):
+    """
+    Initializes tortoise-orm.
+
+    Args:
+        app (Sanic): Sanic Framework app.
+    """
+
+    @app.listener("before_server_start")
+    async def init_orm(app, loop):
+        username = config["TORTOISE"]["username"]
+        password = config["TORTOISE"]["password"]
+        endpoint = config["TORTOISE"]["endpoint"]
+        schema = config["TORTOISE"]["schema"]
+        engine = config["TORTOISE"]["engine"]
+        models = config["TORTOISE"]["models"].replace(" ", "").split(",")
+        url = f"{engine}://{username}:{password}@{endpoint}/{schema}"
+        await Tortoise.init(db_url=url, modules={"models": models})
+        if config["TORTOISE"].getboolean("generate"):
+            await Tortoise.generate_schemas()
+
+    @app.listener("after_server_stop")
+    async def close_orm(app, loop):
+        await Tortoise.close_connections()
