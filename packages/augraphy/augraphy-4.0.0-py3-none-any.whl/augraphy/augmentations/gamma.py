d9e0870d7478a63aaf71aa30d5c33141332b339e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:27 +0000

    Add package augraphy/4.0.0/augraphy-4.0.0-py3-none-any.whl

---
+++ packages/augraphy/augraphy-4.0.0-py3-none-any.whl/augraphy/augmentations/gamma.py
@@ -0,0 +1,40 @@
+import os
+import random
+
+import cv2
+import numpy as np
+
+from augraphy.base.augmentation import Augmentation
+from augraphy.base.augmentationresult import AugmentationResult
+
+
+class Gamma(Augmentation):
+    """Adjusts the gamma of the whole image by a chosen multiplier.
+
+    :param range: Pair of ints determining the range from which to sample the
+           gamma shift.
+    :type range: tuple, optional
+    :param p: The probability that this Augmentation will be applied.
+    :type p: float, optional
+    """
+
+    def __init__(self, range=(0.5, 1.5), p=0.5):
+        """Constructor method"""
+        super().__init__(p=p)
+        self.range = range
+
+    def __repr__(self):
+        return f"Gamma(range={self.range}, p={self.p})"
+
+    def __call__(self, data, force=False):
+        img = data["post"][-1].result
+        img = img.astype(np.uint8)
+        value = random.uniform(self.range[0], self.range[1])
+        invGamma = 1.0 / value
+        table = np.array(
+            [((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)],
+        ).astype("uint8")
+        print(table.shape, img.shape)
+        frame = cv2.LUT(img, table)
+        data["post"].append(AugmentationResult(self, frame))
+        # cv2.imwrite(os.path.join('test_outputs',str(round(value,4))+"gamma.jpg"),np.hstack((img,frame)))
---
+++ packages/augraphy/augraphy-4.0.0-py3-none-any.whl/augraphy/augmentations/lowinkrandomlines.py
@@ -0,0 +1,42 @@
+import random
+
+from augraphy.augmentations.lowinkline import LowInkLine
+from augraphy.base.augmentationresult import AugmentationResult
+
+
+class LowInkRandomLines(LowInkLine):
+    """Adds low ink lines randomly throughout the image.
+
+    :param count_range: Pair of ints determining the range from which the number
+           of lines is sampled.
+    :type count_range: tuple, optional
+    :param use_consistent_lines: Whether or not to vary the width and alpha of
+           generated low ink lines.
+    :type use_consistent_lines: bool, optional
+    :param p: The probability this Augmentation will be applied.
+    :type p: float, optional
+    """
+
+    def __init__(self, count_range=(5, 10), use_consistent_lines=True, p=0.5):
+        """Constructor method"""
+        super().__init__(use_consistent_lines=use_consistent_lines, p=p)
+        self.count_range = count_range
+
+    # Constructs a string representation of this Augmentation.
+    def __repr__(self):
+        return f"LowInkRandomLines(count_range={self.count_range}, use_consistent_lines={self.use_consistent_lines}, p={self.p})"
+
+    # Applies the Augmentation to input data.
+    def __call__(self, data, force=False):
+        if force or self.should_run():
+            mask = data["ink"][-1].result.copy()
+            count = random.randint(self.count_range[0], self.count_range[1])
+
+            for i in range(count):
+                if mask.shape[0] - 1 >= 1:
+                    mask = self.add_transparency_line(
+                        mask,
+                        random.randint(1, mask.shape[0] - 1),
+                    )
+
+            data["ink"].append(AugmentationResult(self, mask))
