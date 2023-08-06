560a327e4b63a83bf86cdfd0127bdd06bb075b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:45 +0000

    Add package pypatchin/0.0.2/pypatchin-0.0.2.tar.gz

---
+++ packages/pypatchin/pypatchin-0.0.2.tar.gz/pypatchin-0.0.2/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="pypatchin",
+    description="Do you want a fast and clean solution to patch your files and repositories? Use Python pypatchin package!",
+    license="GNU GENERAL PUBLIC LICENSE v3",
+    version="0.0.2",
+    author="Giulia Milan",
+    author_email="giuliapuntoit96@gmail.com",
+    maintainer="Giulia Milan",
+    maintainer_email="giuliapuntoit96@gmail.com",
+    url="https://github.com/giuliapuntoit/pypatch",
+    download_url='https://github.com/giuliapuntoit/pypatch.git',
+    keywords=['python', 'patch'],
+    packages=['pypatchin'],
+    install_requires=['diff_match_patch']
+)
