5b7a9d059c78c1190c0b0ae71de9d2384cfd6d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package tdl-pybasic/0.1.12/tdl_pybasic-0.1.12.tar.gz

---
+++ packages/tdl-pybasic/tdl_pybasic-0.1.12.tar.gz/tdl_pybasic-0.1.12/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tdl_pybasic',
+    packages=find_packages(),
+    package_data={'': ['assets/c6.wav']},
+
+    version='0.1.12', # バージョン
+
+    license='MIT', # ライセンス
+
+    install_requires=[],
+
+    author='Atsushi Shibata',
+    author_email='shibata@m-info.co.jp',
+
+    url='',
+
+    description='A library for Toushin Digital University Python BASIC.',
+    long_description='',
+    long_description_content_type='text/markdown',
+    keywords='',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
