623feb50016dc274a0ba50cb5a9f3c07d2bdede7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:41 +0000

    Add package mtopencv/1.1.202109291512/mtopencv-1.1.202109291512-py3-none-any.whl

---
+++ packages/mtopencv/mtopencv-1.1.202109291512-py3-none-any.whl/mt/cv/__init__.py
@@ -0,0 +1,7 @@
+from mt.opencv import cv2, logger
+from cv2 import *
+from mt.opencv.polygon import *
+from mt.opencv.warping import *
+from mt.opencv.image import *
+from mt.opencv.ansi import *
+__version__ = cv2.__version__
