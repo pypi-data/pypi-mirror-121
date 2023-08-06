736a15c9b9c950e6bf08679bbb2363ca88d99bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package mysql2postgresql/0.8/mysql2postgresql-0.8-py3.9.egg

---
+++ packages/mysql2postgresql/mysql2postgresql-0.8-py3.9.egg/mysql2postgresql/__main__.py
@@ -0,0 +1,38 @@
+
+if __name__ == '__main__':
+    print("""
+        # Example
+
+        from mysql2postgresql import mysql2postgresql
+
+        a = mysql2postgresql()
+
+        #connect mysql server
+        a.connect_mysql(host='localhost',
+                    port='3306',
+                    user='root',
+                    passwd='',
+                    db='database_name')
+
+        #connect postgresql server
+        a.connect_postgresql(host='localhost',             
+                            port=5432,
+                            user='postgres',                                
+                            password='postgres',                     
+                            database='database_name')  
+
+
+        # manual table to transfer data  -> default all table in database
+        a.tables = ['table1', 'table2', ...]
+
+        # manual without table to transfer data  -> default empty
+        a.without = ['table3', 'table4', ...]
+
+        # manual limit to query data -> default 10000 
+        # not limit -> a.limit = 0 
+        a.limit = 10000
+
+
+        # program run
+        a.run()   
+    """)
