f3b4d0c885f16177cc29469a9975295b348c4847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:42 +0000

    Add package kama-telem-plugin/0.0.5/kama-telem-plugin-0.0.5.tar.gz

---
+++ packages/kama-telem-plugin/kama-telem-plugin-0.0.5.tar.gz/kama-telem-plugin-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="kama-telem-plugin",
+  version="0.0.5",
+  author="NMachine",
+  author_email="xavier@nmachine.io",
+  description="Telemetry plugin for the kama-sdk-py",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/nmachine-io/kama-sdk-py",
+  packages=setuptools.find_packages(exclude=['kama_sdk']),
+  package_data={
+    'kama_telem_plugin': [
+      'assets/*.*',
+      'descriptors'
+    ]
+  },
+  include_package_data=True,
+  install_requires=[
+    'kama-sdk-py',
+  ],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.8'
+)
