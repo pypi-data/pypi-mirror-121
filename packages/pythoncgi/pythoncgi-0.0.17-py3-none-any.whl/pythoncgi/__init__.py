5b84c2356961a2c17ba788ab640e90a417bc0038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:17 +0000

    Add package pythoncgi/0.0.17/pythoncgi-0.0.17-py3-none-any.whl

---
+++ packages/pythoncgi/pythoncgi-0.0.17-py3-none-any.whl/pythoncgi/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.17"
+__keywords__ = ["python cgi apache"]
+
+
+# if not __version__.endswith(".0"):
+#     import re
+#     print("version {} is deployed for automatic commitments only".format(__version__), flush=True)
+#     print("install version " + re.sub(r"([0-9]+\.[0-9]+\.)[0-9]+", r"\g<1>0", __version__) + " instead")
+#     import os
+#     os._exit(1)
+
+
+from .core import _SERVER, _GET, _POST, _SESSION, _COOKIE, _HEADERS, set_status, set_header, execute, print, main, log, log_construct
+
