daeaf7b5feb7ef4aa33f69930fee30cc6a79550e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:32 +0000

    Add package Pytorch-NLU/0.0.1/Pytorch_NLU-0.0.1-py2.py3-none-any.whl

---
+++ packages/Pytorch-NLU/Pytorch_NLU-0.0.1-py2.py3-none-any.whl/test/tet.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# @time    : 2021/9/7 23:33
+# @author  : Mo
+# @function:
+
+
+grid = [[[0 for _ in range(50)] for _ in range(50)] for _ in range(9)]
+grid[1][3][3] = 1
+print(grid)
+ee = 0
