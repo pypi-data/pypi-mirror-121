134599edaaa53e6bb2a288b693f9bb5250b7f0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:49 +0000

    Add package glasswall/0.2.8/glasswall-0.2.8-py3-none-any.whl

---
+++ packages/glasswall/glasswall-0.2.8-py3-none-any.whl/glasswall/content_management/config_elements/sysConfig.py
@@ -0,0 +1,27 @@
+
+
+from glasswall.content_management import switches
+from glasswall.content_management.config_elements.config_element import ConfigElement
+
+
+class sysConfig(ConfigElement):
+    """ A sysConfig ConfigElement.
+
+    Key word arguments can be specified to change individual switch values:
+    sysConfig(interchange_type="xml", interchange_pretty="false")
+    """
+
+    def __init__(self, **kwargs):
+        self.name = self.__class__.__name__
+        self.switches_module = switches.sys
+        self.default_switches = [
+            self.switches_module.interchange_type(value="sisl"),
+            self.switches_module.interchange_pretty(value="false"),
+        ]
+
+        super().__init__(
+            name=self.name,
+            switches_module=self.switches_module,
+            default_switches=self.default_switches,
+            config=kwargs
+        )
---
+++ packages/glasswall/glasswall-0.2.8-py3-none-any.whl/glasswall/content_management/config_elements/tiffConfig.py
@@ -0,0 +1,31 @@
+
+
+from glasswall.content_management import switches
+from glasswall.content_management.config_elements.config_element import ConfigElement
+
+
+class tiffConfig(ConfigElement):
+    """ A tiffConfig ConfigElement.
+
+    Args:
+        default (str): The default action: allow, disallow, or sanitise.
+
+    Key word arguments can be specified to change individual switch values:
+    tiffConfig(geotiff="sanitise")
+    """
+
+    def __init__(self, default: str = "sanitise", **kwargs):
+        self.name = self.__class__.__name__
+        self.default = default
+        self.switches_module = switches.tiff
+        self.default_switches = [
+            self.switches_module.geotiff(value=default),
+        ]
+
+        super().__init__(
+            name=self.name,
+            default=self.default,
+            switches_module=self.switches_module,
+            default_switches=self.default_switches,
+            config=kwargs
+        )
