ac5a2451fc8bc06353238a73fae6681b772b3e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:05 +0000

    Add package di/0.4.9/di-0.4.9.tar.gz

---
+++ packages/di/di-0.4.9.tar.gz/di-0.4.9/di/_concurrency.py
@@ -0,0 +1,33 @@
+import contextvars
+import functools
+from typing import Any, Awaitable, Callable, TypeVar, Union, cast
+
+import anyio
+
+from di._inspect import is_coroutine_callable
+
+T = TypeVar("T")
+
+
+def curry_context(call: Callable[..., T]) -> Callable[..., T]:
+    ctx = contextvars.copy_context()
+
+    def inner(*args: Any, **kwargs: Any) -> T:
+        return ctx.run(functools.partial(call, *args, **kwargs))
+
+    return inner
+
+
+def callable_in_thread_pool(call: Callable[..., T]) -> Callable[..., Awaitable[T]]:
+    def inner(*args: Any, **kwargs: Any) -> Awaitable[T]:
+        return cast(Awaitable[T], anyio.to_thread.run_sync(curry_context(call)))  # type: ignore
+
+    return inner
+
+
+def gurantee_awaitable(
+    call: Union[Callable[..., Awaitable[T]], Callable[..., T]]
+) -> Callable[..., Awaitable[T]]:
+    if not is_coroutine_callable(call):
+        return callable_in_thread_pool(cast(Callable[..., T], call))
+    return cast(Callable[..., Awaitable[T]], call)
