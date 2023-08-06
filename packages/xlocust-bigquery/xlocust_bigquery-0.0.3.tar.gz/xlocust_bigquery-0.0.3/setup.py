9cb5b5f0a4d98ff2cda9bcc9c388f38a96a52df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:53 +0000

    Add package xlocust-bigquery/0.0.3/xlocust_bigquery-0.0.3.tar.gz

---
+++ packages/xlocust-bigquery/xlocust_bigquery-0.0.3.tar.gz/xlocust_bigquery-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xlocust_bigquery',
+    version='0.0.3',
+    packages=find_packages(),
+    url='https://github.com/KelvinSan/xlocust_bigquery',
+    license='',
+    author='kelvin sanyaolu',
+    author_email='kelvin.sanyaolu@gmail.com',
+    description='A Google cloud GP Client for locustio to test dataloads and query extraction'
+)
