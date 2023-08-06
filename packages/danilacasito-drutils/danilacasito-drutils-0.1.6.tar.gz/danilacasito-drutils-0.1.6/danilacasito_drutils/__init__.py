2db2a8e1ed03b6337c722e9823800fdf17b31f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:09 +0000

    Add package danilacasito-drutils/0.1.6/danilacasito-drutils-0.1.6.tar.gz

---
+++ packages/danilacasito-drutils/danilacasito-drutils-0.1.6.tar.gz/danilacasito-drutils-0.1.6/danilacasito_drutils/__init__.py
@@ -0,0 +1,11 @@
+"""
+drutils.
+
+My Multi Use Python Package
+"""
+
+__version__ = "0.1.6"
+__author__ = "danilacasito"
+__credits__ = "Only me, i'm the only who maintains this"
+import danilacasito_drutils.parse
+import danilacasito_drutils.network
