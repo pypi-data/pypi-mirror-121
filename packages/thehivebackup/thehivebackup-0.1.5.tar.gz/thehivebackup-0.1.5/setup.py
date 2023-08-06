a249e72a91ec909dd01e33fb5b736b53984f27a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:54 +0000

    Add package thehivebackup/0.1.5/thehivebackup-0.1.5.tar.gz

---
+++ packages/thehivebackup/thehivebackup-0.1.5.tar.gz/thehivebackup-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='thehivebackup',
+    version='0.1.5',
+    url='https://github.com/IFX-CDC/thehivebackup',
+    author='Anton Piiroja & Jonas Plum',
+    description='Backup and restore TheHive remotely',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=['thehivebackup'],
+    install_requires=[
+        'requests==2.25.1',
+        'thehive4py==1.8.1',
+    ],
+    entry_points={
+        'console_scripts':
+            ['thehivebackup = thehivebackup.main:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+)
---
+++ packages/thehivebackup/thehivebackup-0.1.5.tar.gz/thehivebackup-0.1.5/thehivebackup/main.py
@@ -0,0 +1,154 @@
+import argparse
+import datetime
+import logging
+import sys
+import time
+
+import urllib3
+
+from thehivebackup.backup import Backupper
+from thehivebackup.empty import Deletor
+from thehivebackup.migrate3to4 import migrate
+from thehivebackup.restore import Restorer
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="thehivebackup", description="Backup and restore TheHive remotely.")
+    parser.add_argument("-v", "--verbose", help="Show logging.", dest='verbose', action="store_true", default=False)
+
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", required=True)
+
+    parser_backup = subparsers.add_parser('backup', description='Backup TheHive')
+    parser_backup.add_argument('--key', type=str, required=True, help='TheHive api key')
+    parser_backup.add_argument('--org', type=str, required=False, help='Organisation')
+    parser_backup.add_argument('--year', type=int, help='Pass --year and --month to backup a single month')
+    parser_backup.add_argument('--month', type=int, help='Pass --year and --month to backup a single month')
+    parser_backup.add_argument('--day', type=int, help='Pass --year and --month to backup a single month')
+    parser_backup.add_argument("--no-verify", help="Set to false to disable ssl verification", dest='verify',
+                               action="store_false", default=True)
+    parser_backup.add_argument('host', type=str, help='host of TheHive api')
+
+    parser_migrate = subparsers.add_parser('migrate3to4', description='Migrate TheHive')
+    parser_migrate.add_argument('--usermapping', type=str, help='User mapping csv file')
+    parser_migrate.add_argument('--fieldmapping', type=str, help='Custom field mapping csv file')
+    parser_migrate.add_argument('--metricmapping', type=str, help='Metric mapping csv file')
+    parser_migrate.add_argument('--add_old_no', action="store_true", default=False,
+                                help='Add old case number as "old-case-no" and "old-case-id"')
+    parser_migrate.add_argument('--default-user', type=str, required=True, help='default user')
+    parser_migrate.add_argument('backup', type=str, help='TheHive backup')
+
+    parser_restore = subparsers.add_parser('restore', description='Restore TheHive')
+    parser_restore.add_argument('--key', type=str, required=True, help='TheHive api key')
+    parser_restore.add_argument('--org', type=str, required=False, help='Organisation')
+    parser_restore.add_argument('--connections', type=int, help='maximum connections')
+    parser_restore.add_argument('--alerts', action="store_true", default=False, help='restore alerts')
+    parser_restore.add_argument("--no-verify", help="Set to false to disable ssl verification", dest='verify',
+                                action="store_false", default=True)
+    parser_restore.add_argument('backup', type=str, help='TheHive backup')
+    parser_restore.add_argument('host', type=str, help='host of TheHive api')
+
+    parser_clear = subparsers.add_parser('clear', description='Clean TheHive')
+    parser_clear.add_argument('--key', type=str, required=True, help='TheHive api key')
+    parser_clear.add_argument('--org', type=str, required=False, help='Organisation')
+    parser_clear.add_argument('--connections', type=int, help='maximum connections')
+    parser_clear.add_argument("--no-verify", help="Set to false to disable ssl verification", dest='verify',
+                              action="store_false", default=True)
+    parser_clear.add_argument('host', type=str, help='host of TheHive api')
+
+    if len(sys.argv) < 2:
+        parser.print_usage()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if args.subcommand == "backup":
+        backup(args)
+    elif args.subcommand == "migrate3to4":
+        migrate(args)
+    elif args.subcommand == "restore":
+        restore(args)
+    elif args.subcommand == "clear":
+        clear(args)
+
+
+def parse_url(url_str: str) -> (bool, str, int):
+    url = urllib3.util.parse_url(url_str)
+    ssl = True
+    if url.scheme is not None and url.scheme == "http":
+        ssl = False
+    if url.host is None:
+        print("Host invalid.")
+        sys.exit(1)
+    port = url.port
+    if port is None:
+        port = 443
+    return ssl, url.host, port
+
+
+def clear(args):
+    ssl, host, port = parse_url(args.host)
+    deletor = Deletor(host, args.key, port, ssl, args.verify, args.connections)
+    deletor.delete_cases()
+    deletor.delete_alerts()
+
+
+def restore(args):
+    start_time = time.time()
+    ssl, host, port = parse_url(args.host)
+    print(f'Start at {datetime.datetime.now().isoformat()}')
+    restorer = Restorer(args.backup, host, port, args.key, args.org, args.connections, ssl, args.verify)
+    restorer.store_cases()
+    if args.alerts:
+        restorer.restore_alerts()
+    print(f'Restore done in {time.time() - start_time} seconds')
+
+
+def utc_nano_timestamp(year: int, month: int, day: int) -> int:
+    return int(datetime.datetime(year, month, day, tzinfo=datetime.timezone.utc).timestamp() * 1000)
+
+
+def backup(args):
+    start_time = time.time()
+    name = "backup"
+    if args.org:
+        name += f"-{args.org}"
+    if args.year is None and args.month is None and args.day is None:
+        migration = Backupper(name + "-full", args.host, args.key, args.org, args.verify)
+        migration.backup_cases_all()
+        migration.backup_alerts_all()
+    else:
+        if args.year is not None and args.month is not None and args.day is not None:
+            name += f"-{args.year}-{args.month}-{args.day}"
+            start = utc_nano_timestamp(args.year, args.month, args.day)
+            end = start + 60 * 60 * 24 * 1000 - 1
+        elif args.year is not None and args.month is not None:
+            name += f"-{args.year}-{args.month}"
+            start = utc_nano_timestamp(args.year, args.month, 1)
+            next_month = args.month + 1
+            if next_month == 13:
+                next_month = 1
+            end = utc_nano_timestamp(args.year, next_month, 1) - 1
+        elif args.year is not None:
+            name += f"-{args.year}"
+            start = utc_nano_timestamp(args.year, 1, 1)
+            end = utc_nano_timestamp(args.year + 1, 1, 1) - 1
+        else:
+            print("--year needs to be given")
+            sys.exit(1)
+
+        migration = Backupper(name, args.host, args.key, args.org, args.verify)
+        migration.backup_cases_range(start, end)
+        migration.backup_alerts_range(start, end)
+
+    print(f'Backup done in {time.time() - start_time:.2f} seconds')
+
+
+if __name__ == '__main__':
+    main()
