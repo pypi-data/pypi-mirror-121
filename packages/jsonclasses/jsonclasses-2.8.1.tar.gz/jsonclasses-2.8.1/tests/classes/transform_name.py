1796ebfd0d5180ec687a0f62baf34e91e72ab0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package jsonclasses/2.8.1/jsonclasses-2.8.1.tar.gz

---
+++ packages/jsonclasses/jsonclasses-2.8.1.tar.gz/jsonclasses-2.8.1/tests/classes/transform_name.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import Optional
+from jsonclasses import jsonclass, types
+
+
+@jsonclass
+class TransformName:
+    name: Optional[str] = types.str.transform(lambda s: s + 'q')
+
+
+@jsonclass
+class CTransformName:
+    name: Optional[str] = types.str.transform(lambda s, c: s + c.val)
