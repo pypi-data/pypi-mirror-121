824f0185aeb19a0188b86c9b509f24d167c74912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:05 +0000

    Add package autoBOTLib/0.70/autoBOTLib-0.70.tar.gz

---
+++ packages/autoBOTLib/autoBOTLib-0.70.tar.gz/autoBOTLib-0.70/tests/minimal_functionality_test.py
@@ -0,0 +1,34 @@
+## A simple example showcasing the minimal usecase of autoBOTLib on an insults classification data.
+
+import autoBOTLib
+import pandas as pd
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import pipeline  ## A necessary import
+
+
+def test_minimal():
+    ## Load example data frame
+    dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+    train_sequences = dataframe['text_a']
+    train_targets = dataframe['label']
+
+    autoBOTLibObj = autoBOTLib.GAlearner(
+        train_sequences,
+        train_targets,
+        representation_type = "symbolic", ## See the documentation for all possible representation types.
+        n_fold_cv=3,
+        memory_storage ="memory2",
+        sparsity = 0.1,
+        upsample = False, ## Suitable for imbalanced data - randomized upsampling tends to help.
+        time_constraint=0.2).evolve(strategy = "evolution") ## strategy = "direct-learning" trains a single learner.
+
+    dataframe2 = pd.read_csv("../data/insults/test.tsv", sep="\t")
+    test_sequences = dataframe2['text_a']
+    predictions = autoBOTLibObj.predict(test_sequences)
+    prob_predictions = autoBOTLibObj.predict_proba(test_sequences)
+    print(predictions)
+    print(prob_predictions)
+
+    autoBOTLibObj.generate_report(output_folder = "./report/", job_id = "as9y0gb98s")
