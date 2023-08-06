5b9b9bacc68712f13557a61296845243b0f8b1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:28 +0000

    Add package test-override/0.0.2/test-override-0.0.2.tar.gz

---
+++ packages/test-override/test-override-0.0.2.tar.gz/test-override-0.0.2/test_override/methods.py
@@ -0,0 +1,11 @@
+from test_override.my_class import Calculate
+
+def method_test(self, num):
+    return num
+
+def use_class(self):
+    my_class = Calculate()
+    sum = my_class.sum()
+    div = my_class.division()
+
+    return sum, div
