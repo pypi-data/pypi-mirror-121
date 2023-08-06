70e2b7b2db95d0302986655cf996c4082c54d2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package prisma-client/0.2.0/prisma_client-0.2.0-py3-none-any.whl

---
+++ packages/prisma-client/prisma_client-0.2.0-py3-none-any.whl/prisma/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# pyright: reportUnusedImport=false
+
+__title__ = 'prisma'
+__author__ = 'RobertCraigie'
+__license__ = 'APACHE'
+__copyright__ = 'Copyright 2020-2021 RobertCraigie'
+__version__ = '0.2.0'
+
+from .utils import setup_logging
+from . import errors
+
+
+try:
+    from .client import *
+    from .fields import *
+    from . import models, partials, types
+except ModuleNotFoundError:
+    # code has not been generated yet
+    # TODO: this could swallow unexpected errors
+    pass
+
+
+setup_logging()
