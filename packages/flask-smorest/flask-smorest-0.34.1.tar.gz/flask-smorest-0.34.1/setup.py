0c1b25f93b38206d47302bb6784f832613c1fadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:51 +0000

    Add package flask-smorest/0.34.1/flask-smorest-0.34.1.tar.gz

---
+++ packages/flask-smorest/flask-smorest-0.34.1.tar.gz/flask-smorest-0.34.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='flask-smorest',
+    version='0.34.1',
+    description='Flask/Marshmallow-based REST API framework',
+    long_description=long_description,
+    url='https://github.com/marshmallow-code/flask-smorest',
+    author='Jérôme Lafréchoux',
+    author_email='jerome@jolimont.fr',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=[
+        'REST',
+        'openapi',
+        'swagger',
+        'flask',
+        'marshmallow',
+        'apispec'
+        'webargs',
+    ],
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    package_data={
+        '': ['spec/templates/*'],
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'werkzeug>=2.0,<3',
+        'flask>=2.0,<3',
+        'marshmallow>=3.13.0,<4',
+        'webargs>=8.0.0,<9',
+        'apispec>=5.1.0,<6',
+    ],
+)
