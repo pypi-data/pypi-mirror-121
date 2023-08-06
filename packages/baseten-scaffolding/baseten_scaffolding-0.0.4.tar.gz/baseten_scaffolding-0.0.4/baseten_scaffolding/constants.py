f803e84b806293339a81cabddf3351fbf7416eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:40 +0000

    Add package baseten-scaffolding/0.0.4/baseten_scaffolding-0.0.4.tar.gz

---
+++ packages/baseten-scaffolding/baseten_scaffolding-0.0.4.tar.gz/baseten_scaffolding-0.0.4/baseten_scaffolding/constants.py
@@ -0,0 +1,12 @@
+import os
+import pathlib
+
+SKLEARN = 'sklearn'
+TENSORFLOW = 'tensorflow'
+KERAS = 'keras'
+PYTORCH = 'pytorch'
+CUSTOM = 'custom'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CODE_DIR = pathlib.Path(BASE_DIR, 'baseten_scaffolding')
+SERVING_DIR = pathlib.Path(CODE_DIR, 'scaffold_templates')
