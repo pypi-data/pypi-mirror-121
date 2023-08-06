9e1447f6efd1b6c93de32eaf9ce75e83f31ff9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:52 +0000

    Add package das-statistics/0.1/das_statistics-0.1.tar.gz

---
+++ packages/das-statistics/das_statistics-0.1.tar.gz/das_statistics-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='das_statistics',
+      version='0.1',
+      description='Statistical Distributions',
+      packages=['das_statistics'],
+      author = 'Sudip Das',
+      author_email = 'sudip1012@yahoo.com',
+      zip_safe=False)
