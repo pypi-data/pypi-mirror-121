d86246ec16f1e75af4ac449b8d5988c3d4140aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:11 +0000

    Add package sensorpad/0.0.3/sensorpad-0.0.3.tar.gz

---
+++ packages/sensorpad/sensorpad-0.0.3.tar.gz/sensorpad-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sensorpad',
+    description='No-dependencies Python client for Sensorpad.',
+    version='0.0.3',
+    license='MIT',
+    author="Alexander Zaitsev",
+    author_email='alex@sensorpad.io',
+    packages=['sensorpad'],
+    url='https://github.com/sensorpad/sensorpad-python',
+    keywords=['sensorpad', 'monitoring'],
+    install_requires=[],
+)
