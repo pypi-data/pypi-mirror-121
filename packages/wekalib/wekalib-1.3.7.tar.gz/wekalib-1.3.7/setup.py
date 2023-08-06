37eacf42b35887a3449973e5fefe0cace1eb17c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:19 +0000

    Add package wekalib/1.3.7/wekalib-1.3.7.tar.gz

---
+++ packages/wekalib/wekalib-1.3.7.tar.gz/wekalib-1.3.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wekalib',
+    version='1.3.7',
+    author='Vincent Fleming',
+    author_email='vince@weka.io',
+    description='Weka JRPC API library',
+    packages=['wekalib']
+)
+
