b110852f5ea4ec1c4df3e13f115d00da2145897c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package cicdtest/4.4/cicdtest-4.4.tar.gz

---
+++ packages/cicdtest/cicdtest-4.4.tar.gz/cicdtest-4.4/ci_commands/stop_all.py
@@ -0,0 +1,48 @@
+"""
+    Title: stop_all
+    Author: Kushagra A.
+    Modified By: Kushagra A.
+    Language: Python
+    Date Created: 22-09-2021
+    Date Modified: 21-09-2021
+    Description:
+        ###############################################################
+        ##      Remove all repo details from centralized file        ## 
+        ###############################################################
+"""
+
+import click
+from click.decorators import command
+import os
+from buildpan import find_path
+from crontab import CronTab
+
+@click.command()
+def stop_all():
+    '''
+     For stop CI-CD operation
+    \f
+    
+   
+    '''
+
+    find_path.find_path()
+    file_path = find_path.find_path.file_path
+
+    if os.path.exists(file_path + "/info.txt"):
+        os.remove(file_path + "/info.txt")
+        print("CI-CD operation stopped \n Run:- buildpan init to intiate CICD operation")
+    
+    else:
+        print("No CI-CD operation running \n Run:- buildpan init to intiate CICD operation")
+    
+    cron_job = CronTab(user=True)
+    jobs= cron_job.find_comment('buildpan')
+    jobs_list = list(jobs)
+    if len(jobs_list) == 0:
+        print("No buildpan process is runnig!!")
+    else:
+        for job in jobs_list:
+            cron_job.remove(job)
+        cron_job.write()
+        print("buildpan process successfully stopped!")
