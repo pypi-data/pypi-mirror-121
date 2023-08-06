63ecda53bf3fc708f7bc37b772518c1c270f57e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:19 +0000

    Add package ttask/0.0.5/ttask-0.0.5.tar.gz

---
+++ packages/ttask/ttask-0.0.5.tar.gz/ttask-0.0.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ttask']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{':sys_platform == "windows"': ['windows-curses']}
+
+entry_points = \
+{'console_scripts': ['ttask = ttask.main:main']}
+
+setup_kwargs = {
+    'name': 'ttask',
+    'version': '0.0.5',
+    'description': '',
+    'long_description': '',
+    'author': 'sleepntsheep',
+    'author_email': 'contact@papangkorn.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5',
+}
+
+
+setup(**setup_kwargs)
