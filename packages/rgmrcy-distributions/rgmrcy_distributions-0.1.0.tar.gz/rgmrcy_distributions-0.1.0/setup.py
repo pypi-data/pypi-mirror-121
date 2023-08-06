e2ff1a891cebf2f4e6e86bc08850b7d9754b2194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:44 +0000

    Add package rgmrcy-distributions/0.1.0/rgmrcy_distributions-0.1.0.tar.gz

---
+++ packages/rgmrcy-distributions/rgmrcy_distributions-0.1.0.tar.gz/rgmrcy_distributions-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rgmrcy_distributions',
+      version='0.1.0',
+      description='Gaussian distributions',
+      packages=['rgmrcy_distributions'],
+      zip_safe=False)
