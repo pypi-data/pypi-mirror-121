92ec548b63e2668a17626161ad2e492b29933521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package pharmpy-core/0.32.0/pharmpy-core-0.32.0.tar.gz

---
+++ packages/pharmpy-core/pharmpy-core-0.32.0.tar.gz/pharmpy-core-0.32.0/src/pharmpy/__init__.py
@@ -0,0 +1,34 @@
+"""
+=======
+Pharmpy
+=======
+
+Pharmpy is a python package for pharmacometrics modelling.
+
+Definitions
+===========
+"""
+
+__version__ = '0.32.0'
+
+import logging
+
+from .model_factory import Model
+from .parameter import Parameter, Parameters
+from .random_variables import RandomVariable, RandomVariables, VariabilityHierarchy
+from .statements import ModelStatements
+from .symbols import symbol
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+__all__ = [
+    'Model',
+    'ModelStatements',
+    'Parameter',
+    'Parameters',
+    'RandomVariable',
+    'RandomVariables',
+    'VariabilityHierarchy',
+    'symbol',
+]
---
+++ packages/pharmpy-core/pharmpy-core-0.32.0.tar.gz/pharmpy-core-0.32.0/src/pharmpy/modeling/run.py
@@ -0,0 +1,129 @@
+import importlib
+
+import pharmpy.model
+import pharmpy.results
+import pharmpy.tools.common
+import pharmpy.tools.modelfit
+from pharmpy.workflows import execute_workflow
+
+
+def fit(models):
+    """Fit models.
+
+    Parameters
+    ----------
+    models : list
+        List of models or one single model
+
+    Return
+    ------
+    Model
+        Reference to same model
+
+    Examples
+    >>> from pharmpy.modeling import *
+    >>> model = load_example_model("pheno")
+    >>> fit(model)      # doctest: +SKIP
+
+    See also
+    --------
+    run_tool
+
+    """
+    if isinstance(models, pharmpy.model.Model):
+        models = [models]
+        single = True
+    else:
+        single = False
+    tool = pharmpy.tools.modelfit.Modelfit(models)
+    tool.run()
+    if single:
+        return models[0]
+    else:
+        return models
+
+
+def create_results(path, **kwargs):
+    """Create/recalculate results object given path to run directory
+
+    Parameters
+    ----------
+    path : str, Path
+        Path to run directory
+    kwargs
+        Arguments to pass to tool specific create results function
+
+    Returns
+    -------
+    Results
+        Results object for tool
+
+    Examples
+    --------
+    >>> from pharmpy.modeling import *
+    >>> res = create_results("frem_dir1")   # doctest: +SKIP
+
+    See also
+    --------
+    read_results
+
+    """
+    res = pharmpy.tools.common.create_results(path, **kwargs)
+    return res
+
+
+def read_results(path):
+    """Read results object from file
+
+    Parameters
+    ----------
+    path : str, Path
+        Path to results file
+
+    Return
+    ------
+    Results
+        Results object for tool
+
+    Examples
+    --------
+    >>> from pharmpy.modeling import *
+    >>> res = read_resuts("results.json")     # doctest: +SKIP
+
+    See also
+    --------
+    create_results
+
+    """
+    res = pharmpy.results.read_results(path)
+    return res
+
+
+def run_tool(name, *args, **kwargs):
+    """Run tool workflow
+
+    Parameters
+    ----------
+    name : str
+        Name of tool to run
+    args
+        Arguments to pass to tool
+    kwargs
+        Arguments to pass to tool
+
+    Return
+    ------
+    Results
+        Results object for tool
+
+    Examples
+    --------
+    >>> from pharmpy.modeling import *
+    >>> model = load_example_model("pheno")
+    >>> res = run_tool("resmod", model)   # doctest: +SKIP
+
+    """
+    tool = importlib.import_module(f'pharmpy.tools.{name}')
+    wf = tool.create_workflow(*args, **kwargs)
+    res = execute_workflow(wf)
+    return res
