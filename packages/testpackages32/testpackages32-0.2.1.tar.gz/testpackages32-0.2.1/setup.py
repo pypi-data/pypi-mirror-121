00fe35365bdc14b39c9187292c88947f4751356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:58 +0000

    Add package testpackages32/0.2.1/testpackages32-0.2.1.tar.gz

---
+++ packages/testpackages32/testpackages32-0.2.1.tar.gz/testpackages32-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="testpackages32",
+    version="0.2.1",
+    author="Igor Simagin",
+    author_email="igorsim35@gmail.com",
+    install_requires=[
+        "numpy",
+        "pandas"
+    ]
+)
