fe29bc4b177d6cfd1e1d7c658ce3b5fff23e1ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:20 +0000

    Add package mikelint/1.2.0/mikelint-1.2.0.tar.gz

---
+++ packages/mikelint/mikelint-1.2.0.tar.gz/mikelint-1.2.0/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="mikelint",
+    version="1.2.0",
+    description="Linter used for CSSE1001 at UQ",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mike-fam/mikelint",
+    author="Mike Pham",
+    author_email="mikepham1207@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["mikelint"],
+    include_package_data=True,
+    install_requires=[
+        "astroid>=2.5",
+        "docstring-parser>=0.7",
+        "isort>=5.8",
+        "lazy-object-proxy>=1.6",
+        "mccabe>=0.6",
+        "PyYAML>=5.4",
+        "toml>=0.10",
+        "wrapt>=1.12"
+    ],
+    python_requires=">=3.8",
+    # package_dir={"": ""}
+)
