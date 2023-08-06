ae42002986f6e27806d9f01e884bfc62f631f92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package SPLayout/0.1.8/SPLayout-0.1.8-py3-none-any.whl

---
+++ packages/SPLayout/SPLayout-0.1.8-py3-none-any.whl/splayout/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.1.8"
+
+from splayout.AEMDgrating import MAKE_AEMD_GRATING
+from splayout.bend import Bend
+from splayout.doubleconnector import DoubleBendConnector
+from splayout.microring import AddDropMicroring,AddDropMicroringFlat
+from splayout.polygon import Polygon
+from splayout.quarbend import QuarBend,AQuarBend
+from splayout.Selfdefinecomponent import MAKE_COMPONENT
+from splayout.taper import Taper
+from splayout.text import Text
+from splayout.utils import *
+from splayout.waveguide import Waveguide
+from splayout.sbend import SBend,ASBend
+from splayout.filledpattern import Circle,Rectangle
+from splayout.fdtdapi import FDTDSimulation
+from splayout.BinaryBatAlgorithm import BinaryBatAlgorithm
+from splayout.DirectBinarySearchAlgorithm import DirectBianrySearchAlgorithm
