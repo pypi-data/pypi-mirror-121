380af08b74aa538921f2ca8468f1ff3aba2083ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:23 +0000

    Add package ipyslides/0.9.5/ipyslides-0.9.5-py3-none-any.whl

---
+++ packages/ipyslides/ipyslides-0.9.5-py3-none-any.whl/ipyslides/objs_formatter.py
@@ -0,0 +1,95 @@
+"""Format objects like different charting libraries and IPython display object to HTML representation.
+
+"""
+import sys
+import inspect, re, json
+from io import BytesIO
+import matplotlib.pyplot as plt
+from markdown import markdown
+
+def plt2html(plt_fig=None,transparent=True,caption=None):
+    """Write matplotib figure as HTML string to use in `ipyslide.utils.write`.
+    - **Parameters**
+        - plt_fig    : Matplotlib's figure instance, auto picks as well.
+        - transparent: True of False for fig background.
+    """
+    if plt_fig==None:
+        plt_fig = plt.gcf()
+    plot_bytes = BytesIO()
+    plt.savefig(plot_bytes,format='svg',transparent=transparent)
+    plt.clf() # Clear image to avoid other display
+    plt.close() #AVoids throwing text outside figure
+    svg = '<svg' + plot_bytes.getvalue().decode('utf-8').split('<svg')[1]
+    if caption:
+        svg = svg + f'<p style="font-size:80% !important;">{caption}</p>'
+    return f"<div class='fig-container'>{svg}</div>"
+
+def bokeh2html(bokeh_fig,title=""):
+    from bokeh.resources import CDN
+    from bokeh.embed import file_html
+    return file_html(bokeh_fig, CDN, title)
+
+
+# ONLY ADD LIBRARIEs who's required objects either do not have a _repr_html_ method or need ovverride
+
+libraries = [
+    {'name':'matplotlib.pyplot','obj':'Figure','func':plt2html,'args':(),'kwargs': {}},
+    {'name':'altair','obj':'Chart','func': 'to_html','args':(),'kwargs': {}},
+    {'name':'pygal','obj':'Graph','func':'render','args':{},'kwargs':{'is_unicode':True}},
+    {'name':'pydeck','obj':'Deck','func':'to_html','args':(),'kwargs': {'as_string':True}},
+    {'name':'pandas','obj':'DataFrame','func':'to_html','args':(),'kwargs': {}},
+    {'name':'bokeh.plotting','obj':'Figure','func':bokeh2html,'args':(),'kwargs':{'title':''}}
+    
+]
+
+def format_object(obj):
+    try: # If matplotlib axes given
+        _fig = obj.get_figure()
+        return True,plt2html(_fig)
+    except: pass
+    
+    # Some builtin types
+    if isinstance(obj,dict):
+        # First prefer keep_format method. 
+        if '__keep_format__' in obj:
+            return True, obj['__keep_format__']
+        else:
+            return  True, f"<div class='PyRepr'>{json.dumps(obj,indent=4)}</div>"    
+    elif isinstance(obj,(set,list,tuple,int,float)): # Then prefer other builtins
+        return True, f"<div class='PyRepr'>{obj}</div>"
+    
+    # If Code object given
+    for _type in ['class','function','module','method','builtin','generator']:
+        if getattr(inspect,f'is{_type}')(obj):
+            try:
+                source = inspect.getsource(obj)
+                source = re.sub(r'^#\s+','#',source) # Avoid Headings in source
+            except:
+                source = f'Can not get source code of:\n{obj} '
+            return True, markdown(f'```python\n{source}\n```',extensions=['fenced_code','codehilite'])
+    
+    # Other Libraries   
+    try: module_name = obj.__module__
+    except: module_name = '' #str, int etc don't have __module__
+    
+    for lib in libraries:
+        if lib['name'].split('.')[0] in module_name: #MATCH NAMES
+            _module = sys.modules.get(lib['name'],None) # Already imported
+            
+            if not _module:
+                get_ipython().run_cell(f"import {lib['name']} as _module") # Need import to compare
+                
+            _obj = getattr(_module,lib['obj'],None)
+            if isinstance(obj, _obj):
+                if not isinstance(lib['func'],str): # Handle Matplotlib, bokeh, df etc here by making handling functions
+                    return True, lib['func'](obj, *lib['args'],**lib['kwargs'])
+                else:
+                    _func = getattr(obj, lib['func'],None)
+                    if _func:
+                        return True, _func(*lib['args'],**lib['kwargs'])
+
+    # If Nothing found
+    return False, NotImplementedError(f"{obj}'s html representation is not implemented yet!")       
+
+ 
+            
