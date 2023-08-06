af59ac0f1729575b2726b01904ea1cac6f2fe14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:35 +0000

    Add package lumen/0.5.0a25/lumen-0.5.0a25.tar.gz

---
+++ packages/lumen/lumen-0.5.0a25.tar.gz/lumen-0.5.0a25/lumen/tests/conftest.py
@@ -0,0 +1,37 @@
+import tempfile
+
+import pytest
+
+from lumen.config import config
+from lumen.sources import FileSource
+from lumen.state import state
+
+
+@pytest.fixture
+def set_root():
+    root = config._root
+    def _set_root(root):
+        config._root = root
+    yield _set_root
+    config._root = root
+
+
+@pytest.fixture
+def make_filesource():
+    root = config._root
+    def create(root):
+        config._root = root
+        source = FileSource(tables={'test': 'test.csv'},  kwargs={'parse_dates': ['D']})
+        state.sources['original'] = source
+        return source
+    yield create
+    config._root = root
+    state.global_sources.clear()
+
+
+
+@pytest.fixture
+def yaml_file():
+    tf = tempfile.NamedTemporaryFile(mode='w', suffix='.yaml')
+    yield tf
+    tf.close()
