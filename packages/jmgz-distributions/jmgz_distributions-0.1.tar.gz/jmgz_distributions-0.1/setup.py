1e9746bccfc5e9abea367f011b960e2d4f15bc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package jmgz-distributions/0.1/jmgz_distributions-0.1.tar.gz

---
+++ packages/jmgz-distributions/jmgz_distributions-0.1.tar.gz/jmgz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jmgz_distributions',
+      version='0.1',
+      description='Gaussian jmgz_distributions',
+      packages=['jmgz_distributions'],
+      author='James Gachago',
+      author_email='myemail@someotherthing.com',
+      zip_safe=False)
