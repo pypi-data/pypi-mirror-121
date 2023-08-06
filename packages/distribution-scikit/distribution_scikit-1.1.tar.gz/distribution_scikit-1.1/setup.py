b45b5fcf9704bf16e639d474b3cd2c672fbc369f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:59 +0000

    Add package distribution-scikit/1.1/distribution_scikit-1.1.tar.gz

---
+++ packages/distribution-scikit/distribution_scikit-1.1.tar.gz/distribution_scikit-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_scikit',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distribution_scikit'],
+      zip_safe=False)
