791382820af678a269b0f94862c23a31d3d068a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:56 +0000

    Add package serverrunner/1.0.1/serverrunner-1.0.1.tar.gz

---
+++ packages/serverrunner/serverrunner-1.0.1.tar.gz/serverrunner-1.0.1/README.md
@@ -0,0 +1,12 @@
+# Server Runner
+This project is just a fun project i made when i was bored, but still will be maintained
+
+# How to install
+## Unix (Linux, Mac, FreeBSD)
+```pip3 install serverrunner``` or ```python3 -m pip install serverrunner```
+
+## Windows
+```pip install serverrunner```
+
+# Usage
+In your terminal or cmd, you can run ```runserver```. This will give you a prompt and you can fill out it
---
+++ packages/serverrunner/serverrunner-1.0.1.tar.gz/serverrunner-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="serverrunner",
+    version="1.0.1",
+    description="A server runner for certant projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="palmtrww",
+    author_email="palmtrww@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    packages=find_packages(where='serverrunners'),
+    entry_points={
+        'console_scripts': [
+            'runserver=serverruners:__main__'
+        ],
+    },
+
+)
