f9fc1d31486258e370d3af98890ed9d1c83af1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:58 +0000

    Add package cloudplatform-sdks/0.0.29/cloudplatform-sdks-0.0.29.tar.gz

---
+++ packages/cloudplatform-sdks/cloudplatform-sdks-0.0.29.tar.gz/cloudplatform-sdks-0.0.29/cloudplatform_sdks/huaweicloud_models/clients/eip_client.py
@@ -0,0 +1,32 @@
+from .client import HuaweiClient
+from huaweicloudsdkeip.v2 import EipClient, CreatePrePaidPublicipRequest, DeletePublicipRequest, ShowPublicipRequest, \
+    UpdatePublicipRequest
+from huaweicloudsdkeip.v2.region.eip_region import EipRegion
+
+
+class HuaweiEipClient(HuaweiClient):
+    def __init__(self, *args, **kwargs):
+        super(HuaweiEipClient, self).__init__(*args, **kwargs)
+
+    @property
+    def eip_client(self):
+        return self.generate_client(EipClient, EipRegion)
+
+    def create_eip(self, body_params=None):
+        request = CreatePrePaidPublicipRequest(body=body_params)
+        return self.eip_client.create_pre_paid_publicip(request)
+
+    def delete_eip(self, publicip_id=None):
+        request = DeletePublicipRequest(publicip_id=publicip_id)
+        return self.eip_client.delete_publicip(request)
+
+    def describe_eip(self, publicip_id=None):
+        request = ShowPublicipRequest(publicip_id=publicip_id)
+        return self.eip_client.show_publicip(request).publicip
+
+    def update_eip(self, publicip_id=None, body_params=None):
+        request = UpdatePublicipRequest(publicip_id=publicip_id, body=body_params)
+        return self.eip_client.update_publicip(request).publicip
+
+
+
