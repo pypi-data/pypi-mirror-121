0f424ff30bf987284e69115d363a73028f1ad980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:53 +0000

    Add package learning-loop-node/0.4.1/learning_loop_node-0.4.1-py3-none-any.whl

---
+++ packages/learning-loop-node/learning_loop_node-0.4.1-py3-none-any.whl/learning_loop_node/conftest.py
@@ -0,0 +1,36 @@
+import logging
+from learning_loop_node.tests import test_helper
+from learning_loop_node.loop import loop
+import pytest
+import shutil
+import icecream
+
+icecream.install()
+logging.basicConfig(level=logging.DEBUG)
+
+data_folder_for_tests = '/tmp/learning_loop_lib_data'
+
+
+@pytest.fixture()
+def create_project():
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+    project_configuration = {'project_name': 'pytest', 'inbox': 0, 'annotate': 0, 'review': 0, 'complete': 3, 'image_style': 'beautiful',
+                             'categories': 2, 'thumbs': False, 'tags': 0, 'trainings': 1, 'detections': 3, 'annotations': 0, 'skeleton': False}
+    assert test_helper.LiveServerSession().post(f"/api/zauberzeug/projects/generator",
+                                                json=project_configuration).status_code == 200
+    yield
+    test_helper.LiveServerSession().delete(f"/api/zauberzeug/projects/pytest?keep_images=true")
+
+
+@pytest.fixture(scope='function')
+def data_folder():
+    shutil.rmtree(data_folder_for_tests, ignore_errors=True)
+    yield data_folder_for_tests
+    shutil.rmtree(data_folder_for_tests, ignore_errors=True)
+
+
+@pytest.fixture(autouse=True, scope='function')
+def loop_session():
+    loop.session = None
+    yield
+    loop.session = None
