876df31bdd7ee7ca693667f77ed8e69e8a59b5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:54 +0000

    Add package haarpypi/0.1.0/haarpypi-0.1.0.tar.gz

---
+++ packages/haarpypi/haarpypi-0.1.0.tar.gz/haarpypi-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['haarpypi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['testix>=6.0.0,<7.0.0']
+
+setup_kwargs = {
+    'name': 'haarpypi',
+    'version': '0.1.0',
+    'description': 'haarpypi',
+    'long_description': None,
+    'author': 'Yoav Kleinberger',
+    'author_email': 'haarcuba@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
