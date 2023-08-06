ca07d5163483de3e90e6e2f23a8e5cbecb3bd4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:25 +0000

    Add package ouvidoria-unidombosco/0.0.8/ouvidoria_unidombosco-0.0.8.tar.gz

---
+++ packages/ouvidoria-unidombosco/ouvidoria_unidombosco-0.0.8.tar.gz/ouvidoria_unidombosco-0.0.8/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+import os
+
+OS_SEPARATOR = os.path.sep
+
+version = '0.0.8'
+name = 'ouvidoria_unidombosco'
+url = f'https://github.com/SamuelJansen/{name}/'
+
+setup(
+    name = name,
+    packages = [
+        name,
+        f'{name}{OS_SEPARATOR}api',
+        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}resource',
+        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src',
+    ],
+    package_data = {
+        '': ['*.yml']
+    },
+    version = version,
+    license = 'MIT',
+    description = 'Unidombosco ouvidoria package',
+    author = 'Samuel Jansen',
+    author_email = 'samuel.jansenn@gmail.com',
+    url = url,
+    download_url = f'{url}archive/v{version}.tar.gz',
+    keywords = ['goodbye indiference'],
+    install_requires = [
+        'globals<=0.3.4',
+        'selenium==3.141.0',
+        'webdriver_manager==3.2.1'
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
