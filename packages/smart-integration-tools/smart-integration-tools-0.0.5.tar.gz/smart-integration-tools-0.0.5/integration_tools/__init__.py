1ea10e7120446e5d3abc2689f729f6f00ea74891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:40 +0000

    Add package smart-integration-tools/0.0.5/smart-integration-tools-0.0.5.tar.gz

---
+++ packages/smart-integration-tools/smart-integration-tools-0.0.5.tar.gz/smart-integration-tools-0.0.5/integration_tools/__init__.py
@@ -0,0 +1,7 @@
+from .chunk import chunk_by_items, chunk_by_parts
+from .crypt import decode, encode
+from .settings import init_tools_settings
+from .filters import validate_data
+
+__version__ = '0.0.3'
+__author__ = 'bzdvdn'
