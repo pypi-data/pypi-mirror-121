8bee7c42b0cc18edcc5b038b16ecc8595a92b531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:38 +0000

    Add package ws-one/0.0.2/ws_one-0.0.2.tar.gz

---
+++ packages/ws-one/ws_one-0.0.2.tar.gz/ws_one-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='ws_one',
+    version='0.0.2',
+    packages=find_packages(),
+    author='PunchyArchy',
+    author_email='ksmdrmvscthny@gmail.com',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=[
+        'pyserial',
+        'pds_one==0.02'
+    ],
+)
