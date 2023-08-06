ed270182cd6dc3bd6fbdc4daa628541c3ecc1c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:31 +0000

    Add package OarLauncher/0.1.18/OarLauncher-0.1.18.tar.gz

---
+++ packages/OarLauncher/OarLauncher-0.1.18.tar.gz/OarLauncher-0.1.18/OarLauncher/templates.py
@@ -0,0 +1,21 @@
+START_OAR = """
+#!/bin/bash
+
+{oar_command}
+""".strip()
+
+RUNME_SCRIPT = """
+#!/bin/bash
+
+source /etc/profile.d/modules.sh
+module load conda/2020.11-python3.8
+module load gcc/9.2.0
+module load cmake/3.19.6
+module load boost/1.58.0
+
+export MPLBACKEND=Agg
+
+source {activate}
+{python_path}
+python {python_job} $1
+""".strip()
