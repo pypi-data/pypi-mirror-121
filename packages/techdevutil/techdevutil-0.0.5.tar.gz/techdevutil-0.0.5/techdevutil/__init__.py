c1e164cd6d204600490f96ddf11f89219fd8bf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:48 +0000

    Add package techdevutil/0.0.5/techdevutil-0.0.5.tar.gz

---
+++ packages/techdevutil/techdevutil-0.0.5.tar.gz/techdevutil-0.0.5/techdevutil/__init__.py
@@ -0,0 +1,34 @@
+def add_numbers(num1,num2):
+    return num1 + num2
+
+def subtract_numbers(num1,num2):
+    return num1 - num2
+
+def multiply_numbers(num1,num2):
+    return num1 * num2
+
+def divide_numbers(num1,num2):
+    return num1 / num2
+
+def palindrome(word):
+    wordlength = len(word)
+    reversedword=word[wordlength::-1]
+    if word == reversedword:
+        return(f"{word} is a palindrome")
+    else:
+        return(f"{word} is not a palindrome")
+
+def remove_spaces(field):
+    return(field.replace(" ",""))
+
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = "" 
+    
+    # traverse in the string  
+    for ele in s: 
+        str1 += ele  
+    
+    # return string  
+    return str1 
