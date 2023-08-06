591ed6066e991246c9e9bef18d81249b091b6a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:50 +0000

    Add package reliquery-ui/0.1.2/reliquery_ui-0.1.2-py3-none-any.whl

---
+++ packages/reliquery-ui/reliquery_ui-0.1.2-py3-none-any.whl/reliquery_ui/tests/test_main.py
@@ -0,0 +1,53 @@
+from typing import Dict
+
+from fastapi.testclient import TestClient
+
+from ..main import get_app
+
+
+class TestRelic:
+    name: str
+    relic_type: str
+    storage: str
+
+    def __init__(
+        self,
+        name: str,
+        relic_type: str,
+        storage: str = None,
+        storage_name: str = "default",
+        check_exists: bool = True,
+    ):
+        self.name = name
+        self.relic_type = relic_type
+        self.storage = None
+        self.storage_name = storage_name
+        self.check_exists = check_exists
+
+    @classmethod
+    def relic_exists(
+        cls,
+        name: str,
+        relic_type: str,
+        storage: str = None,
+        storage_name: str = "default",
+    ) -> bool:
+        return True
+
+    def describe(self) -> Dict:
+        return {"tutorial": {"arrays": [], "text": [], "html": []}}
+
+
+def test_read_relic():
+    app = get_app(TestRelic)
+    client = TestClient(app)
+    response = client.get("/api/reliquery/default/basic/tutorial")
+    assert response.status_code == 200
+    print(response)
+    assert response.json() == {
+        "name": "tutorial",
+        "relic_type": "basic",
+        "arrays": [],
+        "text": [],
+        "html": [],
+    }
