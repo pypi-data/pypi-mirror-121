45f72c1f6a9bbd3f6bb95272ad7b9731a70747e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:03 +0000

    Add package utils-s/1.9/utils-S-1.9.tar.gz

---
+++ packages/utils-s/utils-S-1.9.tar.gz/utils-S-1.9/utils/os_helper/__init__.py
@@ -0,0 +1,37 @@
+from . import paths
+import os
+import subprocess
+
+
+def get_current_home_dir():
+    return os.path.expanduser('~')
+
+
+def execute_capture(command):
+    os_exec = os.popen(command)
+    Read = os_exec.read()
+    os_exec.detach()
+    return Read
+
+
+def get_ip():
+    return os.popen('ipconfig getifaddr en0').read()[:-1]
+
+
+def waste():
+    pass
+
+
+def command(args: list, quite=False):
+    if quite:
+        sub = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    else:
+        sub = subprocess.Popen(args)
+
+    sub.wait()
+    sub.kill()
+    sub.terminate()
+
+
+def remove(file):
+    command(args=['rm', '-rf', file])
