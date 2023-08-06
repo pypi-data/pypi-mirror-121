9b18d82127780b9fe35e149d241ce539954022a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:25 +0000

    Add package aef-nattanon-py/0.1.7/aef_nattanon_py-0.1.7-py3-none-any.whl

---
+++ packages/aef-nattanon-py/aef_nattanon_py-0.1.7-py3-none-any.whl/aef_nattanon_py/yolov3director.py
@@ -0,0 +1,13 @@
+from director import Director
+import os
+
+
+class YoloV3Director(Director):
+
+    def __init__(self):
+        path_s = os.path.dirname(__file__)
+        super().__init__(
+            path_s+'/files/coco.names',
+            path_s+'/files/yolov3-tiny.cfg',
+            path_s+'/files/yolov3-tiny.weights'
+        )
