710b2cbdb89b8f307b342f11d77ce7eb0be7a016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:43 +0000

    Add package instapy-testers2/1.0/instapy_testers2-1.0.tar.gz

---
+++ packages/instapy-testers2/instapy_testers2-1.0.tar.gz/instapy_testers2-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='instapy_testers2',
+      version='1.0',
+      description='Instapy corrected module for the best usefull',
+      packages=['instapy_testers2'],
+      author_email='lokasan@inbox.ru',
+      zip_safe=False)
