0aefa59c7b983aa7697b26ea061953233987c824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package mxsearchstorage/1.0.2/mxsearchstorage-1.0.2.tar.gz

---
+++ packages/mxsearchstorage/mxsearchstorage-1.0.2.tar.gz/mxsearchstorage-1.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="mxsearchstorage",
+    version="1.0.2",
+    author="blake",
+    author_email="huiblake.li@walmart.com",
+    description="Mexico search object store Library",
+    packages=['mxsearchstorage'],
+    install_requires = ['azure-core','azure-storage-blob','certifi','charset-normalizer',
+                        'cryptography','idna','isodate', 'msrest', 'oauthlib', 'pycparser',
+                        'requests', 'requests-oauthlib', 'six', 'urllib3']
+)
+
