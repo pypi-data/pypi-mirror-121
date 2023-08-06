2b4d5817357194de40f7f3724fc6aa7148ad7322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:22 +0000

    Add package kubesys/1.0.4/kubesys-1.0.4.tar.gz

---
+++ packages/kubesys/kubesys-1.0.4.tar.gz/kubesys-1.0.4/README.md
@@ -0,0 +1,291 @@
+# client-Python
+
+We expect to provide a python client:
+- **Flexibility**. It can support all Kubernetes-based systems with minimized extra development, such as [Openshift](https://www.redhat.com/en/technologies/cloud-computing/openshift), [istio](https://istio.io/), etc.
+- **Usability**. Developers just need to learn to write json/yaml(kubernetes native style) from [Kubernetes documentation](https://kubernetes.io/docs/home/).
+- **Integration**. It can work with the other Kubernetes clients, such as [fabric8](https://github.com/fabric8io/kubernetes-client), [official](https://github.com/kubernetes-client/java/).
+
+
+## Comparison
+
+|                           | [official](https://github.com/kubernetes-client/go) | [cdk8s](https://cdk8s.io/) | [client-Python](https://github.com/kubesys/kubernetes-client-python) |
+|---------------------------|------------------|------------------|-------------------|
+|        Compatibility                      | for kubernetes-native kinds    | for crd kinds                 |  for both |
+|  Support customized Kubernetes resources  |  a lot of development          | a lot of development          |  zero-deployment     |
+|    Works with the other SDKs              |  complex                       | complex                       |  simple              |
+
+## Architecture
+
+![avatar](/docs/arch.png)
+
+## Installation
+
+```shell
+git clone --recursive https://github.com/kubesys/kubernetes-client-python.git
+```
+
+just import the module by:
+
+```python
+from kubesys.client import KubernetesClient
+```
+
+## Usage
+
+- [Usage](#usage)
+    - [中文文档](https://www.yuque.com/kubesys/kubernetes-client/overview)
+    - [Creating a client](#creating-a-client)
+    - [Simple example](#simple-example)
+    - [full-example](#full-example)
+
+#### create token with the following commands:
+
+1. create token
+
+```Bash
+kubectl create -f https://raw.githubusercontent.com/kubesys/client-go/master/account.yaml
+```
+
+2. get token
+
+```Bash
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep kubernetes-client | awk '{print $1}') | grep "token:" | awk -F":" '{print$2}' | sed 's/ //g'
+```
+
+### Creating a client
+
+we assume the following information: 
+
+```yaml
+host name: aliyun
+URL: "https://192.168.1.1:6443"
+Token: "THISISTHETOKEN"					# Just as an assumption, in reality this is a long-string
+```
+
+* you can simply define the dictionary as follows and then initialize the client  
+
+```python
+account={
+    "URL": "https://192.168.1.1:6443",
+    "Token":"THISISTHETOKEN"
+}
+client = KubernetesClient(account_info=account)
+```
+
+* Also, you can make use of  the host informations in your json-configure file. We edit `account.json` with the following ahead of time:
+
+```json
+{
+    "aliyun":{
+        "URL": "https://192.168.1.1:6443",
+        "Token":"THISISTHETOKEN"
+    }
+}
+```
+
+then, we can  initialize the client:
+
+```python
+ # "json_path" have the default value: "account.json", you can specify parameters to modify.
+client = KubernetesClient(host_label="aliyun") 
+```
+
+**If you have set the value of `account_info`, `host_label` will not take effect! **
+
+
+
+### simple-example
+
+Assume you have a json:
+
+```json
+{
+  "apiVersion": "v1",
+  "kind": "Pod",
+  "metadata": {
+    "name": "busybox",
+    "namespace": "default",
+    "labels": {
+      "test": "test"
+    }
+  }
+}
+```
+
+List resources:
+
+```python
+response_dict,OK,http_status_code = client.ListResources("Pod")
+print("response dict: ", response_dict)
+print("is OK: ", OK)
+print("HTTP status code: ", http_status_code)
+```
+
+Create a resource:
+
+```go
+pod_json= '''{
+        "apiVersion": "v1",
+        "kind": "Pod",
+        "metadata": {
+            "name": "busybox",
+            "namespace": "default"
+        },
+        "spec": {
+            "containers": [
+                {
+                    "image": "busybox",
+                    "env": [{
+                        "name": "abc",
+                        "value": "abc"
+                    }],
+                    "command": [
+                        "sleep",
+                        "3600"
+                    ],
+                    "imagePullPolicy": "IfNotPresent",
+                    "name": "busybox"
+                }],
+            "restartPolicy": "Always"
+        }
+	}'''
+
+client.createResource(pod_json);
+print("response_dict: ", response_dict)
+print("is OK: ", OK)
+print("HTTP status code: ", http_status_code)
+```
+
+Get a resource:
+
+```python
+response_dict,OK,http_status_code = client.getResource(kind="Pod",namespace="default", name="busybox")
+print("response_dict: ", response_dict)
+print("is OK: ", OK)
+print("HTTP status code: ", http_status_code)
+```
+
+Delete a resource:
+
+```python
+response_dict,OK,http_status_code = client.deleteResource(kind="Pod", namespace="default",name="busybox")
+print("response_dict: ", response_dict)
+print("is OK: ", OK)
+print("HTTP status code: ", http_status_code)
+```
+
+Watch a resource:
+
+```python
+from kubesys.watcher import KubernetesWatcher
+from kubesys.watch_handler import WatchHandler
+
+def DoAdder(json_dict)->None:
+    print("add pod")
+
+def DoModified(json_dict)->None:
+    print("modifiy pod")
+
+def DoDeleted(json_dict)->None:
+    print("delete pod")
+
+client = KubernetesClient(host_label="default")   
+                                                              # init a client normally
+# you can give the watchHandler by python-function
+watcher = KubernetesWatcher(client,WatchHandler(add_func=DoAdder,modify_func=DoModified,delete_func= DoDeleted))
+# or you can easily give the func-param by python-Lambda
+# watcher = KubernetesWatcher(client,WatchHandler(add_func=lambda json_dict: print("add pod"),modify_func=lambda json_dict: print("modifiy pod"),delete_func=lambda json_dict: print("delete pod")))
+
+client.watchResource(kind="Pod", namespace="default", name="busybox",watcher=watcher)
+```
+
+Show better print for python-dict as json:
+
+```python
+from kubesys.common import goodPrintDict
+
+print(goodPrintDict(response_dict))
+```
+
+
+
+## full-example
+
+see the result in [run-outputs](/out.txt)
+
+```python
+from kubesys.client import KubernetesClient
+from kubesys.common import goodPrintDict
+from kubesys.watcher import KubernetesWatcher
+from kubesys.watch_handler import WatchHandler
+
+def test_CRUD():
+    pod_json= '''{
+                        "apiVersion": "v1",
+                        "kind": "Pod",
+                        "metadata": {
+                            "name": "busybox",
+                            "namespace": "default"
+                        },
+                        "spec": {
+                            "containers": [
+                            {
+                                "image": "busybox",
+                                "env": [{
+                                    "name": "abc",
+                                    "value": "abc"
+                                    }],
+                                "command": [
+                                    "sleep",
+                                    "3600"
+                                    ],
+                                "imagePullPolicy": "IfNotPresent",
+                                "name": "busybox"
+                            }],
+                            "restartPolicy": "Always"
+                        }
+                    }'''
+
+    client = KubernetesClient(host_label="default")
+
+    # test list resources
+    print("--test list resources:")
+    response_dict,OK,http_status_code = client.listResources("Pod")
+    print("response_dict: %s"%(goodPrintDict(response_dict,show_print=False)))
+    print("is OK: ", OK)
+    print("HTTP status code: ", http_status_code,"\n")
+
+    # test create resources
+    print("--test create resources:")
+    response_dict,OK,http_status_code = client.createResource(pod_json)
+    print("response_dict: %s"%(goodPrintDict(response_dict,show_print=False)))
+    print("is OK: ", OK)
+    print("HTTP status code: ", http_status_code,"\n")
+
+    # test get one single Resources
+    print("--test get one single Resources")
+    response_dict,OK,http_status_code = client.getResource(kind="Pod", namespace="default", name="busybox")
+    print("response_dict: %s"%(goodPrintDict(response_dict,show_print=False)))
+    print("is OK: ", OK)
+    print("HTTP status code: ", http_status_code,"\n")
+
+    # test delete pod
+    print("--test delete pod:")
+    response_dict,OK,http_status_code = client.deleteResource(kind="Pod", namespace="default", name="busybox")
+    print("response_dict: %s"%(goodPrintDict(response_dict,show_print=False)))
+    print("is OK: ", OK)
+    print("HTTP status code: ", http_status_code,"\n")
+
+def test_watcher():
+    print("--start to watch...")
+    client = KubernetesClient(host_label="default")
+    watcher = KubernetesWatcher(client,WatchHandler())
+    client.watchResource(kind="Pod", namespace="default", name="busybox",watcher=watcher)
+    
+def main():
+    test_CRUD()
+    test_watcher()
+
+if __name__ == '__main__':
+    main()
+```
---
+++ packages/kubesys/kubesys-1.0.4.tar.gz/kubesys-1.0.4/kubesys/client.py
@@ -0,0 +1,231 @@
+from typing import Union
+from requests import status_codes
+from requests.api import request
+from kubesys.http_request import createRequest,createRequestReturOriginal
+from kubesys.analyzer import KubernetesAnalyzer
+from kubesys.watcher import KubernetesWatcher
+import json
+
+class KubernetesClient():
+    def __init__(self,host_label="default", account_info=None, analyzer=None, verify_SSL=False, json_path="account.json",auto_init=True) -> None:
+        if not account_info:
+            with open("account.json",'r', encoding='UTF-8') as f:
+                account_info_dict = json.load(f)
+                if host_label not in account_info_dict.keys():
+                    print("host label<%s> is not found in %s"%(host_label, json_path))
+                    exit(-2)
+                account_info = account_info_dict[host_label]
+
+        if "URL" in account_info.keys():
+            self.url = account_info["URL"].rstrip("/")
+        else:
+            self.url = ""
+
+        if "Token" in account_info.keys():
+            self.token = account_info["Token"]
+        else:
+            self.token = ""
+
+        if analyzer:
+            self.analyzer = analyzer
+        else:
+            self.analyzer = KubernetesAnalyzer()
+
+        self.verify_SSL = verify_SSL
+        self.http = None
+
+        if auto_init:
+            self.Init()
+
+    def Init(self)->None:
+        self.analyzer.learning(url=self.url, token=self.token, verify_SSL=self.verify_SSL)
+
+    def getNamespace(self,supportNS,value) ->str:
+        if supportNS and len(value)>0:
+            return "namespaces/" + value + "/"
+        
+        return ""
+
+    def getRealKind(self,kind,apiVersion)->str:
+        index = apiVersion.find("/")
+        if index < 0:
+            return kind
+        else:
+            return apiVersion[:index] + "." + kind
+
+    def createResource(self,jsonStr) ->Union[dict,bool,str]:
+        jsonObj = jsonStr
+        if type(jsonObj) is str:
+            jsonObj = json.loads(jsonObj)
+
+        kind = self.getRealKind(str(jsonObj["kind"]),str(jsonObj["apiVersion"]))
+
+        namespace = ""
+        if "namespace" in jsonObj["metadata"].keys():
+            namespace = str(jsonObj["metadata"]["namespace"])
+        
+        url = self.analyzer.FullKindToApiPrefixDict[kind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[kind], namespace)
+        url += self.analyzer.FullKindToNameDict[kind]
+
+        return createRequest(url=url,token=self.token,method="POST",body=jsonStr,keep_json=False)
+
+    def updateResource(self,jsonStr)->Union[dict,bool,str]:
+        jsonObj = jsonStr
+        if type(jsonObj) is str:
+            jsonObj = json.loads(jsonObj)
+            
+        kind = self.getRealKind(str(jsonObj["kind"]),str(jsonObj["apiVersion"]))
+
+        namespace = ""
+        if "namespace" in jsonObj["metadata"].keys():
+            namespace = str(jsonObj["metadata"]["namespace"])
+        
+        url = self.analyzer.FullKindToApiPrefixDict[kind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[kind], namespace)
+        url += self.Analyzer.FullKindToNameMapper[kind] + "/" + jsonObj["metadata"]["name"]
+
+        return createRequest(url=url,token=self.token,method="PUT",body=jsonStr,keep_json=False)
+
+    def checkAndReturnRealKind(self,kind,mapper)->Union[str,str]:
+        index = kind.find(".")
+        if index<0:
+            if len(mapper[kind])==1:
+                return mapper[kind][0], None
+
+            elif len(mapper[kind])==0:
+                return "", "invalid kind"
+
+            else:
+                value = ""
+                for s in mapper[kind]:
+                    value+= "," + s
+
+                return "", "please use fullKind: "+value[1:]
+
+        return kind, None
+
+    def deleteResource(self,kind,namespace,name)->Union[dict,bool,str]:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            return None,error_str
+        
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        url += self.analyzer.FullKindToNameDict[fullKind] + "/" + name
+
+        return createRequest(url=url,token=self.token, method="DELETE",keep_json=False)
+
+    def getResource(self,kind,name,namespace="")->Union[dict,bool,str]:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            return None,error_str
+
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        url += self.analyzer.FullKindToNameDict[fullKind] + "/" + name
+
+        return createRequest(url=url,token=self.token, method="GET",keep_json=False)
+
+    def listResources(self,kind,namespace="")->Union[dict,bool,str]:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            return None,error_str
+
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        url += self.analyzer.FullKindToNameDict[fullKind]
+
+        return createRequest(url=url,token=self.token, method="GET",keep_json=False)
+
+    def bindResource(self,pod,host)->Union[dict,bool,str]:
+        jsonObj = {}
+        jsonObj["apiVersion"] = "v1"
+        jsonObj["kind"] = "Binding"
+
+        meta = {}
+        meta["name"] = pod["metadata"]["name"]
+        meta["namespace"] = pod["metadata"]["namespace"]
+        jsonObj["metadata"] = meta
+
+        target = {}
+        target["apiVersion"] = "v1"
+        target["kind"] = "Node"
+        target["name"] = host
+        jsonObj["target"] = target
+
+        kind = self.getRealKind(pod["kind"], pod["apiVersion"])
+        namespace = pod["metadata"]["namespace"]
+
+        url = self.analyzer.FullKindToApiPrefixDict[kind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[kind], namespace)
+        url += self.analyzer.FullKindToNameDict[kind] + "/"
+        url += pod["metadata"]["name"] + "/binding"
+
+        return createRequest(url=url,token=self.token, method="POST",data=jsonObj, keep_json=False)
+
+    def watchResource(self,kind,namespace,watcher,name=None) ->None:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            print(error_str)
+            return
+
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/watch/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        if name:
+            url += self.analyzer.FullKindToNameDict[fullKind] + "/" + name
+        else:
+            url += self.analyzer.FullKindToNameDict[fullKind]
+
+        watcher.watching(url=url,token=self.token)
+
+    def watchResources(self,kind,namespace,watcher) ->None:
+        self.watchResource(self,kind,namespace,watcher,name=None)
+
+    def updateResourceStatus(self, jsonStr)->Union[dict,bool,str]:
+        jsonObj = jsonStr
+        if type(jsonObj) is str:
+            jsonObj = json.loads(jsonObj)
+        
+        kind = self.getRealKind(jsonObj["kind"], jsonObj["apiVersion"])
+        namespace = ""
+        if "namespace" in jsonObj["metadata"]:
+            namespace = jsonObj["metadata"]["namespace"]
+
+        url = self.analyzer.FullKindToApiPrefixDict[kind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[kind], namespace)
+        url += self.analyzer.FullKindToNameDict[kind] + "/" + jsonObj["metadata"]["name"]
+        url += "/status"
+
+        return createRequest(url=url,token=self.token, method="PUT", body=jsonObj, keep_json=False)
+
+    def listResourcesWithLabelSelector(self,kind, namespace, labels)->Union[dict,bool,str]:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            print(error_str)
+            return
+
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        url += self.analyzer.FullKindToNameDict[fullKind]
+        url += "?labelSelector="
+        for key, value in labels.items():
+            url += key + "%3D" + value + ","
+        url = url[:len(url)-1]
+        return createRequest(url=url,token=self.token, method="GET", keep_json=False)
+
+    def listResourcesWithFieldSelector(self, kind, namespace, fields)->Union[dict,bool,str]:
+        fullKind, error_str = self.checkAndReturnRealKind(kind,self.analyzer.KindToFullKindDict)
+        if error_str:
+            print(error_str)
+            return
+
+        url = self.analyzer.FullKindToApiPrefixDict[fullKind] + "/"
+        url += self.getNamespace(self.analyzer.FullKindToNamespaceDict[fullKind], namespace)
+        url += self.analyzer.FullKindToNameDict[fullKind]
+        url += "?fieldSelector="
+        for key, value in fields.items():
+            url += key + "%3D" + value + ","
+
+        url = url[:len(url)-1]
+        return createRequest(url=url,token=self.token, method="GET", keep_json=False)
