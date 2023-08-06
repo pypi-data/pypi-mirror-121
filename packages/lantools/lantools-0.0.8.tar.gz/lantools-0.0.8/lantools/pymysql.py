54fe59fee4cbd194976068a8bacd72b3f4277a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:03 +0000

    Add package lantools/0.0.8/lantools-0.0.8.tar.gz

---
+++ packages/lantools/lantools-0.0.8.tar.gz/lantools-0.0.8/lantools/pymysql.py
@@ -0,0 +1,25 @@
+import pymysql
+from pymysql.cursors import DictCursor
+
+class Mysql:
+    def __init__(self, *, host, port, db, user, password):
+        self._connect = pymysql.connect( host=host, port=port, db=db, user=user, password=password )
+        self._cursor = self._connect.cursor(DictCursor)
+
+    def ping(self):
+        self._connect.ping(reconnect=True)
+
+    def fetchall(self, sql, args=None):
+        self.ping()
+        self._cursor.execute(sql, args)
+        self._connect.commit()
+        return self._cursor.fetchall()
+
+    def execute(self, sql, args=None):
+        """ update, insert, delete """
+        self.ping()
+        self._cursor.execute(sql, args)
+        self._connect.commit()
+
+    def __del__(self):
+        self._connect.close()
