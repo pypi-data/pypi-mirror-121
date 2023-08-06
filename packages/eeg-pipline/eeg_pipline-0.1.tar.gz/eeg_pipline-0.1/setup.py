f0d16deaa7afddcdaec981c9ab7a30fa0d049c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:38 +0000

    Add package eeg-pipline/0.1/eeg_pipline-0.1.tar.gz

---
+++ packages/eeg-pipline/eeg_pipline-0.1.tar.gz/eeg_pipline-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='eeg_pipline',
+version='0.1',
+description='Testing installation of Package',
+author='Marta Kubinska',
+author_email='mk@optocuetics.com',
+license='MIT',
+packages=['eeg_library'],
+zip_safe=False)
