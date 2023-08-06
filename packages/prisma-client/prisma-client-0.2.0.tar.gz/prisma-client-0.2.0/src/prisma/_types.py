41af72156c9e7a33e07142454a16bc30e2871ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package prisma-client/0.2.0/prisma-client-0.2.0.tar.gz

---
+++ packages/prisma-client/prisma-client-0.2.0.tar.gz/prisma-client-0.2.0/src/prisma/_types.py
@@ -0,0 +1,23 @@
+import sys
+from typing import Callable, Coroutine, TypeVar, Union, List, Dict, Any
+
+from pydantic import BaseModel
+
+
+if sys.version_info >= (3, 8):
+    # pyright: reportUnusedImport=false
+    from typing import (  # pylint: disable=no-name-in-module, unused-import
+        TypedDict,
+        Literal,
+    )
+else:
+    from typing_extensions import TypedDict, Literal
+
+
+Method = Literal['GET', 'POST']
+
+BaseModelT = TypeVar('BaseModelT', bound=BaseModel)
+
+# TODO: use a TypeVar
+FuncType = Callable[..., Any]
+CoroType = Callable[..., Coroutine[Any, Any, Any]]
---
+++ packages/prisma-client/prisma-client-0.2.0.tar.gz/prisma-client-0.2.0/src/prisma/http.py
@@ -0,0 +1,4 @@
+# pyright: reportUnusedImport=false
+
+# TODO: should we default to asynchronous or synchronous http?
+from ._sync_http import HTTP, Response, client
