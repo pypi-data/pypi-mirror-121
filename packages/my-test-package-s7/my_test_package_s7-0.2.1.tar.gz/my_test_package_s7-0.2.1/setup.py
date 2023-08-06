392e038cddc1d91afb4347b23c2621b9b37f012c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:41 +0000

    Add package my-test-package-s7/0.2.1/my_test_package_s7-0.2.1.tar.gz

---
+++ packages/my-test-package-s7/my_test_package_s7-0.2.1.tar.gz/my_test_package_s7-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="my_test_package_s7",
+    version="0.2.1",
+    author="Igor Simagin",
+    author_email="igorsim35@gmail.com",
+    install_requires=[
+        "numpy",
+        "pandas"
+    ]
+)
