9941c5b1d29cd146845ee39d5f5e71e105e910e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:29 +0000

    Add package thjo-distributions/1.2/thjo_distributions-1.2.tar.gz

---
+++ packages/thjo-distributions/thjo_distributions-1.2.tar.gz/thjo_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='thjo_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['thjo_distributions'],
+      author = 'Jonas Theiler',
+      author_email = 'someemail@something.com',
+      zip_safe=False)
