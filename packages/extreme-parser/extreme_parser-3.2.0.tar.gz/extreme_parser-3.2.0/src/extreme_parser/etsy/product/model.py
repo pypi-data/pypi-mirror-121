a5c8e80ca064efd6e6ccaec66a252deb81f33381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:13 +0000

    Add package extreme-parser/3.2.0/extreme_parser-3.2.0.tar.gz

---
+++ packages/extreme-parser/extreme_parser-3.2.0.tar.gz/extreme_parser-3.2.0/src/extreme_parser/etsy/product/model.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Product(BaseModel):
+    title: Optional[str]
+    shop_id: Optional[int]
+    price: Optional[float]
+    favorites: Optional[int]
+    stock: Optional[int]
+    reviews: Optional[int]
+    currency: Optional[str]
