26eb12b8911482ccb87d30325b780c316c43472d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:52 +0000

    Add package pystrategy-test-py/1.0.13/pystrategy_test_py-1.0.13-py3-none-any.whl

---
+++ packages/pystrategy-test-py/pystrategy_test_py-1.0.13-py3-none-any.whl/pystrategy_test_py/resta_function.py
@@ -0,0 +1,8 @@
+class suma_funcion_py:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def resta(a,b):
+        variable_resta = a-b
+        return variable_resta
---
+++ packages/pystrategy-test-py/pystrategy_test_py-1.0.13-py3-none-any.whl/pystrategy_test_py/sum_function.py
@@ -0,0 +1,8 @@
+class suma_funcion_py:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def suma(a,b):
+        variable_suma = a+b
+        return variable_suma
