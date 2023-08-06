b06e9ae3a03cac2a74e611a813d49753610035b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:31 +0000

    Add package emeraldml/0.0.1/emeraldml-0.0.1.tar.gz

---
+++ packages/emeraldml/emeraldml-0.0.1.tar.gz/emeraldml-0.0.1/emerald/optimal_models/__init__.py
@@ -0,0 +1,13 @@
+"""
+Optimal Models
+"""
+
+from ._regressors import OptimalDTRegressor, OptimalKNRegressor, OptimalLinearRegression, OptimalABRegressor, OptimalRFRegressor 
+
+__all__ = [
+    'OptimalDTRegressor', 
+    'OptimalKNRegressor', 
+    'OptimalLinearRegression', 
+    'OptimalABRegressor', 
+    'OptimalRFRegressor'
+]
