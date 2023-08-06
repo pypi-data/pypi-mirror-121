abf461d7bd2016bf14fb96813d3f112689f49606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package sra-py/0.1.1/sra_py-0.1.1.tar.gz

---
+++ packages/sra-py/sra_py-0.1.1.tar.gz/sra_py-0.1.1/sra_py/sra_py/__init__.py
@@ -0,0 +1,11 @@
+'''
+A python wrapper for some-random-api
+
+https://some-random-api.ml
+'''
+
+from .images import *
+from .anime import *
+from .fun import *
+from .pokemon import *
+from .facts import *
