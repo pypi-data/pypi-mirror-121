dcd3746cf91ceb170162e09463c6aaee2ea69789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:50 +0000

    Add package downloader-python/1.10/downloader-python-1.10.tar.gz

---
+++ packages/downloader-python/downloader-python-1.10.tar.gz/downloader-python-1.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+version = '1.10'
+print(f'version:{version}')
+with open('README.md','r',encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='downloader-python',
+    version=version,
+    url='https://github.com',
+    description = 'An fast downloader with ui.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='mc_creater',
+    author_email='guoxiuchen20170402@163.com',
+    license='MIT',
+    packages=['downloader-python','command','pyim','exes','doge'],
+    classifiers=['Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',],
+    python_requires='>=3.6',
+    install_requires=['PyQt5','requests','bs4','lxml','mcpi'],
+    include_package_data=True
+
+)
