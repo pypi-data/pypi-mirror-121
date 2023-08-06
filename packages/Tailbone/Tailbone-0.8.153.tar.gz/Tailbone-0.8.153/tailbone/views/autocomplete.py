c0539bd7e1f16edebde08cc0d1e4f7a0f427b3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:51 +0000

    Add package Tailbone/0.8.153/Tailbone-0.8.153.tar.gz

---
+++ packages/Tailbone/Tailbone-0.8.153.tar.gz/Tailbone-0.8.153/tailbone/views/autocomplete.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8; -*-
+################################################################################
+#
+#  Rattail -- Retail Software Framework
+#  Copyright © 2010-2021 Lance Edgar
+#
+#  This file is part of Rattail.
+#
+#  Rattail is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Rattail is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  Rattail.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+"""
+Autocomplete View
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+import sqlalchemy as sa
+
+from tailbone.views.core import View
+from tailbone.db import Session
+
+
+class AutocompleteView(View):
+    """
+    Base class for generic autocomplete views.
+    """
+
+    def prepare_term(self, term):
+        """
+        If necessary, massage the incoming search term for use with the query.
+        """
+        return term
+
+    def filter_query(self, q):
+        return q
+
+    def make_query(self, term):
+        """
+        Make and return the "complete" query for the given search term.
+        """
+        # we are querying one table (and column) primarily
+        query = Session.query(self.mapped_class)
+        column = getattr(self.mapped_class, self.fieldname)
+
+        # filter according to business logic, if applicable
+        query = self.filter_query(query)
+
+        # filter according to search term(s)
+        criteria = [column.ilike('%{}%'.format(word))
+                    for word in term.split()]
+        query = query.filter(sa.and_(*criteria))
+
+        # sort results by something meaningful
+        query = query.order_by(column)
+        return query
+
+    def query(self, term):
+        return self.make_query(term)
+
+    def display(self, instance):
+        return getattr(instance, self.fieldname)
+
+    def value(self, instance):
+        """
+        Determine the data value for a query result instance.
+        """
+        return instance.uuid
+
+    def get_data(self, term):
+        return self.query(term).all()
+
+    def __call__(self):
+        """
+        View implementation.
+        """
+        term = self.request.params.get(u'term') or u''
+        term = term.strip()
+        if term:
+            term = self.prepare_term(term)
+        if not term:
+            return []
+        results = self.get_data(term)
+        return [{'label': self.display(x), 'value': self.value(x)} for x in results]
