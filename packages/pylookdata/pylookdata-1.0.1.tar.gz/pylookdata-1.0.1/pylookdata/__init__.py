03cd1bbdfa94b47edee560fe8a69a2d6b479d740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:00 +0000

    Add package pylookdata/1.0.1/pylookdata-1.0.1.tar.gz

---
+++ packages/pylookdata/pylookdata-1.0.1.tar.gz/pylookdata-1.0.1/pylookdata/__init__.py
@@ -0,0 +1,3 @@
+from .handler import Config 
+
+__all__ = [Config]
---
+++ packages/pylookdata/pylookdata-1.0.1.tar.gz/pylookdata-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pylookdata',
+      version='1.0.1',
+      description='Control databases configurations',
+      packages=['pylookdata'],
+      author='Phomint',
+      author_email='patrick.dev.atom@gmail.com',
+      zip_safe=False)
