18b2aea083cd5f2cef9077c150e3ff139a4f835c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:56 +0000

    Add package giftsnippet/0.1.0/giftsnippet-0.1.0.tar.gz

---
+++ packages/giftsnippet/giftsnippet-0.1.0.tar.gz/giftsnippet-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+import pkg_resources
+import setuptools
+
+with pathlib.Path('./requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+setuptools.setup(name='giftsnippet',
+      version='0.1.0',
+      description='Integrate highlighted source code images with data uris into your gift questions',
+      keywords='gift moodle code-formatting',
+      url='http://github.com/stefanhuber/giftsnippet',
+      author='Stefan Huber',
+      author_email='mail@stefanhuber.at',
+      license='BSD',
+      packages=['giftsnippet'],
+      install_requires=install_requires,
+      test_suite="giftsnippet.tests",
+      include_package_data=True,
+      zip_safe=False,
+     entry_points={
+         'console_scripts': ['giftsnippet=giftsnippet.console:main'],
+     })
