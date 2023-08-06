4ce3e65a9dda8b32a9eadbd57c96354097709e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:59 +0000

    Add package alertnow-python/1.3.1/alertnow-python-1.3.1.tar.gz

---
+++ packages/alertnow-python/alertnow-python-1.3.1.tar.gz/alertnow-python-1.3.1/logger/src/common/dto/payloadDetailMetadata.py
@@ -0,0 +1,6 @@
+class PayloadDetailMetadata:
+    def __init__(self, filename, function, type, value):
+        self.filename = filename
+        self.function = function
+        self.type = type
+        self.value = value
