551e847d20f3b36a81dac38ff3a01e3e6ac20e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:08 +0000

    Add package run-js/0.0.1/run-js-0.0.1.tar.gz

---
+++ packages/run-js/run-js-0.0.1.tar.gz/run-js-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="run-js",
+    packages=["js"],
+    data_files=[("js", ["js/exists.js", "js/run.js"])],
+    version="0.0.1",
+    description="Goal: The Easiest Way to Run JavaScript in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Daniel J. Dufour",
+    author_email="daniel.j.dufour@gmail.com",
+    url="https://github.com/DanielJDufour/run-js",
+    download_url="https://github.com/DanielJDufour/run-js/tarball/download",
+    keywords=[
+        "functional",
+        "javascript",
+        "python",
+        "package",
+        "library",
+        "universal",
+        "python3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
