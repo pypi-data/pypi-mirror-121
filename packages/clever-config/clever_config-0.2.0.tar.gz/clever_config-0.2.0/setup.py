ee48d93e620fd4561f8e39df05dfa5abefc07361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:01 +0000

    Add package clever-config/0.2.0/clever_config-0.2.0.tar.gz

---
+++ packages/clever-config/clever_config-0.2.0.tar.gz/clever_config-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['clever_config']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<6.0.0', 'attributedict>=0.3.0,<0.4.0']
+
+setup_kwargs = {
+    'name': 'clever-config',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Andrey A. Osipov',
+    'author_email': 'developer.osipov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
