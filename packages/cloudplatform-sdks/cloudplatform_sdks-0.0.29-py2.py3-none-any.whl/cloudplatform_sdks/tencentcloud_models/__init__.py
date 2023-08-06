f063e8d1c52cd975265603dce2d1588bacdbb85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:57 +0000

    Add package cloudplatform-sdks/0.0.29/cloudplatform_sdks-0.0.29-py2.py3-none-any.whl

---
+++ packages/cloudplatform-sdks/cloudplatform_sdks-0.0.29-py2.py3-none-any.whl/cloudplatform_sdks/tencentcloud_models/__init__.py
@@ -0,0 +1,6 @@
+from .clb import TencentClb, TencentClbListener
+from .cdn import TencentCdnDomain
+from .cfs import TencentCfs
+from .vpc import TencentRouteTable, TencentRouteEntry, TencentSubnet
+from .dns import  TencentDnsDomain, TencentDnsRecord
+from .postgres import TencentPostgreSQL
