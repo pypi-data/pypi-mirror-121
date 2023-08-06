56371bb4535f29f29345a520103443f60ddcec48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:37 +0000

    Add package sqlpygen/0.0.3/sqlpygen-0.0.3.tar.gz

---
+++ packages/sqlpygen/sqlpygen-0.0.3.tar.gz/sqlpygen-0.0.3/examples/example1.sql
@@ -0,0 +1,21 @@
+-- module: example1
+
+-- schema: table_stocks
+
+CREATE TABLE stocks (
+    date text,
+    trans text,
+    symbol text,
+    qty real,
+    price real
+) ;
+
+-- query: insert_into_stocks
+-- params: date: str, trans: str, symbol: str, qty: float, price: float
+
+INSERT INTO stocks VALUES (:date, :trans, :symbol, :qty, :price) ;
+
+-- query: select_from_stocks
+-- return: str, str, str, float, float
+
+SELECT * FROM stocks ;
