e9a3834a43a445c0ff2776ddc6d2e4045de5c92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package mons/1.0.4/mons-1.0.4-py3-none-any.whl

---
+++ packages/mons/mons-1.0.4-py3-none-any.whl/mons/clickExt.py
@@ -0,0 +1,209 @@
+import click
+from click._termui_impl import ProgressBar
+
+import shutil
+
+import os
+import sys
+import configparser
+from gettext import ngettext
+
+from .errors import MaybeDefault
+
+class CatchErrorsGroup(click.Group):
+    def main(self, args=None, prog_name=None, complete_var=None, standalone_mode=True, windows_expand_args=True, **extra):
+        debug = False
+        try:
+            debug = '--debug' in sys.argv
+            if debug and not args:
+                sys.argv.remove('--debug')
+            super().main(args=args, prog_name=prog_name, complete_var=complete_var, standalone_mode=standalone_mode, windows_expand_args=windows_expand_args, **extra)
+        except Exception as e:
+            if debug:
+                click.echo('\033[0;31m', nl=False)
+                raise
+            else:
+                click.echo(f'\033[0;31m{type(e).__name__}: {e}\033[0m')
+                click.echo(f'''An unhandled exception was encountered.
+Use the --debug flag to disable clean exception handling.''')
+
+class Install(click.ParamType):
+    name = 'Install'
+
+    def __init__(self, exist: bool=True, resolve_install: bool=False, check_path: bool=True) -> None:
+        super().__init__()
+        self.exist = exist
+        self.resolve_install = resolve_install
+        self.validate_path = check_path
+
+    def convert(self, value, param, ctx):
+        installs: configparser.ConfigParser = ctx.obj.installs
+
+        if self.exist:
+            if not isinstance(value, configparser.SectionProxy):
+                if not installs.has_section(value):
+                    if not param.required:
+                        raise MaybeDefault(default_primary(ctx, param, None))
+                    else:
+                        self.fail(f'Install {value} does not exist.', param, ctx)
+
+                path = installs[value]['Path']
+                if self.validate_path:
+                    error = None
+                    if not os.path.exists(path):
+                        error = 'does not exist.'
+                    elif not os.path.basename(path) == 'Celeste.exe':
+                        error = 'does not point to Celeste.exe'
+
+                    if error:
+                        raise click.UsageError(f'''Install {value} does not have a valid path:
+\033[0;31m{path} {error}\033[0m
+Use the `set-path` command to assign a new path.''', ctx)
+
+                if self.resolve_install:
+                    value = installs[value]
+        else:
+            if installs.has_section(value):
+                self.fail(f'Install {value} already exists.', param, ctx)
+
+        return value
+
+def default_primary(ctx: click.Context, param, value):
+    if value is None:
+        config: configparser.ConfigParser = ctx.obj.config
+        if config.has_option('user', 'PrimaryInstall'):
+            value = config.get('user', 'PrimaryInstall')
+            if isinstance(param.type, Install) and param.type.resolve_install:
+                value = ctx.obj.installs[value]
+        else:
+            ctx.fail('Primary install not found. Use `mons set-primary` to set it.')
+
+    return value
+
+class DefaultArgsCommand(click.Command):
+    """Command type allowing multiple default arguments.
+    
+    ParamTypes must throw a `MaybeDefault` exception containing 
+    the default value for that parameter.
+    Attempts to prepend the `MaybeDefault` value to the argument list,
+    and tries the command again."""
+    def parse_args(self, ctx: click.Context, cmdArgs):
+        if not cmdArgs and self.no_args_is_help and not ctx.resilient_parsing:
+            click.echo(ctx.get_help(), color=ctx.color)
+            ctx.exit()
+
+        parser = self.make_parser(ctx)
+        d_idx = 0
+        while True:
+            opts, args, param_order = parser.parse_args(args=cmdArgs.copy())
+
+            params = click.core.iter_params_for_processing(param_order, self.get_params(ctx))
+            maybe_default = False
+            for param in params:
+                try:
+                    value, args = param.handle_parse_result(ctx, opts, args)
+                except MaybeDefault as d:
+                    cmdArgs.insert(d_idx, d.value)
+                    d_idx += 1
+                    maybe_default = True
+                    break
+
+            if not maybe_default:
+                break
+
+        if args and not ctx.allow_extra_args and not ctx.resilient_parsing:
+            ctx.fail(
+                ngettext(
+                    "Got unexpected extra argument ({args})",
+                    "Got unexpected extra arguments ({args})",
+                    len(args),
+                ).format(args=" ".join(map(str, args)))
+            )
+
+        ctx.args = args
+        return args
+
+class DefaultOption(click.Option):
+    """ Mark this option as being a _default option """
+    register_default = True
+
+    def __init__(self, param_decls=[], **attrs):
+        param_decls = [decl + '_default' for decl in param_decls] or None
+        super(DefaultOption, self).__init__(param_decls, **attrs)
+        self.hidden = True
+
+class ExplicitOption(click.Option):
+    """ Fix the help string for this option to display as an optional argument """
+    def get_help_record(self, ctx):
+        help = super(ExplicitOption, self).get_help_record(ctx)
+        if help:
+            return (help[0].replace(' ', '[=', 1) + ']',) + help[1:]
+
+class CommandWithDefaultOptions(DefaultArgsCommand):
+    """ Command implementation for `DefaultOption` and `ExplicitOption` option types """
+    def parse_args(self, ctx, args):
+        """ Translate any opt to opt_default as needed """
+        options = [o for o in ctx.command.params
+                   if getattr(o, 'register_default', None)]
+        prefixes = {p.replace('_default', '')
+                    for p in sum([o.opts for o in options], [])
+                    if p.startswith('--')}
+        for i, a in enumerate(args):
+            a = a.split('=', 1)
+            if a[0] in prefixes and len(a) == 1:
+                args[i] = a[0] + '_default'
+
+        return super(CommandWithDefaultOptions, self).parse_args(ctx, args)
+
+if os.name == "nt":
+    BEFORE_BAR = "\r"
+    CLEAR_BAR = "\r"
+else:
+    BEFORE_BAR = "\r\033[?25l"
+    CLEAR_BAR = "\033[?25h\r"
+
+class TempProgressBar(ProgressBar):
+    def render_finish(self) -> None:
+        if self.is_hidden:
+            return
+
+        self.file.write(BEFORE_BAR)
+        self.file.write(" " * (shutil.get_terminal_size().columns - len(CLEAR_BAR)))
+        self.file.write(CLEAR_BAR)
+        self.file.flush()
+
+def tempprogressbar(
+    iterable = None,
+    length = None,
+    label = None,
+    show_eta: bool = True,
+    show_percent = None,
+    show_pos: bool = False,
+    item_show_func = None,
+    fill_char: str = "#",
+    empty_char: str = "-",
+    bar_template: str = "%(label)s  [%(bar)s]  %(info)s",
+    info_sep: str = "  ",
+    width: int = 36,
+    file = None,
+    color = None,
+    update_min_steps: int = 1,
+) -> TempProgressBar:
+    color = click.globals.resolve_color_default(color)
+    return TempProgressBar(
+        iterable=iterable,
+        length=length,
+        show_eta=show_eta,
+        show_percent=show_percent,
+        show_pos=show_pos,
+        item_show_func=item_show_func,
+        fill_char=fill_char,
+        empty_char=empty_char,
+        bar_template=bar_template,
+        info_sep=info_sep,
+        file=file,
+        label=label,
+        width=width,
+        color=color,
+        update_min_steps=update_min_steps,
+    )
---
+++ packages/mons/mons-1.0.4-py3-none-any.whl/mons/utils.py
@@ -0,0 +1,251 @@
+import os
+import configparser
+import json
+
+import hashlib
+import zipfile
+import shutil
+from contextlib import contextmanager
+
+import urllib.request
+
+from click import echo, progressbar
+
+import dnfile # https://github.com/malwarefrank/dnfile
+from dnfile.mdtable import AssemblyRefRow
+from pefile import DIRECTORY_ENTRY # https://github.com/erocarrera/pefile
+
+from .config import *
+from .clickExt import tempprogressbar
+
+from typing import Union, cast
+
+VANILLA_HASH = {
+    'f1c4967fa8f1f113858327590e274b69': ('1.4.0.0', 'FNA'),
+    '107cd146973f2c5ec9fb0b4f81c1588a': ('1.4.0.0', 'XNA'),
+}
+
+def fileExistsInFolder(path: str, filename: str, forceName=True, log=False) -> Union[str,None]:
+    installPath = None
+    if os.path.isfile(path):
+        if not forceName or os.path.basename(path) == filename:
+            installPath = path
+        elif log:
+            echo(f'Error: file `{installPath}` must be called {filename}')
+    elif os.path.isdir(path):
+        if os.path.isfile(os.path.join(path, filename)):
+            installPath = os.path.join(path, filename)
+        elif log:
+            echo(f'Error: {filename} file could not be found in `{installPath}`')
+    elif log:
+        echo(f'Error: `{path}` could not be resolved')
+    return installPath
+
+def getMD5Hash(path: str) -> str:
+    with open(path, "rb") as f:
+        file_hash = hashlib.md5()
+        chunk = f.read(8129)
+        while chunk:
+            file_hash.update(chunk)
+            chunk = f.read(8129)
+    return file_hash.hexdigest()
+
+def unpack(zip: zipfile.ZipFile, root: str, prefix='', label='Extracting'):
+    totalSize = 0
+    for zipinfo in zip.infolist():
+        if not prefix or zipinfo.filename.startswith(prefix):
+            totalSize += zipinfo.file_size
+
+    with progressbar(length=totalSize, label=label) as bar:
+        for zipinfo in zip.infolist():
+            if not prefix or zipinfo.filename.startswith(prefix):
+                zip.extract(zipinfo, root)
+                bar.update(zipinfo.file_size)
+
+# shutils.copytree(dirs_exist_ok) replacement https://stackoverflow.com/a/15824216
+def copy_recursive_force(src, dest, ignore=None):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = os.listdir(src)
+        if ignore is not None:
+            ignored = ignore(src, files)
+        else:
+            ignored = set()
+        for f in files:
+            if f not in ignored:
+                copy_recursive_force(os.path.join(src, f),
+                                    os.path.join(dest, f),
+                                    ignore)
+    else:
+        shutil.copyfile(src, dest)
+
+def isUnchanged(src, dest, file):
+    srcFile = os.path.join(src, file)
+    destFile = os.path.join(dest, file)
+    if os.path.exists(destFile):
+        return os.stat(destFile).st_mtime - os.stat(srcFile).st_mtime >= 0
+    return False
+
+@contextmanager
+def relocated_file(src, dest):
+    file = shutil.move(src, dest)
+    try:
+        yield file
+    finally:
+        shutil.move(file, src)
+
+@contextmanager
+def copied_file(src, dest):
+    file = shutil.copy(src, dest)
+    try:
+        yield file
+    finally:
+        os.remove(file)
+
+def getCelesteVersion(path, hash=None):
+    hash = hash or getMD5Hash(path)
+    version = VANILLA_HASH.get(hash, '')
+    if version:
+        return version, True
+
+    orig_path = os.path.join(os.path.dirname(path), 'orig', 'Celeste.exe')
+    if os.path.isfile(orig_path):
+        hash = getMD5Hash(orig_path)
+        version = VANILLA_HASH.get(hash, '')
+        if version:
+            return version, False
+
+    return None, False
+
+def parseExeInfo(path):
+    echo('Loading exe...\r', nl=False)
+    pe = dnfile.dnPE(path, fast_load=True)
+    pe.parse_data_directories(directories=DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR'])
+    stringHeap: dnfile.stream.StringsHeap = pe.net.metadata.streams_list[1]
+
+    hasEverest = False
+    everestBuild = None
+
+    heapSize = stringHeap.sizeof()
+    i = 0
+    with tempprogressbar(length=heapSize, label='Scanning exe') as bar:
+        while i < len(stringHeap.__data__):
+            string = stringHeap.get(i)
+            if string is None:
+                break
+            if string == 'EverestModule':
+                hasEverest = True
+            if str(string).startswith('EverestBuild'):
+                everestBuild = string[len('EverestBuild'):]
+                hasEverest = True
+                break
+            inc = max(len(string), 1)
+            i += inc
+            bar.update(inc)
+
+    assemRef = pe.net.mdtables.AssemblyRef
+    framework = 'FNA' if any(cast(AssemblyRefRow, row).Name == 'FNA' for row in assemRef.rows) else 'XNA'
+
+    return hasEverest, everestBuild, framework
+
+def getInstallInfo(userInfo: UserInfo, install: str) -> configparser.SectionProxy:
+    path = userInfo.installs[install]['Path']
+    mainHash = getMD5Hash(path)
+    if userInfo.cache.has_section(install) and userInfo.cache[install].get('Hash', '') == mainHash:
+        return userInfo.cache[install]
+
+    if mainHash in VANILLA_HASH:
+        version, framework = VANILLA_HASH[mainHash]
+        info = {
+            'Everest': False,
+            'CelesteVersion': version,
+            'Framework': framework,
+            # EverestBuild: None
+        }
+    else:
+        hasEverest, everestBuild, framework = parseExeInfo(path)
+        info = {}
+        if hasEverest:
+            info['Everest'] = True
+            if everestBuild:
+                info['EverestBuild'] = everestBuild
+
+            origHash = getMD5Hash(os.path.join(os.path.dirname(path), 'orig', 'Celeste.exe'))
+            if origHash in VANILLA_HASH:
+                info['CelesteVersion'], _ = VANILLA_HASH[origHash]
+        else:
+            info['Everest'] = False
+
+        info['Framework'] = framework
+
+    info['Hash'] = mainHash
+    userInfo.cache[install] = info # otherwise it makes all keys in info lowercase
+    return userInfo.cache[install]
+
+def buildVersionString(installInfo: configparser.SectionProxy) -> str:
+    versionStr = installInfo.get('CelesteVersion', 'unknown')
+    framework = installInfo.get('Framework', None)
+    if framework:
+        versionStr += f'-{framework.lower()}'
+    everestBuild = installInfo.get('EverestBuild', None)
+    if everestBuild:
+        versionStr += f' + 1.{everestBuild}.0'
+    else:
+        hasEverest = installInfo.get('Everest', None)
+        if hasEverest:
+            versionStr += ' + Everest(unknown version)'
+    return versionStr
+
+def updateCache(userInfo: UserInfo, install: str):
+    path = userInfo.installs[install]['Path']
+    newHash = getMD5Hash(path)
+
+    celesteversion, vanilla = getCelesteVersion(path)
+    userInfo.cache[install] = {
+        'Hash': newHash,
+        'Everest': not vanilla,
+    }
+
+    if celesteversion:
+        userInfo.cache[install]['CelesteVersion'] = celesteversion
+    pass
+
+def parseVersionSpec(string: str):
+    if string.startswith('1.') and string.endswith('.0'):
+        string = string[2:-2]
+    if string.isdigit():
+        buildnumber = int(string)
+    else:
+        buildnumber = getLatestBuild(string)
+
+    return buildnumber
+
+def getLatestBuild(branch: str):
+    base_URL = 'https://dev.azure.com/EverestAPI/Everest/_apis/build/builds?'
+    filters = [
+        'statusFilter=completed',
+        'resultFilter=succeeded',
+        'branchName={0}'.format(branch
+            if branch == '' or branch.startswith(('refs/heads/', 'refs/pull/'))
+            else 'refs/heads/' + branch),
+        'api-version=6.0',
+        '$top=1',
+    ]
+    request = base_URL + '&'.join(filters)
+    response = json.load(urllib.request.urlopen(request))
+    if response['count'] < 1:
+        return None
+    elif response['count'] > 1:
+        raise Exception('Unexpected number of builds: {0}'.format(response['count']))
+
+    build = response['value'][0]
+    id = build['id']
+    try:
+        return int(id) + 700
+    except:
+        pass
+    return None
+
+def getBuildDownload(build: int, artifactName: str='olympus-build'):
+    return urllib.request.urlopen(f'https://dev.azure.com/EverestAPI/Everest/_apis/build/builds/{build - 700}/artifacts?artifactName={artifactName}&api-version=6.0&%24format=zip')
