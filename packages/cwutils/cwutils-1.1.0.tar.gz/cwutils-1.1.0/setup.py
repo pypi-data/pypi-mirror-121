e1f8de6de86d82a2e1cecc0096b6d62ab3ffa35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:03 +0000

    Add package cwutils/1.1.0/cwutils-1.1.0.tar.gz

---
+++ packages/cwutils/cwutils-1.1.0.tar.gz/cwutils-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='cwutils',
+	version='1.1.0',
+	description='... simple utils',
+	author='Chaos and whoami.exe',
+	author_email='nonono@nono.no',
+	url='https://web.kgbot.pp.ua',
+	install_requires = [
+		'humanfriendly'
+	],
+	packages=['cwutils']
+)
