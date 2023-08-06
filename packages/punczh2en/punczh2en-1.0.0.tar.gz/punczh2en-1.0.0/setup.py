466437b5fb8ae69b661e9c13b9ea285cc1c96a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:57 +0000

    Add package punczh2en/1.0.0/punczh2en-1.0.0.tar.gz

---
+++ packages/punczh2en/punczh2en-1.0.0.tar.gz/punczh2en-1.0.0/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='punczh2en',
+      version='1.0.0',
+      author='Malchut',
+      author_email='',
+      url='',
+      packages=['punczh2en'],
+      )
