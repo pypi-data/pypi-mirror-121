0579a230c99001c1a75cbdd743ba65abe5b52c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:10 +0000

    Add package file-remark-icexmoon/0.0.4/file-remark-icexmoon-0.0.4.tar.gz

---
+++ packages/file-remark-icexmoon/file-remark-icexmoon-0.0.4.tar.gz/file-remark-icexmoon-0.0.4/src/file_remark/user_exception.py
@@ -0,0 +1,14 @@
+class UserException (Exception):
+    CODE_NO_PATH = 1  # 文件或目录不存在
+    CODE_DB = 2 # 数据库错误
+
+    def __init__(self, errorCode: int, errorMsg: str = ""):
+        super().__init__()
+        self.errorCode = errorCode
+        self.errorMsg = errorMsg
+
+    def getErrorCode(self):
+        return self.errorCode
+
+    def getErrorMsg(self):
+        return self.errorMsg
