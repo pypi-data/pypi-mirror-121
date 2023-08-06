c6aff12fbba9d2e06964b07f1c7c44687c8a8a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:41 +0000

    Add package lvsfunc/0.3.10/lvsfunc-0.3.10-py3-none-any.whl

---
+++ packages/lvsfunc/lvsfunc-0.3.10-py3-none-any.whl/lvsfunc/aa.py
@@ -0,0 +1,268 @@
+"""
+    Functions for various anti-aliasing functions and wrappers.
+"""
+from typing import Any, Callable, Dict, Optional
+
+import vapoursynth as vs
+from vsutil import get_w, get_y
+
+from . import kernels, util
+from .util import scale_thresh
+
+core = vs.core
+
+
+def clamp_aa(src: vs.VideoNode, weak: vs.VideoNode, strong: vs.VideoNode, strength: float = 1) -> vs.VideoNode:
+    """
+    Clamp stronger AAs to weaker AAs.
+    Useful for clamping upscaled_sraa or eedi3 to nnedi3 for a strong but precise AA.
+
+    Stolen from Zastin.
+
+    :param src:      Non-AA'd source clip.
+    :param weak:     Weakly-AA'd clip (eg: nnedi3)
+    :param strong:   Strongly-AA'd clip (eg: eedi3)
+    :param strength: Clamping strength (Default: 1)
+
+    :return:         Clip with clamped anti-aliasing.
+    """
+    if src.format is None or weak.format is None or strong.format is None:
+        raise ValueError("clamp_aa: 'Variable-format clips not supported'")
+    thr = strength * (1 << (src.format.bits_per_sample - 8)) if src.format.sample_type == vs.INTEGER \
+        else strength/219
+    clamp = core.std.Expr([get_y(src), get_y(weak), get_y(strong)],
+                          expr=f"x y - x z - xor x x y - abs x z - abs < z y {thr} + min y {thr} - max z ? ?"
+                          if thr != 0 else "x y z min max y z max min")
+    return clamp if src.format.color_family == vs.GRAY \
+        else core.std.ShufflePlanes([clamp, src], planes=[0, 1, 2], colorfamily=vs.YUV)
+
+
+def taa(clip: vs.VideoNode, aafun: Callable[[vs.VideoNode], vs.VideoNode]) -> vs.VideoNode:
+    """
+    Perform transpose AA.
+    Example for nnedi3cl: taa(clip, nnedi3(opencl=True))
+
+    :param clip:   Input clip.
+    :param aafun:  Antialiasing function
+
+    :return:       Antialiased clip
+    """
+    if clip.format is None:
+        raise ValueError("taa: 'Variable-format clips not supported'")
+
+    y = get_y(clip)
+
+    aa = aafun(y.std.Transpose())
+    aa = aa.resize.Spline36(height=clip.width, src_top=0.5).std.Transpose()
+    aa = aafun(aa)
+    aa = aa.resize.Spline36(height=clip.height, src_top=0.5)
+
+    return aa if clip.format.color_family == vs.GRAY \
+        else core.std.ShufflePlanes([aa, clip], planes=[0, 1, 2], colorfamily=vs.YUV)
+
+
+def nnedi3(opencl: bool = False, **override: Any) -> Callable[[vs.VideoNode], vs.VideoNode]:
+    """
+    Generate nnedi3 antialiaser.
+
+    Dependencies:
+
+    * vapoursynth-nnedi3
+    * vapoursynth-NNEDI3CL (Optional: opencl)
+
+    :param opencl:   Use OpenCL (Default: False)
+    :param override: nnedi3 parameter overrides
+
+    :return:         Configured nnedi3 function
+    """
+    nnedi3_args: Dict[str, Any] = dict(field=0, dh=True, nsize=3, nns=3, qual=1)
+    nnedi3_args.update(override)
+
+    def _nnedi3(clip: vs.VideoNode) -> vs.VideoNode:
+        return clip.nnedi3cl.NNEDI3CL(**nnedi3_args) if opencl \
+            else clip.nnedi3.nnedi3(**nnedi3_args)
+
+    return _nnedi3
+
+
+def eedi3(opencl: bool = False, **override: Any) -> Callable[[vs.VideoNode], vs.VideoNode]:
+    """
+    Generate eedi3 antialiaser.
+
+    Dependencies:
+
+    * vapoursynth-EEDI3
+
+    :param opencl:   Use OpenCL (Default: False)
+    :param override: eedi3 parameter overrides
+
+    :return:         Configured eedi3 function
+    """
+    eedi3_args: Dict[str, Any] = dict(field=0, dh=True, alpha=0.25, beta=0.5, gamma=40, nrad=2, mdis=20)
+    eedi3_args.update(override)
+
+    def _eedi3(clip: vs.VideoNode) -> vs.VideoNode:
+        return clip.eedi3m.EEDI3CL(**eedi3_args) if opencl \
+            else clip.eedi3m.EEDI3(**eedi3_args)
+
+    return _eedi3
+
+
+def nneedi3_clamp(clip: vs.VideoNode, strength: float = 1,
+                  mask: Optional[vs.VideoNode] = None,
+                  mthr: float = 0.25, opencl: bool = False) -> vs.VideoNode:
+    """
+    A function that clamps eedi3 to nnedi3 for the purpose of reducing eedi3 artifacts.
+    This should fix every issue created by eedi3. For example: https://i.imgur.com/hYVhetS.jpg
+
+    Original function written by Zastin, modified by LightArrowsEXE.
+
+    :param clip:                Input clip
+    :param strength:            Set threshold strength for over/underflow value for clamping eedi3's result
+                                to nnedi3 +/- strength * 256 scaled to 8 bit (Default: 1)
+    :param mask:                Clip to use for custom mask (Default: None)
+    :param mthr:                Binarize threshold for the mask, scaled to float (Default: 0.25)
+    :param opencl:              OpenCL acceleration (Default: False)
+
+    :return:                    Antialiased clip
+    """
+    if clip.format is None:
+        raise ValueError("nneedi3_clamp: 'Variable-format clips not supported'")
+    y = get_y(clip)
+    mask = mask or y.std.Prewitt().std.Binarize(scale_thresh(mthr, y)).std.Maximum().std.Convolution([1]*9)
+    merged = core.std.MaskedMerge(y,
+                                  clamp_aa(y, taa(y, nnedi3(opencl=opencl)), taa(y, eedi3(opencl=opencl)), strength),
+                                  mask)
+    return merged if clip.format.color_family == vs.GRAY else core.std.ShufflePlanes([merged, clip], [0, 1, 2], vs.YUV)
+
+
+def transpose_aa(clip: vs.VideoNode,
+                 eedi3: bool = False,
+                 rep: int = 13) -> vs.VideoNode:
+    """
+    Function that performs anti-aliasing over a clip by using nnedi3/eedi3 and transposing multiple times.
+    This results in overall stronger anti-aliasing.
+    Useful for shows like Yuru Camp with bad lineart problems.
+
+    Original function written by Zastin, modified by LightArrowsEXE.
+
+    Dependencies:
+
+    * RGSF (optional: 32 bit clip)
+    * vapoursynth-EEDI3
+    * vapoursynth-nnedi3
+    * znedi3
+
+    :param clip:      Input clip
+    :param eedi3:     Use eedi3 for the interpolation (Default: False)
+    :param rep:       Repair mode. Pass it 0 to not repair (Default: 13)
+
+    :return:          Antialiased clip
+    """
+    if clip.format is None:
+        raise ValueError("transpose_aa: 'Variable-format clips not supported'")
+
+    clip_y = get_y(clip)
+
+    def _aafun(clip: vs.VideoNode) -> vs.VideoNode:
+        return clip.eedi3m.EEDI3(0, 1, 0, 0.5, 0.2).znedi3.nnedi3(1, 0, 0, 3, 4, 2) if eedi3 \
+            else clip.nnedi3.nnedi3(0, 1, 0, 3, 3, 2).nnedi3.nnedi3(1, 0, 0, 3, 3, 2)
+
+    def _taa(clip: vs.VideoNode) -> vs.VideoNode:
+        aa = _aafun(clip.std.Transpose())
+        aa = aa.resize.Bicubic(clip.height, clip.width, src_top=.5).std.Transpose()
+        aa = _aafun(aa)
+        return aa.resize.Bicubic(clip.width, clip.height, src_top=.5)
+
+    def _csharp(flt: vs.VideoNode, clip: vs.VideoNode) -> vs.VideoNode:
+        blur = core.std.Convolution(flt, [1] * 9)
+        return core.std.Expr([flt, clip, blur], 'x y < x x + z - x max y min x x + z - x min y max ?')
+
+    aaclip = _taa(clip_y)
+    aaclip = _csharp(aaclip, clip_y)
+    aaclip = util.pick_repair(clip_y)(aaclip, clip_y, rep)
+
+    return aaclip if clip.format.color_family is vs.GRAY else core.std.ShufflePlanes([aaclip, clip], [0, 1, 2], vs.YUV)
+
+
+def _nnedi3_supersample(clip: vs.VideoNode, width: int, height: int, opencl: bool = False) -> vs.VideoNode:
+    nnargs: Dict[str, Any] = dict(field=0, dh=True, nsize=0, nns=4, qual=2)
+    _nnedi3 = nnedi3(opencl=opencl, **nnargs)
+    up_y = _nnedi3(get_y(clip))
+    up_y = up_y.resize.Spline36(height=height, src_top=0.5).std.Transpose()
+    up_y = _nnedi3(up_y)
+    up_y = up_y.resize.Spline36(height=width, src_top=0.5)
+    return up_y
+
+
+def _eedi3_singlerate(clip: vs.VideoNode) -> vs.VideoNode:
+    eeargs: Dict[str, Any] = dict(field=0, dh=False, alpha=0.2, beta=0.6, gamma=40, nrad=2, mdis=20)
+    nnargs: Dict[str, Any] = dict(field=0, dh=False, nsize=0, nns=4, qual=2)
+    y = get_y(clip)
+    return eedi3(sclip=nnedi3(**nnargs)(y), **eeargs)(y)
+
+
+def upscaled_sraa(clip: vs.VideoNode,
+                  rfactor: float = 1.5,
+                  width: Optional[int] = None, height: Optional[int] = None,
+                  supersampler: Callable[[vs.VideoNode, int, int], vs.VideoNode] = _nnedi3_supersample,
+                  downscaler: Optional[Callable[[vs.VideoNode, int, int], vs.VideoNode]]
+                  = kernels.Bicubic(b=0, c=1/2).scale,
+                  aafun: Callable[[vs.VideoNode], vs.VideoNode] = _eedi3_singlerate) -> vs.VideoNode:
+    """
+    A function that performs a supersampled single-rate AA to deal with heavy aliasing and broken-up lineart.
+    Useful for heavy antialiasing.
+
+    It works by supersampling the clip, performing AA, and then downscaling again.
+    Downscaling can be disabled by setting `downscaler` to `None`, returning the supersampled luma clip.
+    The dimensions of the downscaled clip can also be adjusted by setting `height` or `width`.
+    Setting either `height` or `width` will also scale the chroma accordingly.
+
+    Original function written by Zastin, heavily modified by LightArrowsEXE.
+
+    Alias for this function is `lvsfunc.sraa`.
+
+    Dependencies:
+
+    * vapoursynth-eedi3 (default aafun)
+    * vapoursynth-nnedi3 (default supersampler and aafun)
+
+    :param clip:            Input clip
+    :param rfactor:         Image enlargement factor. 1.3..2 makes it comparable in strength to vsTAAmbk
+                            It is not recommended to go below 1.3 (Default: 1.5)
+    :param width:           Target resolution width. If None, determined from `height`
+    :param height:          Target resolution height (Default: ``clip.height``)
+    :param supersampler:    Supersampler used for upscaling before AA (Default: nnedi3 supersampler)
+    :param downscaler:      Downscaler to use after supersampling (Default: Bicubic(b=0, c=1/2)
+    :param aafun:           Function used to antialias after supersampling (Default: eedi3 with nnedi3 sclip)
+
+    :return:                Antialiased clip
+    """
+    if clip.format is None:
+        raise ValueError("upscaled_sraa: 'Variable-format clips not supported'")
+
+    luma = get_y(clip)
+
+    if rfactor < 1:
+        raise ValueError("upscaled_sraa: '\"rfactor\" must be above 1'")
+
+    ssw = round(clip.width * rfactor)
+    ssw = (ssw + 1) & ~1
+    ssh = round(clip.height * rfactor)
+    ssh = (ssh + 1) & ~1
+
+    height = height or clip.height
+
+    if width is None:
+        width = clip.width if height == clip.height else get_w(height, aspect_ratio=clip.width / clip.height)
+
+    up_y = supersampler(luma, ssw, ssh)
+
+    aa_y = aafun(aafun(up_y).std.Transpose())
+
+    scaled = aa_y if downscaler is None else downscaler(aa_y, width, height)
+
+    if clip.format.num_planes == 1 or downscaler is None or height != clip.height or width != clip.width:
+        return scaled
+
+    return core.std.ShufflePlanes([scaled, clip], planes=[0, 1, 2], colorfamily=vs.YUV)
---
+++ packages/lvsfunc/lvsfunc-0.3.10-py3-none-any.whl/lvsfunc/deblock.py
@@ -0,0 +1,181 @@
+"""
+    Deblocking functions.
+"""
+from functools import partial
+from typing import Any, Optional, Sequence, Tuple, Union
+
+import vapoursynth as vs
+from vsutil import depth
+
+from .types import Matrix
+from .util import get_prop
+
+core = vs.core
+
+
+dpir_warning = "vsdpir and pytorch are required for this function. " + \
+               "vsdpir can be installed with 'pip install vsdpir'."
+
+
+def autodb_dpir(clip: vs.VideoNode, edgevalue: int = 24,
+                strs: Sequence[float] = [30, 50, 75],
+                thrs: Sequence[Tuple[float, float, float]] = [(1.5, 2.0, 2.0), (3.0, 4.5, 4.5), (5.5, 7.0, 7.0)],
+                matrix: Optional[Union[Matrix, int]] = None,
+                cuda: bool = True, device_index: int = 0,
+                write_props: bool = False
+                ) -> vs.VideoNode:
+    """
+    A rewrite of fvsfunc.AutoDeblock that uses vspdir instead of dfttest to deblock.
+
+    This function checks for differences between a frame and an edgemask with some processing done on it,
+    and for differences between the current frame and the next frame.
+    For frames where both thresholds are exceeded, it will perform deblocking at a specified strength.
+    This will ideally be frames that show big temporal *and* spatial inconsistencies.
+
+    Thresholds and calculations are added to the frameprops to use as reference when setting the thresholds.
+
+    Keep in mind that vsdpir is not perfect; it may cause weird, black dots to appear sometimes.
+    If that happens, you can perform a denoise on the original clip (maybe even using vsdpir's denoising mode)
+    and grab the brightest pixels from your two clips. That should return a perfectly fine clip.
+
+    Thanks Vardë, louis, setsugen_no_ao!
+
+    Dependencies:
+
+    * vs-dpir
+
+    :param clip:            Input clip
+    :param edgevalue:       Remove edges from the edgemask that exceed this threshold (higher means more edges removed)
+    :param strs:            A list of DPIR strength values (higher means stronger deblocking).
+                            You can pass any arbitrary number of values here.
+                            The amount of values in strs and thrs need to be equal.
+    :param thrs:            A list of thresholds, written as [(EdgeValRef, NextFrameDiff, PrevFrameDiff)].
+                            You can pass any arbitrary number of values here.
+                            The amount of values in strs and thrs need to be equal.
+    :param matrix:          Enum for the matrix of the input clip. See ``types.Matrix`` for more info.
+                            If `None`, gets matrix from the "_Matrix" prop of the clip
+    :param cuda:            Device type used for deblocking. Uses CUDA if True, else CPU
+    :param device_index:    The 'device_index' + 1º device of type device type in the system
+    :write_props            Will write verbose props
+
+    :return:                Deblocked clip
+    """
+    try:
+        from vsdpir import DPIR
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(f"autodb_dpir: {dpir_warning}")
+
+    if clip.format is None:
+        raise ValueError("autodb_dpir: 'Variable-format clips not supported'")
+
+    def _eval_db(n: int, f: Sequence[vs.VideoFrame],
+                 clip: vs.VideoNode, db_clips: Sequence[vs.VideoNode],
+                 nthrs: Sequence[Tuple[float, float, float]]) -> vs.VideoNode:
+
+        evref_diff, y_next_diff, y_prev_diff = [
+            get_prop(f[i], prop, float)
+            for i, prop in zip(range(3), ['EdgeValRefDiff', 'YNextDiff', 'YPrevDiff'])
+        ]
+        f_type = get_prop(f[0], '_PictType', bytes).decode('utf-8')
+
+        if f_type == 'I':
+            y_next_diff = (y_next_diff + evref_diff) / 2
+
+        out = clip
+        nthr_used = (-1., ) * 3
+        for dblk, nthr in zip(db_clips, nthrs):
+            if all(p > t for p, t in zip([evref_diff, y_next_diff, y_prev_diff], nthr)):
+                out = dblk
+                nthr_used = nthr
+
+        if write_props:
+            for prop_name, prop_val in zip(
+                ['Adb_EdgeValRefDiff', 'Adb_YNextDiff', 'Adb_YPrevDiff',
+                 'Adb_EdgeValRefDiffThreshold', 'Adb_YNextDiffThreshold', 'Adb_YPrevDiffThreshold'],
+                [evref_diff, y_next_diff, y_prev_diff] + list(nthr_used)
+            ):
+                out = out.std.SetFrameProp(prop_name, floatval=max(prop_val * 255, -1))
+
+        return out
+
+    if len(strs) != len(thrs):
+        raise ValueError('autodb_dpir: You must pass an equal amount of values to '
+                         f'strenght {len(strs)} and thrs {len(thrs)}!')
+
+    nthrs = [tuple(x / 255 for x in thr) for thr in thrs]
+
+    rgb = core.resize.Bicubic(clip, format=vs.RGBS, matrix_in=matrix) \
+        .std.SetFrameProp('Adb_DeblockStrength', intval=0)
+
+    assert rgb.format
+
+    maxvalue = (1 << rgb.format.bits_per_sample) - 1
+    evref = core.std.Prewitt(rgb)
+    evref = core.std.Expr(evref, f"x {edgevalue} >= {maxvalue} x ?")
+    evref_rm = evref.std.Median().std.Convolution(matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
+
+    diffevref = core.std.PlaneStats(evref, evref_rm, prop='EdgeValRef')
+    diffnext = core.std.PlaneStats(rgb, rgb.std.DeleteFrames([0]), prop='YNext')
+    diffprev = core.std.PlaneStats(rgb, rgb[0] + rgb, prop='YPrev')
+
+    db_clips = [
+        DPIR(rgb, strength=st, task='deblock', device_type='cuda' if cuda else 'cpu',
+             device_index=device_index).std.SetFrameProp('Adb_DeblockStrength', intval=int(st)) for st in strs
+    ]
+
+    debl = core.std.FrameEval(
+        rgb, partial(_eval_db, clip=rgb, db_clips=db_clips, nthrs=nthrs),
+        prop_src=[diffevref, diffnext, diffprev]
+    )
+
+    return core.resize.Bicubic(debl, format=clip.format.id, matrix=matrix)
+
+
+def vsdpir(clip: vs.VideoNode, strength: int = 25, mode: str = 'deblock',
+           matrix: Optional[Union[Matrix, int]] = None,
+           cuda: bool = True, device_index: int = 0,
+           i444: bool = False, **vsdpir_args: Any) -> vs.VideoNode:
+    """
+    A simple vs-dpir wrapper for convenience.
+
+    Converts to RGB -> runs vs-dpir -> converts back to original format.
+    For more information, see https://github.com/cszn/DPIR.
+
+    Dependencies:
+
+    * vs-dpir
+
+    :param clip:            Input clip
+    :param strength:        vs-dpir strength.
+                            Sane values lie between 20-50 for ``mode='deblock'``, and 2-5 for ``mode='denoise'``
+    :param mode:            vs-dpir mode. Valid modes are 'deblock' and 'denoise'.
+    :param matrix:          Enum for the matrix of the input clip. See ``types.Matrix`` for more info.
+                            If `None`, gets matrix from the "_Matrix" prop of the clip
+    :param cuda:            Use CUDA if True, else CPU
+    :param i444:            Forces the returned clip to be YUV444PS instead of the input clip's format
+    :param vsdpir_args:     Additional args to pass onto vs-dpir
+                            (Note: strength, task, and device_type can't be overridden!)
+
+    :return:                Deblocked or denoised clip in either the given clip's format or YUV444PS
+    """
+    try:
+        from vsdpir import DPIR
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(f"vsdpir: {dpir_warning}")
+
+    if clip.format is None:
+        raise ValueError("vsdpir: 'Variable-format clips not supported'")
+
+    # TO-DO: Add a check to see if the models have been fully downloaded
+    # and if not, run the installer included in vsdpir?
+
+    vsdpir_args |= dict(strength=strength, task=mode, device_type='cuda' if cuda else 'cpu')
+
+    clip_rgb = depth(clip, 32).std.SetFrameProp('_Matrix', intval=matrix)
+    clip_rgb = core.resize.Bicubic(clip_rgb, format=vs.RGBS)
+
+    run_dpir = DPIR(clip_rgb, **vsdpir_args)
+
+    if i444:
+        return core.resize.Bicubic(run_dpir, format=vs.YUV444PS, matrix=matrix)
+    return core.resize.Bicubic(run_dpir, format=clip.format.id, matrix=matrix)
