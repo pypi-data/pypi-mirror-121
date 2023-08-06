a42bf2e74a229b4e6c87b23d4a46225b898d8c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:36 +0000

    Add package mice/0.1.0/mice-0.1.0.tar.gz

---
+++ packages/mice/mice-0.1.0.tar.gz/mice-0.1.0/mice/plot_config.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+def plot_config():
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['text.latex.preamble'] = [
+        r'\usepackage{amsfonts}'
+        r'\usepackage{amsthm}'
+        r'\usepackage{amsmath}'
+        r'\usepackage{amssymb}'][0]
+    matplotlib.rcParams['font.family'] = 'serif'
+    matplotlib.rcParams['font.serif'] = 'Computer Modern'
+    matplotlib.rcParams['xtick.labelsize'] = 12
+    matplotlib.rcParams['ytick.labelsize'] = 12
+    matplotlib.rcParams.update({'font.size': 12})
+    matplotlib.rcParams['axes.linewidth'] = 1
+    matplotlib.rcParams['axes.facecolor'] = 'white'
+    matplotlib.rcParams['axes.grid'] = True
+    matplotlib.rcParams['grid.linestyle'] = '--'
+    matplotlib.rcParams['grid.color'] = 'grey'
+    matplotlib.rcParams['grid.linewidth'] = 0.5
+    matplotlib.rc('axes', edgecolor='black')
+
+    plt.rc('font', family='sans-serif')
