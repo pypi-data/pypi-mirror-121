3a30c0d96efdd31b527ee2946cdaa3949d7e99ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:12 +0000

    Add package labeller/0.1/labeller-0.1.tar.gz

---
+++ packages/labeller/labeller-0.1.tar.gz/labeller-0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+with open('PyPI.md') as f:
+    long_description = f.read()
+
+setup(name='labeller',
+        packages=['labeller'],
+        version='0.1',
+        description='Create image labelling software for machine learning applications.',
+        license='MIT',
+        author='Marcus D. Bloice',
+        author_email='marcus.bloice@medunigraz.at',
+        url='https://github.com/mdbloice/Labeller',
+        install_requires=[
+        'flask>=2.0',
+        'flask-bootstrap>=3.3'
+        ],
+        # include_package_data=True,  # For the MANIFEST.in file...
+        package_data={'': ['resources/*']}
+        #scripts=['capitalize/bin/cap_script.py'],
+    )
