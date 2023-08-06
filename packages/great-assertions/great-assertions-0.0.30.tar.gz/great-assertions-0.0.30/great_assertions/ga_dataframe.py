bbf874761389bca74c6c80bce8840be16192ddd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:28 +0000

    Add package great-assertions/0.0.30/great-assertions-0.0.30.tar.gz

---
+++ packages/great-assertions/great-assertions-0.0.30.tar.gz/great-assertions-0.0.30/great_assertions/ga_dataframe.py
@@ -0,0 +1,14 @@
+"""Great Assertions."""
+
+
+class GADataFrame:
+    """Great Assertions."""
+
+    def __init__(self, df):
+        """Great Assertions."""
+        self.df = df
+
+    @property
+    def columns(self) -> list:
+        """List of columns from Pandas or PySpark."""
+        return self.df.columns
