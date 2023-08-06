2ccb1412ee0825665a3e746db1bfcb0ca439f535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package husteblume/0.0.2/husteblume-0.0.2.tar.gz

---
+++ packages/husteblume/husteblume-0.0.2.tar.gz/husteblume-0.0.2/README.md
@@ -0,0 +1,28 @@
+# pyHusteblume
+
+## Installation
+Install the dependencies using pip:
+`pip install -r requirements.txt`
+
+## Usage
+`python3 main.py`
+
+## Example
+
+```
+{'user-agent': 'okhttp/4.4.0'}
+<husteblume.api.Stations object at 0x105b713d0>
+'DEHANN'
+<User: appId=6C17808F>
+{'authorization': 'Basic NkMxNzgwOEY6bmxZTGtkVnNpWm5SR2tzTWpwa01aeFFoY2o=',
+ 'user-agent': 'okhttp/4.4.0'}
+<husteblume.api.Forecast object at 0x104d84eb0>
+{'ALNU': [0, 0, 0],
+ 'AMBR': [0, 0, 0],
+ 'ARTE': [1, 1, 1],
+ 'BETU': [0, 0, 0],
+ 'CORY': [0, 0, 0],
+ 'FRAX': [0, 0, 0],
+ 'POAC': [2, 2, 2],
+ 'SECA': [0, 0, 0]}
+```
---
+++ packages/husteblume/husteblume-0.0.2.tar.gz/husteblume-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="husteblume",
+    version="0.0.2",
+    author="Sophie Luna Schumann",
+    author_email="pypi@sophie.lgbt",
+    description="Library to access the TK.de Husteblume API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SharkyRawr/pyHusteblume",
+    project_urls={
+        "Bug Tracker": "https://github.com/SharkyRawr/pyHusteblume/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="husteblume"),
+    python_requires=">=3.6",
+    install_requires=[
+          'requests',
+      ],
+)
