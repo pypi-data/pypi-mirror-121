f54d45a17857373e140a140b3a000818f951c754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:09 +0000

    Add package kama-sdk-py/0.0.19/kama-sdk-py-0.0.19.tar.gz

---
+++ packages/kama-sdk-py/kama-sdk-py-0.0.19.tar.gz/kama-sdk-py-0.0.19/kama_sdk/cli/cli_helper.py
@@ -0,0 +1,56 @@
+from typing import Dict, Optional
+
+from k8kat.auth.kube_broker import broker
+from k8kat.res.ns.kat_ns import KatNs
+
+from kama_sdk.core.core import config_man
+from kama_sdk.utils.logging import lwar, lwin, lerr
+
+
+def warn_ns_not_set():
+  pass
+
+
+def handle_ns(options: Dict, proc: str, allow_empty=False):
+  if broker.is_connected:
+    if mock_ns := options.get(MOCK_NAMESPACE_FLAG):
+      ns = mock_ns
+    else:
+      if broker.is_in_cluster_auth():
+        ns = read_file_ns()
+      else:
+        ns = config_man.read_dev_file_ns()
+    if ns:
+      if KatNs.find(ns):
+        if not broker.is_in_cluster_auth():
+          config_man.coerce_ns(ns)
+        lwin(f"KAMA {proc} started (namespace={ns})")
+      else:
+        if allow_empty:
+          lwar(f"Starting {proc} with with non-existent namespace {ns}")
+        else:
+          raise RuntimeError(f"Cannot start {proc}: namespace "
+                             f"'{ns}' does not exist")
+    else:
+      if allow_empty:
+        lwar(f"Proceeding without ns; set with --n <name>")
+      else:
+        message = f"Cannot start {proc} process without namespace. " \
+                  f"Try again with -n <namespace>"
+        raise RuntimeError(message)
+  else:
+    lwar(f"Starting {proc} without Kubernetes connection!")
+
+
+def read_file_ns() -> Optional[str]:
+  try:
+    with open(_ns_path, 'r') as file:
+      if contents := file.read():
+        return contents.strip()
+      return None
+  except FileNotFoundError:
+    return None
+
+
+_ns_path = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+MOCK_NAMESPACE_FLAG = "namespace"
---
+++ packages/kama-sdk-py/kama-sdk-py-0.0.19.tar.gz/kama-sdk-py-0.0.19/kama_sdk/model/view/card_grid_view_spec.py
@@ -0,0 +1,5 @@
+from kama_sdk.model.view.grid_view_spec import GridViewSpec
+
+
+class CardGridViewSpec(GridViewSpec):
+  pass
