eb59757561a68fa03ff4dd9052a867f2e874d214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:28 +0000

    Add package metacity/0.0.24/metacity-0.0.24.tar.gz

---
+++ packages/metacity/metacity-0.0.24.tar.gz/metacity-0.0.24/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="metacity",
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version="0.0.24",
+    description="Python toolkit for Urban Data processing",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MetacitySuite/Metacity",
+    author="Metacity",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires='>=3.8',                # Minimum version requirement of the package
+    install_requires = [
+        "dotmap>=1.3.23",
+        "earcut>=1.1.4",
+        "numpy>=1.21.1",
+        "tqdm>=4.62.0",
+        "geopandas>=0.9.0"
+    ]
+)
