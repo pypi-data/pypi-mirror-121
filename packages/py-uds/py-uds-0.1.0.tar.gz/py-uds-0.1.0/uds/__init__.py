c567e37f01b31865e6e29290ce829d80a29ef7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:07 +0000

    Add package py-uds/0.1.0/py-uds-0.1.0.tar.gz

---
+++ packages/py-uds/py-uds-0.1.0.tar.gz/py-uds-0.1.0/uds/__init__.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Package for handling Unified Diagnostic Services (UDS) protocol defined by ISO-14229.
+
+The package is meant to provide tools that enables:
+ - monitoring UDS communication
+ - simulation of any UDS node (either a client or a server)
+ - testing of a device that supports UDS
+ - injection of communication faults on any layers 3-7 of UDS OSI Model
+
+The package is created with an idea to support any communication bus:
+ - `CAN <https://en.wikipedia.org/wiki/CAN_bus>`_
+ - `LIN <https://en.wikipedia.org/wiki/Local_Interconnect_Network>`_
+ - `Ethernet <https://en.wikipedia.org/wiki/Ethernet>`_
+ - `FlexRay <https://en.wikipedia.org/wiki/FlexRay>`_
+ - `K-Line <https://en.wikipedia.org/wiki/K-Line>`_
+"""
+
+__all__ = ["messages"]
+__version__ = "0.1.0"
+__author__ = "Maciej Dąbrowski"
+__email__ = "uds-package-development@googlegroups.com"
