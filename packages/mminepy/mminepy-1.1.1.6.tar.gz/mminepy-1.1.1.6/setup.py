ae8307d2c465dd4c430a5c12f65cd08a2198885f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:28 +0000

    Add package mminepy/1.1.1.6/mminepy-1.1.1.6.tar.gz

---
+++ packages/mminepy/mminepy-1.1.1.6.tar.gz/mminepy-1.1.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='mminepy', # 패키지 명
+
+version='1.1.1.6',
+
+description='minepy from elbert06',
+
+author='elbert06',
+
+author_email='elbert06@hanmail.net',
+
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+
+py_modules=['mminepy'], # 패키지에 포함되는 모듈
+
+python_requires='>=3',
+package_data={'minepy': ['minepy.exe']},
+install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+packages=['mminepy'] # 패키지가 들어있는 폴더들
+
+)
