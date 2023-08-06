f554b075ee35cf029217656622d6087bf6009801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:51 +0000

    Add package keeper-secrets-manager-core/16.1.5/keeper-secrets-manager-core-16.1.5.tar.gz

---
+++ packages/keeper-secrets-manager-core/keeper-secrets-manager-core-16.1.5.tar.gz/keeper-secrets-manager-core-16.1.5/keeper_secrets_manager_core/utils.py
@@ -0,0 +1,88 @@
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Secrets Manager
+# Copyright 2021 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+
+import base64
+import json
+import logging
+import os
+from json import JSONDecodeError
+from sys import platform as _platform
+
+from keeper_secrets_manager_core.keeper_globals import logger_name
+
+ENCODING = 'UTF-8'
+
+
+def get_os():
+    if _platform.lower().startswith("linux"):
+        return "linux"
+    elif _platform.lower().startswith("darwin"):
+        return "macOS"
+    # elif _platform.lower().startswith("win32"):
+    #     return "win32"
+    # elif _platform.lower().startswith("win64"):
+    #     return "win64"
+    else:
+        return _platform
+
+
+def bytes_to_string(b):
+    return b.decode(ENCODING)
+
+
+def bytes_to_int(b):
+    return int.from_bytes(b, byteorder='big')
+
+
+def bytes_to_base64(b):
+    return base64.b64encode(b).decode()
+
+
+def base64_to_bytes(s):
+    return base64.urlsafe_b64decode(s)
+
+
+def base64_to_string(b64s):
+    return base64.b64decode(b64s).decode('UTF-8')
+
+
+def string_to_bytes(s):
+    return s.encode(ENCODING)
+
+
+def url_safe_str_to_bytes(s):
+    b = base64.urlsafe_b64decode(s + '==')
+    return b
+
+
+def url_safe_str_to_int(s):
+    b = url_safe_str_to_bytes(s)
+    return bytes_to_int(b)
+
+
+def generate_random_bytes(length):
+    return os.urandom(length)
+
+
+def dict_to_json(dictionary):
+    return json.dumps(dictionary, indent=4)
+
+
+def json_to_dict(json_str):
+
+    try:
+        resp = json.loads(json_str)
+    except JSONDecodeError as jsonDecErr:
+        logging.getLogger(logger_name).warning(jsonDecErr)
+        resp = None
+
+    return resp
+
+
