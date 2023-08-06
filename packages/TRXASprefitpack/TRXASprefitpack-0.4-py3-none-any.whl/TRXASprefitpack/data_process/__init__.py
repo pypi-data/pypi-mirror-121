c971d585ad83b84a2d41775d07095636c303cd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:39 +0000

    Add package TRXASprefitpack/0.4/TRXASprefitpack-0.4-py3-none-any.whl

---
+++ packages/TRXASprefitpack/TRXASprefitpack-0.4-py3-none-any.whl/TRXASprefitpack/data_process/__init__.py
@@ -0,0 +1,10 @@
+'''
+data_process:
+
+submodule of TRXASprefitpack for data processing
+
+:copyright: 2021 by pistack (Junho Lee).
+:license: LGPL3.
+'''
+
+from .data_process import automate_scaling, corr_a_method
---
+++ packages/TRXASprefitpack/TRXASprefitpack-0.4-py3-none-any.whl/TRXASprefitpack/thy/__init__.py
@@ -0,0 +1,9 @@
+'''
+thy:
+submodule for calculating broaded theoritical spectrum
+
+:copyright: 2021 by pistack (Junho Lee).
+:license: LGPL3
+'''
+
+from .thy import gen_theory_data
