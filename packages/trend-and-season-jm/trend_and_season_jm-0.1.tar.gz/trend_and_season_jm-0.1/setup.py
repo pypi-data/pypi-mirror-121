8b76bdddc9bba5b927c9afb3a0b13ac67895aadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:59 +0000

    Add package trend-and-season-jm/0.1/trend_and_season_jm-0.1.tar.gz

---
+++ packages/trend-and-season-jm/trend_and_season_jm-0.1.tar.gz/trend_and_season_jm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='trend_and_season_jm',
+      version='0.1',
+      description='linear regression with seasonality',
+      packages=['trend_and_season_jm'],
+      author='Jason Merwin',
+      author_email='merwijas@yahoo.com',
+      zip_safe=False)
---
+++ packages/trend-and-season-jm/trend_and_season_jm-0.1.tar.gz/trend_and_season_jm-0.1/trend_and_season_jm/__init__.py
@@ -0,0 +1,1 @@
+from .trend_seas_code import trend_season_jm
