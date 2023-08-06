45bc4b8304c2643011bade48789a2634e9bea803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:29 +0000

    Add package protoyang/0.0.2/protoyang-0.0.2.tar.gz

---
+++ packages/protoyang/protoyang-0.0.2.tar.gz/protoyang-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='protoyang',
+    version='0.0.2',
+    packages=['protoyang', 'protoyang.hpproto'],
+    url='https://github.com/HuaweiDatacomm/protoyang',
+    license='Apache License, Version 2.0',
+    author='feng chong',
+    author_email='frank.fengchong@huawei.com',
+    description='a tool generate .proto files according yang modules'
+)
