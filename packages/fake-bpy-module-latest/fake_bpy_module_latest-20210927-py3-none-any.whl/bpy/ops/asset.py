7ecf25b8cc346e0f89f556b6d210271877301e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package fake-bpy-module-latest/20210927/fake_bpy_module_latest-20210927-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20210927-py3-none-any.whl/bpy/ops/asset.py
@@ -0,0 +1,58 @@
+import sys
+import typing
+
+
+def assign_action():
+    ''' Set this pose Action as active Action on the active Object :file: addons/pose_library/operators.py\:196 <https://developer.blender.org/diffusion/BA/addons/pose_library/operators.py$196> _
+
+    '''
+
+    pass
+
+
+def clear():
+    ''' Delete all asset metadata and turn the selected asset data-blocks back into normal data-blocks
+
+    '''
+
+    pass
+
+
+def list_refresh():
+    ''' Trigger a reread of the assets
+
+    '''
+
+    pass
+
+
+def mark():
+    ''' Enable easier reuse of selected data-blocks through the Asset Browser, with the help of customizable metadata (like previews, descriptions and tags)
+
+    '''
+
+    pass
+
+
+def open_containing_blend_file():
+    ''' Open the blend file that contains the active asset :file: startup/bl_operators/assets.py\:111 <https://developer.blender.org/diffusion/B/browse/master/release/scripts/startup/bl_operators/assets.py$111> _
+
+    '''
+
+    pass
+
+
+def tag_add():
+    ''' Add a new keyword tag to the active asset :file: startup/bl_operators/assets.py\:52 <https://developer.blender.org/diffusion/B/browse/master/release/scripts/startup/bl_operators/assets.py$52> _
+
+    '''
+
+    pass
+
+
+def tag_remove():
+    ''' Remove an existing keyword tag from the active asset :file: startup/bl_operators/assets.py\:75 <https://developer.blender.org/diffusion/B/browse/master/release/scripts/startup/bl_operators/assets.py$75> _
+
+    '''
+
+    pass
