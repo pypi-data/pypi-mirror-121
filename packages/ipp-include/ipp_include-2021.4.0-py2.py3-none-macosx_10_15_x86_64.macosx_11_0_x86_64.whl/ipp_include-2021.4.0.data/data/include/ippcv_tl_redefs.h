bd83b47660a42cbde27802f335036aa65f9886e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:32 +0000

    Add package ipp-include/2021.4.0/ipp_include-2021.4.0-py2.py3-none-macosx_10_15_x86_64.macosx_11_0_x86_64.whl

---
+++ packages/ipp-include/ipp_include-2021.4.0-py2.py3-none-macosx_10_15_x86_64.macosx_11_0_x86_64.whl/ipp_include-2021.4.0.data/data/include/ippcv_tl_redefs.h
@@ -0,0 +1,74 @@
+/*
+// Copyright 2020-2021 Intel Corporation All Rights Reserved.
+//
+// The source code, information and material ("Material") contained herein is
+// owned by Intel Corporation or its suppliers or licensors, and title
+// to such Material remains with Intel Corporation or its suppliers or
+// licensors. The Material contains proprietary information of Intel
+// or its suppliers and licensors. The Material is protected by worldwide
+// copyright laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed or disclosed in any way without Intel's prior express written
+// permission. No license under any patent, copyright or other intellectual
+// property rights in the Material is granted to or conferred upon you,
+// either expressly, by implication, inducement, estoppel or otherwise.
+// Any license under such intellectual property rights must be express and
+// approved by Intel in writing.
+//
+// Unless otherwise agreed by Intel in writing,
+// you may not remove or alter this notice or any other notice embedded in
+// Materials by Intel or Intel's suppliers or licensors in any way.
+//
+*/
+
+/*
+//              Intel(R) Integrated Performance Primitives (Intel(R) IPP)
+//              Computer Vision (ippCV_TL)
+//
+//
+*/
+
+
+#if !defined( IPPCV_TL_REDEFS_H__ )
+#define IPPCV_TL_REDEFS_H__
+
+
+#define ippiFilterSeparableInit_16s_L ippiFilterSeparableInit_16s_LT
+#define ippiFilterSeparableInit_32f_L ippiFilterSeparableInit_32f_LT
+#define ippiFilterSeparableGetBufferSize_L ippiFilterSeparableGetBufferSize_LT
+#define ippiFilterSeparableGetSpecSize_L ippiFilterSeparableGetSpecSize_LT
+
+#define ippiFilterSeparable_8u_C1R_L ippiFilterSeparable_8u_C1R_LT
+#define ippiFilterSeparable_8u_C3R_L ippiFilterSeparable_8u_C3R_LT
+#define ippiFilterSeparable_8u_C4R_L ippiFilterSeparable_8u_C4R_LT
+#define ippiFilterSeparable_8u16s_C1R_L ippiFilterSeparable_8u16s_C1R_LT
+#define ippiFilterSeparable_8u16s_C3R_L ippiFilterSeparable_8u16s_C3R_LT
+#define ippiFilterSeparable_8u16s_C4R_L ippiFilterSeparable_8u16s_C4R_LT
+#define ippiFilterSeparable_16s_C1R_L ippiFilterSeparable_16s_C1R_LT
+#define ippiFilterSeparable_16s_C3R_L ippiFilterSeparable_16s_C3R_LT
+#define ippiFilterSeparable_16s_C4R_L ippiFilterSeparable_16s_C4R_LT
+#define ippiFilterSeparable_16u_C1R_L ippiFilterSeparable_16u_C1R_LT
+#define ippiFilterSeparable_16u_C3R_L ippiFilterSeparable_16u_C3R_LT
+#define ippiFilterSeparable_16u_C4R_L ippiFilterSeparable_16u_C4R_LT
+#define ippiFilterSeparable_32f_C1R_L ippiFilterSeparable_32f_C1R_LT
+#define ippiFilterSeparable_32f_C3R_L ippiFilterSeparable_32f_C3R_LT
+#define ippiFilterSeparable_32f_C4R_L ippiFilterSeparable_32f_C4R_LT
+
+#define ippiFilterSeparable_8u_C1R ippiFilterSeparable_8u_C1R_T
+#define ippiFilterSeparable_8u_C3R ippiFilterSeparable_8u_C3R_T
+#define ippiFilterSeparable_8u_C4R ippiFilterSeparable_8u_C4R_T
+#define ippiFilterSeparable_8u16s_C1R ippiFilterSeparable_8u16s_C1R_T
+#define ippiFilterSeparable_8u16s_C3R ippiFilterSeparable_8u16s_C3R_T
+#define ippiFilterSeparable_8u16s_C4R ippiFilterSeparable_8u16s_C4R_T
+#define ippiFilterSeparable_16s_C1R ippiFilterSeparable_16s_C1R_T
+#define ippiFilterSeparable_16s_C3R ippiFilterSeparable_16s_C3R_T
+#define ippiFilterSeparable_16s_C4R ippiFilterSeparable_16s_C4R_T
+#define ippiFilterSeparable_16u_C1R ippiFilterSeparable_16u_C1R_T
+#define ippiFilterSeparable_16u_C3R ippiFilterSeparable_16u_C3R_T
+#define ippiFilterSeparable_16u_C4R ippiFilterSeparable_16u_C4R_T
+#define ippiFilterSeparable_32f_C1R ippiFilterSeparable_32f_C1R_T
+#define ippiFilterSeparable_32f_C3R ippiFilterSeparable_32f_C3R_T
+#define ippiFilterSeparable_32f_C4R ippiFilterSeparable_32f_C4R_T
+
+
+#endif /* IPPCV_TL_REDEFS_H__ */
