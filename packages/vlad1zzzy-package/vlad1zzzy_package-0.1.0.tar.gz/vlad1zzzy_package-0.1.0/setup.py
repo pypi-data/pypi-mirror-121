4f0d86067c179376560efaa053ee185996e6aa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:40 +0000

    Add package vlad1zzzy-package/0.1.0/vlad1zzzy_package-0.1.0.tar.gz

---
+++ packages/vlad1zzzy-package/vlad1zzzy_package-0.1.0.tar.gz/vlad1zzzy_package-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+#matplotlib require numpy>=1.16
+setup(
+    name='vlad1zzzy_package',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/vlad1zzzy/vlad1zzzy_package',
+    author='Vlad Kryukov',
+    author_email='vladikkrukov0204@gmail.com',
+    packages=['vlad1zzzy_package'],
+    install_requires=['numpy<=1.15',
+                      'matplotlib',
+                      'pandas',
+                      'scipy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
