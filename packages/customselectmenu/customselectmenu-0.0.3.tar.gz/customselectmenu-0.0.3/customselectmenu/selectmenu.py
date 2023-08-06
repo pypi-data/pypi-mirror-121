80b73abe0c1e021618fa18a4ad1c6653824cab0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:30 +0000

    Add package customselectmenu/0.0.3/customselectmenu-0.0.3.tar.gz

---
+++ packages/customselectmenu/customselectmenu-0.0.3.tar.gz/customselectmenu-0.0.3/customselectmenu/selectmenu.py
@@ -0,0 +1,53 @@
+from time import sleep
+from option import Option
+import os
+import keyboard
+
+
+class SelectMenu:
+    def __init__(self, *options: Option) -> None:
+        self.options = options
+
+        self.running = False
+        self.selected_index = 0
+
+    def start(self) -> None:
+        self.running = True
+
+        while self.running:
+            option_selected = self.options[self.selected_index]
+
+            for op in self.options:
+                op.active = False
+            option_selected.active = True
+
+            self.refresh()
+
+            key = keyboard.read_key()
+
+            if key == "w" or key == "up":
+                self.selected_index -= 1
+            if key == "s" or key == "down":
+                self.selected_index += 1
+            if key == "enter":
+                if option_selected.exit:
+                    self.stop()
+
+                option_selected.function(option_selected)
+
+            self.selected_index = min(self.selected_index, len(self.options) - 1)
+            self.selected_index = max(self.selected_index, 0)
+
+            sleep(0.1)
+
+    def stop(self) -> None:
+        self.running = False
+
+    def refresh(self) -> None:
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+        for op in self.options:
+            print(op)
