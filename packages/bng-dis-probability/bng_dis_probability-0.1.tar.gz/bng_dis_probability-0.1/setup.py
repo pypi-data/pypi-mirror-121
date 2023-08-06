5380efe0fe630f96851f1f74689d90846273bf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:00 +0000

    Add package bng-dis-probability/0.1/bng_dis_probability-0.1.tar.gz

---
+++ packages/bng-dis-probability/bng_dis_probability-0.1.tar.gz/bng_dis_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bng_dis_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bng_dis_probability'],
+      author='Mohammed Hamza Malik',
+      author_email='m.h.m.i.malik@gmail.com',
+      zip_safe=False)
