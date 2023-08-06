dd5d1c6062015458da7b724168c5d2592c440882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:00 +0000

    Add package pogema/0.53/pogema-0.53-py3-none-any.whl

---
+++ packages/pogema/pogema-0.53-py3-none-any.whl/pogema/__init__.py
@@ -0,0 +1,13 @@
+from gym import register
+from pogema.grid_config import GridConfig
+
+__version__ = '0.53'
+
+__all__ = [
+    'GridConfig',
+]
+
+register(
+    id="Pogema-v0",
+    entry_point="pogema.envs:Pogema",
+)
