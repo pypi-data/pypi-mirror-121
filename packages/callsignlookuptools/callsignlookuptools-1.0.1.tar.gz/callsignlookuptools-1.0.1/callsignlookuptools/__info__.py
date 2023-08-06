b4e4db98f43b63631f2a482fda273d59a52fc8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package callsignlookuptools/1.0.1/callsignlookuptools-1.0.1.tar.gz

---
+++ packages/callsignlookuptools/callsignlookuptools-1.0.1.tar.gz/callsignlookuptools-1.0.1/callsignlookuptools/__info__.py
@@ -0,0 +1,15 @@
+"Package information file"
+# Format and usage strongly inspired by AGProjects/python-application.
+
+
+__project__ = "callsignlookuptools"
+__summary__ = "QRZ, Callook, HamQTH, and QRZCQ API interface with sync and async support"
+__webpage__ = "https://callsignlookuptools.miaow.io"
+
+__version__ = "1.0.1"
+
+__author__ = "classabbyamp, 0x5c"
+__email__ = "dev@kb6.ee, dev@0x5c.io"
+
+__license__ = "BSD 3-Clause"
+__copyright__ = "2021"
