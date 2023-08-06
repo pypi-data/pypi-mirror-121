ae6e3081254fa1f3461a65a6064689f285b81fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:46 +0000

    Add package hscan/0.0.5.0/hscan-0.0.5.0.tar.gz

---
+++ packages/hscan/hscan-0.0.5.0.tar.gz/hscan-0.0.5.0/scan/util.py
@@ -0,0 +1,16 @@
+def get_local_ip():
+    import socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except:
+        pass
+    finally:
+        s.close()
+    return ip
+
+
+def get_local_name():
+    import socket
+    return socket.gethostname()
