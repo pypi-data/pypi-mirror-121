9b787de938bf1342504661887b827c921bb11856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:33 +0000

    Add package testpackages32/0.2.6/testpackages32-0.2.6.tar.gz

---
+++ packages/testpackages32/testpackages32-0.2.6.tar.gz/testpackages32-0.2.6/README.md
@@ -0,0 +1,6 @@
+# I made my package and I want to show off
+~~~python
+    from smth.unreal_file import unreal_func
+~~~
+
+[![Build Status](https://travis-ci.org/Fragers/project_testing.svg?branch=master)](https://travis-ci.org/Fragers/project_testing)
---
+++ packages/testpackages32/testpackages32-0.2.6.tar.gz/testpackages32-0.2.6/smth/unreal_file.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    import numpy as np
+    import pandas as pd
+
+
+def unreal_func(string: str) -> str:
+    '''
+    Get string and return string
+    :param string: string
+    :return: kek
+    '''
+    return "___" + string + "___"
