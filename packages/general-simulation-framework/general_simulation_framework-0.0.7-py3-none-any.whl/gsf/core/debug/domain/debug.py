8bd1986cc531241e81db11eea66fe7a559ae7141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:32 +0000

    Add package general-simulation-framework/0.0.7/general_simulation_framework-0.0.7-py3-none-any.whl

---
+++ packages/general-simulation-framework/general_simulation_framework-0.0.7-py3-none-any.whl/gsf/core/debug/domain/debug.py
@@ -0,0 +1,49 @@
+import functools
+
+from loguru import logger
+import sys
+
+config = {
+    "handlers": [
+        {
+            "sink": sys.stderr,
+            "level": "INFO",
+            "format": "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <level>{"
+            "message}</level>",
+        }
+    ]
+}
+
+logger.configure(**config)
+
+
+def debug(message: str = "", after: bool = False):
+    def real_decorator(function):
+        @functools.wraps(real_decorator)
+        def wrapper(*args, **kwargs):
+            if not after:
+                logger.debug(
+                    "["
+                    + str(function.__module__).split(".")[-1]
+                    + "] "
+                    + function.__name__
+                    + str(args[1:])
+                    + " "
+                    + message
+                )
+                return function(*args, **kwargs)
+            logger.debug(
+                "["
+                + str(function.__module__).split(".")[-1]
+                + "] "
+                + function.__name__
+                + str(args[1:])
+                + " "
+                + message
+            )
+            return function(*args, **kwargs)
+
+        wrapper.__doc__ = function.__doc__
+        return wrapper
+
+    return real_decorator
