a9206d61739559075c348dc96b5e453bedeb8d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:40 +0000

    Add package couchers/0.0.29/couchers-0.0.29-py3-none-any.whl

---
+++ packages/couchers/couchers-0.0.29-py3-none-any.whl/couchers/client.py
@@ -0,0 +1,32 @@
+import grpc
+
+from couchers.services import get_all_stubs
+from pathlib import Path
+
+DEFAULT_SERVER_ADDRESS = "api.couchers.org:8443"
+
+# load the ISRT / Let's Encrypt certs for use as roots
+certs_folder = Path(__file__).parent / "certs"
+x1 = (certs_folder / "isrg-root-x1.pem").read_bytes()
+x2 = (certs_folder / "isrg-root-x2.pem").read_bytes()
+roots = x1 + x2
+
+def get_client(api_key, server_address=DEFAULT_SERVER_ADDRESS, disable_tls=False):
+    """
+    Given an API key returns a client object.
+
+    `disable_tls` connects to the server insecurely. This only works on localhost, thanks to gRPC.
+
+    Returns a new class with attributes corresponding to each stub, with attributes such as `account`, corresponding to
+    an account gRPC stub.
+    """
+    if disable_tls:
+        creds = grpc.local_channel_credentials()
+    else:
+        creds = grpc.ssl_channel_credentials(root_certificates=roots)
+
+    creds = grpc.composite_channel_credentials(creds, grpc.access_token_call_credentials(api_key))
+
+    channel = grpc.secure_channel(server_address, creds)
+    stubs = get_all_stubs(channel)
+    return type("__Client", (), stubs)
