ea4ec3be31f99e63b82a5ff01ef40ee7b654d776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:21 +0000

    Add package mminepy/1.1.1.17/mminepy-1.1.1.17.tar.gz

---
+++ packages/mminepy/mminepy-1.1.1.17.tar.gz/mminepy-1.1.1.17/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+# import os
+# os.system("setx path %path%;c:\java\bin;s")
+setup(name='mminepy', # 패키지 명
+
+version='1.1.1.17',
+
+description='minepy from elbert06',
+
+author='elbert06',
+
+author_email='elbert06@hanmail.net',
+
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+packages=find_packages(),
+py_modules=['mminepy'], # 패키지에 포함되는 모듈
+python_requires='>=3',
+include_package_data=True,
+install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+)
