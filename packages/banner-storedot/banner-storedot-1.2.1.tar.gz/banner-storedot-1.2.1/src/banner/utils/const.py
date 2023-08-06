affa62fdea32a0efef13b7f5c2d6031e3c83bfe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:24 +0000

    Add package banner-storedot/1.2.1/banner-storedot-1.2.1.tar.gz

---
+++ packages/banner-storedot/banner-storedot-1.2.1.tar.gz/banner-storedot-1.2.1/src/banner/utils/const.py
@@ -0,0 +1,29 @@
+FIRST_NEWARE_DATA_TABLE = 'main_first_table'
+SECOND_NEWARE_DATA_TABLE = 'main_second_table'
+FIRST_NEWARE_AUX_TABLE = 'aux_first_table'
+SECOND_NEWARE_AUX_TABLE = 'aux_second_table'
+
+NW_VOLTAGE = 'test_vol'
+NW_CURRENT = 'test_cur'
+NW_TEMP = 'test_tmp'
+NW_AUX_CHANNEL = 'auxchl_id'
+NW_STEP_RANGE = 'cur_step_range'
+NW_TIMESTAMP = 'test_time'
+NW_DATESTAMP = 'test_atime'
+NW_SEQ = 'seq_id'
+
+NW_CHANNEL_GROUP = 'auxchl_'
+
+NEWARE_FACTOR_COLUMNS = [
+    'factor_capchg', 
+    'factor_engchg', 
+    'factor_capdchg', 
+    'factor_engdchg'
+]
+
+PREV_SUFFIX = '_prev'
+
+CACHE_CONNECTION_KWARG = 'cache_connection'
+TTL_KWARG = 'ttl'
+
+OUTLIER_STD_COEFFECIENT = 3
