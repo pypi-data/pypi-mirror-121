717ba17df5f7765cda211cb81626fdc4f7cbec2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package uduak-distributions/0.3/uduak_distributions-0.3.tar.gz

---
+++ packages/uduak-distributions/uduak_distributions-0.3.tar.gz/uduak_distributions-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='uduak_distributions',
+      version='0.3',
+      description='Gaussian_distributions',
+      packages=['uduak_distributions'],
+      author= "uduak umanah",
+      author_email = "uduakumanah10@gmail.com",
+      zip_safe=False)
