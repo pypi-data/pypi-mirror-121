d62c73aa973d3e50c0a97006f6e9d04d9bbfc846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:16 +0000

    Add package gaus-binomial/7.1/gaus_binomial-7.1.tar.gz

---
+++ packages/gaus-binomial/gaus_binomial-7.1.tar.gz/gaus_binomial-7.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_binomial',
+      version='7.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      author='Bayangmbe Mounmo',
+      author_email='bayangp0@gmail.com',
+      zip_safe=False)
