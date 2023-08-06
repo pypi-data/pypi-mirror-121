e470092bff2039c0b82a511aaac8a94e56ae366b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package chaconprobabilidades/0.1/chaconprobabilidades-0.1.tar.gz

---
+++ packages/chaconprobabilidades/chaconprobabilidades-0.1.tar.gz/chaconprobabilidades-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='chaconprobabilidades',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['chaconprobabilidades'],
+      zip_safe=False,
+      link="https://www.linkedin.com/in/fernando-chacon-franco/",
+      author="Fernando Chacon",
+      author_email="francofer7775@gmail.com",
+      install_requires=['matplotlib==3.4.3']
+      )
