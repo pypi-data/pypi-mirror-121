8c79fde5a6e8ac24dc16b38025efb1a887bea1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:47 +0000

    Add package ethereum-kms-signer/0.1.3/ethereum_kms_signer-0.1.3-py3-none-any.whl

---
+++ packages/ethereum-kms-signer/ethereum_kms_signer-0.1.3-py3-none-any.whl/ethereum_kms_signer/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Ethereum KMS Signer."""
+
+__author__ = """Meet Mangukiya"""
+__email__ = "meet@flamy.dev"
+__version__ = "0.1.0"
+
+from .kms import sign_transaction
+from .kms import get_eth_address
