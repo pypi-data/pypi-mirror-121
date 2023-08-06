0eeb3a954fcaccd5cd208e791adfab496760f6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:28 +0000

    Add package perrot/0.15.2/perrot-0.15.2.tar.gz

---
+++ packages/perrot/perrot-0.15.2.tar.gz/perrot-0.15.2/perrot/venn/__init__.py
@@ -0,0 +1,11 @@
+#  Created byMartin.cz
+#  Copyright (c) Martin Strohalm. All rights reserved.
+
+# import regions
+from . regions import Region, EmptyRegion, CircleRegion, RingRegion, ArcsRegion
+
+# import utils
+from . utils import *
+
+# import venn
+from . venn import Venn
