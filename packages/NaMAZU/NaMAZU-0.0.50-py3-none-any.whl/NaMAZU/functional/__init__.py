5ea6c93e0d8c11c9c9b819c88d777446c03c5509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:24 +0000

    Add package NaMAZU/0.0.50/NaMAZU-0.0.50-py3-none-any.whl

---
+++ packages/NaMAZU/NaMAZU-0.0.50-py3-none-any.whl/NaMAZU/functional/__init__.py
@@ -0,0 +1,22 @@
+from .image_control import *
+from .file_control import *
+from .data_science import *
+from .text_control import *
+
+__all__ = [
+    "train_linear_regressor",
+    "rename_file",
+    "collect_file_pathes_by_ext",
+    "zip_files",
+    "img_to_npy",
+    "npy_to_png",
+    "apply_mask_to",
+    "split_image",
+    "compose_two_png",
+    "apply_to_all",
+    "change_frame_rates_in",
+    "save_all_frames",
+    "export_list_str_as",
+    "search_word_from",
+]
+
