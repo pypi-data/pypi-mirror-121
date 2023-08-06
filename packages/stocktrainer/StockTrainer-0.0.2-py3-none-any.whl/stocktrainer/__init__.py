fa4c50cbfa17915999d31e25cc6550d7ffcbdb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package stocktrainer/0.0.2/StockTrainer-0.0.2-py3-none-any.whl

---
+++ packages/stocktrainer/StockTrainer-0.0.2-py3-none-any.whl/stocktrainer/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 by Daniel Prakah-Asante
+# All rights reserved.
+# This file is part of the StockEnv package,
+# and is released under the "GNU  General Public License v3.0". Please see the LICENSE
+# file that should have been included as part of this package.
+
+from StockTrainer import Env
