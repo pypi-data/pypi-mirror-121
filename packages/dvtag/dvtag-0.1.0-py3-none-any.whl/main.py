83a2aa9ccec13a911b78525b0610bdaf8dc67dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package dvtag/0.1.0/dvtag-0.1.0-py3-none-any.whl

---
+++ packages/dvtag/dvtag-0.1.0-py3-none-any.whl/main.py
@@ -0,0 +1,36 @@
+import argparse
+from pathlib import Path
+
+from dvtag import get_rjid, tag
+from w2f import wav_to_flac
+
+
+def start_tagging(dir_path: Path):
+    if get_rjid(dir_path.name):
+        tag(dir_path)
+        return
+
+    for dir in dir_path.iterdir():
+        if not dir_path.is_dir():
+            continue
+        start_tagging(dir)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Doujin Voice Tagging Tool')
+    parser.add_argument('dir_path', type=str, help='a required directory path')
+    parser.add_argument('--w2f',
+                        default=False,
+                        action=argparse.BooleanOptionalAction,
+                        help='converting wav file to flac [LOSELESS]')
+
+    args = parser.parse_args()
+    path = Path(args.dir_path).absolute()
+
+    if args.w2f:
+        wav_to_flac(path)
+    start_tagging(path)
+
+
+if __name__ == '__main__':
+    main()
