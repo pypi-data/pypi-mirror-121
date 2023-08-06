433631dcae41d61dd89326eeb4bcaa77df196562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:35 +0000

    Add package esphome-dashboard/20210927.0/esphome_dashboard-20210927.0-py3-none-any.whl

---
+++ packages/esphome-dashboard/esphome_dashboard-20210927.0-py3-none-any.whl/esphome_dashboard/static/js/esphome/c.4bb51e47.js
@@ -0,0 +1,7 @@
+import{_ as e,n as s,a,y as l}from"./index-02aff447.js";import"./c.235f0fef.js";let o=class extends a{render(){return l`
+      <esphome-process-dialog
+        heading="Update All"
+        .type=${"update-all"}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};o=e([s("esphome-update-all-dialog")],o);
