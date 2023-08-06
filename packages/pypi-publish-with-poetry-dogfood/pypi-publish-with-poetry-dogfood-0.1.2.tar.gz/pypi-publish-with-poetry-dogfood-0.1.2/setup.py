e8335d740e9778183d5182a45ca676dcd3b9eb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:54 +0000

    Add package pypi-publish-with-poetry-dogfood/0.1.2/pypi-publish-with-poetry-dogfood-0.1.2.tar.gz

---
+++ packages/pypi-publish-with-poetry-dogfood/pypi-publish-with-poetry-dogfood-0.1.2.tar.gz/pypi-publish-with-poetry-dogfood-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypi_publish_with_poetry_dogfood']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pypi-publish-with-poetry-dogfood',
+    'version': '0.1.2',
+    'description': 'Empty package to test the GitHub Action `coveooss/pypi-publish-with-poetry`',
+    'long_description': 'This package is used to validate the functionality of the "coveooss/pypi-publish-with-poetry" GitHub action.\n\nYou can read more about the action [here](https://github.com/coveooss/pypi-publish-with-poetry).\n',
+    'author': 'Jonathan Piché',
+    'author_email': 'tools@coveo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/coveooss/pypi-publish-with-poetry',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
