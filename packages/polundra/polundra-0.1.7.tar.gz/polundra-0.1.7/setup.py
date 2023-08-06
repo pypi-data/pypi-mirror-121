13469c4d5c4e585e4439e79948fb8dbd1e02f93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:40 +0000

    Add package polundra/0.1.7/polundra-0.1.7.tar.gz

---
+++ packages/polundra/polundra-0.1.7.tar.gz/polundra-0.1.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['polundra', 'polundra.audio', 'polundra.visual']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dbus-python>=1.2.16,<2.0.0', 'pulsectl>=21.9.1,<22.0.0']
+
+entry_points = \
+{'console_scripts': ['polundra = polundra.__main__:main']}
+
+setup_kwargs = {
+    'name': 'polundra',
+    'version': '0.1.7',
+    'description': 'An extended alert notifier.',
+    'long_description': '# polundra\n\nAn extended alert notifier.\n',
+    'author': 'Artur Svistunov',
+    'author_email': 'poopa@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/smgd/polundra',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
