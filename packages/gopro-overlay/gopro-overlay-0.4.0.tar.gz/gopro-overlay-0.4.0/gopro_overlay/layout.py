66f242d62863613ea2a1fa421f64ea210952a07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:24 +0000

    Add package gopro-overlay/0.4.0/gopro-overlay-0.4.0.tar.gz

---
+++ packages/gopro-overlay/gopro-overlay-0.4.0.tar.gz/gopro-overlay-0.4.0/gopro_overlay/layout.py
@@ -0,0 +1,97 @@
+from PIL import ImageFont
+
+from .point import Coordinate
+from .privacy import NoPrivacyZone
+from .widgets import Text, date, time, Scene, LeftInfoPanel, RightInfoPanel
+from .widgets_map import MovingMap, JourneyMap
+
+
+class Layout:
+
+    def __init__(self, timeseries, map_renderer, privacy_zone=NoPrivacyZone()):
+        self.map_renderer = map_renderer
+        self.timeseries = timeseries
+        self.current_entry = None
+
+        font_title = ImageFont.truetype(font="Roboto-Medium.ttf", size=16)
+        font_metric = ImageFont.truetype(font="Roboto-Medium.ttf", size=32)
+        font_speed = ImageFont.truetype(font="Roboto-Medium.ttf", size=48)
+
+        self.scene = Scene([
+            Text(Coordinate(260, 36), date(lambda: self.entry.dt), font_title, align="right"),
+            Text(Coordinate(260, 60), time(lambda: self.entry.dt), font_metric, align="right"),
+            Text(Coordinate(1900, 36), lambda: "GPS INFO", font_title, align="right"),
+            Text(Coordinate(1780, 60), lambda: f"Lat: {self.entry.point.lat:0.6f}", font_title, align="right"),
+            Text(Coordinate(1900, 60), lambda: f"Lon: {self.entry.point.lon:0.6f}", font_title, align="right"),
+            MovingMap(
+                at=Coordinate(1900 - 256, 100),
+                location=lambda: self.entry.point,
+                azimuth=lambda: self.entry.azi,
+                renderer=map_renderer,
+                zoom=16
+            ),
+            JourneyMap(
+                at=Coordinate(1900 - 256, 100 + 256 + 20),
+                timeseries=timeseries,
+                location=lambda: self.entry.point,
+                renderer=map_renderer,
+                privacy_zone=privacy_zone
+            ),
+            LeftInfoPanel(
+                Coordinate(16, 900),
+                "gauge-1.png",
+                lambda: "MPH",
+                lambda: f"{self.entry.speed.to('MPH').magnitude:.0f}" if self.entry.speed else "-",
+                font_title,
+                font_speed
+            ),
+            LeftInfoPanel(
+                Coordinate(16, 980),
+                "mountain.png",
+                lambda: "ALT(m)",
+                lambda: f"{self.entry.alt.to('m').magnitude:.1f}" if self.entry.alt else "-",
+                font_title,
+                font_metric
+            ),
+            LeftInfoPanel(
+                Coordinate(220, 980),
+                "slope-triangle.png",
+                lambda: "SLOPE(%)",
+                lambda: f"{self.entry.grad.magnitude:.1f}" if self.entry.grad else "-",
+                font_title,
+                font_metric
+            ),
+
+            RightInfoPanel(
+                Coordinate(1900, 820),
+                "thermometer.png",
+                lambda: "TEMP(C)",
+                lambda: f"{self.entry.atemp.magnitude:.0f}" if self.entry.atemp is not None else "-",
+                font_title,
+                font_metric
+            ),
+            RightInfoPanel(
+                Coordinate(1900, 900),
+                "gauge.png",
+                lambda: "RPM",
+                lambda: f"{self.entry.cad.magnitude:.0f}" if self.entry.cad else "-",
+                font_title,
+                font_metric
+            ),
+            RightInfoPanel(
+                Coordinate(1900, 980),
+                "heartbeat.png",
+                lambda: "BPM",
+                lambda: f"{self.entry.hr.magnitude:.0f}" if self.entry.hr else "-",
+                font_title,
+                font_metric
+            ),
+        ])
+
+    @property
+    def entry(self):
+        return self.current_entry
+
+    def draw(self, dt):
+        self.current_entry = self.timeseries.get(dt)
+        return self.scene.draw()
---
+++ packages/gopro-overlay/gopro-overlay-0.4.0.tar.gz/gopro-overlay-0.4.0/gopro_overlay/widgets.py
@@ -0,0 +1,138 @@
+import functools
+import importlib
+import os
+
+from PIL import Image, ImageDraw, ImageFont
+
+from . import icons
+from .point import Coordinate
+
+anchors = {
+    "left": "la",
+    "right": "ra"
+}
+
+
+class Text:
+    def __init__(self, at, value, font, align="left"):
+        self.at = at
+        self.value = value
+        self.font = font
+        self.anchor = anchors[align]
+
+    def draw(self, image, draw):
+        draw.text(
+            self.at.tuple(),
+            self.value(),
+            anchor=self.anchor,
+            font=self.font,
+            fill=(255, 255, 255),
+            stroke_width=2,
+            stroke_fill=(0, 0, 0)
+        )
+
+
+class Drawable:
+    def __init__(self, at, drawable):
+        self.at = at
+        self.drawable = drawable
+
+    def draw(self, image, draw):
+        image.alpha_composite(self.drawable, self.at.tuple())
+
+
+class RightInfoPanel:
+
+    def __init__(self, at, icon, title, value, title_font, value_font):
+        self.value = value
+        self.widgets = [
+            Text(at + Coordinate(-70, 0), title, title_font, align="right"),
+            simple_icon(at + Coordinate(-64, 0), icon),
+            Text(at + Coordinate(-70, 18), value, value_font, align="right"),
+        ]
+
+    def draw(self, image, draw):
+        for w in self.widgets:
+            w.draw(image, draw)
+
+
+class LeftInfoPanel:
+
+    def __init__(self, at, icon, title, value, title_font, value_font):
+        self.value = value
+        self.widgets = [
+            Text(at + Coordinate(70, 0), title, title_font),
+            simple_icon(at + Coordinate(0, 0), icon),
+            Text(at + Coordinate(70, 18), value, value_font),
+        ]
+
+    def draw(self, image, draw):
+        for w in self.widgets:
+            w.draw(image, draw)
+
+
+def time(clock):
+    return lambda: clock().strftime("%H:%M:%S.%f")[:-5]
+
+
+def date(clock):
+    return lambda: clock().strftime("%Y/%m/%d")
+
+
+def icon(file, at, transform=lambda x: x):
+    if os.path.exists(file):
+        image = Image.open(file)
+    else:
+        with importlib.resources.path(icons, file) as f:
+            image = Image.open(f)
+
+    return Drawable(at, transform(image))
+
+
+def simple_icon(at, file):
+    return icon(file, at, transform=compose(
+        functools.partial(transform_resize, (64, 64)),
+        transform_rgba,
+        transform_negative
+    ))
+
+
+def transform_resize(target, img):
+    return img.resize(target)
+
+
+def transform_rgba(img):
+    return img.convert("RGBA") if img.mode == "P" else img
+
+
+def transform_negative(img):
+    if img.mode != "RGBA":
+        raise ValueError(f"I only work on RGBA, not {img.mode}")
+    for i in range(0, img.size[0] - 1):
+        for j in range(0, img.size[1] - 1):
+            pixel = img.getpixel((i, j))
+            img.putpixel((i, j), (255 - pixel[0], 255 - pixel[1], 255 - pixel[2], pixel[3]))
+    return img
+
+
+class Scene:
+
+    def __init__(self, widgets, dimensions=None):
+        self._widgets = widgets
+        self._dimensions = dimensions if dimensions else (1920, 1080)
+
+    def draw(self):
+        image = Image.new("RGBA", self._dimensions, (0, 0, 0, 0))
+        draw = ImageDraw.Draw(image)
+
+        for w in self._widgets:
+            w.draw(image, draw)
+
+        return image
+
+
+def compose(*functions):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), reversed(functions))
+
+
+
