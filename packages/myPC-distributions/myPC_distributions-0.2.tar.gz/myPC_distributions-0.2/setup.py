58c38eafed0921aa99ba8fe2bbfeb387694e39ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package myPC-distributions/0.2/myPC_distributions-0.2.tar.gz

---
+++ packages/myPC-distributions/myPC_distributions-0.2.tar.gz/myPC_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='myPC_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['myPC_distributions'],
+	  author = 'Pedro Carvajal',
+	  author_email = 'peterven25@gmail.com',
+      zip_safe=False)
