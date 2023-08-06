d37b186cd4277a0f26c27fe2e266293d0d44a0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:33 +0000

    Add package panels/0.2.12/panels-0.2.12-cp38-cp38-win_amd64.whl

---
+++ packages/panels/panels-0.2.12-cp38-cp38-win_amd64.whl/panels/version.py
@@ -0,0 +1,13 @@
+# This file is generated automatically by the setup.py
+short_version = '0.2.12'
+version = '0.2.12'
+full_version = '0.2.12'
+svn_revision = ''
+isreleased = True
+if isreleased:
+    __version__ = version
+else:
+    __version__ = full_version
+
+if not isreleased:
+    version = full_version
