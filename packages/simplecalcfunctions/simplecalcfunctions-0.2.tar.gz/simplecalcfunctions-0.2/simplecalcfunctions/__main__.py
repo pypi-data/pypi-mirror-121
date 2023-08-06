8966a39cf664d3ab50ad7aac6e28a4dc30f4b377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package simplecalcfunctions/0.2/simplecalcfunctions-0.2.tar.gz

---
+++ packages/simplecalcfunctions/simplecalcfunctions-0.2.tar.gz/simplecalcfunctions-0.2/simplecalcfunctions/__main__.py
@@ -0,0 +1,26 @@
+print("You Have Imported Simple Calculation Module")
+
+def add(a,b):
+    c=a+b
+    print(f"The Sum of {a} and {b} is {c}")
+def sub(a,b):
+    c=a-b
+    print(f"The Difference of {a} and {b} is {c}")
+def mul(a,b):
+    c=a*b
+    print(f"The Product of {a} and {b} is {c}")
+def div(a,b):
+    c=a/b
+    print(f"The Quotient of {a} and {b} is {c}")
+def rem(a,b):
+    c=a%b
+    print(f"The Remainder of {a} and {b} is {c}")
+def exp(a,b):
+    c=a**b
+    print(f"The Exponential Value of {a} power {b} is {c}")
+
+hel = """Syntax for this modlue calc_name(num1,num2)
+Operation names are \'add\' for addition, \'sub\' for subtraction,
+\'mul\' for multiplication, \'div\' for quotient,
+\'rem\' for getting remainder,\'exp\' for exponential multiplication."""
+helpcalc=print(hel)
