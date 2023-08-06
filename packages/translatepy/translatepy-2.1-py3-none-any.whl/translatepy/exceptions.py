b7838377efcb468afb84fbc1b27585e9b16c2171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:22 +0000

    Add package translatepy/2.1/translatepy-2.1-py3-none-any.whl

---
+++ packages/translatepy/translatepy-2.1-py3-none-any.whl/translatepy/exceptions.py
@@ -0,0 +1,31 @@
+class TranslatepyException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class TranslationError(TranslatepyException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class UnknownLanguage(TranslatepyException):
+    def __init__(self, guessed_language, similarity, *args: object) -> None:
+        super().__init__(*args)
+        self.guessed_language = str(guessed_language)
+        self.similarity = similarity
+
+
+class UnsupportedMethod(TranslatepyException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class RequestStatusError(TranslatepyException):
+    def __init__(self, status_code, *args: object) -> None:
+        super().__init__(*args)
+        self.status_code = int(status_code)
+
+
+class ServiceURLError(TranslatepyException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
