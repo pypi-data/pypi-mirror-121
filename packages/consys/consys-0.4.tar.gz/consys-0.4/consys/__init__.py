fac70a2028613eadeeeaef70ad99151f1de88653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:13 +0000

    Add package consys/0.4/consys-0.4.tar.gz

---
+++ packages/consys/consys-0.4.tar.gz/consys-0.4/consys/__init__.py
@@ -0,0 +1,15 @@
+"""
+Initializing the Python package
+"""
+
+from .base import Attribute
+from .main import make_base
+
+
+__version__ = '0.4'
+
+__all__ = (
+    '__version__',
+    'Attribute',
+    'make_base',
+)
