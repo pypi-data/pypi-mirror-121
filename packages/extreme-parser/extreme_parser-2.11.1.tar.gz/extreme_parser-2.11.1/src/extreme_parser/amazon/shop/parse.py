13c7d89a2714cf96bdc5cf94a36a3c0553864b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package extreme-parser/2.11.1/extreme_parser-2.11.1.tar.gz

---
+++ packages/extreme-parser/extreme_parser-2.11.1.tar.gz/extreme_parser-2.11.1/src/extreme_parser/amazon/shop/parse.py
@@ -0,0 +1,27 @@
+import parsel
+
+from extreme_parser.amazon.shop.model import Shop
+
+
+def parse(html: str, s: Shop):
+    sel = parsel.Selector(text=html)
+    parse_seller(s, selector=sel)
+    parse_name(s, selector=sel)
+    parse_address(s, selector=sel)
+
+
+def parse_seller(s: Shop, selector: parsel.Selector = None):
+    s.seller = selector.xpath("//*[@id='sellerName']/text()").get()
+
+
+def parse_name(s: Shop, selector: parsel.Selector = None):
+    s.name = selector.xpath("//span[contains(text(), 'Business Name')]/following-sibling::text()").get()
+
+
+def parse_address(s: Shop, selector: parsel.Selector = None):
+    address = selector.xpath("//span[contains(text(), 'Business Address')]/following-sibling::ul/li//text()").getall()
+    address = " ".join(address)
+    if address == "":
+        s.address = None
+    else:
+        s.address = address
