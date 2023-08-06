d092d82a7166335c97c67c8504b10d037d4c8140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package mlf-distributions/0.1/mlf_distributions-0.1.tar.gz

---
+++ packages/mlf-distributions/mlf_distributions-0.1.tar.gz/mlf_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlf_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mlf_distributions'],
+      author = 'Chin-Wu Chen',
+      author_email = 'cwchen1976@gmail.com',
+      zip_safe=False)
