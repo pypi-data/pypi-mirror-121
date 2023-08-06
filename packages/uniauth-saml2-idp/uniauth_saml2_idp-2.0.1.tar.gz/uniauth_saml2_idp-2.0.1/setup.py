d02f7f727c1110334a01666e5da930af0cb3b06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:38 +0000

    Add package uniauth-saml2-idp/2.0.1/uniauth_saml2_idp-2.0.1.tar.gz

---
+++ packages/uniauth-saml2-idp/uniauth_saml2_idp-2.0.1.tar.gz/uniauth_saml2_idp-2.0.1/setup.py
@@ -0,0 +1,61 @@
+import re
+import sys
+
+from glob import glob
+from setuptools import setup, find_packages
+
+_src_folder = '.'
+_pkg_name = 'uniauth_saml2_idp'
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements(fname='requirements.txt'):
+    fopen = open(fname, 'r')
+    return fopen.read().splitlines()
+
+
+setup(
+    name="uniauth_saml2_idp",
+    version='2.0.1',
+    description="uniAuth is a Django SAML2 Identity Provider",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author='Giuseppe De Marco, Francesco Filicetti',
+    author_email='giuseppe.demarco@unical.it, francesco.filicetti@unical.it',
+    license="Apache 2.0",
+    url='https://github.com/UniversitaDellaCalabria/uniAuth',
+
+    packages=[f"{_pkg_name}"],
+    package_dir={f"{_pkg_name}": f"{_src_folder}/{_pkg_name}"},
+
+    package_data={f"{_pkg_name}": [i.replace(f'{_src_folder}/{_pkg_name}/', '')
+                                   for i in glob(f'{_src_folder}/{_pkg_name}/**',
+                                                 recursive=True)]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        ],
+    install_requires=get_requirements(),
+    zip_safe=False,
+    )
