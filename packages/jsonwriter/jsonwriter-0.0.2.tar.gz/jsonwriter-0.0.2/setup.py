398bd61a9689f0ae2c4ed491b9e2425d48117ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:23 +0000

    Add package jsonwriter/0.0.2/jsonwriter-0.0.2.tar.gz

---
+++ packages/jsonwriter/jsonwriter-0.0.2.tar.gz/jsonwriter-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='jsonwriter',
+    version='0.0.2',
+    description='Easy JSON Writer',
+    author='Nawaf Alqari',
+    author_email='nawafalqari13@gmail.com',
+    packages=['jsonwriter'],
+    zip_safe=False,
+    license='MIT',
+    url = 'https://github.com/nawafalqari/jsonwriter',
+    keywords=['jsondb', 'json database', 'jsondatabase', 'json editor', 'json', 'json writer'],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
