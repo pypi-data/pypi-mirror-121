78271e0a16a5f0e55d4746a703aa9f2b371876f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:57 +0000

    Add package seti-python/0.0.10/seti-python-0.0.10.tar.gz

---
+++ packages/seti-python/seti-python-0.0.10.tar.gz/seti-python-0.0.10/seti_python/class_tools.py
@@ -0,0 +1,25 @@
+class BasicResponse:
+    msg:str
+    res:int
+    
+class CompleteBasicaResponse(BasicResponse):
+    
+    def __init__(self,data:dict):
+        self.msg=data['msg']
+        self.res=data['res']
+        self.data = ResponseCreateTransactionData(data['data'])
+    
+class ResponseCreateTransactionData:
+    transaction_id:str
+    def __init__(self,data:dict):
+        try:
+            self.transaction_id =data['transactionId']
+        except Exception:
+            return
+                
+class ResponseCreateTransaction(BasicResponse):
+    data: ResponseCreateTransactionData
+    def __init__(self,data:dict):
+        self.msg=data['msg']
+        self.res=data['res']
+        self.data = ResponseCreateTransactionData(data['data'])
