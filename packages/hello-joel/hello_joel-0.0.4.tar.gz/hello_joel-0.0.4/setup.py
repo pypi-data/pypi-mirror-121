8955b98da25c8301c698a51122d838c0cc5eeb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:52 +0000

    Add package hello-joel/0.0.4/hello_joel-0.0.4.tar.gz

---
+++ packages/hello-joel/hello_joel-0.0.4.tar.gz/hello_joel-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Basic hello'
+
+# Setting up
+setup(
+    name="hello_joel",
+    version=VERSION,
+    author="Joel Dsouza",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
