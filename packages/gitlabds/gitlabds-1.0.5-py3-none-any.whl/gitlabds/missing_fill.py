cd017f06487b88e01cfcfdde4449a3cddb274314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:23 +0000

    Add package gitlabds/1.0.5/gitlabds-1.0.5-py3-none-any.whl

---
+++ packages/gitlabds/gitlabds-1.0.5-py3-none-any.whl/gitlabds/missing_fill.py
@@ -0,0 +1,48 @@
+def missing_fill(df=None, columns='all', method='zero', inplace=False):
+    
+    import pandas as pd
+    import numpy as np
+    
+    if inplace == True:
+        df2 = df
+        
+    else:
+        df2 = df.copy(deep=True)
+        
+    #Get all columns with missing values
+    missing_cols = set(df.columns[df.isnull().any()].tolist())
+
+    if columns == 'all':
+        #Pull all numeric columns to miss fill
+        all_numeric = set(df.select_dtypes(include=['number']).columns.tolist())
+        #print(all_numeric)
+        
+        #Remove columns that have no missing values
+        var_list = list(all_numeric & missing_cols)
+        
+    else:
+        var_list = columns
+    
+    print(f'Columns selected for {method} filling: {columns}')
+    print(f'Actual columns with missing values that will be {method} filled: {var_list}')
+            
+    if method == 'zero':
+        df[var_list] = df[var_list].fillna(0)
+
+    if method == 'mean':
+        for v in var_list:
+            fill_value = df[v].mean()
+            df[v] = df[v].fillna(fill_value)
+
+    if method == 'median':
+        for v in var_list:
+            fill_value = df[v].median()
+            df[v] = df[v].fillna(fill_value)
+
+    if method == 'drop_row':
+        print('drop row...nothing will happen. This is future functionality.')
+
+    if method == 'drop_column':
+        print('drop column...nothing will happen. This is future functionality')
+     
+    return df2
