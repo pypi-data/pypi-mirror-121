6d8659c1b182845ff49523b3fd948d40c5e961ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:04 +0000

    Add package notetool/0.5.27/notetool-0.5.27.tar.gz

---
+++ packages/notetool/notetool-0.5.27.tar.gz/notetool-0.5.27/notetool/tool/__init__.py
@@ -0,0 +1,6 @@
+from notetool.tool.build import get_version, version_add
+
+from .compress import decompress
+from .log import log, logger
+from .path import delete_file, exists_file, path_parse, rename
+from .secret import SecretManage, read_secret, set_secret_path, write_secret
