34bd98721ddd8d7da471f9afa39e7c371a1f9b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:16 +0000

    Add package mikro/0.1.9/mikro-0.1.9.tar.gz

---
+++ packages/mikro/mikro-0.1.9.tar.gz/mikro-0.1.9/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mikro', 'mikro.graphql', 'mikro.graphql.mutations', 'mikro.graphql.queries']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['docstring-parser>=0.10,<0.11',
+ 'herre>=0.1.2,<0.2.0',
+ 'inflection>=0.5.1,<0.6.0',
+ 's3fs>=2021.8.1,<2022.0.0',
+ 'xarray>=0.19.0,<0.20.0']
+
+setup_kwargs = {
+    'name': 'mikro',
+    'version': '0.1.9',
+    'description': 'images for arnheim',
+    'long_description': None,
+    'author': 'jhnnsrs',
+    'author_email': 'jhnnsrs@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
