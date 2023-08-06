4c0db6dd9520b817ddea4aab99e44d295927a695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:20 +0000

    Add package PyViCare/2.9.1/PyViCare-2.9.1.tar.gz

---
+++ packages/PyViCare/PyViCare-2.9.1.tar.gz/PyViCare-2.9.1/PyViCare/PyViCareOilBoiler.py
@@ -0,0 +1,25 @@
+from PyViCare.PyViCareDevice import Device
+from PyViCare.PyViCareUtils import handleNotSupported
+
+
+class OilBoiler(Device):
+
+    @handleNotSupported
+    def getActive(self):
+        return self.service.getProperty("heating.burner")["properties"]["active"]["value"]
+
+    @handleNotSupported
+    def getBurnerModulation(self):
+        return self.service.getProperty('heating.burner.modulation')["properties"]["value"]["value"]
+
+    @handleNotSupported
+    def getBoilerTemperature(self):
+        return self.service.getProperty("heating.boiler.sensors.temperature.main")["properties"]["value"]["value"]
+
+    @handleNotSupported
+    def getBurnerHours(self):
+        return self.service.getProperty('heating.burner.statistics')['properties']['hours']['value']
+
+    @handleNotSupported
+    def getBurnerStarts(self):
+        return self.service.getProperty('heating.burner.statistics')['properties']['starts']['value']
