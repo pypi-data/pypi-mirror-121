17ae1bb00f8c32e161df03440e74d828278af067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:08 +0000

    Add package addydaddy/0.0/addydaddy-0.0.tar.gz

---
+++ packages/addydaddy/addydaddy-0.0.tar.gz/addydaddy-0.0/addydaddy/Binomialdistribution.py
@@ -0,0 +1,31 @@
+from IPython.display import display, HTML
+
+class TableColor(Distribution):
+    """ Class to color table
+            
+    """
+    
+    
+    def __init__(self, DataFrame):
+                
+        self.df = DataFrame
+    
+    def red(self):
+    
+        """Function to calculate the mean from p and n
+        
+        Args: 
+            None
+        
+        Returns: 
+            float: mean of the data set
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: lightblue; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
