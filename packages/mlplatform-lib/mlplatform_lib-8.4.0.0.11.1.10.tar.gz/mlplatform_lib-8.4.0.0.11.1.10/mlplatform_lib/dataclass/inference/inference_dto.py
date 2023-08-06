6e41a0ce7172ef31bc9ac3463b825e1fdcb33b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:43 +0000

    Add package mlplatform-lib/8.4.0.0.11.1.10/mlplatform_lib-8.4.0.0.11.1.10.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.0.0.11.1.10.tar.gz/mlplatform_lib-8.4.0.0.11.1.10/mlplatform_lib/dataclass/inference/inference_dto.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass, field
+from mlplatform_lib.dataclass.experiment.type import ExperimentType
+
+@dataclass
+class InferenceDto:
+    id: int = field(init=False, default=0)
+    experiment_type: ExperimentType = field(init=False)
+    inference_path: str
