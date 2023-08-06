96ba0d54398c1ff102131dce8a6b3378b59e5f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:45 +0000

    Add package qtarmsim/0.5.5/qtarmsim-0.5.5.tar.gz

---
+++ packages/qtarmsim/qtarmsim-0.5.5.tar.gz/qtarmsim-0.5.5/qtarmsim/ui/help.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'help.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Help(object):
+    def setupUi(self, Help):
+        if not Help.objectName():
+            Help.setObjectName(u"Help")
+        Help.resize(562, 524)
+
+        self.retranslateUi(Help)
+
+        QMetaObject.connectSlotsByName(Help)
+    # setupUi
+
+    def retranslateUi(self, Help):
+        Help.setWindowTitle(QCoreApplication.translate("Help", u"Help", None))
+    # retranslateUi
+
