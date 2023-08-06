dbd53e10c26165d262e26ea72a9e6b08fcbeb1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:25 +0000

    Add package distributions-nd-mestevam/0.2/distributions_nd_mestevam-0.2.tar.gz

---
+++ packages/distributions-nd-mestevam/distributions_nd_mestevam-0.2.tar.gz/distributions_nd_mestevam-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_nd_mestevam',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_nd_mestevam'],
+      author = 'Marcelo Estevam',
+      authoremail = 'marcelo.a.estevam@gmail.com',
+      zip_safe=False)
