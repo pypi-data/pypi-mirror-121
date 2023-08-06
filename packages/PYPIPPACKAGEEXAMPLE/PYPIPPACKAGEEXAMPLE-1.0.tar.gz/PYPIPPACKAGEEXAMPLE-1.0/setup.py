aa481345c2ca51cd5c5684d629d1e30d81b58ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:40 +0000

    Add package PYPIPPACKAGEEXAMPLE/1.0/PYPIPPACKAGEEXAMPLE-1.0.tar.gz

---
+++ packages/PYPIPPACKAGEEXAMPLE/PYPIPPACKAGEEXAMPLE-1.0.tar.gz/PYPIPPACKAGEEXAMPLE-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages, version
+
+setup(
+    name= 'PYPIPPACKAGEEXAMPLE',
+    version= '1.0',
+    license= 'MIT',
+    description= 'Paquete de prueba para una clase de Udemy',
+    author= 'Stefano Berro',
+    packages= find_packages(), #sirve para poder encontrar los paquetes internos
+    #install_requires= ['math','numpy'], #Se usa para cuando se deben instalar librerías adicionales
+    url= 'https://github.com/Stefanoberro/pypipackageexample'
+)
