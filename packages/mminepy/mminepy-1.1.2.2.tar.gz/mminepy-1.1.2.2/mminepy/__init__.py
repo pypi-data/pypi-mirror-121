806d8f9d006fec2eb04fc69a8e6703ff8fe02aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:08 +0000

    Add package mminepy/1.1.2.2/mminepy-1.1.2.2.tar.gz

---
+++ packages/mminepy/mminepy-1.1.2.2.tar.gz/mminepy-1.1.2.2/mminepy/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+m = sys.version
+mr = m.split(".")
+h = mr[0]+mr[1]
+os.system('setx path "%path%;C:/Users/KIMMJU/AppData/Local/Programs/Python/Python'+h+'/Lib/site-packages/mminepy-1.1.1.17.dist-info;"')
+print('setx path "%path%;C:/Users/KIMMJU/AppData/Local/Programs/Python/Python'+h+'/Lib/site-packages/mminepy-1.1.1.17.dist-info;"')
+__all__ = ['minepy.py']
---
+++ packages/mminepy/mminepy-1.1.2.2.tar.gz/mminepy-1.1.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='mminepy', # 패키지 명
+
+version='1.1.2.2',
+
+description='minepy from elbert06',
+
+author='elbert06',
+
+author_email='elbert06@hanmail.net',
+
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+packages=find_packages(),
+py_modules=['mminepy'], # 패키지에 포함되는 모듈
+python_requires='>=3',
+include_package_data=True,
+install_requires=[], # 패키지 사용을 위해 필요한 추가 설치 패키지
+)
