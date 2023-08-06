3286e1db5439098e0ba69449eb909afe2510a7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:02 +0000

    Add package forecasting-mle-decathlon/0.1.1/forecasting-mle-decathlon-0.1.1.tar.gz

---
+++ packages/forecasting-mle-decathlon/forecasting-mle-decathlon-0.1.1.tar.gz/forecasting-mle-decathlon-0.1.1/README.md
@@ -0,0 +1,3 @@
+# MLE Decathlon
+
+[![Upload Python Package](https://github.com/Pie33000/mle_deacthlon/actions/workflows/python-publish.yml/badge.svg?branch=main)](https://github.com/Pie33000/mle_deacthlon/actions/workflows/python-publish.yml)
---
+++ packages/forecasting-mle-decathlon/forecasting-mle-decathlon-0.1.1.tar.gz/forecasting-mle-decathlon-0.1.1/forecasting_mle_decathlon/modeling/modeling_pipeline.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from forecasting.modeling.prophet_estimator import ProphetEstimator
+from typing import List, Tuple
+
+
+def create_modeling_pipeline(
+        holidays_df: pd.DataFrame
+) -> List[Tuple[str, ProphetEstimator]]:
+    return [
+        ('model', ProphetEstimator(holidays_df))
+    ]
