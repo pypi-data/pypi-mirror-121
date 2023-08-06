e2bf65cd0fb2bbc06a9ead637adbd8d90c27bf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:18 +0000

    Add package addydaddy/0.2/addydaddy-0.2.tar.gz

---
+++ packages/addydaddy/addydaddy-0.2.tar.gz/addydaddy-0.2/addydaddy/Binomialdistribution.py
@@ -0,0 +1,112 @@
+from IPython.display import display, HTML
+
+class TableColor():
+    """ Class to color table
+            
+    """
+    
+    
+    def __init__(self, DataFrame):
+                
+        self.df = DataFrame
+		lightgrey(self.df)
+    
+    def lightblue(self):
+    
+        """Function for blue lightcolor
+        
+        Args: 
+            None
+        
+        Returns: 
+            dataframe: colored
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: lightblue; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
+		
+	def lightred(self):
+    
+        """Function for red lightcolor
+        
+        Args: 
+            None
+        
+        Returns: 
+            dataframe: colored
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: lightred; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
+		
+	def lightgreen(self):
+    
+        """Function for red lightcolor
+        
+        Args: 
+            None
+        
+        Returns: 
+            dataframe: colored
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: lightgreen; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
+		
+	def yello(self):
+    
+        """Function for yello lightcolor
+        
+        Args: 
+            None
+        
+        Returns: 
+            dataframe: colored
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: yellow; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
+		
+	def lightgrey(self):
+    
+        """Function for gray lightcolor
+        
+        Args: 
+            None
+        
+        Returns: 
+            dataframe: colored
+    
+        """
+        
+        self.r=HTML('''
+        <style>
+            .df tbody tr:nth-child(even) { background-color: lightgrey; }
+        </style>
+        ''' + self.df.to_html(classes="df"))
+                
+        return self.r
