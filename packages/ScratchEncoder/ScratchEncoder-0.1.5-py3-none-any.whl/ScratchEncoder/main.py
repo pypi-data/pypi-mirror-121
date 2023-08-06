5c93de70508c2a17a11b4dfefbe0af682d20e3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package ScratchEncoder/0.1.5/ScratchEncoder-0.1.5-py3-none-any.whl

---
+++ packages/ScratchEncoder/ScratchEncoder-0.1.5-py3-none-any.whl/ScratchEncoder/main.py
@@ -0,0 +1,24 @@
+import logging
+class Encoder():
+    global chars
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789+-. _"
+    def encode(self, text):
+        global chars
+        text = text.lower()
+        encoded = ""
+        length = int(len(text))
+        for i in range(0,length):
+            try:
+                x = int(chars.index(text[i])+int(11))
+                encoded = encoded + str(x)
+            except ValueError:
+                logging.error('Character not supported')
+        return encoded
+    def decode(self, text):
+        decoded = ""
+        y = 0
+        for i in range(0,len(str(text))//2):
+            x = chars[int(str(text[y])+str(text[int(y)+1]))-11]
+            decoded = str(decoded)+str(x)
+            y += 2
+        return decoded
