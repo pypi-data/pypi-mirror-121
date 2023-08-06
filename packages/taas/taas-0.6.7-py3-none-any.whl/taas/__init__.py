b6f19359903e5e8ef504c482dc58a41c6b75f15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:07 +0000

    Add package taas/0.6.7/taas-0.6.7-py3-none-any.whl

---
+++ packages/taas/taas-0.6.7-py3-none-any.whl/taas/__init__.py
@@ -0,0 +1,4 @@
+from .config import register, is_registered, unregister, get_registered_data_loaders_names, get_worker_info
+from .taas_data_loader import prepare_data_loaders, restore_data_loaders
+from .taas_data_server import data_sender
+
