809229c061a289afa506e3bac28e3027390a675e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:10 +0000

    Add package LoopStructural/1.3.3/LoopStructural-1.3.3-cp38-cp38-win32.whl

---
+++ packages/LoopStructural/LoopStructural-1.3.3-cp38-cp38-win32.whl/LoopStructural/utils/exceptions.py
@@ -0,0 +1,19 @@
+import logging
+
+from LoopStructural.utils import getLogger
+logger = getLogger(__name__)
+
+class LoopException(Exception):
+    """
+    Base loop exception
+    """
+    
+class LoopImportError(LoopException):
+    """
+    """
+    pass
+
+class InterpolatorError(LoopException):
+    pass
+
+
