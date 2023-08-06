105fd2ca66f983f6219a83248ebb884ba03d8424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:29 +0000

    Add package zeroday/0.0.0/zeroday-0.0.0.tar.gz

---
+++ packages/zeroday/zeroday-0.0.0.tar.gz/zeroday-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Zeroday | Comming Soon...'
+
+setup(
+    name="zeroday",
+    version=VERSION,
+    author="S41R4J",
+    author_email="<v1b7rc8eb@relay.firefox.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
