1562de7c9196468de86d2fecf778df45bf194de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:45 +0000

    Add package BIT-framework/0.0.1/BIT_framework-0.0.1.tar.gz

---
+++ packages/BIT-framework/BIT_framework-0.0.1.tar.gz/BIT_framework-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md","r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BIT_framework",
+    version="0.0.1",
+    author="wxl",
+    author_email="847084723@qq.com",
+    description="A dialog framework",
+    #packages=setuptools.find_packages(),
+    packages=['BIT_DL'],
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ]
+)
