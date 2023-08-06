eb39681f3844c0f3548880a5c6300433dfdd5357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:09 +0000

    Add package virtool-cli/0.1.0/virtool-cli-0.1.0.tar.gz

---
+++ packages/virtool-cli/virtool-cli-0.1.0.tar.gz/virtool-cli-0.1.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['virtool_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofiles>0.5.0',
+ 'aiohttp==3.6.2',
+ 'aiojobs==0.2.2',
+ 'arrow==0.16.0',
+ 'biopython>=1.79,<2.0',
+ 'click==7.0',
+ 'rich>=7.0.0,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['virtool = virtool_cli.run:cli']}
+
+setup_kwargs = {
+    'name': 'virtool-cli',
+    'version': '0.1.0',
+    'description': 'CLI Tool for working with Virtool data',
+    'long_description': None,
+    'author': 'Ian Boyes',
+    'author_email': 'igboyes@gmail.com',
+    'maintainer': 'Ian Boyes',
+    'maintainer_email': 'igboyes@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
