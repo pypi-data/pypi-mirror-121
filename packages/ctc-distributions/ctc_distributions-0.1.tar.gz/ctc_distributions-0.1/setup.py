00f84bd203249959a6ef9b4df81c57eef7d1c4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:43 +0000

    Add package ctc-distributions/0.1/ctc_distributions-0.1.tar.gz

---
+++ packages/ctc-distributions/ctc_distributions-0.1.tar.gz/ctc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ctc_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ctc_distributions'],
+      author = 'Conrado T. Catindig',
+      author_email = 'nowelsi@yahoo.com',
+      zip_safe=False)
