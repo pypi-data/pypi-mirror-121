27ef0a7306a6befc1c3f72e44472295b16f09085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:18 +0000

    Add package pytento/1.0.6/pytento-1.0.6.tar.gz

---
+++ packages/pytento/pytento-1.0.6.tar.gz/pytento-1.0.6/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'pytento',
+  packages = ['pytento'],
+  version = '1.0.6',
+  license='MIT',
+  description = 'This is a staticly-typed testing framework',
+  author = 'Demir Antay',
+  author_email = 'demir99antay@gmail.com',
+  url = 'https://github.com/demirantay/pytento',
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['unit testing', 'python', 'testing', 'framework'],
+  install_requires=[],
+)
