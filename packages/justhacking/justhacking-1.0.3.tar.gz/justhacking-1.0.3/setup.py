cfcb47084d40f152b789c6066f9d4264328aadcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:38 +0000

    Add package justhacking/1.0.3/justhacking-1.0.3.tar.gz

---
+++ packages/justhacking/justhacking-1.0.3.tar.gz/justhacking-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '1.0.3'
+DESCRIPTION = 'Just Hacking is a Python CLI script that stimulates as you are hacking.'
+
+# Setting up
+setup(
+    name="justhacking",
+    version=VERSION,
+    author="Divinemonk",
+    author_email="<v1b7rc8eb@relay.firefox.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['progressbar', 'tqdm'],
+    keywords=['python', 'justhacking', 'divinemonk', 'hacking stimulation', 'cli'],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "justhacking=justhacking.__main__:starthack",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
