ff67e2a28f735402ddcb518dc87332a3a3e69c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package aliyunpan/2.7.15/aliyunpan-2.7.15.tar.gz

---
+++ packages/aliyunpan/aliyunpan-2.7.15.tar.gz/aliyunpan-2.7.15/aliyunpan/api/type.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+
+__all__ = ['FileInfo', 'UserInfo', 'ShareInfo', 'AlibumInfo']
+
+_file_info = (
+    'name', 'id', 'pid', 'type', 'ctime', 'update_time', 'hidden', 'category', 'content_type', 'size',
+    'content_hash_name',
+    'content_hash', 'download_url',
+    'video_media_metadata', 'video_preview_metadata')
+FileInfo = namedtuple('FileInfo', _file_info)
+FileInfo.__new__.__defaults__ = ('',) * len(_file_info)
+UserInfo = namedtuple('UserInfo', ['id', 'nick_name', 'ctime', 'phone', 'drive_id'])
+UserInfo.__new__.__defaults__ = ('',) * 5
+ShareInfo = namedtuple('ShareInfo', ['name', 'content_hash', 'proof_code', 'content_hash_name', 'size', 'path'])
+ShareInfo.__new__.__defaults__ = ('',) * 6
+AlibumInfo = namedtuple('AlibumInfo', ['drive_name', 'drive_id'])
+AlibumInfo.__new__.__defaults__ = ('',) * 2
