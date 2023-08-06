8573634c097d04d61e3fe99eba546ff66d968fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:32 +0000

    Add package ON-Package-anishyadav-2021/0.0.2/ON_Package-anishyadav-2021-0.0.2.tar.gz

---
+++ packages/ON-Package-anishyadav-2021/ON_Package-anishyadav-2021-0.0.2.tar.gz/ON_Package-anishyadav-2021-0.0.2/README.md
@@ -0,0 +1,5 @@
+# ON_Package
+Package for Perceptron class
+## URL
+[github actions](https://docs.github.com/en/actions/automating-builds-and-testsbuilding-and-testing-python#publishing-to-package-registries)
+ 
---
+++ packages/ON-Package-anishyadav-2021/ON_Package-anishyadav-2021-0.0.2.tar.gz/ON_Package-anishyadav-2021-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+Project_Name="ON_Package"
+User_Name="anishyadav-2021"
+
+setuptools.setup(
+    name=f"{Project_Name}-{User_Name}",
+    version="0.0.2",
+    author=User_Name,
+    author_email="anishyadav1994@gmail.com",
+    description="It is a Simple Neuron Implimentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{User_Name}/{Project_Name}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{User_Name}/{Project_Name}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "tqdm"
+        "logging"
+    ]
+)
