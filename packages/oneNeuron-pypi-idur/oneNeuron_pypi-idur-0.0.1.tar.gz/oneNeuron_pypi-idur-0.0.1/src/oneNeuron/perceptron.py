9b3d3710b1ffbebd0fbe707be3f634c17805d884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:54 +0000

    Add package oneNeuron-pypi-idur/0.0.1/oneNeuron_pypi-idur-0.0.1.tar.gz

---
+++ packages/oneNeuron-pypi-idur/oneNeuron_pypi-idur-0.0.1.tar.gz/oneNeuron_pypi-idur-0.0.1/src/oneNeuron/perceptron.py
@@ -0,0 +1,55 @@
+import numpy as np
+import logging
+from tqdm import tqdm
+
+class Perceptron():
+
+	def __init__(self, eta, epochs):
+		self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHTS INIT
+		self.eta = eta   # Learning Rate
+		self.epochs = epochs
+
+		logging.info(f'initial weights before training :\n{self.weights}')
+
+	def activationFunction(self, inputs, weights):
+
+		z = np.dot(inputs, weights)   # Z = W * X
+		dt = np.where(z > 0, 1, 0)  # CONDITION IF TRUE ELSE
+		#print(f"dot : ", dt)
+		return dt
+
+	def fit(self, X, y):
+		self.X = X
+		self.y = y
+
+		X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
+		logging.info(f'X with bias : \n{X_with_bias}')  # CONCATINATION
+
+		for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
+			logging.info("--"*10)
+			logging.info(f'for epoch :{epoch}')
+			logging.info("--"*10)
+
+			y_hat = self.activationFunction(X_with_bias, self.weights) # FORWORD PROPAGATION
+
+			logging.info(f'predicted values  after Forword pass: \n{y_hat}')
+
+			self.error = self.y - y_hat
+			logging.info(f"Error :\n{self.error}")
+
+			self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error)  # BACK WORK PROPAGATION
+			logging.info(f"updated weights after epoch :\n{epoch}/{self.epochs} : \n{self.weights}")
+
+			logging.info("###"*10)
+
+	def predict(self, X):
+		X_with_bias = np.c_[X, -np.ones((len(X),1))]
+		logging.info(f'predict x X_with_bias : \n{X_with_bias}')
+		return self.activationFunction(X_with_bias, self.weights)
+
+	def total_loss(self):
+		total_loss = np.sum(self.error)
+		logging.info(f'total_loss : {total_loss}')
+		return total_loss
+
+
