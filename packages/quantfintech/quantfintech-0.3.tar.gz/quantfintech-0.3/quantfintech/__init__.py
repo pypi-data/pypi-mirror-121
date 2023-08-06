b52d3539cfebe907611461864d58ddfd1a966168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:56 +0000

    Add package quantfintech/0.3/quantfintech-0.3.tar.gz

---
+++ packages/quantfintech/quantfintech-0.3.tar.gz/quantfintech-0.3/quantfintech/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 11:26:56 2021
+
+@author: sayandebhowmick
+"""
+
+__author__ = 'Sayan De Bhowmick'
+from .indicators import *
+from .assist import *
+from .Security_init import *
+from .srts import *
+from .candles import *
+
