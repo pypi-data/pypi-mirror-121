28509980256e31df072177bf2e355ff88cef6b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:53 +0000

    Add package tdl-pybasic/0.1.9/tdl_pybasic-0.1.9.tar.gz

---
+++ packages/tdl-pybasic/tdl_pybasic-0.1.9.tar.gz/tdl_pybasic-0.1.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tdl_pybasic',
+    packages=['tdl_pybasic'],
+
+    version='0.1.9', # バージョン
+
+    license='MIT', # ライセンス
+
+    install_requires=[],
+
+    author='Atsushi Shibata',
+    author_email='shibata@m-info.co.jp',
+
+    url='',
+
+    description='A library for Toushin Digital University Python BASIC.',
+    long_description='',
+    long_description_content_type='text/markdown',
+    keywords='',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
