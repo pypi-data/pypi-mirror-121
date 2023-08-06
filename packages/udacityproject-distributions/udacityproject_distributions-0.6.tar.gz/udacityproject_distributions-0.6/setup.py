98d007d0fc6c17c6ea952c12d888afe4c601b055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:10 +0000

    Add package udacityproject-distributions/0.6/udacityproject_distributions-0.6.tar.gz

---
+++ packages/udacityproject-distributions/udacityproject_distributions-0.6.tar.gz/udacityproject_distributions-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacityproject_distributions',
+      version='0.6',
+      description='Distributions',
+      author='Kimosh7',
+      author_email = 'kimotho.wangari@aims-cameroon.org',
+      packages=['udacityproject_distributions'],
+      zip_safe=False)
