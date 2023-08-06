de740279ab77d21855e83738ff673fc64abddf9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:08 +0000

    Add package afdd-distributions/0.1/afdd_distributions-0.1.tar.gz

---
+++ packages/afdd-distributions/afdd_distributions-0.1.tar.gz/afdd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='afdd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['afdd_distributions'],
+      author='Andres Dorado',
+      author_email='andres.dorado90@gmail.com',
+      zip_safe=False)
