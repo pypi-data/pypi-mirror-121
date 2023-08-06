2da157eb5235305d5f95f26934f30ab1a41f0d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package naver-dbcore/0.0.3.post1/naver-dbcore-0.0.3.post1.tar.gz

---
+++ packages/naver-dbcore/naver-dbcore-0.0.3.post1.tar.gz/naver-dbcore-0.0.3.post1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='naver-dbcore',  
+     version='0.0.3-1',
+     packages=['naver_dbcore'] ,
+     author="Jose Cuevas",
+     author_email="jose.cuevas.cv@gmail.com",
+     description="A DB Persistence Ancestor Library",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/jacr6/naver-dbcore", 
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],install_requires=[
+         'Flask-MySQLdb',
+          'Flask-SQLAlchemy',
+          'python-decouple',
+          'sanic',
+      ],
+ )
