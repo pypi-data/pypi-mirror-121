6253c5b797babf5a33e70d110659f629a68d95e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:08 +0000

    Add package geosparql-dggs/0.2/geosparql_dggs-0.2-py3-none-any.whl

---
+++ packages/geosparql-dggs/geosparql_dggs-0.2-py3-none-any.whl/gsdggs/__init__.py
@@ -0,0 +1,22 @@
+from .sf_functions import (
+    contains,
+    overlaps,
+    equals,
+    touches,
+    within,
+    disjoint,
+    intersects,
+)
+from rdflib import Namespace
+from rdflib.plugins.sparql.operators import register_custom_function
+
+__version__ = "0.2"
+DGGS = Namespace("https://placeholder.com/dggsfuncs/")
+
+register_custom_function(DGGS.sfContains, contains)
+register_custom_function(DGGS.sfEquals, equals)
+register_custom_function(DGGS.sfOverlaps, overlaps)
+register_custom_function(DGGS.sfDisjoint, disjoint)
+register_custom_function(DGGS.sfWithin, within)
+register_custom_function(DGGS.sfTouches, touches)
+register_custom_function(DGGS.sfIntersects, intersects)
